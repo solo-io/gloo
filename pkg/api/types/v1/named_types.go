a86d08d228b16a9d0f2821af5680e43b45d4b0ed
ilackarms <sdw35@cornell.edu>
2018-02-14 17:42:40 -0500

    create interface for glueobject types

--- pkg/api/types/v1/named_types.go
+++ pkg/api/types/v1/named_types.go
@@ -1,7 +1,15 @@
 package v1
 
-import "github.com/gogo/protobuf/types"
+import (
+	"github.com/gogo/protobuf/proto"
+	"github.com/gogo/protobuf/types"
+)
 
 type UpstreamType string
 type UpstreamSpec *types.Struct
 type FunctionSpec *types.Struct
+
+type GlueObject interface {
+	proto.Message
+	GetMetadata() *Metadata
+}
