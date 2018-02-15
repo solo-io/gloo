58c0cb024a05d83fca1a373536bd353d0540830a
ilackarms <sdw35@cornell.edu>
2018-02-15 18:37:46 -0500

    add intentionally failing vault tests as a reminder to eventaully address them

---
+++ internal/secretwatcher/vault/vault_secret_watcher_test.go
@@ -0,0 +1,12 @@
+package vault_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/solo-io/glue/internal/secretwatcher/vault"
+)
+
+var _ = Describe("VaultSecretWatcher", func() {
+	TODO
+})
