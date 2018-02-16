1c741df07354a585f2674fa875e9e69990c3985d
ilackarms <sdw35@cornell.edu>
2018-02-16 14:30:50 -0500

    extract registry to be public

---
+++ pkg/plugin/registry.go
@@ -0,0 +1,30 @@
+package plugin
+
+import (
+	"github.com/solo-io/glue/internal/bootstrap"
+	"github.com/solo-io/glue/pkg/endpointdiscovery"
+)
+
+var defaultRegistry = &registry{}
+
+type EndpointDiscoveryInitFunc func(options bootstrap.Options, stopCh <-chan struct{}) (endpointdiscovery.Interface, error)
+
+func Register(plugin TranslatorPlugin, startEndpointDiscovery EndpointDiscoveryInitFunc) {
+	defaultRegistry.plugins = append(defaultRegistry.plugins, plugin)
+	if startEndpointDiscovery != nil {
+		defaultRegistry.endpointDiscoveries = append(defaultRegistry.endpointDiscoveries, startEndpointDiscovery)
+	}
+}
+
+func RegisteredPlugins() []TranslatorPlugin {
+	return defaultRegistry.plugins
+}
+
+func EndpointDiscoveryInitializers() []EndpointDiscoveryInitFunc {
+	return defaultRegistry.endpointDiscoveries
+}
+
+type registry struct {
+	plugins             []TranslatorPlugin
+	endpointDiscoveries []EndpointDiscoveryInitFunc
+}
