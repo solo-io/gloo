2969678e0b72062ae0190d8b15786f5c45cab769
ilackarms <sdw35@cornell.edu>
2018-02-21 16:23:21 -0500

    hackrun enables kube testing, turns on debug logging

--- hack/run-local.sh
+++ hack/run-local.sh
@@ -5,10 +5,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 FAIL=0
 
 echo "Starting gloo"
-DEBUG=1 ${DIR}/gloo --file.config.dir hack/gen-config-yaml/_gloo_config --file.secret.dir hack/gen-config-yaml/_gloo_secrets &
+DEBUG=1 ${DIR}/gloo --file.config.dir hack/gen-config-yaml/_gloo_config --file.secret.dir hack/gen-config-yaml/_gloo_secrets --kubeconfig $HOME/.kube/config &
 
 echo "Starting envoy"
 ${DIR}/envoy -c ${DIR}/envoy.yaml --v2-config-only --service-cluster envoy --service-node envoy &
+
+curl localhost:19000/logging?config=debug
+curl localhost:19000/logging?router=debug
+curl localhost:19000/logging?connection=debug
 
 trap 'kill $(jobs -p)' EXIT
 
