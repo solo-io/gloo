e370992e2a694ccd4e89fd4265da88cca3f16754
ilackarms <sdw35@cornell.edu>
2018-02-20 23:55:51 -0500

    update kube resources

--- e2e/containers/helloservice/main.go
+++ e2e/containers/helloservice/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/golang/glog"
 )
 
 const (
@@ -17,22 +16,21 @@ type handler struct{}
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/text")
 	if _, err := w.Write([]byte(hello)); err != nil {
-		glog.Error(err)
+		log.Printf("err: %v", err)
 	}
 }
 
-// RunHTTP opens a simple listener on the port.
-func RunHTTP(port uint) {
-	glog.Infof("upstream listening HTTP1.1 on %d", port)
+func serve(port uint) {
+	log.Printf("upstream listening HTTP1.1 on %d", port)
 	h := handler{}
 	server := &http.Server{Addr: fmt.Sprintf(":%d", port), Handler: h}
 	if err := server.ListenAndServe(); err != nil {
-		glog.Fatal(err)
+		log.Fatal(err)
 	}
 }
 
 func main() {
 	port := flag.Uint("port", 8080, "listener port")
 	flag.Parse()
-	RunHTTP(*port)
+	serve(*port)
 }
