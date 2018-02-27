1048fd3f8078a27011abbd557d31f891a4e3c1fd
ilackarms <sdw35@cornell.edu>
2018-02-26 19:37:22 -0500

    refactor and improve transformation plugin

--- transformation/plugin.go
+++ transformation/plugin.go
@@ -5,11 +5,9 @@ import (
 	envoyroute "github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
 	envoyhttp "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/http_connection_manager/v2"
 
-	"regexp"
-
-	"strings"
-
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/gogo/protobuf/types"
 	"github.com/mitchellh/hashstructure"
@@ -40,17 +38,44 @@ func (p *Plugin) GetDependencies(_ *v1.Config) *plugin.Dependencies {
 	return nil
 }
 
-func parsePathParameterToRegex(path string) ([]string, string) {
+func parseParameterToRegex(paramString string) ([]string, string) {
 	// escape regex
 	// TODO: make sure all envoy regex is being escaped here
-	path = regexp.QuoteMeta(path)
+	paramString = regexp.QuoteMeta(paramString)
 	rxp := regexp.MustCompile("\\{([[:word:]]+)\\}")
-	parameterNames := rxp.FindAllString(path, -1)
+	parameterNames := rxp.FindAllString(paramString, -1)
 	for i, name := range parameterNames {
 		parameterNames[i] = strings.TrimSuffix(strings.TrimPrefix(name, "{"), "}")
 	}
 	// remember that the order of the param names correlates with their order in the regex
-	return parameterNames, rxp.ReplaceAllString(path, "([_[:alnum:]]+)")
+	return parameterNames, rxp.ReplaceAllString(paramString, "([_[:alnum:]]+)")
+}
+
+func (p *Plugin) setHeaderFromParam(header, parameter string, extractors map[string]*Extraction, out *envoyroute.Route) {
+	if parameter != "" {
+		paramNames, pathRegexMatcher := parseParameterToRegex(parameter)
+		// if no regex, this is a "default variable" that the user gets for free
+		if len(paramNames) == 0 {
+			// extract everything
+			// TODO(yuval): create a special extractor that doesn't use regex when we just want the whole thing
+			extract := &Extraction{
+				Header:   header,
+				Regex:    "(.*)",
+				Subgroup: uint32(1),
+			}
+			extractors[strings.TrimPrefix(header, ":")] = extract
+		}
+
+		// otherwise it's regex, and we need to create an extraction for each variable name they defined
+		for i, name := range paramNames {
+			extract := &Extraction{
+				Header:   header,
+				Regex:    pathRegexMatcher,
+				Subgroup: uint32(i + 1),
+			}
+			extractors[name] = extract
+		}
+	}
 }
 
 func (p *Plugin) ProcessRoute(_ *plugin.RoutePluginParams, in *v1.Route, out *envoyroute.Route) error {
@@ -64,18 +89,18 @@ func (p *Plugin) ProcessRoute(_ *plugin.RoutePluginParams, in *v1.Route, out *en
 
 	extractors := make(map[string]*Extraction)
 
-	if spec.Input.PathParameter != "" {
-		paramNames, pathRegexMatcher := parsePathParameterToRegex(spec.Input.PathParameter)
-		for i, name := range paramNames {
-			extract := &Extraction{
-				Header:   ":path",
-				Regex:    pathRegexMatcher,
-				Subgroup: uint32(i + 1),
-			}
-			extractors[name] = extract
-		}
+	// special http2 headers
+	p.setHeaderFromParam(":path", spec.Input.PathParameter, extractors, out)
+	p.setHeaderFromParam(":method", spec.Input.MethodParameter, extractors, out)
+	p.setHeaderFromParam(":scheme", spec.Input.MethodParameter, extractors, out)
+	p.setHeaderFromParam(":authority", spec.Input.AuthorityParameter, extractors, out)
+	// extra headers
+	for headerName, headerValue := range spec.Input.HeaderParameters {
+		p.setHeaderFromParam(headerName, headerValue, extractors, out)
 	}
 
+	// create templates
+	// right now it's just a no-op, user writes inja directly
 	headerTemplates := make(map[string]*InjaTemplate)
 	for k, v := range spec.Output.HeaderTemplates {
 		headerTemplates[k] = &InjaTemplate{Text: v}
--- transformation/spec.go
+++ transformation/spec.go
@@ -11,14 +11,30 @@ type TransformationSpec struct {
 }
 
 type Input struct {
-	HeaderParameters map[string]string `json:"header_parameters"`
-	// we will parse this
-	// get X capture groups
-	// generate regex string from it
-	// and header == :path
-	PathParameter string `json:"path_parameter,omitempty"`
+	// headers that will be used to derive the data for processing the output templates
+	// if no syntax containing {variables} are detected in the header value,
+	// the whole value will be substituted by its name into the template
+	// for example:
+	/*
+		input:
+			header_parmeters:
+		      x-header-foo: bar
+		output:
+			body_template: "{\"path\": {{ path }}}"
+	*/
+	HeaderParameters   map[string]string `json:"header_parameters"`
+	PathParameter      string            `json:"path_parameter,omitempty"`
+	MethodParameter    string            `json:"method_parameter"`
+	SchemeParameter    string            `json:"scheme_parameter"`
+	AuthorityParameter string            `json:"authority_parameter"`
 }
 
+// SPECIAL VARIABLE NAMES:
+// these variables will be provided even if there is no parameter source
+// path
+// method
+// scheme
+// authority
 type Output struct {
 	HeaderTemplates map[string]string `json:"header_templates"`
 	BodyTemplate    string            `json:"body_template"`
