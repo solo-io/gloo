15c0350cfecf6fc9287279280f92218a0b9b5387
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-02-26 21:47:10 -0500

    google plugin - untested

---
+++ google/plugin.go
@@ -0,0 +1,101 @@
+package gfunc
+
+import (
+	envoyapi "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	envoyauth "github.com/envoyproxy/go-control-plane/envoy/api/v2/auth"
+	envoycore "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+	envoyroute "github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
+	envoyhttp "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/http_connection_manager/v2"
+
+	"github.com/gogo/protobuf/types"
+	"github.com/pkg/errors"
+
+	"github.com/solo-io/gloo-api/pkg/api/types/v1"
+	"github.com/solo-io/gloo-plugins/common"
+	"github.com/solo-io/gloo/pkg/plugin"
+)
+
+func init() {
+	plugin.Register(&Plugin{}, nil)
+}
+
+type Plugin struct{}
+
+const (
+	// define Upstream type name
+	UpstreamTypeGoogle = "google"
+
+	// generic plugin info
+	filterName  = "io.solo.gcloudfunc"
+	pluginStage = plugin.OutAuth
+
+	googleRegion = "region"
+
+	// function-specific metadata
+	functionHost = "host"
+	functionPath = "path"
+)
+
+func (p *Plugin) GetDependencies(cfg *v1.Config) *plugin.Dependencies {
+
+	return nil
+}
+
+func (p *Plugin) HttpFilter(params *plugin.FilterPluginParams) (*envoyhttp.HttpFilter, plugin.Stage) {
+	return &envoyhttp.HttpFilter{Name: filterName}, pluginStage
+}
+
+func (p *Plugin) ProcessRoute(_ *plugin.RoutePluginParams, in *v1.Route, out *envoyroute.Route) error {
+	return nil
+}
+
+func (p *Plugin) ProcessUpstream(params *plugin.UpstreamPluginParams, in *v1.Upstream, out *envoyapi.Cluster) error {
+	if in.Type != UpstreamTypeGoogle {
+		return nil
+	}
+
+	out.Type = envoyapi.Cluster_LOGICAL_DNS
+	// need to make sure we use ipv4 only dns
+	out.DnsLookupFamily = envoyapi.Cluster_V4_ONLY
+
+	googleUpstream, err := DecodeUpstreamSpec(in.Spec)
+	if err != nil {
+		return errors.Wrap(err, "invalid Google upstream spec")
+	}
+
+	out.Hosts = append(out.Hosts, &envoycore.Address{Address: &envoycore.Address_SocketAddress{SocketAddress: &envoycore.SocketAddress{
+		Address:       googleUpstream.GetGFuncHostname(),
+		PortSpecifier: &envoycore.SocketAddress_PortValue{PortValue: 443},
+	}}})
+	out.TlsContext = &envoyauth.UpstreamTlsContext{
+		Sni: googleUpstream.GetGFuncHostname(),
+	}
+
+	if out.Metadata == nil {
+		out.Metadata = &envoycore.Metadata{}
+	}
+	common.InitFilterMetadata(filterName, out.Metadata)
+	out.Metadata.FilterMetadata[filterName] = &types.Struct{
+		Fields: map[string]*types.Value{
+			googleRegion: {Kind: &types.Value_StringValue{StringValue: googleUpstream.Region}},
+		},
+	}
+
+	return nil
+}
+
+func (p *Plugin) ParseFunctionSpec(params *plugin.FunctionPluginParams, in v1.FunctionSpec) (*types.Struct, error) {
+	if params.UpstreamType != UpstreamTypeGoogle {
+		return nil, nil
+	}
+	functionSpec, err := DecodeFunctionSpec(in)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid google function spec")
+	}
+	return &types.Struct{
+		Fields: map[string]*types.Value{
+			functionHost: {Kind: &types.Value_StringValue{StringValue: functionSpec.host}},
+			functionPath: {Kind: &types.Value_StringValue{StringValue: functionSpec.path}},
+		},
+	}, nil
+}
---
+++ google/spec.go
@@ -0,0 +1,100 @@
+package gfunc
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+
+	"github.com/solo-io/gloo-api/pkg/api/types/v1"
+	"github.com/solo-io/gloo/pkg/protoutil"
+)
+
+type UpstreamSpec struct {
+	Region    string `json:"region"`
+	ProjectId string `json:"project_id"`
+}
+
+func DecodeUpstreamSpec(generic v1.UpstreamSpec) (*UpstreamSpec, error) {
+	s := new(UpstreamSpec)
+	if err := protoutil.UnmarshalStruct(generic, s); err != nil {
+		return nil, err
+	}
+	return s, s.validateUpstream()
+}
+
+func (s *UpstreamSpec) validateUpstream() error {
+	switch s.Region {
+
+	case "northamerica-northeast1":
+		return nil
+	case "us-central1":
+		return nil
+	case "us-west1":
+		return nil
+	case "us-east4":
+		return nil
+	case "us-east1":
+		return nil
+	case "southamerica-east1":
+		return nil
+	case "europe-west1":
+		return nil
+	case "europe-west2":
+		return nil
+	case "europe-west3":
+		return nil
+	case "europe-west4":
+		return nil
+	case "asia-south1":
+		return nil
+	case "asia-southeast1":
+		return nil
+	case "asia-east1":
+		return nil
+	case "asia-northeast1":
+		return nil
+	case "australia-southeast1":
+		return nil
+
+	}
+	return errors.New("no such region")
+}
+
+func (s *UpstreamSpec) GetGFuncHostname() string {
+	return fmt.Sprintf("%s-%s.cloudfunctions.net", s.Region, s.ProjectId)
+}
+
+type FunctionSpec struct {
+	Type string `json:"function_name"`
+	URL  string `json:"qualifier"`
+
+	path string
+	host string
+}
+
+func DecodeFunctionSpec(generic v1.FunctionSpec) (*FunctionSpec, error) {
+	s := new(FunctionSpec)
+	if err := protoutil.UnmarshalStruct(generic, s); err != nil {
+		return nil, err
+	}
+	err := s.ValidateGFunc()
+	if err != nil {
+		return s, err
+	}
+
+	parsedUrl, err := url.Parse(s.URL)
+	if err != nil {
+		return s, err
+	}
+
+	s.path = parsedUrl.Path
+	s.host = parsedUrl.Host
+	return s, nil
+}
+
+func (s *FunctionSpec) ValidateGFunc() error {
+	if s.URL == "" {
+		return errors.New("invalid function url")
+	}
+	return nil
+}
