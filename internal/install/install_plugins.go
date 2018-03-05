e5dded91a23c4f81c310f3e0a1110ab7c05370b4
ilackarms <sdw35@cornell.edu>
2018-03-05 15:55:19 -0500

    include gcloud plugin by default

--- internal/install/install_plugins.go
+++ internal/install/install_plugins.go
@@ -2,7 +2,7 @@ package install
 
 import (
 	_ "github.com/solo-io/gloo-plugins/aws"
-	//_ "github.com/solo-io/gloo-plugins/google"
+	_ "github.com/solo-io/gloo-plugins/google"
 	_ "github.com/solo-io/gloo-plugins/kubernetes"
 	_ "github.com/solo-io/gloo-plugins/transformation"
 )
