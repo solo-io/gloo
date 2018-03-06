c51c99478c6f0f485e0f8ce6503ecc9caf621cf8
ilackarms <sdw35@cornell.edu>
2018-03-06 14:10:25 -0500

    fix log line

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -5,39 +5,43 @@ import (
 	"os"
 	"runtime"
 
+	"regexp"
+
 	"github.com/k0kubun/pp"
 )
 
 var debugMode = os.Getenv("DEBUG") == "1"
 
+var rxp = regexp.MustCompile(".*/src/")
+
 func Sprintf(format string, a ...interface{}) string {
-	_, file, line, _ := runtime.Caller(1)
-	return pp.Sprintf("%v:%v\n"+format, append([]interface{}{file, line}, a...)...)
+	return pp.Sprintf("%v\t"+format, append([]interface{}{line()}, a...)...)
 }
 
 func GreyPrintf(format string, a ...interface{}) {
-	_, file, line, _ := runtime.Caller(1)
-	fmt.Printf("%v:%v\n"+format+"\n", append([]interface{}{file, line}, a...)...)
+	fmt.Printf("%v\t"+format+"\n", append([]interface{}{line()}, a...)...)
 }
 
 func Printf(format string, a ...interface{}) {
-	_, file, line, _ := runtime.Caller(1)
-	pp.Printf("%v:%v\n"+format+"\n", append([]interface{}{file, line}, a...)...)
+	pp.Printf("%v\t"+format+"\n", append([]interface{}{line()}, a...)...)
 }
 
 func Warnf(format string, a ...interface{}) {
-	_, file, line, _ := runtime.Caller(1)
-	pp.Printf("WARNING: %v:%v\n"+format+"\n", append([]interface{}{file, line}, a...)...)
+	pp.Printf("WARNING: %v\t"+format+"\n", append([]interface{}{line()}, a...)...)
 }
 
 func Debugf(format string, a ...interface{}) {
 	if debugMode {
-		_, file, line, _ := runtime.Caller(1)
-		pp.Printf("%v:%v\n"+format+"\n", append([]interface{}{file, line}, a...)...)
+		pp.Printf("%v\t"+format+"\n", append([]interface{}{line()}, a...)...)
 	}
 }
 
 func Fatalf(format string, a ...interface{}) {
-	_, file, line, _ := runtime.Caller(1)
-	pp.Fatalf("%v:%v\n"+format+"\n", append([]interface{}{file, line}, a...)...)
+	pp.Fatalf("%v\t"+format+"\n", append([]interface{}{line()}, a...)...)
+}
+
+func line() string {
+	_, file, line, _ := runtime.Caller(2)
+	file = rxp.ReplaceAllString(file, "")
+	return fmt.Sprintf("%v:%v", file, line)
 }
