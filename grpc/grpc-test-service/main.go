f5c1127b3315627606ac6eb9c064eac3ce846982
ilackarms <sdw35@cornell.edu>
2018-03-13 15:17:40 -0400

    grpc test service building

--- grpc/grpc-test-service/main.go
+++ grpc/grpc-test-service/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/solo-io/gloo-plugins/grpc/grpc-test-service/bookstore"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	port := flag.Int("p", 8080, "port")
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	bookstore.RegisterBookstoreServer(grpcServer, NewServer())
+	log.Printf("listening on %v", *port)
+	grpcServer.Serve(lis)
+}
