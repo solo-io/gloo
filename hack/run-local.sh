eea0789f9a505b020c4c27b685355340dc90b74b
Scott Weiss <sdw35@cornell.edu>
2018-03-25 11:10:35 -0400

    update runlocal to use host network and file storage

--- hack/run-local.sh
+++ hack/run-local.sh
@@ -1,31 +1,40 @@
 #!/usr/bin/env bash
 
+set -x -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 
 
 CONFIG_DIR=${CONFIG_DIR:-${DIR}/hack/gen-config-yaml/_gloo_config/}
 SECRETS_DIR=${CONFIG_DIR}/secrets/
+FILES_DIR=${CONFIG_DIR}/files
 
 mkdir -p ${CONFIG_DIR}/upstreams
 mkdir -p ${CONFIG_DIR}/virtualhosts
-mkdir -p ${CONFIG_DIR}/secrets
+mkdir -p ${SECRETS_DIR}
+mkdir -p ${FILES_DIR}
 
 FAIL=0
 
 echo "Starting gloo"
-docker run --rm -i \
-    -e DEBUG=1 \
-    -v ${CONFIG_DIR}:/config \
-    -v ${SECRETS_DIR}:/secrets \
-    --name gloo \
-    soloio/gloo:v0.1.local \
-    --file.config.dir /config \
-    --file.secret.dir /secrets &
+#docker run --rm -i \
+#    -e DEBUG=1 \
+#    -v ${CONFIG_DIR}:/config \
+#    -v ${SECRETS_DIR}:/secrets \
+#    --name gloo \
+#    soloio/gloo:v0.1.local \
+#    --file.config.dir /config \
+#    --file.secret.dir /secrets &
+
+./gloo --file.config.dir ${CONFIG_DIR} \
+       --file.secret.dir ${SECRETS_DIR} \
+       --file.secret.dir ${FILES_DIR} &
 
 sleep 1
 
 echo "Starting envoy"
-GLOO_IP=$(docker inspect gloo -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')
+#GLOO_IP=$(docker inspect gloo -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')
+GLOO_IP=127.0.0.1
 cat > ${CONFIG_DIR}/envoy.yaml <<EOF
 node:
   cluster: ingress
@@ -64,10 +73,11 @@ EOF
 docker run --rm -i \
     -v ${CONFIG_DIR}:/config \
     --name envoy \
-    -p 8080:8080 \
-    -p 8443:8443 \
-    -p 19000:19000 \
-    envoyproxy/envoy:latest \
+    --net=host \
+#    -p 8080:8080 \
+#    -p 8443:8443 \
+#    -p 19000:19000 \
+    envoyproxy/envoy:be0d5f72 \
     envoy \
     -c /config/envoy.yaml \
     --service-cluster envoy \
