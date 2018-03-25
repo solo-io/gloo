dc5f84de9669da6d2e373c87e8ce4b8e46c44d9d
Scott Weiss <sdw35@cornell.edu>
2018-03-24 21:49:00 -0400

    add new plugins to dep and install

--- internal/install/install_plugins.go
+++ internal/install/install_plugins.go
@@ -3,6 +3,7 @@ package install
 import (
 	_ "github.com/solo-io/gloo-plugins/aws"
 	_ "github.com/solo-io/gloo-plugins/google"
+	_ "github.com/solo-io/gloo-plugins/grpc"
 	_ "github.com/solo-io/gloo-plugins/kubernetes"
-	_ "github.com/solo-io/gloo-plugins/transformation"
+	_ "github.com/solo-io/gloo-plugins/rest"
 )
