6bf04586cca0ee1d7e44425b261b84f1fe7f738b
Tal Nordan <tal.nordan@solo.io>
2018-03-30 23:32:22 -0700

    Introduce azure/spec.go

---
+++ azure/spec.go
@@ -0,0 +1,100 @@
+package azure
+
+import (
+	"errors"
+	"regexp"
+
+	"github.com/gogo/protobuf/types"
+	"github.com/solo-io/gloo-api/pkg/api/types/v1"
+	"github.com/solo-io/gloo/pkg/protoutil"
+
+	multierror "github.com/hashicorp/go-multierror"
+)
+
+type UpstreamSpec struct {
+	FunctionAppName string `json:"function_app_name"`
+	SecretRef       string `json:"secret_ref"`
+}
+
+func EncodeUpstreamSpec(spec UpstreamSpec) *types.Struct {
+	v1Spec, err := protoutil.MarshalStruct(spec)
+	if err != nil {
+		panic(err)
+	}
+	return v1Spec
+}
+
+func DecodeUpstreamSpec(generic v1.UpstreamSpec) (*UpstreamSpec, error) {
+	s := new(UpstreamSpec)
+	if err := protoutil.UnmarshalStruct(generic, s); err != nil {
+		return nil, err
+	}
+	return s, s.Validate()
+}
+
+func (s *UpstreamSpec) Validate() error {
+	if IsValidFunctionAppName(s.FunctionAppName) {
+		return nil
+	} else {
+		return errors.New("invalid function app name")
+	}
+}
+
+type FunctionSpec struct {
+	FunctionName string `json:"function_name"`
+	AuthLevel    string `json:"auth_level"`
+}
+
+func EncodeFunctionSpec(spec FunctionSpec) *types.Struct {
+	v1Spec, err := protoutil.MarshalStruct(spec)
+	if err != nil {
+		panic(err)
+	}
+	return v1Spec
+}
+
+func DecodeFunctionSpec(generic v1.FunctionSpec) (*FunctionSpec, error) {
+	s := new(FunctionSpec)
+	if err := protoutil.UnmarshalStruct(generic, s); err != nil {
+		return nil, err
+	}
+	return s, s.Validate()
+}
+
+func (s *FunctionSpec) Validate() error {
+	var result error
+
+	if !IsValidFunctionName(s.FunctionName) {
+		result = multierror.Append(result, errors.New("invalid function name"))
+	}
+	if !IsValidAuthLevel(s.AuthLevel) {
+		result = multierror.Append(result, errors.New("invalid authentication level"))
+	}
+
+	return result
+}
+
+func IsValidFunctionAppName(functionAppName string) bool {
+	// Valid characters are `a-z`, `0-9`, and `-`.
+	return regexp.MustCompile("^[[:alpha:][:digit:]-]+$").MatchString(functionAppName)
+}
+
+func IsValidFunctionName(functionName string) bool {
+	// The name must be unique within a Function App. It must start with a letter and can contain
+	// letters, numbers (0-9), dashes ("-"), and underscores ("_").
+	return regexp.MustCompile("^[[:alpha:]][[:alpha:][:digit:]_-]*$").MatchString(functionName)
+}
+
+func IsValidAuthLevel(authLevel string) bool {
+	// The authorization level can be one of the following values:
+	// * "anonymous" - No API key is required.
+	// * "function" - A function-specific API key is required. This is the default value if none is
+	//                provided.
+	// * "admin" - The master key is required.
+	for _, str := range []string{"anonymous", "function", "admin"} {
+		if authLevel == str {
+			return true
+		}
+	}
+	return false
+}
---
+++ azure/spec_test.go
@@ -0,0 +1,188 @@
+package azure_test
+
+import (
+	"github.com/gogo/protobuf/types"
+	multierror "github.com/hashicorp/go-multierror"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/solo-io/gloo-plugins/azure"
+)
+
+var _ = Describe("Function app name", func() {
+	Describe("validation", func() {
+		Context("of an empty name", func() {
+			It("should fail", func() {
+				Expect(IsValidFunctionAppName("")).To(Equal(false))
+			})
+		})
+		Context("of a valid name", func() {
+			It("should succeed", func() {
+				Expect(IsValidFunctionAppName("azure-function-app-1")).To(Equal(true))
+			})
+		})
+		Context("of a name starting with a digit", func() {
+			It("should succeed", func() {
+				Expect(IsValidFunctionAppName("1-azure-function-app")).To(Equal(true))
+			})
+		})
+		Context("of a name starting with a dash", func() {
+			It("should succeed", func() {
+				Expect(IsValidFunctionAppName("-azure-function-app-1")).To(Equal(true))
+			})
+		})
+		Context("of a name containing an underscore", func() {
+			It("should fail", func() {
+				Expect(IsValidFunctionAppName("azure-function_app-1")).To(Equal(false))
+			})
+		})
+		Context("of a name containing an invalid character", func() {
+			It("should fail", func() {
+				Expect(IsValidFunctionAppName("azure-function-1!")).To(Equal(false))
+			})
+		})
+	})
+})
+
+var _ = Describe("Function name", func() {
+	Describe("validation", func() {
+		Context("of an empty name", func() {
+			It("should fail", func() {
+				Expect(IsValidFunctionName("")).To(Equal(false))
+			})
+		})
+		Context("of a valid name", func() {
+			It("should succeed", func() {
+				Expect(IsValidFunctionName("azure-function-1")).To(Equal(true))
+			})
+		})
+		Context("of a name starting with a digit", func() {
+			It("should fail", func() {
+				Expect(IsValidFunctionName("1-azure-function")).To(Equal(false))
+			})
+		})
+		Context("of a name starting with a dash", func() {
+			It("should fail", func() {
+				Expect(IsValidFunctionName("-azure-function-1")).To(Equal(false))
+			})
+		})
+		Context("of a name starting with an underscore", func() {
+			It("should fail", func() {
+				Expect(IsValidFunctionName("_azure-function-1")).To(Equal(false))
+			})
+		})
+		Context("of a name containing an invalid character", func() {
+			It("should fail", func() {
+				Expect(IsValidFunctionName("azure-function-1!")).To(Equal(false))
+			})
+		})
+	})
+})
+
+var _ = Describe("Authentication level", func() {
+	Describe("validation", func() {
+		Context("of an empty string", func() {
+			It("should fail", func() {
+				Expect(IsValidAuthLevel("")).To(Equal(false))
+			})
+		})
+		Context("of \"anonymous\"", func() {
+			It("should succeed", func() {
+				Expect(IsValidAuthLevel("anonymous")).To(Equal(true))
+			})
+		})
+		Context("of \"function\"", func() {
+			It("should succeed", func() {
+				Expect(IsValidAuthLevel("function")).To(Equal(true))
+			})
+		})
+		Context("of \"admin\"", func() {
+			It("should succeed", func() {
+				Expect(IsValidAuthLevel("admin")).To(Equal(true))
+			})
+		})
+		Context("of an invalid value", func() {
+			It("should fail", func() {
+				Expect(IsValidAuthLevel("invalid")).To(Equal(false))
+			})
+		})
+	})
+})
+
+func decodeUpstreamSpec(functionAppName string, secretRef string) (*UpstreamSpec, error) {
+	m := &types.Struct{
+		Fields: map[string]*types.Value{
+			"function_app_name": {Kind: &types.Value_StringValue{StringValue: functionAppName}},
+			"secret_ref":        {Kind: &types.Value_StringValue{StringValue: secretRef}},
+		},
+	}
+	return DecodeUpstreamSpec(m)
+}
+
+var _ = Describe("UpstreamSpec", func() {
+	Describe("decoding from a map", func() {
+		Context("with valid parameters", func() {
+			It("should succeed", func() {
+				spec, err := decodeUpstreamSpec("azure-function-app-1", "my-azure-sec")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(spec.FunctionAppName).To(Equal("azure-function-app-1"))
+				Expect(spec.SecretRef).To(Equal("my-azure-sec"))
+			})
+		})
+		Context("with an invlid function app name", func() {
+			It("should error", func() {
+				_, err := decodeUpstreamSpec("_azure-function-app-1", "my-azure-sec")
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+})
+
+func decodeFunctionSpec(functionName string, authLevel string) (*FunctionSpec, error) {
+	m := &types.Struct{
+		Fields: map[string]*types.Value{
+			"function_name": {Kind: &types.Value_StringValue{StringValue: functionName}},
+			"auth_level":    {Kind: &types.Value_StringValue{StringValue: authLevel}},
+		},
+	}
+	return DecodeFunctionSpec(m)
+}
+
+var _ = Describe("FunctionSpec", func() {
+	Describe("decoding from a map", func() {
+		Context("with valid parameters", func() {
+			It("should succeed", func() {
+				spec, err := decodeFunctionSpec("azure-function-1", "anonymous")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(spec.FunctionName).To(Equal("azure-function-1"))
+				Expect(spec.AuthLevel).To(Equal("anonymous"))
+			})
+		})
+		Context("with an invlid function name", func() {
+			It("should result in one error", func() {
+				_, err := decodeFunctionSpec("_azure-function-1", "anonymous")
+				Expect(err).To(HaveOccurred())
+				merr, ok := err.(*multierror.Error)
+				Expect(ok).To(Equal(true))
+				Expect(len(merr.Errors)).To(Equal(1))
+			})
+		})
+		Context("with an invlid authorization level", func() {
+			It("should result in one error", func() {
+				_, err := decodeFunctionSpec("azure-function-1", "invalid")
+				Expect(err).To(HaveOccurred())
+				merr, ok := err.(*multierror.Error)
+				Expect(ok).To(Equal(true))
+				Expect(len(merr.Errors)).To(Equal(1))
+			})
+		})
+		Context("with an invlid function name and an invlid authorization level", func() {
+			It("should result in two errors", func() {
+				_, err := decodeFunctionSpec("_azure-function-1", "invalid")
+				Expect(err).To(HaveOccurred())
+				merr, ok := err.(*multierror.Error)
+				Expect(ok).To(Equal(true))
+				Expect(len(merr.Errors)).To(Equal(2))
+			})
+		})
+	})
+})
