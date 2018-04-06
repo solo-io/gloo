2b52ab9a95614d69e537fa7c5e1d0f1201157ad4
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-04-06 00:22:08 -0400

    initial e2e test work @ilackarms take notice

--- test/helpers/build_images.go
+++ test/helpers/build_images.go
@@ -15,15 +15,34 @@ func BuildPushContainers(push bool) error {
 	if os.Getenv("SKIP_BUILD") == "1" {
 		return nil
 	}
+	os.Setenv("IMAGE_TAG", ImageTag)
+
+	// make the gloo containers
+	for _, component := range []string{"control-plane", "function-discovery", "kube-ingress-controller", "kube-upstream-discovery"} {
+		arg := component
+		if push {
+			arg += "-docker-push"
+		} else {
+			arg += "-docker"
+		}
+
+		cmd := exec.Command("make", arg)
+		cmd.Dir = SoloDirectory()
+		cmd.Stdout = os.Stderr
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			return err
+		}
+
+	}
+
 	for _, path := range []string{
-		filepath.Join(SoloDirectory(), "gloo"),
 		filepath.Join(E2eDirectory(), "containers", "helloservice"),
 		filepath.Join(E2eDirectory(), "containers", "testrunner"),
 		filepath.Join(E2eDirectory(), "containers", "event-emitter"),
 		filepath.Join(E2eDirectory(), "containers", "upstream-for-events"),
 		filepath.Join(E2eDirectory(), "containers", "grpc-test-service"),
 	} {
-		os.Setenv("IMAGE_TAG", ImageTag)
 		dockerUser := os.Getenv("DOCKER_USER")
 		if dockerUser == "" {
 			dockerUser = "soloio"
