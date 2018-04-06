80ba3069fd06e6b3608331e4a5e1ba159e0760be
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-04-05 21:30:23 -0400

    add sleep to give nats time to start

--- internal/function-discovery/nats-streaming/discover_nats_test.go
+++ internal/function-discovery/nats-streaming/discover_nats_test.go
@@ -5,9 +5,10 @@ import (
 	. "github.com/onsi/gomega"
 
 	"fmt"
+	"time"
 
-	"github.com/solo-io/gloo/pkg/api/types/v1"
 	. "github.com/solo-io/gloo/internal/function-discovery/nats-streaming"
+	"github.com/solo-io/gloo/pkg/api/types/v1"
 	"github.com/solo-io/gloo/pkg/plugins/nats-streaming"
 )
 
@@ -17,6 +18,7 @@ var _ = Describe("DiscoverNats", func() {
 			It("returns service info for nats-streaming", func() {
 				err = natsStreamingInstance.Run()
 				Expect(err).NotTo(HaveOccurred())
+				time.Sleep(2 * time.Second)
 				detector := NewNatsDetector(natsStreamingInstance.ClusterId())
 				svcInfo, annotations, err := detector.DetectFunctionalService(&v1.Upstream{Name: "Test"}, fmt.Sprintf("localhost:%v", natsStreamingInstance.NatsPort()))
 				Expect(err).To(BeNil())
