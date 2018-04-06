893fc18ca7541089ead044a2ac70b421754c78ca
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-04-06 17:06:19 -0400

    ability to use a debugger in local e2e now just for func-discovery now; will be extended.

--- test/helpers/local/functions.go
+++ test/helpers/local/functions.go
@@ -2,10 +2,14 @@ package localhelpers
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
+	"time"
 
+	"github.com/keybase/go-ps"
 	"github.com/solo-io/gloo/test/helpers"
 
 	"github.com/onsi/ginkgo"
@@ -15,6 +19,12 @@ type FunctionDiscoveryFactory struct {
 	srcpath           string
 	funcDiscoveryPath string
 	wasbuilt          bool
+}
+
+type FunctionDiscoveryInstance struct {
+	funcDiscoveryPath string
+	srcpath           string
+	cmd               *exec.Cmd
 }
 
 func NewFunctionDiscoveryFactory() (*FunctionDiscoveryFactory, error) {
@@ -58,6 +68,7 @@ func (fdf *FunctionDiscoveryFactory) NewFunctionDiscoveryInstance() (*FunctionDi
 
 	fdi := &FunctionDiscoveryInstance{
 		funcDiscoveryPath: fdf.funcDiscoveryPath,
+		srcpath:           fdf.srcpath,
 	}
 
 	return fdi, nil
@@ -67,12 +78,6 @@ func (fdf *FunctionDiscoveryFactory) Clean() error {
 	return nil
 }
 
-type FunctionDiscoveryInstance struct {
-	funcDiscoveryPath string
-
-	cmd *exec.Cmd
-}
-
 func (fdi *FunctionDiscoveryInstance) Run(datadir string) error {
 
 	cmd := exec.Command(fdi.funcDiscoveryPath,
@@ -80,17 +85,51 @@ func (fdi *FunctionDiscoveryInstance) Run(datadir string) error {
 		"--storage.refreshrate=1s",
 		"--secrets.type=file",
 		"--secrets.refreshrate=1s",
+		"--files.type=file",
+		"--files.refreshrate=1s",
 	)
 
 	cmd.Dir = datadir
 	cmd.Stdout = ginkgo.GinkgoWriter
 	cmd.Stderr = ginkgo.GinkgoWriter
-	err := cmd.Start()
+	cmd, err := fdi.run(cmd)
 	if err != nil {
 		return err
 	}
 	fdi.cmd = cmd
 	return nil
+}
+
+func (fdi *FunctionDiscoveryInstance) waitForExternalProcess() error {
+	for {
+		procs, err := ps.Processes()
+		if err != nil {
+			panic(err)
+		}
+		for _, proc := range procs {
+			str, err := proc.Path()
+			if err != nil {
+				continue
+			}
+			if strings.Contains(str, fdi.srcpath) {
+				return nil
+			}
+
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func (fdi *FunctionDiscoveryInstance) run(c *exec.Cmd) (*exec.Cmd, error) {
+	if os.Getenv("USE_DEBUGGER") == "1" {
+		fmt.Println("Please run the following command in your debugger:\n")
+		fmt.Printf("%v %v\n", c.Path, c.Args)
+		fmt.Printf("CWD %v\n", c.Dir)
+		fmt.Println("looking for processes started from", fdi.srcpath)
+
+		return nil, fdi.waitForExternalProcess()
+	}
+	return c, c.Start()
 }
 
 func (fdi *FunctionDiscoveryInstance) Clean() error {
