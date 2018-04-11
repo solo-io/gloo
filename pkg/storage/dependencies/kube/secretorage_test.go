be8bef861bdd7f8ac58f8f9888d1b877b68515d4
ilackarms <sdw35@cornell.edu>
2018-04-11 16:44:03 -0400

    unfocus test
    Signed-off-by: ilackarms <sdw35@cornell.edu>

--- pkg/storage/dependencies/kube/secretorage_test.go
+++ pkg/storage/dependencies/kube/secretorage_test.go
@@ -27,7 +27,7 @@ func toStringMap(m map[string][]byte) map[string]string {
 	return sm
 }
 
-var _ = FDescribe("Secret Storage Client", func() {
+var _ = Describe("Secret Storage Client", func() {
 	if os.Getenv("RUN_KUBE_TESTS") != "1" {
 		log.Printf("This test creates kubernetes resources and is disabled by default. To enable, set RUN_KUBE_TESTS=1 in your env.")
 		return
