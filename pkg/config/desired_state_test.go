40c1d365b62789e280da806e94c4bd650d5b4bdf
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-04-13 16:54:28 -0400

    initial syner tests

---
+++ pkg/config/desired_state_test.go
@@ -0,0 +1,83 @@
+package config_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/solo-io/gloo/pkg/api/types/v1"
+	. "github.com/solo-io/gloo/pkg/config"
+	"github.com/solo-io/gloo/pkg/storage"
+	"github.com/solo-io/gloo/pkg/storage/file"
+)
+
+func makeTestUpstream(number int) *v1.Upstream {
+	return &v1.Upstream{
+		Name: fmt.Sprintf("test-us-%d", number),
+	}
+}
+
+const OwnerTest = "test"
+
+var _ = Describe("DesiredState", func() {
+	var (
+		syncer       *UpstreamSyncer
+		glooClient   storage.Interface
+		dir          string
+		desiredState []*v1.Upstream
+		desiredError error
+	)
+	BeforeEach(func() {
+		var err error
+		dir, err = ioutil.TempDir("", "DesiredStateTest")
+		Expect(err).NotTo(HaveOccurred())
+		glooClient, err = file.NewStorage(dir, time.Millisecond)
+		Expect(err).NotTo(HaveOccurred())
+		err = glooClient.V1().Register()
+		Expect(err).NotTo(HaveOccurred())
+
+		syncer = &UpstreamSyncer{
+			GlooStorage:      glooClient,
+			Owner:           OwnerTest,
+			DesiredUpstreams: func() ([]*v1.Upstream, error) { return desiredState, desiredError },
+		}
+	})
+	AfterEach(func() {
+		if dir != "" {
+			os.RemoveAll(dir)
+		}
+	})
+
+	It("Should create upstreams when storage is empty", func() {
+		origUs:= makeTestUpstream(1)
+		desiredState = append(desiredState,origUs)
+		err := syncer.SyncDesiredState()
+		Expect(err).NotTo(HaveOccurred())
+
+		createdUpstreams, err := glooClient.V1().Upstreams().List()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(createdUpstreams).To(HaveLen(1))
+
+		us := createdUpstreams[0]
+		Expect(us.Name).To(Equal(origUs.Name))
+		Expect(us.Metadata.Annotations[OwnerAnnotationKey]).To(Equal(OwnerTest))
+
+	})
+
+	It("Should not create duplicate upstreams", func() {
+		desiredState = append(desiredState, makeTestUpstream(1))
+		err := syncer.SyncDesiredState()
+		Expect(err).NotTo(HaveOccurred())
+		err = syncer.SyncDesiredState()
+		Expect(err).NotTo(HaveOccurred())
+
+		createdUpstreams, err := glooClient.V1().Upstreams().List()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(createdUpstreams).To(HaveLen(1))
+	})
+
+})
