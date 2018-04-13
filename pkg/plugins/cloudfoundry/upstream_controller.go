33f900807130d972be619a3062aaac4d49d6e370
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-04-13 18:42:40 -0400

    update upstream controller with UpstreamSyncer

--- pkg/plugins/cloudfoundry/upstream_controller.go
+++ pkg/plugins/cloudfoundry/upstream_controller.go
@@ -6,34 +6,59 @@ import (
 
 	"code.cloudfoundry.org/copilot"
 	"github.com/solo-io/gloo/pkg/api/types/v1"
+	"github.com/solo-io/gloo/pkg/config"
+	"github.com/solo-io/gloo/pkg/storage"
 )
 
 const generatedBy = "cloudfoundry-upstream-discovery"
 
 type ServiceController struct {
+	errors chan error
+
 	resyncDuration time.Duration
 	ctx            context.Context
 	client         copilot.IstioClient
+
+	syncer config.UpstreamSyncer
+}
+
+func NewServiceController(ctx context.Context, configStore storage.Interface, client copilot.IstioClient, resyncDuration time.Duration) *ServiceController {
+	sc := &ServiceController{
+		errors: make(chan error),
+
+		resyncDuration: resyncDuration,
+		ctx:            ctx,
+		client:         client,
+
+		syncer: config.UpstreamSyncer{
+			Owner:       generatedBy,
+			GlooStorage: configStore,
+		},
+	}
+	sc.syncer.DesiredUpstreams = sc.getDesiredUpstreams
+
+	return sc
 }
 
 func (sc *ServiceController) Run(stop <-chan struct{}) {
 	ResyncLoop(sc.ctx, stop, sc.resync, sc.resyncDuration)
 }
 
-func (sc *ServiceController) GetDesiredUpstreams() ([]*v1.Upstream, error) {
+func (c *ServiceController) Error() <-chan error {
+	return c.errors
+}
+
+func (sc *ServiceController) getDesiredUpstreams() ([]*v1.Upstream, error) {
 	return GetUpstreams(sc.ctx, sc.client)
 }
 
 func (sc *ServiceController) resync() {
 	err := sc.resyncWithError()
 	if err != nil {
-		// sc.errors <- err
+		sc.errors <- err
 	}
 }
-func (sc *ServiceController) resyncWithError() error {
-	return syncUpstreams()
-}
 
-func syncUpstreams() error {
-	panic("replace me when merged")
+func (sc *ServiceController) resyncWithError() error {
+	return sc.syncer.SyncDesiredState()
 }
