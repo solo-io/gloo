3da5e5ae402cdd94885a5021e576a54c652a4d8d
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-04-13 15:40:39 -0400

    added less function to Endpoint for easy sorting

--- pkg/endpointdiscovery/endpoint_discovery.go
+++ pkg/endpointdiscovery/endpoint_discovery.go
@@ -23,3 +23,13 @@ type Interface interface {
 	// should show valid if the most recent update passed, otherwise a useful error
 	Error() <-chan error
 }
+
+func Less(e1, e2 *Endpoint) bool {
+	if e1.Address < e2.Address {
+		return true
+	}
+	if e1.Port < e2.Port {
+		return true
+	}
+	return false
+}
