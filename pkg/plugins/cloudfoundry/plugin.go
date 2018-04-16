a627196abb107c3d851d809d8ad6f72c9c002a2e
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-04-16 16:11:18 -0400

    make cf upstreams use EDS

--- pkg/plugins/cloudfoundry/plugin.go
+++ pkg/plugins/cloudfoundry/plugin.go
@@ -3,15 +3,25 @@ package cloudfoundry
 import (
 	"context"
 
+	envoyapi "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	envoycore "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+
 	"github.com/pkg/errors"
 
 	"github.com/solo-io/gloo/pkg/bootstrap"
 	"github.com/solo-io/gloo/pkg/endpointdiscovery"
 	"github.com/solo-io/gloo/pkg/plugins"
+
+	"github.com/solo-io/gloo/pkg/api/types/v1"
 )
 
 func init() {
-	plugins.Register(nil, createEndpointDiscovery)
+	plugins.Register(&Plugin{}, createEndpointDiscovery)
+}
+
+var _ plugins.UpstreamPlugin = &Plugin{}
+
+type Plugin struct {
 }
 
 func createEndpointDiscovery(opts bootstrap.Options) (endpointdiscovery.Interface, error) {
@@ -25,4 +35,26 @@ func createEndpointDiscovery(opts bootstrap.Options) (endpointdiscovery.Interfac
 		return nil, errors.Wrap(err, "failed to start copilot endpoint discovery")
 	}
 	return disc, err
+}
+
+func (p *Plugin) GetDependencies(cfg *v1.Config) *plugins.Dependencies {
+	return nil
+}
+
+func (p *Plugin) ProcessUpstream(params *plugins.UpstreamPluginParams, in *v1.Upstream, out *envoyapi.Cluster) error {
+	if in.Type != UpstreamTypeCF {
+		return nil
+	}
+
+	out.Type = envoyapi.Cluster_EDS
+
+	out.EdsClusterConfig = &envoyapi.Cluster_EdsClusterConfig{
+		EdsConfig: &envoycore.ConfigSource{
+			ConfigSourceSpecifier: &envoycore.ConfigSource_Ads{
+				Ads: &envoycore.AggregatedConfigSource{},
+			},
+		},
+	}
+
+	return nil
 }
