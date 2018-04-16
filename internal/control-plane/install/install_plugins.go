53ec65d0b38ad3dbb1bf2f7299ec36388c130946
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-04-16 16:10:58 -0400

    install cf plugin; add it to features.json

--- internal/control-plane/install/install_plugins.go
+++ internal/control-plane/install/install_plugins.go
@@ -3,6 +3,7 @@ package install
 import (
 	_ "github.com/solo-io/gloo/pkg/plugins/aws"
 	_ "github.com/solo-io/gloo/pkg/plugins/azure"
+	_ "github.com/solo-io/gloo/pkg/plugins/cloudfoundry"
 	_ "github.com/solo-io/gloo/pkg/plugins/google"
 	_ "github.com/solo-io/gloo/pkg/plugins/grpc"
 	_ "github.com/solo-io/gloo/pkg/plugins/kubernetes"
