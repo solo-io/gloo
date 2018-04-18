a3a1d94a93ae499235dfe5b4e7de1c80bae6331a
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-04-18 15:30:37 -0400

    fatal error if plugin is nil

--- pkg/plugins/registry.go
+++ pkg/plugins/registry.go
@@ -3,6 +3,7 @@ package plugins
 import (
 	"github.com/solo-io/gloo/pkg/bootstrap"
 	"github.com/solo-io/gloo/pkg/endpointdiscovery"
+	"github.com/solo-io/gloo/pkg/log"
 )
 
 var defaultRegistry = &registry{}
@@ -10,9 +11,10 @@ var defaultRegistry = &registry{}
 type EndpointDiscoveryInitFunc func(options bootstrap.Options) (endpointdiscovery.Interface, error)
 
 func Register(plugin TranslatorPlugin, startEndpointDiscovery EndpointDiscoveryInitFunc) {
-	if plugin != nil {
-		defaultRegistry.plugins = append(defaultRegistry.plugins, plugin)
+	if plugin == nil {
+		log.Fatalf("plugin can not be nil")
 	}
+	defaultRegistry.plugins = append(defaultRegistry.plugins, plugin)
 	if startEndpointDiscovery != nil {
 		defaultRegistry.endpointDiscoveries = append(defaultRegistry.endpointDiscoveries, startEndpointDiscovery)
 	}
