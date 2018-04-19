0fa1470171153cd7830a569007458cdf11a5192b
ilackarms <sdw35@cornell.edu>
2018-04-19 19:24:06 -0400

    test for upstream discovery
    Signed-off-by: ilackarms <sdw35@cornell.edu>

--- internal/upstream-discovery/consul/upstream_controller_test.go
+++ internal/upstream-discovery/consul/upstream_controller_test.go
@@ -3,9 +3,17 @@ package consul
 import (
 	"os"
 
+	"time"
+
+	"sort"
+
+	"github.com/gogo/protobuf/types"
+	"github.com/hashicorp/consul/api"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/solo-io/gloo/pkg/api/types/v1"
 	"github.com/solo-io/gloo/pkg/log"
+	consulstorage "github.com/solo-io/gloo/pkg/storage/consul"
 	"github.com/solo-io/gloo/test/helpers"
 	"github.com/solo-io/gloo/test/helpers/local"
 )
@@ -29,7 +37,7 @@ var _ = Describe("Consul UpstreamController Units", func() {
 	})
 })
 
-var _ = Describe("Consul UpstreamController Units", func() {
+var _ = Describe("Consul UpstreamController Integration", func() {
 	if os.Getenv("RUN_CONSUL_TESTS") != "1" {
 		log.Printf("This test downloads and runs consul and is disabled by default. To enable, set RUN_CONSUL_TESTS=1 in your env.")
 		return
@@ -55,16 +63,235 @@ var _ = Describe("Consul UpstreamController Units", func() {
 		consulFactory.Clean()
 	})
 
+	var (
+		consul   *api.Client
+		cfg      = api.DefaultConfig()
+		rootPath string
+	)
+
+	BeforeEach(func() {
+		var err error
+		consul, err = api.NewClient(cfg)
+		helpers.Must(err)
+		rootPath = helpers.RandString(4)
+	})
+	AfterEach(func() {
+		consul.KV().DeleteTree(rootPath, nil)
+	})
+
 	Describe("uniqueTagSets", func() {
 		It("takes a list of unsorted, duplicated tags and sorts and de-dupes them", func() {
-			input := [][]string{
-				{"foo", "bar", "baz"},
-				{"foo", "baz"},
-				{"baz", "foo"},
-				{"baz", "fooa"},
-			}
-			out := uniqueTagSets(input)
-			Expect(out).To(Equal(nil))
+			cfg.WaitTime = time.Minute
+
+			store, err := consulstorage.NewStorage(cfg, rootPath, time.Minute)
+			Expect(err).NotTo(HaveOccurred())
+
+			ctl, err := NewUpstreamController(cfg, store)
+			Expect(err).NotTo(HaveOccurred())
+
+			ch := make(chan struct{})
+			defer close(ch)
+			go ctl.Run(ch)
+
+			consul, err := api.NewClient(cfg)
+			Expect(err).NotTo(HaveOccurred())
+
+			svc2 := newConsulSvc("svc2", nil, "3.4.5.6", 3456)
+			svc3notags := newConsulSvc("svc3", []string{}, "4.5.6.7", 3456)
+			svc3sometags := newConsulSvc("svc3", []string{"a"}, "5.6.7.8", 3456)
+			svc3alltags := newConsulSvc("svc3", []string{"a", "b"}, "6.7.8.9", 3456)
+			svc3alltags2 := newConsulSvc("svc3", []string{"a", "b"}, "7.7.8.9", 3456)
+
+			err = consul.Agent().ServiceRegister(svc2)
+			Expect(err).NotTo(HaveOccurred())
+			err = consul.Agent().ServiceRegister(svc3notags)
+			Expect(err).NotTo(HaveOccurred())
+			err = consul.Agent().ServiceRegister(svc3sometags)
+			Expect(err).NotTo(HaveOccurred())
+			err = consul.Agent().ServiceRegister(svc3alltags)
+			Expect(err).NotTo(HaveOccurred())
+			err = consul.Agent().ServiceRegister(svc3alltags2)
+			Expect(err).NotTo(HaveOccurred())
+
+			var ups []*v1.Upstream
+
+			Eventually(func() ([]*v1.Upstream, error) {
+				select {
+				case err := <-ctl.Error():
+					return nil, err
+				default:
+					us, err := store.V1().Upstreams().List()
+					removeResourceVersion(us)
+					sort.SliceStable(us, func(i, j int) bool {
+						return us[i].Name < us[j].Name
+					})
+					ups = us
+					return us, err
+				}
+			}).Should(HaveLen(5))
+
+			Expect(ups).To(ContainElement(&v1.Upstream{
+				Name: "svc2",
+				Type: "consul",
+				Spec: &types.Struct{
+					Fields: map[string]*types.Value{
+						"service_name": {
+							Kind: &types.Value_StringValue{
+								StringValue: "svc2",
+							},
+						},
+						"service_tags": {
+							Kind: &types.Value_ListValue{
+								ListValue: &types.ListValue{
+									Values: nil,
+								},
+							},
+						},
+					},
+				},
+				Metadata: &v1.Metadata{
+					Annotations: map[string]string{
+						"generated_by": "consul-upstream-discovery",
+					},
+				},
+			}))
+
+			Expect(ups).To(ContainElement(&v1.Upstream{
+				Name: "svc2",
+				Type: "consul",
+				Spec: &types.Struct{
+					Fields: map[string]*types.Value{
+						"service_name": {
+							Kind: &types.Value_StringValue{
+								StringValue: "svc2",
+							},
+						},
+						"service_tags": {
+							Kind: &types.Value_ListValue{
+								ListValue: &types.ListValue{
+									Values: nil,
+								},
+							},
+						},
+					},
+				},
+				Metadata: &v1.Metadata{
+					Annotations: map[string]string{
+						"generated_by": "consul-upstream-discovery",
+					},
+				},
+			}))
+
+			Expect(ups).To(ContainElement(&v1.Upstream{
+				Name: "svc3",
+				Type: "consul",
+				Spec: &types.Struct{
+					Fields: map[string]*types.Value{
+						"service_name": {
+							Kind: &types.Value_StringValue{
+								StringValue: "svc3",
+							},
+						},
+						"service_tags": {
+							Kind: &types.Value_ListValue{
+								ListValue: &types.ListValue{
+									Values: nil,
+								},
+							},
+						},
+					},
+				},
+				Metadata: &v1.Metadata{
+					Annotations: map[string]string{
+						"generated_by": "consul-upstream-discovery",
+					},
+				},
+			}))
+
+			Expect(ups).To(ContainElement(&v1.Upstream{
+				Name: "svc3-a",
+				Type: "consul",
+				Spec: &types.Struct{
+					Fields: map[string]*types.Value{
+						"service_name": {
+							Kind: &types.Value_StringValue{
+								StringValue: "svc3",
+							},
+						},
+						"service_tags": {
+							Kind: &types.Value_ListValue{
+								ListValue: &types.ListValue{
+									Values: []*types.Value{
+										{
+											Kind: &types.Value_StringValue{
+												StringValue: "a",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				Metadata: &v1.Metadata{
+					Annotations: map[string]string{
+						"generated_by": "consul-upstream-discovery",
+					},
+				},
+			}))
+
+			Expect(ups).To(ContainElement(&v1.Upstream{
+				Name: "svc3-a-b",
+				Type: "consul",
+				Spec: &types.Struct{
+					Fields: map[string]*types.Value{
+						"service_name": {
+							Kind: &types.Value_StringValue{
+								StringValue: "svc3",
+							},
+						},
+						"service_tags": {
+							Kind: &types.Value_ListValue{
+								ListValue: &types.ListValue{
+									Values: []*types.Value{
+										{
+											Kind: &types.Value_StringValue{
+												StringValue: "a",
+											},
+										},
+										{
+											Kind: &types.Value_StringValue{
+												StringValue: "b",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				Metadata: &v1.Metadata{
+					Annotations: map[string]string{
+						"generated_by": "consul-upstream-discovery",
+					},
+				},
+			}))
 		})
 	})
 })
+
+func newConsulSvc(name string, tags []string, address string, port int) *api.AgentServiceRegistration {
+	return &api.AgentServiceRegistration{
+		ID:      helpers.RandString(4),
+		Name:    name,
+		Tags:    tags,
+		Port:    port,
+		Address: address,
+	}
+}
+
+func removeResourceVersion(items []*v1.Upstream) {
+	for _, item := range items {
+		item.Metadata.ResourceVersion = ""
+	}
+}
