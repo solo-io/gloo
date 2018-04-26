2a195f0e74c89341a46a053b78963e8e4c9b6e36
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-04-25 20:54:29 -0400

    AuthLevel changed to function

--- test/local_e2e/azure_functions_test.go
+++ test/local_e2e/azure_functions_test.go
@@ -93,7 +93,7 @@ var _ = Describe("Azure Functions", func() {
 
 		funcSpec := azure.FunctionSpec{
 			FunctionName: azureFunction,
-			AuthLevel:    "anonymous",
+			AuthLevel:    "function",
 		}
 
 		v1FSpec, err := protoutil.MarshalStruct(funcSpec)
