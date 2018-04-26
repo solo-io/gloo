bf0047c97181c9909280747c0b827f9e1e00c433
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-04-26 15:52:57 -0400

    logs them kube boys

--- test/kube_e2e/kubernetes_suite_test.go
+++ test/kube_e2e/kubernetes_suite_test.go
@@ -14,6 +14,21 @@ func TestKubernetes(t *testing.T) {
 		log.Printf("This test creates kubernetes resources and is disabled by default. To enable, set RUN_KUBE_TESTS=1 in your env.")
 		return
 	}
+
+	helpers.RegisterPreFailHandler(func() {
+		var logs string
+		for _, component := range []string{"control-plane", "ingress"} {
+			l, err := helpers.KubectlOut("logs", "-l", "gloo="+component)
+			logs += string(l) + "\n"
+			if err != nil {
+				logs += "error getting logs for " + component + ": " + err.Error()
+			}
+		}
+
+		log.Printf("\n****************************************" +
+			"\nLOGS FROM THE KUBE BOYS: \n\n" + logs + "\n************************************")
+	})
+
 	helpers.RegisterCommonFailHandlers()
 	log.DefaultOut = GinkgoWriter
 	RunSpecs(t, "Kubernetes Suite")
