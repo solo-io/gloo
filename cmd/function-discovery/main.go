6e96ef8fc54d2a5bcb1243bf5362988037b91c18
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-05-14 12:43:35 -0400

    minor fixes

--- cmd/function-discovery/main.go
+++ cmd/function-discovery/main.go
@@ -66,7 +66,7 @@ func init() {
 	rootCmd.PersistentFlags().BoolVar(&discoveryOpts.AutoDiscoverGRPC, "detect-grpc-upstreams", true, "enable automatic discovery of upstreams that are running gRPC Services and haeve reflection enabled.")
 	rootCmd.PersistentFlags().BoolVar(&discoveryOpts.AutoDiscoverFaaS, "detect-openfaas", true, "enable automatic discovery for OpenFaaS functions.")
 	rootCmd.PersistentFlags().BoolVar(&discoveryOpts.AutoDiscoverFission, "detect-fission", true, "enable automatic discovery for Fission functions.")
-	rootCmd.PersistentFlags().BoolVar(&discoveryOpts.AutoDiscoverProjectFn, "detect-project-fn-upstreams", true, "enable automatic discovery project fn upstreams.")
+	rootCmd.PersistentFlags().BoolVar(&discoveryOpts.AutoDiscoverProjectFn, "detect-project-fn", true, "enable automatic discovery project fn upstreams.")
 	rootCmd.PersistentFlags().StringSliceVar(&discoveryOpts.SwaggerUrisToTry, "swagger-uris", []string{}, "paths function discovery should try to use to discover swagger services. function discovery will query http://<upstream>/<uri> for the swagger.json document. "+
 		"if found, REST functions will be discovered for this upstream.")
 }
--- internal/function-discovery/updater/projectfn/imported/gen.go
+++ internal/function-discovery/updater/projectfn/imported/gen.go
@@ -1,3 +1,12 @@
 package imported
 
 //go:generate swagger generate client
+
+/**
+In this package generates the client code for ProjectFn.
+The swagger.yaml in this folder was copied from here:
+https://github.com/fnproject/fn/blob/master/docs/swagger.yml
+
+And was trimmed down to container only the definitions that we use (to avoid undeeded generated
+code).
+*/
