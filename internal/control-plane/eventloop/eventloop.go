178653acabdd2f0a98c48ce7b2bb1aa964f92811
Scott Weiss <sdw35@cornell.edu>
2018-05-15 11:23:49 -0400

    remove old doc
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- internal/control-plane/eventloop/eventloop.go
+++ internal/control-plane/eventloop/eventloop.go
@@ -5,6 +5,7 @@ import (
 
 	envoycache "github.com/envoyproxy/go-control-plane/pkg/cache"
 	"github.com/pkg/errors"
+	"github.com/solo-io/gloo/pkg/endpointdiscovery"
 
 	"github.com/solo-io/gloo/internal/control-plane/bootstrap"
 	"github.com/solo-io/gloo/internal/control-plane/configwatcher"
