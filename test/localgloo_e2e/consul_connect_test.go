a8d86e419f9d416405d752f1bfd3f250fc19b53b
Scott Weiss <sdw35@cornell.edu>
2018-06-25 16:30:05 -0400

    e2e test
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

---
+++ test/localgloo_e2e/consul_connect_test.go
@@ -0,0 +1,176 @@
+package localgloo_e2e_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+)
+
+type ConsulService struct {
+	Service Service `json:"service"`
+}
+
+type Service struct {
+	Name    string  `json:"name"`
+	Port    int     `json:"port"`
+	Connect Connect `json:"connect"`
+}
+
+type Connect struct {
+	Proxy Proxy `json:"proxy"`
+}
+
+type Proxy struct {
+	ExecMode string   `json:"exec_mode"`
+	Command  []string `json:"command"`
+	Config   Config   `json:"config"`
+}
+
+type Config struct {
+	Upstreams []Upstream `json:"upstreams"`
+}
+
+type Upstream struct {
+	DestinationName string `json:"destination_name"`
+	LocalBindPort   int    `json:"local_bind_port"`
+}
+
+type ProxyInfo struct {
+	ProxyServiceID    string
+	TargetServiceID   string
+	TargetServiceName string
+	ContentHash       string
+	ExecMode          string
+	Command           []string
+	Config            ProxyConfig
+}
+
+type ProxyConfig struct {
+	BindAddress         string     `json:"bind_address"`
+	BindPort            uint       `json:"bind_port"`
+	LocalServiceAddress string     `json:"local_service_address"`
+	Upstreams           []Upstream `json:"upstreams"`
+}
+
+var _ = Describe("ConsulConnect", func() {
+	var tmpdir string
+	var consulConfigDir string
+	var consulSession *gexec.Session
+	var pathToGlooBridge string
+
+	BeforeEach(func() {
+		pathToGlooBridge, err = gexec.Build("github.com/solo-io/gloo-consul-bridge/cmd")
+		Expect(err).ShouldNot(HaveOccurred())
+
+		envoypath := os.Getenv("ENVOY_PATH")
+		Expect(envoypath).ToNot(BeEmpty())
+		// generate the template
+
+		var err error
+		tmpdir, err = ioutil.TempDir("", "")
+		Expect(err).NotTo(HaveOccurred())
+
+		bridgeConfigDir := filepath.Join(tmpdir, "bridge-config")
+		err = os.Mkdir(bridgeConfigDir, 0755)
+		Expect(err).NotTo(HaveOccurred())
+
+		consulConfigDir = filepath.Join(tmpdir, "consul-config")
+		err = os.Mkdir(consulConfigDir, 0755)
+		Expect(err).NotTo(HaveOccurred())
+
+		args := []string{
+			pathToGlooBridge,
+			"--gloo-address",
+			"localhost",
+			"--gloo-port",
+			fmt.Sprintf("%v", xdsPort),
+			"--conf-dir",
+			bridgeConfigDir,
+			"--envoypath",
+			envoypath,
+		}
+		svc := ConsulService{
+			Service: Service{
+				Name: "web",
+				Port: 9090,
+				Connect: Connect{
+					Proxy: Proxy{
+						ExecMode: "daemon",
+						Command: args,
+						Config: Config{
+							Upstreams: []Upstream{
+								{
+									DestinationName: "redis",
+									LocalBindPort: 1234,
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		data, err := json.Marshal(svc)
+		Expect(err).NotTo(HaveOccurred())
+
+
+		err = ioutil.WriteFile(filepath.Join(consulConfigDir, "service.json"), data, 0644)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		gexec.TerminateAndWait("5s")
+		consulSession = nil
+		gexec.CleanupBuildArtifacts()
+
+		if tmpdir != "" {
+			os.RemoveAll(tmpdir)
+		}
+	})
+
+	runConsul := func() {
+		consul := exec.Command("consul", "agent", "-dev", "--config-dir="+consulConfigDir)
+		session, err := gexec.Start(consul, GinkgoWriter, GinkgoWriter)
+		consulSession = session
+
+		Expect(err).NotTo(HaveOccurred())
+	}
+
+	It("should start envoy", func() {
+		runConsul()
+		time.Sleep(1 * time.Second)
+		Expect(consulSession).ShouldNot(gexec.Exit())
+		Eventually(consulSession.Out, "5s").Should(gbytes.Say("agent/proxy: starting proxy:"))
+
+		// check that a port was opened where consul says it should have been opened (get the port from consul connect and check that it is open)
+		resp, err := http.Get("http://127.0.0.1:8500/v1/agent/connect/proxy/web-proxy")
+		Expect(err).NotTo(HaveOccurred())
+		body, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		Expect(err).NotTo(HaveOccurred())
+
+		var cfg ProxyInfo
+		json.Unmarshal(body, &cfg)
+
+		//runFakeXds(cfg.Config.BindAddress, cfg.Config.BindPort)
+
+		time.Sleep(5 * time.Second)
+
+		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", cfg.Config.BindAddress, cfg.Config.BindPort))
+		Expect(err).NotTo(HaveOccurred())
+
+		// We are connected! good enough!
+		conn.Close()
+	})
+})
--- test/localgloo_e2e/localgloo_e2e_test.go
+++ test/localgloo_e2e/localgloo_e2e_test.go
@@ -15,58 +15,53 @@ import (
 	"github.com/solo-io/gloo/pkg/localgloo"
 )
 
-var _ = Describe("LocalglooE2e", func() {
-	var (
-		tmpDir string
-		err    error
-		xdsPort = 8081
+var (
+	tmpDir  string
+	err     error
+	xdsPort = 8081
 
-		baseOpts              bootstrap.Options
-		controlPlaneOpts      = controlplanebootstrap.Options{
-			IngressOptions: controlplanebootstrap.IngressOptions{
-				BindAddress: "::",
-				Port: uint32(xdsPort),
-				SecurePort: uint32(xdsPort+1),
-			},
-		}
-		upstreamDiscoveryOpts upstreamdiscbootstrap.Options
-		functionDiscoveryOpts = functiondiscoveryopts.DiscoveryOptions{
-			//AutoDiscoverSwagger:   true,
-			//AutoDiscoverNATS:      true,
-			//AutoDiscoverFaaS:      true,
-			//AutoDiscoverFission:   true,
-			//AutoDiscoverProjectFn: true,
-			//AutoDiscoverGRPC:      true,
-		}
-	)
-	BeforeEach(func() {
-		tmpDir, err = ioutil.TempDir("", "localgloo-test")
-		Expect(err).To(BeNil())
-		configDir := tmpDir
-		filesDir := filepath.Join(tmpDir, "files")
-		secretsDir := filepath.Join(tmpDir, "secrets")
-		os.MkdirAll(configDir, 0755)
-		os.MkdirAll(filesDir, 0755)
-		os.MkdirAll(secretsDir, 0755)
-		baseOpts.ConfigStorageOptions.Type = "file"
-		baseOpts.FileStorageOptions.Type = "file"
-		baseOpts.SecretStorageOptions.Type = "file"
-		baseOpts.FileOptions.ConfigDir = configDir
-		baseOpts.FileOptions.SecretDir = secretsDir
-		baseOpts.FileOptions.FilesDir = filesDir
-		baseOpts.ConfigStorageOptions.SyncFrequency = time.Second
-		baseOpts.FileStorageOptions.SyncFrequency = time.Second
-		baseOpts.SecretStorageOptions.SyncFrequency = time.Second
-		controlPlaneOpts.Options = baseOpts
-		upstreamDiscoveryOpts.Options = baseOpts
-	})
-	AfterEach(func() {
-		os.RemoveAll(tmpDir)
-	})
-	It("runs everything", func() {
-		stop := make(chan struct{})
-		localgloo.Run(stop, xdsPort, baseOpts, controlPlaneOpts, upstreamDiscoveryOpts, functionDiscoveryOpts)
+	baseOpts         bootstrap.Options
+	controlPlaneOpts = controlplanebootstrap.Options{
+		IngressOptions: controlplanebootstrap.IngressOptions{
+			BindAddress: "::",
+			Port:        uint32(xdsPort),
+			SecurePort:  uint32(xdsPort + 1),
+		},
+	}
+	upstreamDiscoveryOpts upstreamdiscbootstrap.Options
+	functionDiscoveryOpts = functiondiscoveryopts.DiscoveryOptions{
+		//AutoDiscoverSwagger:   true,
+		//AutoDiscoverNATS:      true,
+		//AutoDiscoverFaaS:      true,
+		//AutoDiscoverFission:   true,
+		//AutoDiscoverProjectFn: true,
+		//AutoDiscoverGRPC:      true,
+	}
+)
+var _ = BeforeSuite(func() {
+	tmpDir, err = ioutil.TempDir("", "localgloo-test")
+	Expect(err).To(BeNil())
+	configDir := tmpDir
+	filesDir := filepath.Join(tmpDir, "files")
+	secretsDir := filepath.Join(tmpDir, "secrets")
+	os.MkdirAll(configDir, 0755)
+	os.MkdirAll(filesDir, 0755)
+	os.MkdirAll(secretsDir, 0755)
+	baseOpts.ConfigStorageOptions.Type = "file"
+	baseOpts.FileStorageOptions.Type = "file"
+	baseOpts.SecretStorageOptions.Type = "file"
+	baseOpts.FileOptions.ConfigDir = configDir
+	baseOpts.FileOptions.SecretDir = secretsDir
+	baseOpts.FileOptions.FilesDir = filesDir
+	baseOpts.ConfigStorageOptions.SyncFrequency = time.Second
+	baseOpts.FileStorageOptions.SyncFrequency = time.Second
+	baseOpts.SecretStorageOptions.SyncFrequency = time.Second
+	controlPlaneOpts.Options = baseOpts
+	upstreamDiscoveryOpts.Options = baseOpts
 
-		Expect(err).To(BeNil())
-	})
+	stop := make(chan struct{})
+	go localgloo.Run(stop, xdsPort, baseOpts, controlPlaneOpts, upstreamDiscoveryOpts, functionDiscoveryOpts)
+})
+var _ = AfterSuite(func() {
+	os.RemoveAll(tmpDir)
 })
