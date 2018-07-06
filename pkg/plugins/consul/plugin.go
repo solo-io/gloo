9f87f7638f59c4f784aacab2f350ebc05088bbd8
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-07-06 12:19:06 -0400

    add ssl context to clusters if in connect mode

--- pkg/plugins/consul/plugin.go
+++ pkg/plugins/consul/plugin.go
@@ -2,8 +2,13 @@ package consul
 
 import (
 	envoyapi "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	envoyauth "github.com/envoyproxy/go-control-plane/envoy/api/v2/auth"
 	envoycore "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+
 	"github.com/pkg/errors"
+
+	defaultv1 "github.com/solo-io/gloo/pkg/api/defaults/v1"
+	"github.com/solo-io/gloo/pkg/secretwatcher"
 
 	"github.com/solo-io/gloo/pkg/api/types/v1"
 	"github.com/solo-io/gloo/pkg/bootstrap"
@@ -17,6 +22,9 @@ func init() {
 
 func (p *Plugin) SetupEndpointDiscovery(opts bootstrap.Options) (endpointdiscovery.Interface, error) {
 	cfg := opts.ConsulOptions.ToConsulConfig()
+	// TODO(yuval-k): This is a big ugly it should happend durint init
+	// refactor this - perhaps register a plugin factory?
+	p.connect = opts.ConsulOptions.Connect
 	disc, err := NewEndpointController(cfg, opts.ConsulOptions.Connect)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to start consul endpoint discovery")
@@ -24,7 +32,9 @@ func (p *Plugin) SetupEndpointDiscovery(opts bootstrap.Options) (endpointdiscove
 	return disc, err
 }
 
-type Plugin struct{}
+type Plugin struct {
+	connect bool
+}
 
 const (
 	// define Upstream type name
@@ -35,7 +45,7 @@ func (p *Plugin) GetDependencies(_ *v1.Config) *plugins.Dependencies {
 	return nil
 }
 
-func (p *Plugin) ProcessUpstream(_ *plugins.UpstreamPluginParams, in *v1.Upstream, out *envoyapi.Cluster) error {
+func (p *Plugin) ProcessUpstream(params *plugins.UpstreamPluginParams, in *v1.Upstream, out *envoyapi.Cluster) error {
 	if in.Type != UpstreamTypeConsul {
 		return nil
 	}
@@ -53,5 +63,63 @@ func (p *Plugin) ProcessUpstream(_ *plugins.UpstreamPluginParams, in *v1.Upstrea
 			},
 		},
 	}
+	if p.connect {
+		// TODO(yuval-k): refactor this to be shared with gloo-connect code
+		const CertitificateSecretName = "certificates"
+
+		_, ok := params.Secrets[CertitificateSecretName]
+		if ok {
+			certChain, privateKey, rootCa, err := getSslSecrets(CertitificateSecretName, params.Secrets)
+			if err != nil {
+				return err
+			}
+			certChainData := &envoycore.DataSource{
+				Specifier: &envoycore.DataSource_InlineString{
+					InlineString: certChain,
+				},
+			}
+			privateKeyData := &envoycore.DataSource{
+				Specifier: &envoycore.DataSource_InlineString{
+					InlineString: privateKey,
+				},
+			}
+			rootCaData := &envoycore.DataSource{
+				Specifier: &envoycore.DataSource_InlineString{
+					InlineString: rootCa,
+				},
+			}
+
+			var validationContext *envoyauth.CertificateValidationContext
+			if rootCa != "" {
+				validationContext = &envoyauth.CertificateValidationContext{
+					TrustedCa: rootCaData,
+				}
+			}
+			out.TlsContext = &envoyauth.UpstreamTlsContext{
+				CommonTlsContext: &envoyauth.CommonTlsContext{
+					TlsParams: &envoyauth.TlsParameters{},
+					TlsCertificates: []*envoyauth.TlsCertificate{
+						{
+							CertificateChain: certChainData,
+							PrivateKey:       privateKeyData,
+						},
+					},
+					ValidationContext: validationContext,
+				},
+			}
+		}
+	}
 	return nil
+}
+
+// TODO(yuval-k): un-copy-paste this from route_config.go
+func getSslSecrets(ref string, secrets secretwatcher.SecretMap) (string, string, string, error) {
+	sslSecrets, ok := secrets[ref]
+	if !ok {
+		return "", "", "", errors.Errorf("ssl secret not found for ref %v", ref)
+	}
+	certChain, ok := sslSecrets.Data[defaultv1.SslCertificateChainKey]
+	privateKey, ok := sslSecrets.Data[defaultv1.SslPrivateKeyKey]
+	rootCa := sslSecrets.Data[defaultv1.SslRootCaKey]
+	return certChain, privateKey, rootCa, nil
 }
