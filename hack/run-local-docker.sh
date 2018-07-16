4f06039f2a3cf4ae684de8e854c5eefb6448177a
Scott Weiss <sdw35@cornell.edu>
2018-07-16 17:20:11 -0400

    bump versions
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- hack/run-local-docker.sh
+++ hack/run-local-docker.sh
@@ -86,7 +86,7 @@ docker run --rm  -i \
     -v ${CONFIG_DIR}:/config \
     --net=host \
     --name envoy \
-    soloio/envoy:0.3.1 \
+    soloio/envoy:0.4.0 \
     envoy \
     -c /config/envoy.yaml \
     --v2-config-only &
--- install/nomad/install.nomad
+++ install/nomad/install.nomad
@@ -88,7 +88,7 @@ job "gloo" {
 
       driver = "docker"
       config {
-        image = "soloio/envoy:0.3.1"
+        image = "soloio/envoy:0.4.0"
         port_map {
           http = 8080
           https = 8443
