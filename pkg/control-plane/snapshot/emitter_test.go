ac05c0df43cd6cc172e204b2afa86e3fff985a0a
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-07-26 20:35:43 -0400

    return -> Skip

--- pkg/control-plane/snapshot/emitter_test.go
+++ pkg/control-plane/snapshot/emitter_test.go
@@ -51,8 +51,7 @@ var _ = Describe("Emitter", func() {
 	Describe("Snapshot()", func() {
 		Context("using kubernetes for config, endpoints, secrets, and files", func() {
 			if os.Getenv("RUN_KUBE_TESTS") != "1" {
-				log.Printf("This test creates kubernetes resources and is disabled by default. To enable, set RUN_KUBE_TESTS=1 in your env.")
-				return
+				Skip("This test creates kubernetes resources and is disabled by default. To enable, set RUN_KUBE_TESTS=1 in your env.")
 			}
 			BeforeEach(func() {
 				namespace = helpers.RandString(8)
