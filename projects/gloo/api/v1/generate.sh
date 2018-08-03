ae2adfae0ba3cd21a79eecbe2eaa12a15bcd0dd1
Scott Weiss <sdw35@cornell.edu>
2018-08-03 17:12:17 -0400

    start working on gloo as a kit project
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

---
+++ projects/gloo/api/v1/generate.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+OUT=../../pkg/api/v1/
+mkdir -p ${OUT}
+protoc -I=./ -I=${GOPATH}/src/github.com/gogo/protobuf/  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I=${GOPATH}/src/github.com/gogo/protobuf/protobuf/ -I=${GOPATH}/src --gogo_out=plugins=grpc:${GOPATH}/src --plugin=protoc-gen-solo-kit=${GOPATH}/bin/protoc-gen-solo-kit --solo-kit_out=${OUT} *.proto
