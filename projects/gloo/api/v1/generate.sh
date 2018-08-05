72a74386e10e978c5484e7cb3a984024406e2ec5
Scott Weiss <sdw35@cornell.edu>
2018-08-05 16:53:25 -0400

    fix protoc command, make metadata not nullable
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- projects/gloo/api/v1/generate.sh
+++ projects/gloo/api/v1/generate.sh
@@ -2,4 +2,11 @@
 
 OUT=../../pkg/api/v1/
 mkdir -p ${OUT}
-protoc -I=./ -I=${GOPATH}/src/github.com/gogo/protobuf/  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I=${GOPATH}/src/github.com/gogo/protobuf/protobuf/ -I=${GOPATH}/src --gogo_out=plugins=grpc:${GOPATH}/src --plugin=protoc-gen-solo-kit=${GOPATH}/bin/protoc-gen-solo-kit --solo-kit_out=${OUT} *.proto
+protoc -I=./ \
+    -I=${GOPATH}/src/github.com/gogo/protobuf/ \
+    -I=${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -I=${GOPATH}/src/github.com/gogo/protobuf/protobuf/ \
+    -I=${GOPATH}/src \
+    --gogo_out=Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types:${GOPATH}/src/ \
+    --plugin=protoc-gen-solo-kit=${GOPATH}/bin/protoc-gen-solo-kit --solo-kit_out=${OUT} \
+    *.proto
