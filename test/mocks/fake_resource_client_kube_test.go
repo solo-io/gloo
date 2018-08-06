63c99dd6291a1902fb8c25d4b405591b180df235
Scott Weiss <sdw35@cornell.edu>
2018-08-06 11:54:59 -0400

    refactor generated test
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- test/mocks/fake_resource_client_kube_test.go
+++ test/mocks/fake_resource_client_kube_test.go
@@ -19,159 +19,186 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-var _ = Describe("FakeResourceClient", func() {
+var _ = FDescribe("FakeResourceClient", func() {
 	var (
 		namespace string
 		client    FakeResourceClient
 	)
-	if os.Getenv("RUN_KUBE_TESTS") != "1" {
-		log.Printf("This test creates kubernetes resources and is disabled by default. To enable, set RUN_KUBE_TESTS=1 in your env.")
-		return
+	for description, test := range map[string]struct {
+		skip     func() bool
+		setup    func() FakeResourceClient
+		teardown func()
+	}{
+		"kube_crd": {
+			skip: func() bool {
+				if os.Getenv("RUN_KUBE_TESTS") != "1" {
+					log.Printf("This test creates kubernetes resources and is disabled by default. To enable, set RUN_KUBE_TESTS=1 in your env.")
+					return false
+				}
+				return true
+			},
+			setup: func() FakeResourceClient {
+				namespace = helpers.RandString(8)
+				err := services.SetupKubeForTest(namespace)
+				Expect(err).NotTo(HaveOccurred())
+				kubeconfigPath := filepath.Join(os.Getenv("HOME"), ".kube", "config")
+				cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+				Expect(err).NotTo(HaveOccurred())
+				clientFactory := factory.NewResourceClientFactory(&factory.KubeResourceClientOpts{
+					Crd: FakeResourceCrd,
+					Cfg: cfg,
+				})
+				client, err := NewFakeResourceClient(clientFactory)
+				Expect(err).NotTo(HaveOccurred())
+				return client
+			},
+			teardown: func() {
+				services.TeardownKube(namespace)
+			},
+		},
+	} {
+		if test.skip() {
+			continue
+		}
+		Context(description, func() {
+			BeforeEach(func() {
+				client = test.setup()
+			})
+			AfterEach(test.teardown)
+			It("CRUDs on resource type FakeResource", func() {
+				testClient(namespace, client)
+			})
+		})
 	}
-	BeforeEach(func() {
-		namespace = helpers.RandString(8)
-		err := services.SetupKubeForTest(namespace)
-		Expect(err).NotTo(HaveOccurred())
-		kubeconfigPath := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-		cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
-		Expect(err).NotTo(HaveOccurred())
-		clientFactory := factory.NewResourceClientFactory(&factory.KubeResourceClientOpts{
-			Crd: FakeResourceCrd,
-			Cfg: cfg,
-		})
-		client, err = NewFakeResourceClient(clientFactory)
-		Expect(err).NotTo(HaveOccurred())
+
+})
+
+func testClient(namespace string, client FakeResourceClient) {
+	err := client.Register()
+	Expect(err).NotTo(HaveOccurred())
+
+	name := "foo"
+	input := NewFakeResource(namespace, name)
+	input.Metadata.Namespace = namespace
+	r1, err := client.Write(input, clients.WriteOpts{})
+	Expect(err).NotTo(HaveOccurred())
+
+	_, err = client.Write(input, clients.WriteOpts{})
+	Expect(err).To(HaveOccurred())
+	Expect(errors.IsExist(err)).To(BeTrue())
+
+	Expect(r1).To(BeAssignableToTypeOf(&FakeResource{}))
+	Expect(r1.GetMetadata().Name).To(Equal(name))
+	Expect(r1.GetMetadata().Namespace).To(Equal(namespace))
+	Expect(r1.GetMetadata().ResourceVersion).NotTo(Equal("7"))
+	Expect(r1.Count).To(Equal(input.Count))
+
+	_, err = client.Write(input, clients.WriteOpts{
+		OverwriteExisting: true,
 	})
-	AfterEach(func() {
-		services.TeardownKube(namespace)
+	Expect(err).To(HaveOccurred())
+
+	input.Metadata.ResourceVersion = r1.GetMetadata().ResourceVersion
+	r1, err = client.Write(input, clients.WriteOpts{
+		OverwriteExisting: true,
 	})
-	It("CRUDs resources", func() {
-		err := client.Register()
-		Expect(err).NotTo(HaveOccurred())
+	Expect(err).NotTo(HaveOccurred())
 
-		name := "foo"
-		input := NewFakeResource(namespace, name)
-		input.Metadata.Namespace = namespace
-		r1, err := client.Write(input, clients.WriteOpts{})
-		Expect(err).NotTo(HaveOccurred())
+	read, err := client.Read(namespace, name, clients.ReadOpts{})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(read).To(Equal(r1))
 
-		_, err = client.Write(input, clients.WriteOpts{})
-		Expect(err).To(HaveOccurred())
-		Expect(errors.IsExist(err)).To(BeTrue())
+	_, err = client.Read("doesntexist", name, clients.ReadOpts{})
+	Expect(err).To(HaveOccurred())
+	Expect(errors.IsNotExist(err)).To(BeTrue())
 
-		Expect(r1).To(BeAssignableToTypeOf(&FakeResource{}))
-		Expect(r1.GetMetadata().Name).To(Equal(name))
-		Expect(r1.GetMetadata().Namespace).To(Equal(namespace))
-		Expect(r1.GetMetadata().ResourceVersion).NotTo(Equal("7"))
-		Expect(r1.Count).To(Equal(input.Count))
+	name = "boo"
+	input = &FakeResource{}
 
-		_, err = client.Write(input, clients.WriteOpts{
-			OverwriteExisting: true,
+	// ignore return error because interfaces / oneofs mess it up
+	faker.FakeData(input)
+
+	input.Metadata = core.Metadata{
+		Name:      name,
+		Namespace: namespace,
+	}
+
+	r2, err := client.Write(input, clients.WriteOpts{})
+	Expect(err).NotTo(HaveOccurred())
+
+	list, err := client.List(namespace, clients.ListOpts{})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(list).To(ContainElement(r1))
+	Expect(list).To(ContainElement(r2))
+
+	err = client.Delete(namespace, "adsfw", clients.DeleteOpts{})
+	Expect(err).To(HaveOccurred())
+	Expect(errors.IsNotExist(err)).To(BeTrue())
+
+	err = client.Delete(namespace, "adsfw", clients.DeleteOpts{
+		IgnoreNotExist: true,
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	err = client.Delete(namespace, r2.GetMetadata().Name, clients.DeleteOpts{})
+	Expect(err).NotTo(HaveOccurred())
+	list, err = client.List(namespace, clients.ListOpts{})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(list).To(ContainElement(r1))
+	Expect(list).NotTo(ContainElement(r2))
+
+	w, errs, err := client.Watch(namespace, clients.WatchOpts{
+		RefreshRate: time.Hour,
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	var r3 resources.Resource
+	wait := make(chan struct{})
+	go func() {
+		defer close(wait)
+		defer GinkgoRecover()
+
+		resources.UpdateMetadata(r2, func(meta *core.Metadata) {
+			meta.ResourceVersion = ""
 		})
-		Expect(err).To(HaveOccurred())
-
-		input.Metadata.ResourceVersion = r1.GetMetadata().ResourceVersion
-		r1, err = client.Write(input, clients.WriteOpts{
-			OverwriteExisting: true,
-		})
+		r2, err = client.Write(r2, clients.WriteOpts{})
 		Expect(err).NotTo(HaveOccurred())
 
-		read, err := client.Read(namespace, name, clients.ReadOpts{})
-		Expect(err).NotTo(HaveOccurred())
-		Expect(read).To(Equal(r1))
-
-		_, err = client.Read("doesntexist", name, clients.ReadOpts{})
-		Expect(err).To(HaveOccurred())
-		Expect(errors.IsNotExist(err)).To(BeTrue())
-
-		name = "boo"
+		name = "goo"
 		input = &FakeResource{}
-		
-		// ignore return error because interfaces / oneofs mess it up 
+		// ignore return error because interfaces / oneofs mess it up
 		faker.FakeData(input)
-		
+		Expect(err).NotTo(HaveOccurred())
 		input.Metadata = core.Metadata{
 			Name:      name,
 			Namespace: namespace,
 		}
 
-		r2, err := client.Write(input, clients.WriteOpts{})
+		r3, err = client.Write(input, clients.WriteOpts{})
 		Expect(err).NotTo(HaveOccurred())
+	}()
+	<-wait
 
-		list, err := client.List(namespace, clients.ListOpts{})
+	select {
+	case err := <-errs:
 		Expect(err).NotTo(HaveOccurred())
-		Expect(list).To(ContainElement(r1))
-		Expect(list).To(ContainElement(r2))
+	case list = <-w:
+	case <-time.After(time.Millisecond * 5):
+		Fail("expected a message in channel")
+	}
 
-		err = client.Delete(namespace, "adsfw", clients.DeleteOpts{})
-		Expect(err).To(HaveOccurred())
-		Expect(errors.IsNotExist(err)).To(BeTrue())
-
-		err = client.Delete(namespace, "adsfw", clients.DeleteOpts{
-			IgnoreNotExist: true,
-		})
-		Expect(err).NotTo(HaveOccurred())
-
-		err = client.Delete(namespace, r2.GetMetadata().Name, clients.DeleteOpts{})
-		Expect(err).NotTo(HaveOccurred())
-		list, err = client.List(namespace, clients.ListOpts{})
-		Expect(err).NotTo(HaveOccurred())
-		Expect(list).To(ContainElement(r1))
-		Expect(list).NotTo(ContainElement(r2))
-
-		w, errs, err := client.Watch(namespace, clients.WatchOpts{
-			RefreshRate: time.Hour,
-		})
-		Expect(err).NotTo(HaveOccurred())
-
-		var r3 resources.Resource
-		wait := make(chan struct{})
-		go func() {
-			defer close(wait)
-			defer GinkgoRecover()
-
-			resources.UpdateMetadata(r2, func(meta *core.Metadata) {
-				meta.ResourceVersion = ""
-			})
-			r2, err = client.Write(r2, clients.WriteOpts{})
-			Expect(err).NotTo(HaveOccurred())
-
-			name = "goo"
-			input = &FakeResource{}
-			// ignore return error because interfaces / oneofs mess it up 
-			faker.FakeData(input)
-			Expect(err).NotTo(HaveOccurred())
-			input.Metadata = core.Metadata{
-				Name:      name,
-				Namespace: namespace,
-			}
-
-			r3, err = client.Write(input, clients.WriteOpts{})
-			Expect(err).NotTo(HaveOccurred())
-		}()
-		<-wait
-
+drain:
+	for {
 		select {
+		case list = <-w:
 		case err := <-errs:
 			Expect(err).NotTo(HaveOccurred())
-		case list = <-w:
-		case <-time.After(time.Millisecond * 5):
-			Fail("expected a message in channel")
+		case <-time.After(time.Millisecond * 500):
+			break drain
 		}
+	}
 
-	drain:
-		for {
-			select {
-			case list = <-w:
-			case err := <-errs:
-				Expect(err).NotTo(HaveOccurred())
-			case <-time.After(time.Millisecond * 500):
-				break drain
-			}
-		}
-
-		Expect(list).To(ContainElement(r1))
-		Expect(list).To(ContainElement(r2))
-		Expect(list).To(ContainElement(r3))
-	})
-})
+	Expect(list).To(ContainElement(r1))
+	Expect(list).To(ContainElement(r2))
+	Expect(list).To(ContainElement(r3))
+}
