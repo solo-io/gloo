16677a6392b0a80f43a5e953a3d4dc6a991a4e76
Scott Weiss <sdw35@cornell.edu>
2018-08-07 23:48:22 -0400

    lay down foundation for virtualhost and route config
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- projects/gloo/pkg/translator/route_config.go
+++ projects/gloo/pkg/translator/route_config.go
@@ -6,19 +6,25 @@ import (
 	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
 	defaultv1 "github.com/solo-io/gloo/pkg/api/defaults/v1"
-
+	envoycore "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	"github.com/solo-io/solo-kit/pkg/api/v1/reporter"
 	"github.com/solo-io/solo-kit/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/solo-kit/projects/gloo/pkg/plugins"
 	"github.com/gogo/protobuf/types"
 )
 
+type reportErr func(error error, format string, args ...interface{})
+
 func (t *translator) computeRouteConfig(proxy *v1.Proxy, listener *v1.Listener, routeCfgName string, snap *v1.Snapshot, resourceErrs reporter.ResourceErrors) *envoyapi.RouteConfiguration {
-	virtualHosts := t.computeVirtualHosts(proxy, listener, snap, resourceErrs)
+	report := func(err error, format string, args ...interface{}) {
+		resourceErrs.AddError(proxy, errors.Wrapf(err, format, args...))
+	}
+
+	virtualHosts := t.computeVirtualHosts(listener, snap, report)
 
 	// validate ssl config if the listener specifies any
 	if err := validateListenerSslConfig(listener, snap.SecretList); err != nil {
-		resourceErrs.AddError(proxy, errors.Wrapf(err, "invalid listener %v", listener.Name))
+		report(err, "invalid listener %v", listener.Name)
 	}
 
 	return &envoyapi.RouteConfiguration{
@@ -27,29 +33,28 @@ func (t *translator) computeRouteConfig(proxy *v1.Proxy, listener *v1.Listener, 
 	}
 }
 
-func (t *translator) computeVirtualHosts(proxy *v1.Proxy, listener *v1.Listener, snap *v1.Snapshot, resourceErrs reporter.ResourceErrors) []envoyroute.VirtualHost {
+func (t *translator) computeVirtualHosts(listener *v1.Listener, snap *v1.Snapshot, report reportErr) []envoyroute.VirtualHost {
 	httpListener, ok := listener.ListenerType.(*v1.Listener_HttpListener)
 	if !ok {
 		panic("non-HTTP listeners are not currently supported in Gloo")
 	}
 	virtualHosts := httpListener.HttpListener.VirtualHosts
 	if err := validateVirtualHostDomains(virtualHosts); err != nil {
-		resourceErrs.AddError(proxy, errors.Wrapf(err, "invalid listener %v", listener.Name))
+		report(err, "invalid listener %v", listener.Name)
 	}
 	requireTls := len(listener.SslConfiguations) > 0
 	var envoyVirtualHosts []envoyroute.VirtualHost
 	for _, virtualHost := range virtualHosts {
-		envoyVirtualHosts = append(envoyVirtualHosts, t.computeVirtualHost(proxy, virtualHost, requireTls, snap, resourceErrs))
+		envoyVirtualHosts = append(envoyVirtualHosts, t.computeVirtualHost(virtualHost, requireTls, snap, report))
 	}
 	return envoyVirtualHosts
 }
 
-func (t *translator) computeVirtualHost(proxy *v1.Proxy, virtualHost *v1.VirtualHost, requireTls bool, snap *v1.Snapshot, resourceErrs reporter.ResourceErrors) envoyroute.VirtualHost {
-
+func (t *translator) computeVirtualHost(virtualHost *v1.VirtualHost, requireTls bool, snap *v1.Snapshot, report reportErr) envoyroute.VirtualHost {
 	var envoyRoutes []envoyroute.Route
 	for _, route := range virtualHost.Routes {
-		envoyRoute := t.envoyRoute(route)
-		envoyRoutes = append(envoyRoutes, out)
+		envoyRoute := t.envoyRoute(snap, report, route)
+		envoyRoutes = append(envoyRoutes, envoyRoute)
 	}
 	domains := virtualHost.Domains
 	if len(domains) == 0 || (len(domains) == 1 && domains[0] == "") {
@@ -66,7 +71,7 @@ func (t *translator) computeVirtualHost(proxy *v1.Proxy, virtualHost *v1.Virtual
 		Domains:    domains,
 		Routes:     envoyRoutes,
 		RequireTls: envoyRequireTls,
-		//TODO (ilackarms):
+		//TODO (ilackarms): plugins for these
 		//VirtualClusters: nil,
 		//RateLimits: nil,
 		//RequestHeadersToAdd: nil,
@@ -74,6 +79,85 @@ func (t *translator) computeVirtualHost(proxy *v1.Proxy, virtualHost *v1.Virtual
 		//Cors: nil,
 		//Auth: nil,
 	}
+}
+
+func (t *translator) envoyRoute(snap *v1.Snapshot, report reportErr, in *v1.Route) envoyroute.Route {
+	out := &envoyroute.Route{}
+
+	setMatch(in, out)
+
+	setAction(snap, report, in, out)
+
+	// run the plugins
+	params := plugins.PluginParams{
+		Snapshot: snap,
+	}
+	for _, plug := range t.plugins {
+		routePlugin, ok := plug.(plugins.RoutePlugin)
+		if !ok {
+			continue
+		}
+		if err := routePlugin.ProcessRoute(params, in, out); err != nil {
+			report(err, "invalid route")
+		}
+	}
+	return *out
+}
+
+func setMatch(in *v1.Route, out *envoyroute.Route) {
+	match := envoyroute.RouteMatch{
+		Headers:         envoyHeaderMatcher(in.Matcher.Headers),
+		QueryParameters: envoyQueryMatcher(in.Matcher.QueryParameters),
+	}
+	// need to do this because Go's proto implementation makes oneofs private
+	// which genius thought of that?
+	setEnvoyPathMatcher(in.Matcher, &match)
+
+	out.Match = match
+}
+
+func setAction(snap *v1.Snapshot, report reportErr, in *v1.Route, out *envoyroute.Route) {
+	switch action := in.Action.(type) {
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//
+	// TODO(ilackarms): rethink this block of code
+	//
+	case *v1.Route_RouteAction:
+		if err := validateRouteDestinations(snap.UpstreamList, action.RouteAction); err != nil {
+			report(err, "invalid route")
+		}
+	case *v1.Route_DirectResponseAction:
+		out.Action = &envoyroute.Route_DirectResponse{
+			DirectResponse: &envoyroute.DirectResponseAction{
+				Status: action.DirectResponseAction.Status,
+				Body:   DataSourceFromString(action.DirectResponseAction.Body),
+			},
+		}
+	case *v1.Route_RedirectAction:
+		out.Action = &envoyroute.Route_Redirect{
+			Redirect: &envoyroute.RedirectAction{
+				HostRedirect:  action.RedirectAction.HostRedirect,
+				ResponseCode:  envoyroute.RedirectAction_RedirectResponseCode(action.RedirectAction.ResponseCode),
+				HttpsRedirect: action.RedirectAction.HttpsRedirect,
+				StripQuery:    action.RedirectAction.StripQuery,
+			},
+		}
+
+		switch pathRewrite := action.RedirectAction.PathRewriteSpecifier.(type) {
+		case *v1.RedirectAction_PathRedirect:
+			out.Action.(*envoyroute.Route_Redirect).Redirect.PathRewriteSpecifier = &envoyroute.RedirectAction_PathRedirect{
+				PathRedirect: pathRewrite.PathRedirect,
+			}
+		case *v1.RedirectAction_PrefixRewrite:
+			out.Action.(*envoyroute.Route_Redirect).Redirect.PathRewriteSpecifier = &envoyroute.RedirectAction_PrefixRewrite{
+				PrefixRewrite: pathRewrite.PrefixRewrite,
+			}
+		}
+	}
+	//
+	//
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 }
 
 func setEnvoyPathMatcher(in *v1.RouteMatcher, out *envoyroute.RouteMatch) {
@@ -119,42 +203,6 @@ func envoyQueryMatcher(in []*v1.QueryParameterMatcher) []*envoyroute.QueryParame
 			},
 		}
 		out = append(out, envoyMatch)
-	}
-	return out
-}
-
-func (t *translator) envoyRoute(proxy *v1.Proxy, in *v1.Route, snap *v1.Snapshot, resourceErrs reporter.ResourceErrors) envoyroute.Route {
-	params := plugins.PluginParams{
-		Snapshot: snap,
-	}
-	match := &envoyroute.RouteMatch{
-		Headers:         envoyHeaderMatcher(in.Matcher.Headers),
-		QueryParameters: envoyQueryMatcher(in.Matcher.QueryParameters),
-	}
-	// need to do this because Go's proto implementation makes oneofs private
-	// which genius thought of that?
-	setEnvoyPathMatcher(in.Matcher, match)
-
-	out := envoyroute.Route{
-		Match: *match,
-	}
-	switch action := in.Action.(type) {
-	case *v1.Route_RouteAction:
-		if err := validateRouteDestinations(snap.UpstreamList, action.RouteAction); err != nil {
-			resourceErrs.AddError(proxy, errors.Wrapf(err, "invalid route"))
-		}
-		for _, plug := range t.plugins {
-			routePlugin, ok := plug.(plugins.RoutePlugin)
-			if !ok {
-				continue
-			}
-			if err := routePlugin.ProcessRoute(params, in, &out); err != nil {
-				resourceErrs.AddError(proxy, errors.Wrapf(err, "invalid route"))
-			}
-		}
-		TODO(ilackarms):dome
-	case *v1.Route_DirectResponseAction:
-	case *v1.Route_RedirectAction:
 	}
 	return out
 }
@@ -222,7 +270,7 @@ func validateListenerSslConfig(listener *v1.Listener, secrets []*v1.Secret) erro
 	for _, ssl := range listener.SslConfiguations {
 		switch secret := ssl.SslSecrets.(type) {
 		case *v1.SSLConfig_SecretRef:
-			if _, _, _, err := getSslSecrets(secret.SecretRef, secrets); err != nil {
+			if _, _, _, err := GetSslSecrets(secret.SecretRef, secrets); err != nil {
 				return err
 			}
 		case *v1.SSLConfig_SslFiles:
@@ -232,12 +280,13 @@ func validateListenerSslConfig(listener *v1.Listener, secrets []*v1.Secret) erro
 	return nil
 }
 
+//TODO(ilackarms): move to a common utils file
 const (
 	deprecatedSslCertificateChainKey = "ca_chain"
 	deprecatedSslPrivateKeyKey       = "private_key"
 )
 
-func getSslSecrets(ref string, secrets []*v1.Secret) (string, string, string, error) {
+func GetSslSecrets(ref string, secrets []*v1.Secret) (string, string, string, error) {
 	var sslSecret *v1.Secret
 	for _, sec := range secrets {
 		if sec.Metadata.Name == ref {
@@ -254,7 +303,8 @@ func getSslSecrets(ref string, secrets []*v1.Secret) (string, string, string, er
 	if !ok {
 		certChain, ok = sslSecret.Data[deprecatedSslCertificateChainKey]
 		if !ok {
-			return "", "", "", errors.Errorf("neither %v nor %v key not found in ssl secrets", defaultv1.SslCertificateChainKey, deprecatedSslCertificateChainKey)
+			return "", "", "", errors.Errorf("neither %v nor %v key not found in ssl secrets",
+				defaultv1.SslCertificateChainKey, deprecatedSslCertificateChainKey)
 		}
 	}
 
@@ -262,10 +312,19 @@ func getSslSecrets(ref string, secrets []*v1.Secret) (string, string, string, er
 	if !ok {
 		privateKey, ok = sslSecret.Data[deprecatedSslPrivateKeyKey]
 		if !ok {
-			return "", "", "", errors.Errorf("neither %v nor %v key not found in ssl secrets", defaultv1.SslPrivateKeyKey, deprecatedSslPrivateKeyKey)
+			return "", "", "", errors.Errorf("neither %v nor %v key not found in ssl secrets",
+				defaultv1.SslPrivateKeyKey, deprecatedSslPrivateKeyKey)
 		}
 	}
 
 	rootCa := sslSecret.Data[defaultv1.SslRootCaKey]
 	return certChain, privateKey, rootCa, nil
+}
+
+func DataSourceFromString(str string) *envoycore.DataSource {
+	return &envoycore.DataSource{
+		Specifier: &envoycore.DataSource_InlineString{
+			InlineString: str,
+		},
+	}
 }
