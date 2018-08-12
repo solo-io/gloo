5e2d7d43dfbc4bd8e522ae7d5d3dbc516ff927f3
Scott Weiss <sdw35@cornell.edu>
2018-08-12 13:18:10 -0400

    generate aws protos too
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- projects/gloo/api/v1/generate.sh
+++ projects/gloo/api/v1/generate.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
+GOGO_OUT_FLAG="--gogo_out=Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types:${GOPATH}/src/"
+SOLO_KIT_FLAG="--plugin=protoc-gen-solo-kit=${GOPATH}/bin/protoc-gen-solo-kit --solo-kit_out"
+
 OUT=../../pkg/api/v1/
 mkdir -p ${OUT}
 protoc -I=./ \
     -I=${GOPATH}/src/github.com/gogo/protobuf/ \
-    -I=${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-    -I=${GOPATH}/src/github.com/gogo/protobuf/protobuf/ \
     -I=${GOPATH}/src/github.com/solo-io/solo-kit/projects/gloo/ \
     -I=${GOPATH}/src \
-    --gogo_out=Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types:${GOPATH}/src/ \
-    --plugin=protoc-gen-solo-kit=${GOPATH}/bin/protoc-gen-solo-kit --solo-kit_out=${OUT} \
+    ${GOGO_OUT_FLAG} \
+    ${SOLO_KIT_FLAG}=${OUT} \
+    *.proto
+
+# AWS Plugin
+cd ././../../pkg/plugins/aws/ && \
+protoc -I=./ \
+    -I=${GOPATH}/src/github.com/gogo/protobuf/ \
+    ${GOGO_OUT_FLAG} \
+    ${SOLO_KIT_FLAG}=. \
     *.proto
