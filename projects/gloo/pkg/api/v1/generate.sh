ff6bab43cd94c282de46da99d4f8af45962bf9cf
Scott Weiss <sdw35@cornell.edu>
2018-08-14 14:00:58 -0400

    move api stuff into pkg
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- projects/gloo/pkg/api/v1/generate.sh
+++ projects/gloo/pkg/api/v1/generate.sh
@@ -3,7 +3,9 @@
 GOGO_OUT_FLAG="--gogo_out=Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types:${GOPATH}/src/"
 SOLO_KIT_FLAG="--plugin=protoc-gen-solo-kit=${GOPATH}/bin/protoc-gen-solo-kit --solo-kit_out"
 
-OUT=../../pkg/api/v1/
+ROOT=${GOPATH}/src/github.com/solo-io/solo-kit/projects/gloo
+
+OUT=${ROOT}/pkg/api/v1/
 mkdir -p ${OUT}
 protoc -I=./ \
     -I=${GOPATH}/src/github.com/gogo/protobuf/ \
@@ -13,23 +15,23 @@ protoc -I=./ \
     ${SOLO_KIT_FLAG}=${OUT} \
     *.proto
 
+# Core Plugin protos
+(cd ${ROOT}/pkg/plugins/core/ && \
+protoc -I=./ \
+    -I=${GOPATH}/src/github.com/gogo/protobuf/ \
+    ${GOGO_OUT_FLAG} \
+    *.proto)
+
 # AWS Plugin
-(cd ././../../pkg/plugins/aws/ && \
+(cd ${ROOT}/pkg/plugins/aws/ && \
 protoc -I=./ \
     -I=${GOPATH}/src/github.com/gogo/protobuf/ \
     ${GOGO_OUT_FLAG} \
     ${SOLO_KIT_FLAG}=.   \
     *.proto)
 
-# Core Plugin
-(cd ././../../pkg/plugins/core/ && \
-protoc -I=./ \
-    -I=${GOPATH}/src/github.com/gogo/protobuf/ \
-    ${GOGO_OUT_FLAG} \
-    *.proto)
-
 # Kubernetes Plugin
-(cd ././../../pkg/plugins/kubernetes/ && \
+(cd ${ROOT}/pkg/plugins/kubernetes/ && \
 protoc -I=./ \
     -I=${GOPATH}/src/github.com/gogo/protobuf/ \
     -I=${GOPATH}/src/github.com/solo-io/solo-kit/projects/gloo/ \
