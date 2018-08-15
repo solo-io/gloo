95566d2fc06b1aa124fcf4d845a962d21e936528
Scott Weiss <sdw35@cornell.edu>
2018-08-15 11:58:31 -0400

    upstream reconciler
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- projects/gloo/pkg/reconciler/upstream_reconciler.go
+++ projects/gloo/pkg/reconciler/upstream_reconciler.go
@@ -1,13 +1,9 @@
 package reconciler
 
 import (
-	"context"
-
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	"github.com/solo-io/solo-kit/pkg/api/v1/reconcile"
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources"
-	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
-	"github.com/solo-io/solo-kit/pkg/errors"
 	"github.com/solo-io/solo-kit/pkg/utils/contextutils"
 	"github.com/solo-io/solo-kit/projects/gloo/pkg/api/v1"
 )
@@ -19,7 +15,7 @@ type UpstreamReconciler interface {
 	Reconcile(namespace string, desiredResources []*v1.Upstream, opts clients.ListOpts) error
 }
 
-func upstreamsToResources(list ...*v1.Upstream) []resources.Resource {
+func upstreamsToResources(list v1.UpstreamList) []resources.Resource {
 	var resourceList []resources.Resource
 	for _, upstream := range list {
 		resourceList = append(resourceList, upstream)
@@ -27,89 +23,24 @@ func upstreamsToResources(list ...*v1.Upstream) []resources.Resource {
 	return resourceList
 }
 
-func resourcesToUpstreams(list ...resources.Resource) []*v1.Upstream {
-	var upstreamList []*v1.Upstream
-	for _, resource := range list {
-		upstreamList = append(upstreamList, resource.(*v1.Upstream))
+func NewUpstreamReconciler(client v1.UpstreamClient, transition TransitionUpstreamsFunc) UpstreamReconciler {
+	var transitionResources reconcile.TransitionResourcesFunc
+	if transition != nil {
+		transitionResources = func(original, desired resources.Resource) {
+			transition(original.(*v1.Upstream), desired.(*v1.Upstream))
+		}
 	}
-	return upstreamList
-}
-
-func NewUpstreamReconciler(client v1.UpstreamClient) {
-	edsReconciler := reconcile.NewReconciler(endpointsResourceClient)
-	edsReconciler.Reconcile()
-}
-
-type ListResources func() ([]*v1.Upstream, error)
-
-type Reconciler interface {
-	Reconcile(namespace string, opts clients.ListOpts, kind string, desiredResources []*v1.Upstream) error
-}
-
-type reconciler struct {
-	resourceClient v1.UpstreamClient
-}
-
-func NewReconciler(resourceClient v1.UpstreamClient) Reconciler {
-	return &reconciler{
-		resourceClient: resourceClient,
+	return &upstreamReconciler{
+		base: reconcile.NewReconciler(client.BaseClient(), transitionResources),
 	}
 }
 
-func (r *reconciler) Reconcile(namespace string, opts clients.ListOpts, kind string, desiredResources []*v1.Upstream) error {
+type upstreamReconciler struct {
+	base reconcile.Reconciler
+}
+
+func (r *upstreamReconciler) Reconcile(namespace string, desiredResources []*v1.Upstream, opts clients.ListOpts) error {
 	opts = opts.WithDefaults()
-	opts.Ctx = contextutils.WithLogger(opts.Ctx, "reconciler")
-	originalResources, err := r.resourceClient.List(namespace, opts)
-	if err != nil {
-		return err
-	}
-	for _, desired := range desiredResources {
-		if err := r.syncResource(opts.Ctx, desired, originalResources); err != nil {
-			return errors.Wrapf(err, "reconciling resource %v", desired.GetMetadata().Name)
-		}
-	}
-	// delete unused
-	for _, original := range originalResources {
-		unused := findResource(original.GetMetadata().Namespace, original.GetMetadata().Name, desiredResources) == nil
-		if unused {
-			if err := r.deleteStaleResource(opts.Ctx, original); err != nil {
-				return errors.Wrapf(err, "deleting stale resource %v", original.GetMetadata().Name)
-			}
-		}
-	}
-
-	return nil
-}
-
-func (r *reconciler) syncResource(ctx context.Context, desired *v1.Upstream, originalResources []*v1.Upstream) error {
-	var overwriteExisting bool
-	original := findResource(desired.GetMetadata().Namespace, desired.GetMetadata().Name, originalResources)
-	if original != nil {
-		// if this is an update,
-		// update resource version
-		// set status to 0, needs to be re-processed
-		overwriteExisting = true
-		resources.UpdateMetadata(desired, func(meta *core.Metadata) {
-			meta.ResourceVersion = original.GetMetadata().ResourceVersion
-		})
-		desired.SetStatus(core.Status{})
-	}
-	_, err := r.resourceClient.Write(desired, clients.WriteOpts{Ctx: ctx, OverwriteExisting: overwriteExisting})
-	return err
-}
-
-func (r *reconciler) deleteStaleResource(ctx context.Context, original *v1.Upstream) error {
-	return r.resourceClient.Delete(original.GetMetadata().Namespace, original.GetMetadata().Name, clients.DeleteOpts{
-		Ctx:            ctx,
-		IgnoreNotExist: true,
-	})
-}
-
-func findResource(namespace, name string, rss []*v1.Upstream) *v1.Upstream {
-	for _, resource := range rss {
-		if resource.GetMetadata().Namespace == namespace && resource.GetMetadata().Name == name {
-			return resource
-		}
-	}
-	return nil
+	opts.Ctx = contextutils.WithLogger(opts.Ctx, "upstream_reconciler")
+	return r.base.Reconcile(namespace, upstreamsToResources(desiredResources), opts)
 }
