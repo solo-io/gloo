a4c89f68dc97f9ed02f27e7260a2e014d4260f3e
Scott Weiss <sdw35@cornell.edu>
2018-08-28 10:53:55 -0400

    wIP: xds server
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

---
+++ projects/gloo/pkg/xds/envoy.go
@@ -0,0 +1,41 @@
+package xds
+
+import (
+	"context"
+	"strings"
+
+	"github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+	envoycache "github.com/envoyproxy/go-control-plane/pkg/cache"
+	"github.com/solo-io/solo-kit/pkg/utils/contextutils"
+)
+
+// TODO(ilackarms): pass a list of the known
+
+// used to let nodes know they have a bad config
+// we assign a "fix me" virtualhost for bad nodes
+const badNodeKey = "misconfigured-node"
+
+type hasher struct {
+	ctx context.Context
+}
+
+const errorString = `
+Envoy proxies are assigned configuration by Gloo based on their Node ID.
+Proxies must register to Gloo with their node ID in the format "NAMESPACE~NAME"
+Where NAMESPACE and NAME are the namespace and name of the correlating Proxy resource.`
+
+func (h hasher) ID(node *core.Node) string {
+	parts := strings.SplitN(node.Id, "~", 2)
+	if len(parts) != 2 {
+		contextutils.LoggerFrom(h.ctx).Warnf("invalid id provided by Envoy: %v", node.Id)
+		contextutils.LoggerFrom(h.ctx).Debugf(errorString)
+		return badNodeKey
+	}
+	contextutils.LoggerFrom(h.ctx).Debugf("node %v registered with role %v", parts[1], parts[0])
+	return parts[0]
+}
+
+func NewEnvoyXds(ctx context.Context) {
+	ctx = contextutils.WithLogger(ctx, "envoy-xds-server")
+	envoyCache := envoycache.NewSnapshotCache(true, hasher{}, contextutils.LoggerFrom(ctx))
+}
