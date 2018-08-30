20a1d0f2213df4a117df93573206282f222cf048
Scott Weiss <sdw35@cornell.edu>
2018-08-30 10:16:11 -0400

    kube plugin: add interface for get/set service spec
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

---
+++ projects/gloo/pkg/api/v1/extensions.go
@@ -0,0 +1,16 @@
+package v1
+
+import (
+	"github.com/solo-io/solo-kit/projects/gloo/pkg/api/v1/plugins"
+)
+
+/*
+	Add these two methods to any upstream spec that supports 
+*/
+func (us *UpstreamSpec_Kube) GetServiceSpec() *plugins.ServiceSpec {
+	return us.Kube.ServiceSpec
+}
+
+func (us *UpstreamSpec_Kube) SetServiceSpec(spec *plugins.ServiceSpec) {
+	us.Kube.ServiceSpec = spec
+}
