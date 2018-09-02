8fd1dd2633fe1e1d772a1281781304119c85fac8
Scott Weiss <sdw35@cornell.edu>
2018-09-02 16:18:24 -0400

    remove solo-kit from apiserver generate
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- pkg/api/v1/apiserver/generate.go
+++ pkg/api/v1/apiserver/generate.go
@@ -1,3 +1,3 @@
 package apiserver
 
-//go:generate protoc -I=./ -I=${GOPATH}/src/github.com/gogo/protobuf/ -I=${GOPATH}/src/github.com/gogo/protobuf/protobuf/ -I=${GOPATH}/src --gogo_out=plugins=grpc,import_path=apiserver,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types:${GOPATH}/src --plugin=protoc-gen-solo-kit=${GOPATH}/bin/protoc-gen-solo-kit --solo-kit_out=./project.json:. api_server.proto
+//go:generate protoc -I=./ -I=${GOPATH}/src/github.com/gogo/protobuf/ -I=${GOPATH}/src/github.com/gogo/protobuf/protobuf/ -I=${GOPATH}/src --gogo_out=plugins=grpc,import_path=apiserver,Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types:${GOPATH}/src --plugin=protoc-gen-solo-kit=${GOPATH}/bin/protoc-gen-solo-kit api_server.proto
