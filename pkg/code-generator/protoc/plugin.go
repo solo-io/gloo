5625b563a39dddeeec708f92ef677b8cb27e26b2
Scott Weiss <sdw35@cornell.edu>
2018-09-02 13:29:21 -0400

    remove unused code
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- pkg/code-generator/protoc/plugin.go
+++ pkg/code-generator/protoc/plugin.go
@@ -6,30 +6,12 @@ import (
 
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/protoc-gen-go/plugin"
-	"github.com/iancoleman/strcase"
 	"github.com/solo-io/solo-kit/pkg/code-generator/codegen"
 	"github.com/solo-io/solo-kit/pkg/utils/log"
 )
 
 // plugin is an implementation of protokit.Plugin
 type Plugin struct{}
-
-type resouceTemplateFunc func(params ResourceLevelTemplateParams) (string, error)
-type packageTemplateFunc func(params PackageLevelTemplateParams) (string, error)
-
-var resourceFilesToGenerate = map[string]resouceTemplateFunc{
-//"_client.go":      templates.GenerateResourceClientCode,
-//"_client_test.go": templates.GenerateResourceClientTestCode,
-//"_reconciler.go":  templates.GenerateReconcilerCode,
-}
-
-var packageFilesToGenerate = map[string]packageTemplateFunc{
-//"_suite_test.go":      templates.GenerateTestSuiteCode,
-//"_cache.go":           templates.GenerateCacheCode,
-//"_cache_test.go":      templates.GenerateCacheTestCode,
-//"_event_loop.go":      templates.GenerateEventLoopCode,
-//"_event_loop_test.go": templates.GenerateEventLoopTestCode,
-}
 
 func (p *Plugin) Generate(req *plugin_go.CodeGeneratorRequest) (*plugin_go.CodeGeneratorResponse, error) {
 	log.DefaultOut = &bytes.Buffer{}
@@ -59,31 +41,4 @@ func (p *Plugin) Generate(req *plugin_go.CodeGeneratorRequest) (*plugin_go.CodeG
 	}
 
 	return resp, nil
-}
-
-func generateResourceLevelFile(params ResourceLevelTemplateParams, suffix string, genFunc resouceTemplateFunc) (*plugin_go.CodeGeneratorResponse_File, error) {
-	fileName := strcase.ToSnake(params.ResourceType) + suffix
-	content, err := genFunc(params)
-	if err != nil {
-		return nil, err
-	}
-	return &plugin_go.CodeGeneratorResponse_File{
-		Name:    proto.String(fileName),
-		Content: proto.String(content),
-	}, nil
-}
-
-func generatePackageLevelFile(params PackageLevelTemplateParams, suffix string, genFunc packageTemplateFunc) (*plugin_go.CodeGeneratorResponse_File, error) {
-	fileName := params.PackageName + suffix
-	content, err := genFunc(params)
-	if err != nil {
-		return nil, err
-	}
-	if err != nil {
-		return nil, err
-	}
-	return &plugin_go.CodeGeneratorResponse_File{
-		Name:    proto.String(fileName),
-		Content: proto.String(content),
-	}, nil
 }
