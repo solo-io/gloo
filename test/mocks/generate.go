4dd740ee2fa0619875c3374391843bf986588cd5
Scott Weiss <sdw35@cornell.edu>
2018-09-02 14:48:02 -0400

    use absolute path in protoc plugin command
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- test/mocks/generate.go
+++ test/mocks/generate.go
@@ -1,3 +1,3 @@
 package mocks
 
-//go:generate protoc -I=./ -I=${GOPATH}/src/github.com/gogo/protobuf/  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I=${GOPATH}/src/github.com/gogo/protobuf/protobuf/ -I=${GOPATH}/src --gogo_out=plugins=grpc:${GOPATH}/src --plugin=protoc-gen-solo-kit=${GOPATH}/bin/protoc-gen-solo-kit --solo-kit_out=./project.json:. mock_resources.proto
+//go:generate protoc -I=./ -I=${GOPATH}/src/github.com/gogo/protobuf/  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I=${GOPATH}/src/github.com/gogo/protobuf/protobuf/ -I=${GOPATH}/src --gogo_out=plugins=grpc:${GOPATH}/src --plugin=protoc-gen-solo-kit=${GOPATH}/bin/protoc-gen-solo-kit --solo-kit_out=${PWD}/project.json:. mock_resources.proto
