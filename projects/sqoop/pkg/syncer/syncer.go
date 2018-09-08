9a04b7373c249d5c466b6068842b9fc25442003e
Scott Weiss <sdw35@cornell.edu>
2018-09-08 19:51:58 -0400

    structure for syncer
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- projects/sqoop/pkg/syncer/syncer.go
+++ projects/sqoop/pkg/syncer/syncer.go
@@ -3,14 +3,50 @@ package syncer
 import (
 	"github.com/solo-io/solo-kit/projects/sqoop/pkg/api/v1"
 	"context"
-	"github.com/solo-io/qloo/pkg/graphql"
-	"github.com/solo-io/qloo/pkg/resolvers"
-	"github.com/solo-io/qloo/pkg/exec"
+	"github.com/solo-io/solo-kit/pkg/utils/contextutils"
+	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
+	"github.com/solo-io/solo-kit/pkg/errors"
+	"github.com/solo-io/solo-kit/projects/sqoop/pkg/translator"
+	gloov1 "github.com/solo-io/solo-kit/projects/gloo/pkg/api/v1"
+	"github.com/solo-io/solo-kit/pkg/api/v1/reporter"
 )
 
-type Syncer struct {}
+type Syncer struct {
+	writeNamespace  string
+	reporter        reporter.Reporter
+	writeErrs       chan error
+	proxyReconciler gloov1.ProxyReconciler
+}
 
 func (s *Syncer) Sync(ctx context.Context, snap *v1.ApiSnapshot) error {
+	ctx = contextutils.WithLogger(ctx, "syncer")
+
+	logger := contextutils.LoggerFrom(ctx)
+	logger.Infof("begin sync %v (%v schemas %v resolverMaps)",
+		snap.Hash(),
+		len(snap.Schemas),
+		len(snap.ResolverMaps),
+	)
+	defer logger.Infof("end sync %v", snap.Hash())
+	logger.Debugf("%v", snap)
+
+	proxy, resourceErrs := translator.Translate(s.writeNamespace, snap)
+	if err := s.reporter.WriteReports(ctx, resourceErrs); err != nil {
+		return errors.Wrapf(err, "writing reports")
+	}
+	if err := resourceErrs.Validate(); err != nil {
+		logger.Warnf("gateway %v was rejected due to invalid config: %v\nxDS cache will not be updated.", err)
+		return nil
+	}
+	// proxy was deleted / none desired
+	if proxy == nil {
+		return s.proxyReconciler.Reconcile(s.writeNamespace, nil, nil, clients.ListOpts{})
+	}
+	logger.Debugf("creating proxy %v", proxy.Metadata.Ref())
+	if err := s.proxyReconciler.Reconcile(s.writeNamespace, gloov1.ProxyList{proxy}, nil, clients.ListOpts{}); err != nil {
+		return err
+	}
+
 	/*
 	1. create new graphql endpoints
 	2. update router with graphql endpoints
@@ -18,35 +54,27 @@ func (s *Syncer) Sync(ctx context.Context, snap *v1.ApiSnapshot) error {
 	4. configure gloo
 	# tip: use multierror instead of early returns (when possible)
 	 */
-	 for _, schema := range snap.Schemas.List() {
+	var endpoints []*router.Endpoint
+	for _, schema := range snap.Schemas.List() {
+		endpoint, err := createEndpointForSchema()
+		if err != nil {
+			// multierror / report
+		}
+		endpoints = append(endpoints, endpoint)
+	}
+	s.router.SetEndpoints(endpoints)
 
-	 }
+	// start propagating for new set of resources
+	// TODO(ilackarms): reinstate propagator
+	return nil // s.propagator.PropagateStatuses(snap, proxy, clients.WatchOpts{Ctx: ctx})
+
 }
+
+func desiredResources() (*gloov1.Proxy, []*v1.ResolverMap, error) {}
 
 // for supporting multiple schemas on the same port, essentially
-func createRouterEndpoints() {}
-func createEndpointForSchema() {}
-func generateSkeletonResolvermap() {}
-func generateEmptyResolvermap() {}
-func configureGloo() {}
-
-func (el *EventLoop) createGraphqlEndpoint(schema *v1.Schema, resolverMap *v1.ResolverMap) (*graphql.Endpoint, error, error) {
-	resolverFactory := resolvers.NewResolverFactory(el.proxyAddr, resolverMap)
-	parsedSchema, err := parseSchemaString(schema)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to parse schema"), nil
-	}
-	executableResolvers, err := exec.NewExecutableResolvers(parsedSchema, resolverFactory.CreateResolver)
-	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed to generate resolvers from map")
-	}
-	el.operator.ApplyResolvers(resolverMap)
-	executableSchema := exec.NewExecutableSchema(parsedSchema, executableResolvers)
-	return &graphql.Endpoint{
-		SchemaName: schema.Name,
-		RootPath:   "/" + schema.Name,
-		QueryPath:  "/" + schema.Name + "/query",
-		ExecSchema: executableSchema,
-	}, nil, nil
-}
-
+func createRouterEndpoints()                     {}
+func createEndpointForSchema()                   {}
+func generateSkeletonResolvermap()               {}
+func generateEmptyResolvermap(schema *v1.Schema) {}
+func configureGloo()                             {}
