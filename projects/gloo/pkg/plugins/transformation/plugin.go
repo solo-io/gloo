9abee431df171ef23b98f049abcd675b4ffd4383
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-09-08 10:14:13 -0400

    add transformation rotue plugin

--- projects/gloo/pkg/plugins/transformation/plugin.go
+++ projects/gloo/pkg/plugins/transformation/plugin.go
@@ -1,7 +1,10 @@
 package transformation
 
 import (
+	envoyroute "github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
 	envoyhttp "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/http_connection_manager/v2"
+	"github.com/envoyproxy/go-control-plane/pkg/util"
+	"github.com/gogo/protobuf/types"
 	"github.com/solo-io/solo-kit/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/solo-kit/projects/gloo/pkg/plugins"
 )
@@ -21,6 +24,28 @@ func NewPlugin() *Plugin {
 
 func (p *Plugin) Init(params plugins.InitParams) error {
 	p.RequireTransformationFilter = false
+	return nil
+}
+
+// TODO(yuval-k): We need to figure out what\if to do in edge cases where there is cluster weight transform
+func (p *Plugin) ProcessRoute(params plugins.Params, in *v1.Route, out *envoyroute.Route) error {
+	if in.RoutePlugins == nil {
+		return nil
+	}
+	if in.RoutePlugins.Transformations == nil {
+		return nil
+	}
+	if out.PerFilterConfig == nil {
+		out.PerFilterConfig = make(map[string]*types.Struct)
+	}
+
+	configStruct, err := util.MessageToStruct(in.RoutePlugins.Transformations)
+	if err != nil {
+		return err
+	}
+
+	p.RequireTransformationFilter = true
+	out.PerFilterConfig[FilterName] = configStruct
 	return nil
 }
 
