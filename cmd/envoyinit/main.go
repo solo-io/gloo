0825f83e06d78e4003b3e2cc6675ca7a52706574
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-09-09 08:14:58 -0400

    add envoy init

---
+++ cmd/envoyinit/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+	"syscall"
+
+	"github.com/solo-io/envoy-operator/pkg/downward"
+)
+
+func main() {
+	inputfile := "/config/envoy.yaml"
+	outfile := "/tmp/envoy.yaml"
+
+	transformer := downward.NewTransformer()
+	err := transformer.TransformFiles(inputfile, outfile)
+	if err != nil {
+		log.Fatalf("initializer failed: %v", err)
+	}
+	env := os.Environ()
+	args := []string{
+		"/usr/local/bin/envoy", "-c", outfile, "--v2-config-only",
+	}
+	if err := syscall.Exec(args[0], args, env); err != nil {
+		panic(err)
+	}
+
+}
