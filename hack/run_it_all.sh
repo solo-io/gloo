f61d5e24f5672e6c955a9549ff4209a241382e38
Scott Weiss <sdw35@cornell.edu>
2018-09-13 15:18:06 -0400

    play with hack script
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- hack/run_it_all.sh
+++ hack/run_it_all.sh
@@ -5,13 +5,22 @@ set -x -e
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 
 # minikube should be running
-go run projects/sqoop/cmd/main.go &
+echo RUNNING SQOOP+GLOO+GATEWAY+DISCOVERY
+#go run projects/sqoop/cmd/main.go &
+
+echo RUNNING APISERVER
 go run projects/apiserver/cmd/main.go &
+
+echo RUNNING UI
 docker run -i --rm --net=host soloio/gloo-i:dev &
+
+echo RUNNING Petstore example
 docker run -i -p 9091:8080 --rm soloio/petstore-example:latest &
 
+echo "Don't forget to add a static upstream for 127.0.0.1:9091"
+
 ## requires latest envoy in solo-kit/..
-sudo ../envoy -c hack/envoy.yaml --disable-hot-restart &
+../envoy -c hack/envoy.yaml --disable-hot-restart &
 
 trap 'sudo kill $(jobs -p)' EXIT
 
@@ -20,7 +29,5 @@ do
 echo ${job}
     wait ${job} || let "FAIL+=1"
 done
-
-sudo killall envoy
 
 echo ${FAIL} failed
