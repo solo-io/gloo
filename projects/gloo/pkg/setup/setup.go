5f249c69b4d4fbaec4b53e2c12bb8abcc6381979
Scott Weiss <sdw35@cornell.edu>
2018-09-20 17:21:38 -0400

    merge two files in gloo

--- projects/gloo/pkg/setup/setup.go
+++ projects/gloo/pkg/setup/setup.go
@@ -3,7 +3,6 @@ package setup
 import (
 	"context"
 	"time"
-
 	"github.com/gogo/protobuf/types"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients/factory"
@@ -11,8 +10,20 @@ import (
 	"github.com/solo-io/solo-kit/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/solo-kit/projects/gloo/pkg/syncer"
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
+	"github.com/solo-io/solo-kit/projects/gloo/pkg/bootstrap"
+	"github.com/solo-io/solo-kit/pkg/utils/kubeutils"
+	"k8s.io/client-go/kubernetes"
+	"google.golang.org/grpc"
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	"go.uber.org/zap"
+	"github.com/solo-io/solo-kit/projects/gloo/pkg/defaults"
+	"github.com/solo-io/solo-kit/pkg/namespacing/static"
+	"net"
 )
 
+// TODO(ilackarms): remove this or move it to a test package, only use settings watch for prodution gloo
 func writeSettings(settingsDir string) error {
 	cli, err := v1.NewSettingsClient(&factory.FileResourceClientFactory{RootDir: settingsDir})
 	if err != nil {
@@ -65,4 +76,56 @@ func Main(settingsDir string) error {
 		contextutils.LoggerFrom(ctx).Errorf("error in setup: %v", err)
 	}
 	return nil
+}
+
+func DefaultKubernetesConstructOpts() (bootstrap.Opts, error) {
+	cfg, err := kubeutils.GetConfig("", "")
+	if err != nil {
+		return bootstrap.Opts{}, err
+	}
+	clientset, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		return bootstrap.Opts{}, err
+	}
+	ctx := contextutils.WithLogger(context.Background(), "gloo")
+	logger := contextutils.LoggerFrom(ctx)
+	grpcServer := grpc.NewServer(grpc.StreamInterceptor(
+		grpc_middleware.ChainStreamServer(
+			grpc_ctxtags.StreamServerInterceptor(),
+			grpc_zap.StreamServerInterceptor(zap.NewNop()),
+			func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+				logger.Infof("gRPC call: %v", info.FullMethod)
+				return handler(srv, ss)
+			},
+		)),
+	)
+	return bootstrap.Opts{
+		WriteNamespace: defaults.GlooSystem,
+		Upstreams: &factory.KubeResourceClientFactory{
+			Crd: v1.UpstreamCrd,
+			Cfg: cfg,
+		},
+		Proxies: &factory.KubeResourceClientFactory{
+			Crd: v1.ProxyCrd,
+			Cfg: cfg,
+		},
+		Secrets: &factory.KubeSecretClientFactory{
+			Clientset: clientset,
+		},
+		Artifacts: &factory.KubeConfigMapClientFactory{
+			Clientset: clientset,
+		},
+		Namespacer: static.NewNamespacer([]string{"default", defaults.GlooSystem}),
+		WatchOpts: clients.WatchOpts{
+			Ctx:         ctx,
+			RefreshRate: defaults.RefreshRate,
+		},
+		BindAddr: &net.TCPAddr{
+			IP:   net.ParseIP("0.0.0.0"),
+			Port: 8080,
+		},
+		GrpcServer: grpcServer,
+		KubeClient: clientset,
+		DevMode:    true,
+	}, nil
 }
