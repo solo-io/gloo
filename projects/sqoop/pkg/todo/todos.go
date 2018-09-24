e7afcda1e57e8222c3ff570f70a36ccdc1501649
Scott Weiss <sdw35@cornell.edu>
2018-09-24 17:32:44 -0400

    lol
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- projects/sqoop/pkg/todo/todos.go
+++ projects/sqoop/pkg/todo/todos.go
@@ -2,14 +2,11 @@ package TODO
 
 import (
 	"github.com/solo-io/solo-kit/projects/gloo/pkg/api/v1"
-	"github.com/solo-io/solo-kit/projects/gloo/pkg/defaults"
 )
 
 const SqoopSidecarBindAddr = "127.0.0.1"
 const SqoopSidecarBindPort = 9090
 const SqoopServerBindPort = 9095
-const SqoopSidecarName = "sqoop-sidecar"
-const SqoopNamespace = defaults.GlooSystem
 
 // TODO(ilackarms): make this a global function
 func TransitionFunction(original, desired *v1.Proxy) (bool, error) {
