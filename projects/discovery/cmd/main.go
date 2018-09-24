6cd1e2fa4fe22d36d26d13ab9f15454f7a29b4e9
Scott Weiss <sdw35@cornell.edu>
2018-09-24 13:41:37 -0400

    create a main for discovery
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- projects/discovery/cmd/main.go
+++ projects/discovery/cmd/main.go
@@ -1,3 +1,50 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"os"
+	"path/filepath"
+
+	"github.com/solo-io/solo-kit/pkg/utils/log"
+	"github.com/solo-io/solo-kit/pkg/utils/stats"
+	fdssetup "github.com/solo-io/solo-kit/projects/discovery/pkg/fds/setup"
+	uds "github.com/solo-io/solo-kit/projects/discovery/pkg/uds/setup"
+	gloosetup "github.com/solo-io/solo-kit/projects/gloo/pkg/setup"
+)
+
+func main() {
+	stats.StartStatsServer()
+	if err := run(); err != nil {
+		log.Fatalf("err in main: %v", err.Error())
+	}
+}
+
+func run() error {
+	dir := flag.String("dir", "gloo", "directory for config")
+	flag.Parse()
+	os.MkdirAll(filepath.Join(*dir, "settings"), 0755)
+	errs := make(chan error)
+	go func() {
+		errs <- runUds()
+	}()
+	go func() {
+		errs <- runFds()
+	}()
+	return <-errs
+}
+
+func runUds() error {
+	opts, err := gloosetup.DefaultKubernetesConstructOpts()
+	if err != nil {
+		return err
+	}
+	return uds.Setup(opts)
+}
+
+func runFds() error {
+	opts, err := gloosetup.DefaultKubernetesConstructOpts()
+	if err != nil {
+		return err
+	}
+	return fdssetup.Setup(opts)
+}
