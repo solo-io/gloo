7ab9065f475177c49b5c13c657d684e3b2f37651
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-09-25 14:54:14 -0400

    added index page

--- pkg/utils/stats/stats.go
+++ pkg/utils/stats/stats.go
@@ -1,6 +1,9 @@
 package stats
 
 import (
+	"sort"
+	"text/template"
+
 	"net/http"
 	"net/http/pprof"
 
@@ -37,6 +40,65 @@ func StartStatsServer() {
 		}
 
 		zpages.Handle(mux, "/zpages")
+
+		mux.HandleFunc("/", Index)
+
 		http.ListenAndServe("localhost:9091", mux)
 	}()
 }
+
+func Index(w http.ResponseWriter, r *http.Request) {
+
+	type profile struct {
+		Name string
+		Href string
+		Desc string
+	}
+	var profiles []profile
+
+	// Adding other profiles exposed from within this package
+	for p, pd := range profileDescriptions {
+		profiles = append(profiles, profile{
+			Name: p,
+			Href: p,
+			Desc: pd,
+		})
+	}
+
+	sort.Slice(profiles, func(i, j int) bool {
+		return profiles[i].Name < profiles[j].Name
+	})
+
+	indexTmpl.Execute(w, profiles)
+}
+
+var profileDescriptions = map[string]string{
+	"/debug/pprof/": `PProf related things:<br/>
+	<a href="/debug/pprof/goroutine?debug=2">full goroutine stack dump</a>
+	`,
+	"/zpages":  `Tracing. See <a href="/zpages/tracez">list of spans</a>`,
+	"/logging": "View \\ change the log level of the program",
+	"/metrics": "Prometheus format metrics",
+}
+
+var indexTmpl = template.Must(template.New("index").Parse(`<!DOCTYPE html><html>
+<head>
+<title>/debug/pprof/</title>
+<style>
+.profile-name{
+	display:inline-block;
+	width:6rem;
+}
+</style>
+</head>
+<body>
+Things to do:
+{{range .}}
+<h2><a href={{.Href}}>{{.Name}}</a></h2>
+<p>
+{{.Desc}}
+</p>
+{{end}}
+</body>
+</html>
+`))
