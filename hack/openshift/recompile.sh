67f684c9a6fe5593a83e49e80f75f42e68772c62
Scott Weiss <sdw35@cornell.edu>
2018-09-27 15:34:38 -0400

    fix local scripts

---
+++ hack/openshift/recompile.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -ex
+
+PROJECT=$1
+export VERSION=$2
+if [ "$#" -ne 2 ]; then
+    echo "invalid, to run: ./hack/recompile.sh PROJECT VERSION"
+    exit 1
+fi
+
+# set up user, only needed once
+oc login -u system:admin
+oc adm policy add-cluster-role-to-user cluster-admin gloo
+oc login -u gloo -p gloo
+
+eval $(minishift docker-env)
+make $PROJECT
+
+docker tag solo-io/$PROJECT-ee:$VERSION solo-io/$PROJECT-ee:$VERSION
--- hack/openshift/start.sh
+++ hack/openshift/start.sh
@@ -6,4 +6,16 @@ oc apply -f hack/openshift/template.yaml
 oc process gloo-ee-installation-template \
  -p APISERVER_OPENSHIFT_MASTER_IP=$(minishift ip) \
   | oc apply -f -
+
+cat << EOF | oc apply -f -
+kind: OAuthClient
+apiVersion: oauth.openshift.io/v1
+metadata:
+ name: gloo
+secret: gloo
+redirectURIs:
+ - "http://localhost:8080"
+grantMethod: prompt
+EOF
+
 kubectl port-forward deployment/apiserver-ui 8080:80
