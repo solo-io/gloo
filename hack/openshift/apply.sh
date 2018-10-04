8dc345511e15afd2e3f4494af9d1bbc645f57b22
Scott Weiss <sdw35@cornell.edu>
2018-10-04 10:59:42 -0400

    fix script error
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- hack/openshift/apply.sh
+++ hack/openshift/apply.sh
@@ -8,8 +8,8 @@ oc process -f ${BASEDIR}/template.yaml \
  -p APISERVER_OPENSHIFT_MASTER_IP=$(minishift ip) \
   | oc apply -f -
 
-if [ "$1" -eq "-w" ]; then
+if [[ "$1" = "-w" ]]; then
     echo "attempting to attach to ui"
     sleep 1 # TODO implement actual wait (do we really care?)
-    oc port-forward deployment/apiserver-ui 8080
+    kubectl port-forward deployment/apiserver-ui 8080
 fi
