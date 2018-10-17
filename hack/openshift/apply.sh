1642ae22e7a105803917d1e00c5282bf8497315d
Scott Weiss <sdw35@cornell.edu>
2018-10-17 15:33:07 -0400

    small hack nit
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- hack/openshift/apply.sh
+++ hack/openshift/apply.sh
@@ -3,9 +3,12 @@ set -ex
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+pullPolicy=IfNotPresent
+
 oc project gloo-system
 oc process -f ${BASEDIR}/template.yaml \
  -p APISERVER_OPENSHIFT_MASTER_IP=$(minishift ip) \
+ -p IMAGE_PULL_POLICY=${pullPolicy} \
   | oc apply -f -
 
 if [[ "$1" = "-w" ]]; then
