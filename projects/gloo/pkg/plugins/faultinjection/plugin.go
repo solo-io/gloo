ff829f8b49dbf4fdb7b3ccf1f60346e1e4bd9403
Rick Ducott <rick.ducott@solo.io>
2018-10-24 15:32:11 -0400

    WIP

--- projects/gloo/pkg/plugins/faultinjection/plugin.go
+++ projects/gloo/pkg/plugins/faultinjection/plugin.go
@@ -1,6 +1,10 @@
 package faultinjection
 
 import (
+	envoyfault "github.com/envoyproxy/go-control-plane/envoy/config/filter/http/fault/v2"
+	envoyhttp "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/http_connection_manager/v2"
+    envoytype "github.com/envoyproxy/go-control-plane/envoy/type"
+	"github.com/solo-io/solo-kit/pkg/utils/protoutils"
 
 	"github.com/solo-io/solo-kit/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/solo-kit/projects/gloo/pkg/plugins"
@@ -23,5 +27,34 @@ func (p *Plugin) Init(params plugins.InitParams) error {
 }
 
 func HttpFilters(params plugins.Params, listener *v1.HttpListener) ([]plugins.StagedHttpFilter, error) {
-	return nil, nil
+	// TODO (rick): switch to MarshalPbStruct (?) when rate limit PR merges
+	conf, err := protoutils.MarshalStruct(generateEnvoyConfigForHttpFault())
+	if err != nil {
+		return nil, err
+	}
+	return []plugins.StagedHttpFilter{
+		{
+			HttpFilter: &envoyhttp.HttpFilter{Name: FilterName, Config: conf},
+			Stage:      pluginStage,
+		},
+	}, nil
+}
+
+
+
+func generateEnvoyConfigForHttpFault() *envoyfault.HTTPFault {
+	percentage := envoytype.FractionalPercent{
+		Denominator: envoytype.FractionalPercent_HUNDRED,
+		Numerator: 0,
+	}
+	abort := envoyfault.FaultAbort{
+		Percentage: &percentage,
+		ErrorType: 503,
+	}
+	httpfault := envoyfault.HTTPFault{
+		Abort: &abort,
+		Delay: &delay,
+
+	}
+	return &httpfault
 }
