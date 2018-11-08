3e731cd8d104390158cb35b6682f4764390a8486
Mitch Kelley <mitch.kelley@solo.io>
2018-11-08 18:11:28 -0500

    refactor vs sync

--- projects/vcs/pkg/syncer/syncer.go
+++ projects/vcs/pkg/syncer/syncer.go
@@ -2,7 +2,6 @@ package syncer
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	"github.com/solo-io/solo-kit/projects/gateway/pkg/api/v1"
@@ -12,29 +11,33 @@ import (
 func ApplyVcsToDeployment(ctx context.Context, dc file.DualClientSet) {
 	// TODO - pass this as an arg
 	namespace := "gloo-system"
-
-	sourceResources, err := dc.File.VirtualServiceClient.List(namespace, clients.ListOpts{
-		Ctx: ctx,
-	})
-	if err != nil {
-		fmt.Printf("source list err: %v\n", err)
-		return
-	}
-
 	aggregatedErrs := make(chan error)
-	virtualServiceReconciler := v1.NewVirtualServiceReconciler(dc.Kube.VirtualServiceClient)
-	if err := virtualServiceReconciler.Reconcile(namespace, sourceResources, transitionVirtualServices, clients.ListOpts{
-		Ctx: ctx,
-	}); err != nil {
-		aggregatedErrs <- err
-	}
 
-	fmt.Println("here")
+	go func() {
+		err := updateVirtualServices(ctx, dc, namespace)
+		if err != nil {
+			aggregatedErrs <- err
+		}
+	}()
+
 	return
-	// return aggregatedErrs, nil
 }
 
 func transitionVirtualServices(original, desired *v1.VirtualService) (bool, error) {
 	return true, nil
 	// TODO - implement check
+}
+
+func updateVirtualServices(ctx context.Context, dc file.DualClientSet, namespace string) error {
+	sourceResources, err := dc.File.VirtualServiceClient.List(namespace, clients.ListOpts{
+		Ctx: ctx,
+	})
+	if err != nil {
+		return err
+	}
+	virtualServiceReconciler := v1.NewVirtualServiceReconciler(dc.Kube.VirtualServiceClient)
+	return virtualServiceReconciler.Reconcile(namespace, sourceResources, transitionVirtualServices, clients.ListOpts{
+		Ctx: ctx,
+	})
+
 }
