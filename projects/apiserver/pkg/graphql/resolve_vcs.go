ef071790db9c912406c391948322c84d99d0d296
Mitch Kelley <mitch.kelley@solo.io>
2018-11-13 13:09:10 -0500

    add dev messages

--- projects/apiserver/pkg/graphql/resolve_vcs.go
+++ projects/apiserver/pkg/graphql/resolve_vcs.go
@@ -2,6 +2,7 @@ package graphql
 
 import (
 	"context"
+	"fmt"
 
 	// "github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	// "github.com/solo-io/solo-kit/pkg/errors"
@@ -12,31 +13,37 @@ import (
 type vcsMutationResolver struct{ *ApiResolver }
 
 func (r *vcsMutationResolver) Commit(ctx context.Context, v *customtypes.VcsMutation, message string) (*string, error) {
-	msg := "TODO"
+	msg := "TODO - commit"
+	fmt.Println(msg)
 	return &msg, nil
 }
 
 func (r *vcsMutationResolver) ClearError(ctx context.Context, v *customtypes.VcsMutation) (*string, error) {
-	msg := "TODO"
+	msg := "TODO - clear error"
+	fmt.Println(msg)
 	return &msg, nil
 }
 
 func (r *vcsMutationResolver) CreateBranch(ctx context.Context, v *customtypes.VcsMutation, branchName string) (*string, error) {
-	msg := "TODO"
+	msg := "TODO - create branch"
+	fmt.Println(msg)
 	return &msg, nil
 }
 
 func (r *vcsMutationResolver) ResetChanges(ctx context.Context, v *customtypes.VcsMutation) (*string, error) {
-	msg := "TODO"
+	msg := "TODO - reset changes"
+	fmt.Println(msg)
 	return &msg, nil
 }
 
 func (r *vcsMutationResolver) CheckoutBranch(ctx context.Context, v *customtypes.VcsMutation, branchName string) (*string, error) {
-	msg := "TODO"
+	msg := "TODO - checkout branch"
+	fmt.Println(msg)
 	return &msg, nil
 }
 
 func (r *vcsMutationResolver) CheckoutCommit(ctx context.Context, v *customtypes.VcsMutation, hash string) (*string, error) {
-	msg := "TODO"
+	msg := "TODO - checkout commit"
+	fmt.Println(msg)
 	return &msg, nil
 }
