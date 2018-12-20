7177793e8d0097cbad38df64a5b7c433b12e5de2
Scott Weiss <sdw35@cornell.edu>
2018-12-20 09:57:20 -0500

    Docs 3 (#230)
    * update readme file
    
    * clean up main readme page, remove obsolete docs, add links to mkdocs
    
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>
    
    * fix arch doc
    
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>
    
    * fix concepts doc
    
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>
    
    * fix spacing in readme
    
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- projects/gloo/doc/docs/getting_started/aws/helloWorld.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-exports.handler = (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        body: 'AWS Lambda, brought to you by Gloo.'
-    };
-    callback(null, response);
-};
--- projects/gloo/doc/docs/plugins/route_extensions.md
+++
@@ -1,4 +0,0 @@
-# Basic Route Extensions Plugin
-
-In Gloo, common features for routes are specified in the `extensions` field on [routes](../v1/virtualservice.md#Route)
-
