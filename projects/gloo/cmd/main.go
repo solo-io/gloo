32a167e71cb1bbe736ec209d1042978ec2e1d1d0
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-01-08 14:40:17 -0500

    add stats server for gloo (#243)

--- projects/gloo/cmd/main.go
+++ projects/gloo/cmd/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/solo-io/gloo/projects/gloo/pkg/setup"
 	"github.com/solo-io/solo-kit/pkg/utils/log"
+	"github.com/solo-io/solo-kit/pkg/utils/stats"
 )
 
 func main() {
+	stats.StartStatsServer()
 	if err := setup.Main(); err != nil {
 		log.Fatalf("err in main: %v", err.Error())
 	}
