8dd41f88cde51602a83c8d1778caecbad8bdd4bc
Scott Weiss <sdw35@cornell.edu>
2019-01-11 17:07:15 -0500

    add retries for envoy routes (#275)
    * add retries plugin
    
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>
    
    * cleanup comment, regen
    
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- projects/gloo/pkg/plugins/basicroute/plugin.go
+++ projects/gloo/pkg/plugins/basicroute/plugin.go
@@ -2,6 +2,7 @@ package basicroute
 
 import (
 	envoyroute "github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
+	"github.com/gogo/protobuf/types"
 	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
 	"github.com/solo-io/solo-kit/pkg/errors"
@@ -26,6 +27,9 @@ func (p *Plugin) ProcessRoute(params plugins.Params, in *v1.Route, out *envoyrou
 		return err
 	}
 	if err := applyTimeout(in, out); err != nil {
+		return err
+	}
+	if err := applyRetries(in, out); err != nil {
 		return err
 	}
 
@@ -62,5 +66,32 @@ func applyTimeout(in *v1.Route, out *envoyroute.Route) error {
 	}
 
 	routeAction.Route.Timeout = in.RoutePlugins.Timeout
+	return nil
+}
+
+func applyRetries(in *v1.Route, out *envoyroute.Route) error {
+	policy := in.RoutePlugins.Retries
+	if policy == nil {
+		return nil
+	}
+	routeAction, ok := out.Action.(*envoyroute.Route_Route)
+	if !ok {
+		return errors.Errorf("retries is only available for Route Actions")
+	}
+	if routeAction.Route == nil {
+		return errors.Errorf("internal error: route %v specified a prefix, but output Envoy object "+
+			"had nil route", in.Action)
+	}
+
+	numRetries := policy.NumRetries
+	if numRetries == 0 {
+		numRetries = 1
+	}
+
+	routeAction.Route.RetryPolicy = &envoyroute.RouteAction_RetryPolicy{
+		RetryOn:       policy.RetryOn,
+		NumRetries:    &types.UInt32Value{Value: numRetries},
+		PerTryTimeout: policy.PerTryTimeout,
+	}
 	return nil
 }
--- projects/gloo/pkg/plugins/basicroute/plugin_test.go
+++ projects/gloo/pkg/plugins/basicroute/plugin_test.go
@@ -3,14 +3,15 @@ package basicroute_test
 import (
 	"time"
 
+	envoyroute "github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
+	"github.com/gogo/protobuf/types"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	. "github.com/solo-io/gloo/projects/gloo/pkg/plugins/basicroute"
-
-	envoyroute "github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
 	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
+	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/plugins/retries"
 	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/plugins/transformation"
 	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
+	. "github.com/solo-io/gloo/projects/gloo/pkg/plugins/basicroute"
 )
 
 var _ = Describe("prefix rewrite", func() {
@@ -55,5 +56,36 @@ var _ = Describe("timeout", func() {
 		Expect(err).NotTo(HaveOccurred())
 		Expect(routeAction.Timeout).NotTo(BeNil())
 		Expect(*routeAction.Timeout).To(Equal(t))
+	})
+})
+
+var _ = Describe("retries", func() {
+	It("works", func() {
+		t := time.Minute
+		p := NewPlugin()
+		routeAction := &envoyroute.RouteAction{}
+		out := &envoyroute.Route{
+			Action: &envoyroute.Route_Route{
+				Route: routeAction,
+			},
+		}
+		err := p.ProcessRoute(plugins.Params{}, &v1.Route{
+			RoutePlugins: &v1.RoutePlugins{
+				Retries: &retries.RetryPolicy{
+					RetryOn:       "if at first you don't succeed",
+					NumRetries:    5,
+					PerTryTimeout: &t,
+				},
+			},
+		}, out)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(routeAction.RetryPolicy).NotTo(BeNil())
+		Expect(*routeAction.RetryPolicy).To(Equal(envoyroute.RouteAction_RetryPolicy{
+			RetryOn: "if at first you don't succeed",
+			NumRetries: &types.UInt32Value{
+				Value: 5,
+			},
+			PerTryTimeout: &t,
+		}))
 	})
 })
