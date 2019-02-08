e8f1617d6dddf0ed234ba9314980a5609eb42583
Mitch Kelley <mitchdraft@users.noreply.github.com>
2019-02-08 11:56:20 -0500

    pin repos error message enhancement (#389)

--- pin_repos.go
+++ pin_repos.go
@@ -1,26 +1,22 @@
 package main
 
 import (
-	"github.com/solo-io/go-utils/versionutils"
+	version "github.com/solo-io/go-utils/versionutils"
 	"github.com/solo-io/solo-kit/pkg/utils/log"
 )
 
 func main() {
 	tomlTree, err := version.ParseToml()
-	if err != nil {
-		fatal(err)
-	}
+	fatalCheck(err, "parsing error")
 
 	soloKitVersion, err := version.GetVersion(version.SoloKitPkg, tomlTree)
-	if err != nil {
-		fatal(err)
-	}
-	err = version.PinGitVersion("../solo-kit", soloKitVersion)
-	if err != nil {
-		fatal(err)
-	}
+	fatalCheck(err, "getting solo-kit version")
+
+	fatalCheck(version.PinGitVersion("../solo-kit", soloKitVersion), "consider git fetching in solo-kit repo")
 }
 
-func fatal(err error) {
-	log.Fatalf("unable to pin repos!: %v", err)
+func fatalCheck(err error, msg string) {
+	if err != nil {
+		log.Fatalf("Error (%v) unable to pin repos!: %v", msg, err)
+	}
 }
