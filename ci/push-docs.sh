6f02a5aff2fa517f3af3a53acacf4a928836d67a
Rick Ducott <rick.ducott@solo.io>
2019-02-21 10:35:53 -0500

    Update to use refactored repo (#439)
    * Update dir
    
    * Rename repo

--- ci/push-docs.sh
+++ ci/push-docs.sh
@@ -18,49 +18,49 @@ github_token_no_spaces=$(echo $GITHUB_TOKEN | tr -d '[:space:]')
 branch="docs-gloo-$tag"
 
 set +x
-echo "Cloning gloo-docs repo"
-git clone https://soloio-bot:$github_token_no_spaces@github.com/solo-io/gloo-docs.git
+echo "Cloning solo-docs repo"
+git clone https://soloio-bot:$github_token_no_spaces@github.com/solo-io/solo-docs.git
 [ "$TRACE" ] && set -x
 
 git config --global user.name "soloio-bot"
-(cd gloo-docs && git checkout -b $branch)
+(cd solo-docs && git checkout -b $branch)
 
 # Gloo
-if [ -d "gloo-docs/docs/v1/github.com/solo-io/gloo" ]; then
-	rm -r gloo-docs/docs/v1/github.com/solo-io/gloo
+if [ -d "solo-docs/gloo/docs/v1/github.com/solo-io/gloo" ]; then
+	rm -r solo-docs/gloo/docs/v1/github.com/solo-io/gloo
 fi
-cp -r docs/v1/github.com/solo-io/gloo gloo-docs/docs/v1/github.com/solo-io/gloo
+cp -r docs/v1/github.com/solo-io/gloo solo-docs/gloo/docs/v1/github.com/solo-io/gloo
 
 # Solo Kit
-if [ -d "gloo-docs/docs/v1/github.com/solo-io/solo-kit" ]; then
-	rm -r gloo-docs/docs/v1/github.com/solo-io/solo-kit
+if [ -d "solo-docs/gloo/docs/v1/github.com/solo-io/solo-kit" ]; then
+	rm -r solo-docs/gloo/docs/v1/github.com/solo-io/solo-kit
 fi
-cp -r docs/v1/github.com/solo-io/solo-kit gloo-docs/docs/v1/github.com/solo-io/solo-kit
+cp -r docs/v1/github.com/solo-io/solo-kit solo-docs/gloo/docs/v1/github.com/solo-io/solo-kit
 
 # Gogoproto
-if [ -d "gloo-docs/docs/v1/gogoproto" ]; then
-	rm -r gloo-docs/docs/v1/gogoproto
+if [ -d "solo-docs/gloo/docs/v1/gogoproto" ]; then
+	rm -r solo-docs/gloo/docs/v1/gogoproto
 fi
-cp -r docs/v1/gogoproto gloo-docs/docs/v1/gogoproto
+cp -r docs/v1/gogoproto solo-docs/gloo/docs/v1/gogoproto
 
 # Google
-if [ -d "gloo-docs/docs/v1/google" ]; then
-	rm -r gloo-docs/docs/v1/google
+if [ -d "solo-docs/gloo/docs/v1/google" ]; then
+	rm -r solo-docs/gloo/docs/v1/google
 fi
-cp -r docs/v1/google gloo-docs/docs/v1/google
+cp -r docs/v1/google solo-docs/gloo/docs/v1/google
 
-(cd gloo-docs && git add .)
+(cd solo-docs && git add .)
 
-if [[ $( (cd gloo-docs && git status --porcelain) | wc -l) -eq 0 ]]; then
+if [[ $( (cd solo-docs && git status --porcelain) | wc -l) -eq 0 ]]; then
   echo "No changes to gloo docs, exiting."
-  rm -rf gloo-docs
+  rm -rf solo-docs
   exit 0;
 fi
 
-(cd gloo-docs && git commit -m "Add docs for tag $tag")
-(cd gloo-docs && git push --set-upstream origin $branch)
+(cd solo-docs && git commit -m "Add docs for tag $tag")
+(cd solo-docs && git push --set-upstream origin $branch)
 
-curl -v -H "Authorization: token $github_token_no_spaces" -H "Content-Type:application/json" -X POST https://api.github.com/repos/solo-io/gloo-docs/pulls -d \
+curl -v -H "Authorization: token $github_token_no_spaces" -H "Content-Type:application/json" -X POST https://api.github.com/repos/solo-io/solo-docs/pulls -d \
 '{"title":"Update docs for gloo '"$tag"'", "body": "Update docs for gloo '"$tag"'", "head": "'"$branch"'", "base": "master"}'
 
-rm -rf gloo-docs
+rm -rf solo-docs
