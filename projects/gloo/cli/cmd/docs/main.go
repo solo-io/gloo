73e4b24a87e74844b8853c8fdc1c4b081f8ce153
Rick Ducott <rick.ducott@solo.io>
2019-03-01 10:11:58 -0500

    Push changelog (#482)
    * Start implementing changelog
    
    * Changelog works
    
    * Rename to push_docs
    
    * Update makefile to use new push docs
    
    * Update dep
    
    * Start updating to use push_docs
    
    * update go-utils
    
    * Dep
    
    * Update lock
    
    * Move changelog version
    
    * Update make and cloudbuild
    
    * Fix script
    
    * Fix for push changelog, and testing CLI
    
    * Now that testing is done, stop shipping CLI docs
    
    * more concise
    
    * Update dep
    
    * Update to new go-utils
    
    * Update go-utils
    
    * Update
    
    * Simplify push_docs cli
    
    * Changelog
    
    * Update
    
    * Update docs gen
    
    * Update changelog
    
    * Fix typo

--- projects/gloo/cli/cmd/docs/main.go
+++ projects/gloo/cli/cmd/docs/main.go
@@ -1,67 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"log"
-	"path/filepath"
-	"strings"
-	"text/template"
+	"github.com/solo-io/go-utils/clidoc"
 
 	"github.com/solo-io/gloo/pkg/version"
 	"github.com/solo-io/gloo/projects/gloo/cli/pkg/cmd"
-	"github.com/spf13/cobra"
-	"github.com/spf13/cobra/doc"
 )
-
-const FrontMatter = `---
-title: "{{ replace .Name "_" " " }}"
-weight: 5
----
-`
-
-var funcMap = template.FuncMap{
-	"title":   strings.Title,
-	"replace": func(s, old, new string) string { return strings.Replace(s, old, new, -1) },
-}
-
-var FrontMatterTemplate = template.Must(template.New("frontmatter").Funcs(funcMap).Parse(FrontMatter))
-
-func renderFrontMatter(filename string) string {
-	_, justFilename := filepath.Split(filename)
-	ext := filepath.Ext(justFilename)
-	justFilename = justFilename[:len(justFilename)-len(ext)]
-	info := struct {
-		Name string
-	}{
-		Name: justFilename,
-	}
-	var buf bytes.Buffer
-	err := FrontMatterTemplate.Execute(&buf, info)
-	if err != nil {
-		panic(err)
-	}
-	return buf.String()
-}
 
 func main() {
 	app := cmd.GlooCli(version.Version)
-	disableAutoGenTag(app)
-	//emptyStr := func(s string) string { return "" }
-	linkHandler := func(s string) string {
-		if strings.HasSuffix(s, ".md") {
-			return filepath.Join("..", s[:len(s)-3])
-		}
-		return s
-	}
-	err := doc.GenMarkdownTreeCustom(app, "./docs/cli", renderFrontMatter, linkHandler)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func disableAutoGenTag(c *cobra.Command) {
-	c.DisableAutoGenTag = true
-	for _, c := range c.Commands() {
-		disableAutoGenTag(c)
-	}
+	clidoc.MustGenerateCliDocs(app)
 }
--- push_docs.go
+++ push_docs.go
@@ -1,31 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"github.com/solo-io/go-utils/docsutils"
-	"github.com/solo-io/go-utils/versionutils"
-	"github.com/solo-io/solo-kit/pkg/utils/log"
-	"os"
 )
 
 func main() {
-	tag, present := os.LookupEnv("TAGGED_VERSION")
-	if !present || tag == "" {
-		fmt.Printf("TAGGED_VERSION not found in environment, skipping docs PR.")
-		os.Exit(0)
+	spec := docsutils.DocsPRSpec{
+		Owner: "solo-io",
+		Repo: "gloo",
+		Product: "gloo",
+		ChangelogPrefix: "gloo",
+		ApiPaths: []string {
+			"docs/v1/github.com/solo-io/gloo",
+			"docs/v1/github.com/solo-io/solo-kit",
+			"docs/v1/gogoproto",
+			"docs/v1/google",
+		},
 	}
-	_, err := versionutils.ParseVersion(tag)
-	if err != nil {
-		fmt.Printf("TAGGED_VERSION %s is not a valid semver version, skipping docs PR.")
-		os.Exit(0)
-	}
-	// assumes changelog goes into solo-docs/gloo/docs/changelog/gloo-changelog
-	err = docsutils.CreateDocsPRSimple("solo-io", "gloo", tag,
-		"docs/v1/github.com/solo-io/gloo",
-		"docs/v1/github.com/solo-io/solo-kit",
-		"docs/v1/gogoproto",
-		"docs/v1/google")
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
+	docsutils.PushDocsCli(&spec)
 }
