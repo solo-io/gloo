e55ce6cf73e00838a09025fd72bdbc2c1628f1b4
Eitan Yarmush <eitan.yarmush@solo.io>
2019-04-01 09:13:15 -0400

    proxy url export (#647)
    * proxy url export
    * Change function args

--- pkg/cliutil/uri.go
+++ pkg/cliutil/uri.go
@@ -1,13 +1,22 @@
 package cliutil
 
 import (
+	"bytes"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 
-	"github.com/solo-io/go-utils/errors"
+	"github.com/solo-io/gloo/projects/gloo/cli/pkg/cmd/options"
+	"github.com/solo-io/solo-kit/pkg/errors"
+	"github.com/solo-io/solo-kit/pkg/utils/kubeutils"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/kubernetes"
 )
 
 // Get the resource identified by the given URI.
@@ -40,4 +49,107 @@ func GetResource(uri string) (io.ReadCloser, error) {
 
 	// Write the body to file
 	return file, nil
+}
+
+func GetIngressHost(opts *options.Proxy, namespace string) (string, error) {
+	restCfg, err := kubeutils.GetConfig("", "")
+	if err != nil {
+		return "", errors.Wrapf(err, "getting kube rest config")
+	}
+	kube, err := kubernetes.NewForConfig(restCfg)
+	if err != nil {
+		return "", errors.Wrapf(err, "starting kube client")
+	}
+	svc, err := kube.CoreV1().Services(namespace).Get(opts.Name, metav1.GetOptions{})
+	if err != nil {
+		return "", errors.Wrapf(err, "could not detect '%v' service in %v namespace. "+
+			"Check that Gloo has been installed properly and is running with 'kubectl get pod -n gloo-system'",
+			opts.Name, namespace)
+	}
+	var svcPort *v1.ServicePort
+	switch len(svc.Spec.Ports) {
+	case 0:
+		return "", errors.Errorf("service %v is missing ports", opts.Name)
+	case 1:
+		svcPort = &svc.Spec.Ports[0]
+	default:
+		for _, p := range svc.Spec.Ports {
+			if p.Name == opts.Port {
+				svcPort = &p
+				break
+			}
+		}
+		if svcPort == nil {
+			return "", errors.Errorf("named port %v not found on service %v", opts.Port, opts.Name)
+		}
+	}
+
+	var host, port string
+	// gateway-proxy is an externally load-balanced service
+	if len(svc.Status.LoadBalancer.Ingress) == 0 || opts.LocalCluster {
+		// assume nodeport on kubernetes
+		// TODO: support more types of NodePort services
+		host, err = getNodeIp(svc, kube)
+		if err != nil {
+			return "", errors.Wrapf(err, "")
+		}
+		port = fmt.Sprintf("%v", svcPort.NodePort)
+	} else {
+		host = svc.Status.LoadBalancer.Ingress[0].Hostname
+		if host == "" {
+			host = svc.Status.LoadBalancer.Ingress[0].IP
+		}
+		port = fmt.Sprintf("%v", svcPort.Port)
+	}
+	return host + ":" + port, nil
+}
+
+func getNodeIp(svc *v1.Service, kube kubernetes.Interface) (string, error) {
+	// pick a node where one of our pods is running
+	pods, err := kube.CoreV1().Pods(svc.Namespace).List(metav1.ListOptions{
+		LabelSelector: labels.SelectorFromSet(svc.Spec.Selector).String(),
+	})
+	if err != nil {
+		return "", err
+	}
+	var nodeName string
+	for _, pod := range pods.Items {
+		if pod.Spec.NodeName != "" {
+			nodeName = pod.Spec.NodeName
+			break
+		}
+	}
+	if nodeName == "" {
+		return "", errors.Errorf("no node found for %v's pods. ensure at least one pod has been deployed "+
+			"for the %v service", svc.Name, svc.Name)
+	}
+	// special case for minikube
+	// we run `minikube ip` which avoids an issue where
+	// we get a NAT network IP when the minikube provider is virtualbox
+	if nodeName == "minikube" {
+		return minikubeIp()
+	}
+
+	node, err := kube.CoreV1().Nodes().Get(nodeName, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	for _, addr := range node.Status.Addresses {
+		return addr.Address, nil
+	}
+
+	return "", errors.Errorf("no active addresses found for node %v", node.Name)
+}
+
+func minikubeIp() (string, error) {
+	minikubeCmd := exec.Command("minikube", "ip")
+
+	hostname := &bytes.Buffer{}
+
+	minikubeCmd.Stdout = hostname
+	minikubeCmd.Stderr = os.Stderr
+	err := minikubeCmd.Run()
+
+	return strings.TrimSuffix(hostname.String(), "\n"), err
 }
--- projects/gloo/cli/pkg/cmd/gateway/url.go
+++ projects/gloo/cli/pkg/cmd/gateway/url.go
@@ -1,24 +1,12 @@
 package gateway
 
 import (
-	"bytes"
 	"fmt"
-	"os"
-	"os/exec"
-	"strings"
 
-	"k8s.io/apimachinery/pkg/labels"
-
-	v1 "k8s.io/api/core/v1"
-
-	"github.com/solo-io/go-utils/cliutils"
-
+	"github.com/solo-io/gloo/pkg/cliutil"
 	"github.com/solo-io/gloo/projects/gloo/cli/pkg/cmd/options"
-	"github.com/solo-io/solo-kit/pkg/errors"
-	"github.com/solo-io/solo-kit/pkg/utils/kubeutils"
+	"github.com/solo-io/go-utils/cliutils"
 	"github.com/spf13/cobra"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 )
 
 func urlCmd(opts *options.Options, optionsFunc ...cliutils.OptionsFunc) *cobra.Command {
@@ -27,7 +15,7 @@ func urlCmd(opts *options.Options, optionsFunc ...cliutils.OptionsFunc) *cobra.C
 		Short: "print the http endpoint for a proxy",
 		Long:  "Use this command to view the HTTP URL of a Proxy reachable from outside the cluster. You can connect to this address from a host on the same network (such as the host machine, in the case of minikube/minishift).",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			ingressHost, err := getIngressHost(opts)
+			ingressHost, err := cliutil.GetIngressHost(&opts.Proxy, opts.Metadata.Namespace)
 			if err != nil {
 				return err
 			}
@@ -53,7 +41,7 @@ func addressCmd(opts *options.Options, optionsFunc ...cliutils.OptionsFunc) *cob
 		Short: "print the socket address for a proxy",
 		Long:  "Use this command to view the address (host:port) of a Proxy reachable from outside the cluster. You can connect to this address from a host on the same network (such as the host machine, in the case of minikube/minishift).",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			ingressHost, err := getIngressHost(opts)
+			ingressHost, err := cliutil.GetIngressHost(&opts.Proxy, opts.Metadata.Namespace)
 			if err != nil {
 				return err
 			}
@@ -67,107 +55,4 @@ func addressCmd(opts *options.Options, optionsFunc ...cliutils.OptionsFunc) *cob
 			"to true if LoadBalanced services are not assigned external IPs by your cluster")
 	cliutils.ApplyOptions(cmd, optionsFunc)
 	return cmd
-}
-
-func getIngressHost(opts *options.Options) (string, error) {
-	restCfg, err := kubeutils.GetConfig("", "")
-	if err != nil {
-		return "", errors.Wrapf(err, "getting kube rest config")
-	}
-	kube, err := kubernetes.NewForConfig(restCfg)
-	if err != nil {
-		return "", errors.Wrapf(err, "starting kube client")
-	}
-	svc, err := kube.CoreV1().Services(opts.Metadata.Namespace).Get(opts.Proxy.Name, metav1.GetOptions{})
-	if err != nil {
-		return "", errors.Wrapf(err, "could not detect '%v' service in %v namespace. "+
-			"Check that Gloo has been installed properly and is running with 'kubectl get pod -n gloo-system'",
-			opts.Proxy.Name, opts.Metadata.Namespace)
-	}
-	var svcPort *v1.ServicePort
-	switch len(svc.Spec.Ports) {
-	case 0:
-		return "", errors.Errorf("service %v is missing ports", opts.Proxy.Name)
-	case 1:
-		svcPort = &svc.Spec.Ports[0]
-	default:
-		for _, p := range svc.Spec.Ports {
-			if p.Name == opts.Proxy.Port {
-				svcPort = &p
-				break
-			}
-		}
-		if svcPort == nil {
-			return "", errors.Errorf("named port %v not found on service %v", opts.Proxy.Port, opts.Proxy.Name)
-		}
-	}
-
-	var host, port string
-	// gateway-proxy is an externally load-balanced service
-	if len(svc.Status.LoadBalancer.Ingress) == 0 || opts.Proxy.LocalCluster {
-		// assume nodeport on kubernetes
-		// TODO: support more types of NodePort services
-		host, err = getNodeIp(svc, kube)
-		if err != nil {
-			return "", errors.Wrapf(err, "")
-		}
-		port = fmt.Sprintf("%v", svcPort.NodePort)
-	} else {
-		host = svc.Status.LoadBalancer.Ingress[0].Hostname
-		if host == "" {
-			host = svc.Status.LoadBalancer.Ingress[0].IP
-		}
-		port = fmt.Sprintf("%v", svcPort.Port)
-	}
-	return host + ":" + port, nil
-}
-
-func getNodeIp(svc *v1.Service, kube kubernetes.Interface) (string, error) {
-	// pick a node where one of our pods is running
-	pods, err := kube.CoreV1().Pods(svc.Namespace).List(metav1.ListOptions{
-		LabelSelector: labels.SelectorFromSet(svc.Spec.Selector).String(),
-	})
-	if err != nil {
-		return "", err
-	}
-	var nodeName string
-	for _, pod := range pods.Items {
-		if pod.Spec.NodeName != "" {
-			nodeName = pod.Spec.NodeName
-			break
-		}
-	}
-	if nodeName == "" {
-		return "", errors.Errorf("no node found for %v's pods. ensure at least one pod has been deployed "+
-			"for the %v service", svc.Name, svc.Name)
-	}
-	// special case for minikube
-	// we run `minikube ip` which avoids an issue where
-	// we get a NAT network IP when the minikube provider is virtualbox
-	if nodeName == "minikube" {
-		return minikubeIp()
-	}
-
-	node, err := kube.CoreV1().Nodes().Get(nodeName, metav1.GetOptions{})
-	if err != nil {
-		return "", err
-	}
-
-	for _, addr := range node.Status.Addresses {
-		return addr.Address, nil
-	}
-
-	return "", errors.Errorf("no active addresses found for node %v", node.Name)
-}
-
-func minikubeIp() (string, error) {
-	minikubeCmd := exec.Command("minikube", "ip")
-
-	hostname := &bytes.Buffer{}
-
-	minikubeCmd.Stdout = hostname
-	minikubeCmd.Stderr = os.Stderr
-	err := minikubeCmd.Run()
-
-	return strings.TrimSuffix(hostname.String(), "\n"), err
 }
