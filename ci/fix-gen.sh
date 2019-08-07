a13506c536b8d2bf97de76dfe38945307d416f7a
Kevin Dorosh <kcdorosh@gmail.com>
2019-08-07 09:45:10 -0400

    Update our fix-gen.sh script to work on BSD and GNU machines, so OSX … (#837)
    * Update our fix-gen.sh script to work on BSD and GNU machines, so OSX devs don't need to blow away their installed sed with gsed to run 'make generated-ui'
    * Changelog for sed BSD and GNU support
    * Fix spacing

--- ci/fix-gen.sh
+++ ci/fix-gen.sh
@@ -4,13 +4,13 @@ set -e
 
 for file in $(find projects/gloo-ui/src/proto/github.com -type f | grep "_pb.js")
 do
-	sed -i "s|validate/validate_pb.js|github.com/solo-io/solo-kit/api/external/validate/validate_pb.js|g" $file
-	sed -i "s|google/api/annotations_pb.js|github.com/solo-io/solo-kit/api/external/google/api/annotations_pb.js|g" $file
-	sed -i "s|google/api/http_pb.js|github.com/solo-io/solo-kit/api/external/google/api/http_pb.js|g" $file
-	sed -i "s|google/rpc/status_pb.js|github.com/solo-io/solo-kit/api/external/google/rpc/status_pb.js|g" $file
-	sed -i "s|envoy/api/v2/discovery_pb.js|github.com/solo-io/solo-kit/api/external/envoy/api/v2/discovery_pb.js|g" $file
-	sed -i "s|envoy/api/v2/core/base_pb.js|github.com/solo-io/solo-kit/api/external/envoy/api/v2/core/base_pb.js|g" $file
-	sed -i "s|envoy/type/percent_pb.js|github.com/solo-io/solo-kit/api/external/envoy/type/percent_pb.js|g" $file
-    sed -i "s|../../../../../gogoproto/gogo_pb.js|../../../../gogo/protobuf/gogoproto/gogo_pb.js|g" $file
-    printf '%s\n%s\n' "/* eslint-disable */" "$(cat $file)" >$file
+  sed "s|validate/validate_pb.js|github.com/solo-io/solo-kit/api/external/validate/validate_pb.js|g" "$file" > "$file".tmp && mv "$file".tmp "$file"
+  sed "s|google/api/annotations_pb.js|github.com/solo-io/solo-kit/api/external/google/api/annotations_pb.js|g" "$file" > "$file".tmp && mv "$file".tmp "$file"
+  sed "s|google/api/http_pb.js|github.com/solo-io/solo-kit/api/external/google/api/http_pb.js|g" "$file" > "$file".tmp && mv "$file".tmp "$file"
+  sed "s|google/rpc/status_pb.js|github.com/solo-io/solo-kit/api/external/google/rpc/status_pb.js|g" "$file" > "$file".tmp && mv "$file".tmp "$file"
+  sed "s|envoy/api/v2/discovery_pb.js|github.com/solo-io/solo-kit/api/external/envoy/api/v2/discovery_pb.js|g" "$file" > "$file".tmp && mv "$file".tmp "$file"
+  sed "s|envoy/api/v2/core/base_pb.js|github.com/solo-io/solo-kit/api/external/envoy/api/v2/core/base_pb.js|g" "$file" > "$file".tmp && mv "$file".tmp "$file"
+  sed "s|envoy/type/percent_pb.js|github.com/solo-io/solo-kit/api/external/envoy/type/percent_pb.js|g" "$file" > "$file".tmp && mv "$file".tmp "$file"
+  sed "s|../../../../../gogoproto/gogo_pb.js|../../../../gogo/protobuf/gogoproto/gogo_pb.js|g" "$file" > "$file".tmp && mv "$file".tmp "$file"
+  printf '%s\n%s\n' "/* eslint-disable */" "$(cat "$file")" > "$file"
 done
