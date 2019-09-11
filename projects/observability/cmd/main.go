8c1b6f610d02fcd48abcda14ad3a7399d561b3aa
Graham Goudeau <grahamgoudeau@gmail.com>
2019-09-11 17:01:10 -0400

    Only start the observability setup loop with a valid license (#1087)
    * Only start the observability setup loop with a valid license

--- projects/observability/cmd/main.go
+++ projects/observability/cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"os"
 
-	"github.com/solo-io/go-utils/log"
+	"github.com/solo-io/go-utils/contextutils"
+	"github.com/solo-io/solo-projects/pkg/license"
+	"github.com/solo-io/solo-projects/pkg/version"
+
 	"github.com/solo-io/go-utils/stats"
 	"github.com/solo-io/solo-projects/projects/observability/pkg/syncer"
 )
@@ -16,7 +20,19 @@ func main() {
 	if os.Getenv(START_STATS_SERVER) != "" {
 		stats.StartStatsServer()
 	}
+
+	loggingContext := []interface{}{"version", version.Version}
+	ctx := contextutils.WithLogger(context.Background(), "observability")
+	ctx = contextutils.WithLoggerValues(ctx, loggingContext...)
+
+	logger := contextutils.LoggerFrom(ctx)
+
+	err := license.LicenseStatus(ctx)
+	if err != nil {
+		logger.Fatalf("License is invalid, crashing - " + err.Error())
+	}
+
 	if err := syncer.Main(); err != nil {
-		log.Fatalf("err in main: %v", err.Error())
+		logger.Fatalf("err in main: %v", err.Error())
 	}
 }
