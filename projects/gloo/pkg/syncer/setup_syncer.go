9ac4a187ca11367ff19c947997d446d37a639de0
Kevin Dorosh <kcdorosh@gmail.com>
2019-09-18 14:33:34 -0400

    Fix ratelimit (enterprise-only) translator syncer setup (#1210)
    * Fix rl translator syncer setup
    * Shorthand to accomplish the same thing
    * Formatting

--- projects/gloo/pkg/syncer/setup_syncer.go
+++ projects/gloo/pkg/syncer/setup_syncer.go
@@ -6,6 +6,8 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/enterprise/plugins/ratelimit"
+
 	"github.com/solo-io/gloo/projects/gloo/pkg/validation"
 
 	consulapi "github.com/hashicorp/consul/api"
@@ -360,6 +362,9 @@ func RunGlooWithExtensions(opts bootstrap.Opts, extensions Extensions) error {
 	var syncerExtensions []TranslatorSyncerExtension
 	params := TranslatorSyncerExtensionParams{
 		SettingExtensions: opts.Settings.Extensions,
+		RateLimitDescriptorSettings: ratelimit.EnvoySettings{
+			CustomConfig: opts.Settings.GetRatelimitDescriptors().GetCustomConfig(),
+		},
 	}
 	for _, syncerExtensionFactory := range extensions.SyncerExtensions {
 		syncerExtension, err := syncerExtensionFactory(watchOpts.Ctx, params)
