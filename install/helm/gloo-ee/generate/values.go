e8f3e9c03ae002fe35bb1f1113febc2eae611f90
Graham Goudeau <grahamgoudeau@gmail.com>
2019-09-20 11:21:59 -0400

    Remove extraneous field from values.go (#1144)
    * Remove extraneous field from values.go

--- install/helm/gloo-ee/generate/values.go
+++ install/helm/gloo-ee/generate/values.go
@@ -87,12 +87,11 @@ type ObservabilityDeployment struct {
 }
 
 type CustomGrafana struct {
-	Enabled                   bool   `json:"enabled",omitempty,desc:"Set to true to indicate that the observability pod should talk to a custom grafana instance"`
-	Username                  string `json:"username",omitempty,desc:"Set this and the 'password' field to authenticate to the custom grafana instance using basic auth"`
-	Password                  string `json:"password",omitempty,desc:"Set this and the 'username' field to authenticate to the custom grafana instance using basic auth"`
-	ApiKey                    string `json:"apiKey",omitempty,desc:"Authenticate to the custom grafana instance using this api key"`
-	Url                       string `json:"url",omitempty,desc:"The URL for the custom grafana instance"`
-	UpstreamDashboardTemplate string `json:"upstreamDashboardTemplate",omitempty,desc:"The template to use when auto-generating dashboards for each upstream"`
+	Enabled  bool   `json:"enabled",omitempty,desc:"Set to true to indicate that the observability pod should talk to a custom grafana instance"`
+	Username string `json:"username",omitempty,desc:"Set this and the 'password' field to authenticate to the custom grafana instance using basic auth"`
+	Password string `json:"password",omitempty,desc:"Set this and the 'username' field to authenticate to the custom grafana instance using basic auth"`
+	ApiKey   string `json:"apiKey",omitempty,desc:"Authenticate to the custom grafana instance using this api key"`
+	Url      string `json:"url",omitempty,desc:"The URL for the custom grafana instance"`
 }
 
 type ExtAuth struct {
