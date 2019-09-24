78bd8768908d46bb98c3bfe168cfb6c8526834a6
Marco Schmidt <marcog.schmidt@gmail.com>
2019-09-24 10:23:15 -0400

    Make HTTP extauth e2e tests run locally on MacOS (#1156)
    * Make HTTP extauth e2e tests run locally on MacOS

--- test/e2e/extauth_http_test.go
+++ test/e2e/extauth_http_test.go
@@ -7,7 +7,7 @@ import (
 	"sync/atomic"
 
 	envoyutil "github.com/envoyproxy/go-control-plane/pkg/util"
-	extauth2 "github.com/solo-io/solo-projects/projects/gloo/pkg/plugins/extauth"
+	extauthplugin "github.com/solo-io/solo-projects/projects/gloo/pkg/plugins/extauth"
 
 	"github.com/gogo/protobuf/types"
 
@@ -38,19 +38,67 @@ var _ = Describe("External http", func() {
 		cancel          context.CancelFunc
 		testClients     services.TestClients
 		extauthSettings *extauth.Settings
-		extauthport     uint32
-		extauthserver   *gloov1.Upstream
+		extauthServer   *gloov1.Upstream
+
+		envoyInstance *services.EnvoyInstance
+		testUpstream  *v1helpers.TestUpstream
+		envoyPort     = uint32(8080)
+		cache         memory.InMemoryResourceCache
+
+		err error
 	)
 
-	BeforeEach(func() {
-		extauthport = atomic.AddUint32(&baseExtauthPort, 1) + uint32(config.GinkgoConfig.ParallelNode*1000)
+	startLocalHttpExtAuthServer := func(ctx context.Context, port uint32) {
+		srvMux := http.NewServeMux()
+		fail := func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusUnauthorized)
+		}
+		ok := func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}
+		srvMux.HandleFunc("/deny", fail)
+		srvMux.HandleFunc("/allow", ok)
+		srvMux.HandleFunc("/prefix/prefixdeny", fail)
+		srvMux.HandleFunc("/prefix/prefixallow", ok)
 
+		var srv http.Server
+		srv.Handler = srvMux
+		srv.Addr = fmt.Sprintf(":%d", port)
+		go func() {
+			<-ctx.Done()
+			_ = srv.Close()
+		}()
+		err := srv.ListenAndServe()
+		if err != http.ErrServerClosed {
+			Expect(err).NotTo(HaveOccurred())
+		}
+	}
+
+	BeforeEach(func() {
 		logger := zaptest.LoggerWriter(GinkgoWriter)
 		contextutils.SetFallbackLogger(logger.Sugar())
 
 		ctx, cancel = context.WithCancel(context.Background())
 
-		extauthserver = &gloov1.Upstream{
+		// Get test clients
+		cache = memory.NewInMemoryResourceCache()
+		testClients = services.GetTestClients(cache)
+		testClients.GlooPort = int(services.AllocateGlooPort())
+
+		// Start Envoy
+		envoyInstance, err = envoyFactory.NewEnvoyInstance()
+		Expect(err).NotTo(HaveOccurred())
+
+		err = envoyInstance.Run(testClients.GlooPort)
+		Expect(err).NotTo(HaveOccurred())
+
+		// Start HTTP ext auth server
+		extauthPort := atomic.AddUint32(&baseExtauthPort, 1) + uint32(config.GinkgoConfig.ParallelNode*1000)
+		go func(testCtx context.Context) {
+			defer GinkgoRecover()
+			startLocalHttpExtAuthServer(testCtx, extauthPort)
+		}(ctx)
+		extauthServer = &gloov1.Upstream{
 			Metadata: core.Metadata{
 				Name:      "extauth-server",
 				Namespace: "default",
@@ -59,192 +107,137 @@ var _ = Describe("External http", func() {
 				UpstreamType: &gloov1.UpstreamSpec_Static{
 					Static: &gloov1static.UpstreamSpec{
 						Hosts: []*gloov1static.Host{{
-							Addr: "localhost",
-							Port: extauthport,
+							Addr: envoyInstance.LocalAddr(),
+							Port: extauthPort,
 						}},
 					},
 				},
 			},
 		}
+		_, err = testClients.UpstreamClient.Write(extauthServer, clients.WriteOpts{})
+		Expect(err).NotTo(HaveOccurred())
 
-		ref := extauthserver.Metadata.Ref()
+		// Create a test upstream
+		testUpstream = v1helpers.NewTestHttpUpstream(ctx, envoyInstance.LocalAddr())
+		_, err = testClients.UpstreamClient.Write(testUpstream.Upstream, clients.WriteOpts{Ctx: ctx})
+		Expect(err).NotTo(HaveOccurred())
+
+		// Start Gloo, pointing it to the ext auth server
+		ref := extauthServer.Metadata.Ref()
 		extauthSettings = &extauth.Settings{
 			ExtauthzServerRef: &ref,
 			HttpService:       &extauth.HttpService{},
 		}
-
 	})
+
 	JustBeforeEach(func() {
-		cache := memory.NewInMemoryResourceCache()
-
-		testClients = services.GetTestClients(cache)
-		testClients.GlooPort = int(services.AllocateGlooPort())
-
-		_, err := testClients.UpstreamClient.Write(extauthserver, clients.WriteOpts{})
-		Expect(err).NotTo(HaveOccurred())
-
 		settingsStruct, err := envoyutil.MessageToStruct(extauthSettings)
 		Expect(err).NotTo(HaveOccurred())
 
 		extensions := &gloov1.Extensions{
 			Configs: map[string]*types.Struct{
-				extauth2.ExtensionName: settingsStruct,
+				extauthplugin.ExtensionName: settingsStruct,
 			},
 		}
-
 		what := services.What{
 			DisableGateway: true,
 			DisableUds:     true,
 			DisableFds:     true,
 		}
 		services.RunGlooGatewayUdsFdsOnPort(ctx, cache, int32(testClients.GlooPort), what, defaults.GlooSystem, nil, extensions)
-		go func(testctx context.Context) {
-			defer GinkgoRecover()
-			srvMux := http.NewServeMux()
-			fail := func(w http.ResponseWriter, r *http.Request) {
-				w.WriteHeader(http.StatusUnauthorized)
-			}
-			ok := func(w http.ResponseWriter, r *http.Request) {
-				w.WriteHeader(http.StatusOK)
-			}
-			srvMux.HandleFunc("/deny", fail)
-			srvMux.HandleFunc("/allow", ok)
-			srvMux.HandleFunc("/prefix/prefixdeny", fail)
-			srvMux.HandleFunc("/prefix/prefixallow", ok)
-
-			var srv http.Server
-			srv.Handler = srvMux
-			srv.Addr = fmt.Sprintf(":%d", extauthport)
-			go func() {
-				<-testctx.Done()
-				srv.Close()
-			}()
-			err := srv.ListenAndServe()
-			if err != http.ErrServerClosed {
-				Expect(err).NotTo(HaveOccurred())
-			}
-		}(ctx)
 	})
 
 	AfterEach(func() {
 		cancel()
+		if envoyInstance != nil {
+			_ = envoyInstance.Clean()
+		}
 	})
 
-	Context("With envoy", func() {
-
-		var (
-			envoyInstance *services.EnvoyInstance
-			testUpstream  *v1helpers.TestUpstream
-			envoyPort     = uint32(8080)
-		)
+	Context("custom sanity tests", func() {
 
 		JustBeforeEach(func() {
-			var err error
-			envoyInstance, err = envoyFactory.NewEnvoyInstance()
+			// drain channel as we dont care about it
+			go func() {
+				for range testUpstream.C {
+				}
+			}()
+
+			proxy := getProxyCustomAuth(envoyPort, testUpstream.Upstream.Metadata.Ref())
+
+			_, err := testClients.ProxyClient.Write(proxy, clients.WriteOpts{})
 			Expect(err).NotTo(HaveOccurred())
 
-			err = envoyInstance.Run(testClients.GlooPort)
-			Expect(err).NotTo(HaveOccurred())
+			Eventually(func() (core.Status, error) {
+				proxyFromStorage, err := testClients.ProxyClient.Read(proxy.Metadata.Namespace, proxy.Metadata.Name, clients.ReadOpts{})
+				if err != nil {
+					return core.Status{}, err
+				}
 
-			testUpstream = v1helpers.NewTestHttpUpstream(ctx, envoyInstance.LocalAddr())
-
-			var opts clients.WriteOpts
-			up := testUpstream.Upstream
-			_, err = testClients.UpstreamClient.Write(up, opts)
-			Expect(err).NotTo(HaveOccurred())
-
+				return proxyFromStorage.Status, nil
+			}, "5s", "0.1s").Should(MatchFields(IgnoreExtras, Fields{
+				"Reason": BeEmpty(),
+				"State":  Equal(core.Status_Accepted),
+			}))
 		})
 
-		AfterEach(func() {
-			if envoyInstance != nil {
-				envoyInstance.Clean()
-			}
+		It("should deny ext auth envoy", func() {
+			Eventually(func() (int, error) {
+				resp, err := http.Get(fmt.Sprintf("http://%s:%d/deny", "localhost", envoyPort))
+				if err != nil {
+					return 0, err
+				}
+				return resp.StatusCode, nil
+			}, "5s", "0.5s").Should(Equal(http.StatusUnauthorized))
 		})
 
-		Context("custom sanity tests", func() {
-			JustBeforeEach(func() {
+		It("should allow ext auth envoy", func() {
+			Eventually(func() (int, error) {
+				resp, err := http.Get(fmt.Sprintf("http://%s:%d/allow", "localhost", envoyPort))
+				if err != nil {
+					return 0, err
+				}
+				return resp.StatusCode, nil
+			}, "5s", "0.5s").Should(Equal(http.StatusOK))
+		})
 
-				// drain channel as we dont care about it
-				go func() {
-					for range testUpstream.C {
-					}
-				}()
+		Context("with prefix", func() {
 
-				proxy := getProxyCustomAuth(envoyPort, testUpstream.Upstream.Metadata.Ref())
-
-				_, err := testClients.ProxyClient.Write(proxy, clients.WriteOpts{})
-				Expect(err).NotTo(HaveOccurred())
-
-				Eventually(func() (core.Status, error) {
-					proxy, err := testClients.ProxyClient.Read(proxy.Metadata.Namespace, proxy.Metadata.Name, clients.ReadOpts{})
-					if err != nil {
-						return core.Status{}, err
-					}
-
-					return proxy.Status, nil
-				}, "5s", "0.1s").Should(MatchFields(IgnoreExtras, Fields{
-					"Reason": BeEmpty(),
-					"State":  Equal(core.Status_Accepted),
-				}))
-			})
-
-			It("should deny ext auth envoy", func() {
-				Eventually(func() (int, error) {
-					resp, err := http.Get(fmt.Sprintf("http://%s:%d/deny", "localhost", envoyPort))
-					if err != nil {
-						return 0, err
-					}
-					return resp.StatusCode, nil
-				}, "5s", "0.5s").Should(Equal(http.StatusUnauthorized))
+			BeforeEach(func() {
+				ref := extauthServer.Metadata.Ref()
+				extauthSettings = &extauth.Settings{
+					ExtauthzServerRef: &ref,
+					HttpService: &extauth.HttpService{
+						// test that prefix with trailing slash is handled correctly
+						PathPrefix: "/prefix/",
+					},
+				}
 			})
 
 			It("should allow ext auth envoy", func() {
 				Eventually(func() (int, error) {
-					resp, err := http.Get(fmt.Sprintf("http://%s:%d/allow", "localhost", envoyPort))
+					resp, err := http.Get(fmt.Sprintf("http://%s:%d/prefixallow", "localhost", envoyPort))
 					if err != nil {
 						return 0, err
 					}
 					return resp.StatusCode, nil
 				}, "5s", "0.5s").Should(Equal(http.StatusOK))
 			})
-			Context("with prefix", func() {
-				BeforeEach(func() {
-					ref := extauthserver.Metadata.Ref()
-					extauthSettings = &extauth.Settings{
-						ExtauthzServerRef: &ref,
-						HttpService: &extauth.HttpService{
-							// test that prefix with traling slash is handled correctly
-							PathPrefix: "/prefix/",
-						},
+
+			It("should deny ext auth envoy", func() {
+				Eventually(func() (int, error) {
+					resp, err := http.Get(fmt.Sprintf("http://%s:%d/prefixdeny", "localhost", envoyPort))
+					if err != nil {
+						return 0, err
 					}
-				})
-
-				It("should allow ext auth envoy", func() {
-					Eventually(func() (int, error) {
-						resp, err := http.Get(fmt.Sprintf("http://%s:%d/prefixallow", "localhost", envoyPort))
-						if err != nil {
-							return 0, err
-						}
-						return resp.StatusCode, nil
-					}, "5s", "0.5s").Should(Equal(http.StatusOK))
-				})
-
-				It("should deny ext auth envoy", func() {
-					Eventually(func() (int, error) {
-						resp, err := http.Get(fmt.Sprintf("http://%s:%d/prefixdeny", "localhost", envoyPort))
-						if err != nil {
-							return 0, err
-						}
-						return resp.StatusCode, nil
-					}, "5s", "0.5s").Should(Equal(http.StatusUnauthorized))
-				})
+					return resp.StatusCode, nil
+				}, "5s", "0.5s").Should(Equal(http.StatusUnauthorized))
 			})
 		})
 	})
 })
 
 func getProxyCustomAuth(envoyPort uint32, upstream core.ResourceRef) *gloov1.Proxy {
-
 	extauthCfg := &extauth.VhostExtension{
 		AuthConfig: &extauth.VhostExtension_CustomAuth{}}
 	return getProxyExtAuth(envoyPort, upstream, extauthCfg)
