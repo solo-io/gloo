3934073dba46a53f3696723d1959c298a0339c1f
Kevin Dorosh <kcdorosh@gmail.com>
2019-10-02 15:40:07 -0400

    Fix upload release targets make target (#1192)
    * Fix upload release targets make target
    * I think this looks cleaner
    * Update changelog

--- ci/upload_github_release_assets.go
+++ ci/upload_github_release_assets.go
@@ -3,29 +3,15 @@ package main
 import "github.com/solo-io/go-utils/githubutils"
 
 func main() {
-	assets := make([]githubutils.ReleaseAssetSpec, 5)
-	assets[0] = githubutils.ReleaseAssetSpec{
-		Name:       "glooctl-linux-amd64",
-		ParentPath: "_output",
-		UploadSHA:  true,
-	}
-	assets[1] = githubutils.ReleaseAssetSpec{
-		Name:       "glooctl-darwin-amd64",
-		ParentPath: "_output",
-		UploadSHA:  true,
-	}
-	assets[2] = githubutils.ReleaseAssetSpec{
-		Name:       "glooctl-windows-amd64.exe",
-		ParentPath: "_output",
-		UploadSHA:  true,
-	}
-	assets[3] = githubutils.ReleaseAssetSpec{
-		Name:       "glooe-release.yaml",
-		ParentPath: "install/manifest",
-	}
-	assets[4] = githubutils.ReleaseAssetSpec{
-		Name:       "gloo-with-read-only-ui-release.yaml",
-		ParentPath: "install/manifest",
+	assets := []githubutils.ReleaseAssetSpec{
+		{
+			Name:       "glooe-release.yaml",
+			ParentPath: "install/manifest",
+		},
+		{
+			Name:       "gloo-with-read-only-ui-release.yaml",
+			ParentPath: "install/manifest",
+		},
 	}
 	spec := githubutils.UploadReleaseAssetSpec{
 		Owner:             "solo-io",
