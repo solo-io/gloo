69a04e343c3cf87b75fcd28590cddd3591b4ba83
Scott Weiss <sdw35@cornell.edu>
2019-10-24 18:39:47 -0400

    add Generated kube types (#1522)
    * all prereqs should be met
    * regen
    * add test for kube types
    * ch
    * bump solokit and regen
    * Merge branch 'master' into generate-kube-types
    * Add native generated Kubernetes types for Gloo config resources
    * fix
    * fix ch

---
+++ projects/gateway/pkg/api/v2/kube/generated_kube_suite_test.go
@@ -0,0 +1,29 @@
+package kube_test
+
+import (
+	"testing"
+
+	"github.com/avast/retry-go"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/solo-io/gloo/test/kube2e"
+	"github.com/solo-io/go-utils/testutils/clusterlock"
+)
+
+func TestKube(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Generated Kube Types Suite")
+}
+
+var locker *clusterlock.TestClusterLocker
+
+var _ = BeforeSuite(func() {
+	var err error
+	locker, err = clusterlock.NewTestClusterLocker(kube2e.MustKubeClient(), clusterlock.Options{})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(locker.AcquireLock(retry.Attempts(40))).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	locker.ReleaseLock()
+})
