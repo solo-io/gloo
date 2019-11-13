16ae1a33bfdae550737ebbf955cc377a0b7cc3ba
Kevin Dorosh <kcdorosh@gmail.com>
2019-11-13 13:44:44 -0500

    Simpler disable of validation/certgenjob (#1691)
    * Simpler disable of validation/certgenjob
    * Formatting
    * More descriptive docs

--- install/helm/gloo/generate/values.go
+++ install/helm/gloo/generate/values.go
@@ -153,6 +153,7 @@ type ServiceAccount struct {
 }
 
 type GatewayValidation struct {
+	Enabled               bool   `json:"enabled" desc:"enable Gloo API Gateway validation hook (default true)"`
 	AlwaysAcceptResources *bool  `json:"alwaysAcceptResources" desc:"unless this is set this to false in order to ensure validation webhook rejects invalid resources. by default, validation webhook will only log and report metrics for invalid resource admission without rejecting them outright."`
 	SecretName            string `json:"secretName" desc:"Name of the Kubernetes Secret containing TLS certificates used by the validation webhook server. This secret will be created by the certGen Job if the certGen Job is enabled."`
 	FailurePolicy         string `json:"failurePolicy" desc:"failurePolicy defines how unrecognized errors from the Gateway validation endpoint are handled - allowed values are 'Ignore' or 'Fail'. Defaults to Ignore "`
@@ -173,6 +174,7 @@ type Job struct {
 
 type CertGenJob struct {
 	Job
+	Enabled                 bool `json:"enabled" desc:"enable the job that generates the certificates for the validating webhook at install time (default true)"`
 	SetTtlAfterFinished     bool `json:"setTtlAfterFinished" desc:"Set ttlSecondsAfterFinished (a k8s feature in Alpha) on the job. Defaults to true"`
 	TtlSecondsAfterFinished int  `json:"ttlSecondsAfterFinished" desc:"Clean up the finished job after this many seconds. Defaults to 60"`
 }
