85077d3d04562ee9878216d1bf4e3350311414fd
Graham Goudeau <grahamgoudeau@gmail.com>
2019-12-17 19:33:08 -0500

    Observability should retry the Grafana service calls (#1399)
    * Observability should retry the Grafana service calls
    * changelog
    * Properly report and retry on all network errors
    * Revert go.mod changes
    * Code review feedback

--- projects/observability/pkg/syncer/grafana_dashboard_syncer.go
+++ projects/observability/pkg/syncer/grafana_dashboard_syncer.go
@@ -5,15 +5,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"sync"
+	"time"
 
-	"github.com/solo-io/solo-projects/projects/observability/pkg/grafana/template"
-
-	"go.uber.org/zap"
-
-	v1 "github.com/solo-io/solo-projects/projects/observability/pkg/api/v1"
-
+	"github.com/avast/retry-go"
+	"github.com/hashicorp/go-multierror"
 	"github.com/solo-io/go-utils/contextutils"
+	"github.com/solo-io/go-utils/errors"
+	v1 "github.com/solo-io/solo-projects/projects/observability/pkg/api/v1"
 	"github.com/solo-io/solo-projects/projects/observability/pkg/grafana"
+	"github.com/solo-io/solo-projects/projects/observability/pkg/grafana/template"
+	"go.uber.org/zap"
 )
 
 type grafanaState struct {
@@ -35,7 +36,13 @@ const (
 	dynamicTag = "dynamic"
 )
 
-var tags = []string{glooTag, dynamicTag}
+var (
+	tags = []string{glooTag, dynamicTag}
+
+	// exponential backoff retry with an initial period of 0.1s for 7 iterations, which will mean a cumulative retry period of ~20s
+	// used to avoid a race condition where observability is ready and Sync'ing before Grafana is ready to accept requests
+	grafanaSyncRetryOpts = []retry.Option{retry.Delay(time.Millisecond * 100), retry.Attempts(7), retry.DelayType(retry.BackOffDelay)}
+)
 
 type GrafanaDashboardsSyncer struct {
 	synced                bool
@@ -60,21 +67,40 @@ func (s *GrafanaDashboardsSyncer) Synced() bool {
 }
 
 func (s *GrafanaDashboardsSyncer) Sync(ctx context.Context, snap *v1.DashboardsSnapshot) error {
+	// The observability deployment may finish and this Sync may start running before the Grafana deployment is ready
+	// Retry the Sync until Grafana is ready
+	// https://github.com/solo-io/solo-projects/issues/307
 	ctx = contextutils.WithLogger(ctx, "observabilitySyncer")
 	logger := contextutils.LoggerFrom(ctx)
 
-	gs, err := s.getCurrentGrafanaState()
-	if err != nil {
-		return err
-	}
-	// handle new upstreams
-	s.createGrafanaContent(logger, snap, gs)
+	syncAttempt := 0
+	return retry.Do(func() error {
+		syncAttempt++
 
-	// handle deleted upstreams
-	s.deleteGrafanaContent(logger, snap, gs)
+		if syncAttempt == 1 {
+			logger.Info("First attempt to sync upstream state with Grafana")
+		} else {
+			logger.Warnf("Attempt number %d to sync with Grafana", syncAttempt)
+		}
+		gs, err := s.getCurrentGrafanaState()
+		if err != nil {
+			return err
+		}
+		// handle new upstreams
+		networkErrs := s.createGrafanaContent(logger, snap, gs)
+		if err := networkErrs.ErrorOrNil(); err != nil {
+			return errors.Errorf("Encountered errors while communicating with Grafana while creating new content: %+v", err)
+		}
 
-	logger.Infof("finished updating all upstream dashboards")
-	return nil
+		// handle deleted upstreams
+		networkErrs = s.deleteGrafanaContent(logger, snap, gs)
+		if err := networkErrs.ErrorOrNil(); err != nil {
+			return errors.Errorf("Encountered errors while communicating with Grafana while deleting old content: %+v", err)
+		}
+
+		logger.Infof("finished updating all upstream dashboards")
+		return nil
+	}, grafanaSyncRetryOpts...)
 }
 
 // we regenerate a dashboard if:
@@ -100,7 +126,10 @@ func (s *GrafanaDashboardsSyncer) shouldRegenDashboard(logger *zap.SugaredLogger
 	return true, nil
 }
 
-func (s *GrafanaDashboardsSyncer) createGrafanaContent(logger *zap.SugaredLogger, snap *v1.DashboardsSnapshot, gs *grafanaState) {
+// returns a list of errors encountered while communicating over the network with Grafana
+func (s *GrafanaDashboardsSyncer) createGrafanaContent(logger *zap.SugaredLogger, snap *v1.DashboardsSnapshot, gs *grafanaState) *multierror.Error {
+	errs := &multierror.Error{}
+
 	for _, upstream := range snap.Upstreams {
 		upstreamName := upstream.Metadata.GetName()
 		templateGenerator := template.NewTemplateGenerator(upstream, s.dashboardJsonTemplate)
@@ -108,7 +137,10 @@ func (s *GrafanaDashboardsSyncer) createGrafanaContent(logger *zap.SugaredLogger
 
 		shouldRegenDashboard, err := s.shouldRegenDashboard(logger, upstreamUid)
 		if err != nil {
-			logger.Errorf("Skipping dashboard for upstream %s because of error - %s", upstreamUid, err.Error())
+			err := errors.Wrapf(err, "Skipping dashboard for upstream %s", upstreamUid)
+			logger.Warn(err.Error())
+			errs = multierror.Append(errs, err)
+
 			continue
 		}
 		if !shouldRegenDashboard {
@@ -125,7 +157,10 @@ func (s *GrafanaDashboardsSyncer) createGrafanaContent(logger *zap.SugaredLogger
 		}
 		err = s.dashboardClient.SetRawDashboard(dash)
 		if err != nil {
-			logger.Warnf("failed to save dashboard to grafana for upstream: %s. %s", upstreamName, err)
+			err := errors.Wrapf(err, "failed to save dashboard to grafana for upstream: %s", upstreamName)
+			logger.Warn(err.Error())
+			errs = multierror.Append(errs, err)
+
 			continue
 		}
 
@@ -144,11 +179,16 @@ func (s *GrafanaDashboardsSyncer) createGrafanaContent(logger *zap.SugaredLogger
 			}
 			_, err = s.snapshotClient.SetRawSnapshot(snap)
 			if err != nil {
-				logger.Warnf("failed to save snapshot to grafana for upstream: %s. %s", upstreamName, err)
+				err := errors.Wrapf(err, "failed to save snapshot to grafana for upstream: %s", upstreamName)
+				logger.Warn(err.Error())
+				errs = multierror.Append(errs, err)
+
 				continue
 			}
 		}
 	}
+
+	return errs
 }
 
 func (s *GrafanaDashboardsSyncer) isEditedByUser(rawDashboard []byte) (bool, error) {
@@ -184,7 +224,10 @@ func (s *GrafanaDashboardsSyncer) isEditedByUser(rawDashboard []byte) (bool, err
 	return mostRecentVersion.Message != template.DefaultCommitMessage, nil
 }
 
-func (s *GrafanaDashboardsSyncer) deleteGrafanaContent(logger *zap.SugaredLogger, snap *v1.DashboardsSnapshot, gs *grafanaState) {
+// returns a list of errors encountered while communicating over the network with Grafana
+func (s *GrafanaDashboardsSyncer) deleteGrafanaContent(logger *zap.SugaredLogger, snap *v1.DashboardsSnapshot, gs *grafanaState) *multierror.Error {
+	errs := &multierror.Error{}
+
 	for _, board := range gs.boards {
 		missing := true
 		for _, upstream := range snap.Upstreams {
@@ -198,7 +241,9 @@ func (s *GrafanaDashboardsSyncer) deleteGrafanaContent(logger *zap.SugaredLogger
 			logger.Infof("deleting dashboard for missing upstream: %s", board.UID)
 			_, err := s.dashboardClient.DeleteDashboard(board.UID)
 			if err != nil {
-				logger.Warnf("failed to delete dashboard for upstream: %s. %s", board.UID, err)
+				err := errors.Wrapf(err, "failed to delete dashboard for upstream: %s", board.UID)
+				logger.Warn(err.Error())
+				errs = multierror.Append(errs, err)
 
 			}
 		}
@@ -217,11 +262,14 @@ func (s *GrafanaDashboardsSyncer) deleteGrafanaContent(logger *zap.SugaredLogger
 			logger.Infof("deleting snapshot for missing upstream: %s", snapshot.Name)
 			err := s.snapshotClient.DeleteSnapshot(snapshot.Key)
 			if err != nil {
-				logger.Warnf("failed to delete snapshot for upstream: %s. %s", snapshot.Name, err)
+				err := errors.Wrapf(err, "failed to delete snapshot for upstream: %s", snapshot.Name)
+				logger.Warn(err.Error())
+				errs = multierror.Append(errs, err)
 			}
 		}
 	}
 
+	return errs
 }
 
 func (s *GrafanaDashboardsSyncer) getCurrentGrafanaState() (*grafanaState, error) {
