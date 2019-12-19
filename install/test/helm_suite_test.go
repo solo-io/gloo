a1ad17b6bad6efeb8841a4f56134e16da75ae8f8
Kevin Dorosh <kcdorosh@gmail.com>
2019-12-19 13:50:23 -0500

    Fix CI (#2011)
    * Test if this fixes CI
    * Make with -B to ensure we don't have tagged version of helm file lying around in CI
    * Revert makefile change
    * Add changelog
    * Move changelog

--- install/test/helm_suite_test.go
+++ install/test/helm_suite_test.go
@@ -52,7 +52,7 @@ func TestHelm(t *testing.T) {
 
 var _ = BeforeSuite(func() {
 	// generate the values.yaml and Chart.yaml files
-	MustMake(".", "-C", "../../", "generate-helm-files")
+	MustMake(".", "-C", "../../", "generate-helm-files", "-B")
 })
 
 type renderTestCase struct {
