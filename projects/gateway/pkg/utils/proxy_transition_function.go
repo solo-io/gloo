2c9027f4b0ec2fbd4193911bdd3b9bc1fabf1a8b
Kevin Dorosh <kcdorosh@gmail.com>
2020-04-14 09:56:41 -0400

    Port fix status race (#2783)
    * Fix proxy status race (#2779)
    
    * Fix proxy status race
    * Add changelog
    * Update comments in proto
    * Update transition function so gateway and gloo resync match
    * Revert "Update comments in proto"
    
    This reverts commit 4965c68dddf4d7f32004bb2afbaaafc5048b96bb.
    * Revert "Fix proxy status race"
    
    This reverts commit 39ab82cf2dcb6f5b691a21a0778351b644b73c76.
    * Update default behavior that we should never hit
    * Revert "Update transition function so gateway and gloo resync match"
    
    This reverts commit a8f7660d4e3339974f002f0adaa4af2d1485b584.
    * Revert "Update default behavior that we should never hit"
    
    This reverts commit 8880ebf9a3e275b483e1ce66ca475c9d68873761.
    * Copy over status during proxy transition function if required
    * Formatting
    * Add test
    * Refactor to DRY
    * Move changelog
    * Make changelog more accurate

--- projects/gateway/pkg/utils/proxy_transition_function.go
+++ projects/gateway/pkg/utils/proxy_transition_function.go
@@ -2,16 +2,33 @@ package utils
 
 import (
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
+	"github.com/solo-io/go-utils/hashutils"
 )
 
 func TransitionFunction(original, desired *v1.Proxy) (bool, error) {
 	if len(original.Listeners) != len(desired.Listeners) {
+		updateDesiredStatus(original, desired)
 		return true, nil
 	}
 	for i := range original.Listeners {
 		if !original.Listeners[i].Equal(desired.Listeners[i]) {
+			updateDesiredStatus(original, desired)
 			return true, nil
 		}
 	}
 	return false, nil
+}
+
+func updateDesiredStatus(original, desired *v1.Proxy) {
+	// we made an update to the proxy from the gateway's point of view.
+	// let's make sure we update the status for gloo if the hash hasn't changed.
+	// the proxy can change from the gateway's point of view but not from gloo's if,
+	// for example, the generation changes on a listener.
+	//
+	// this is sort of a hack around using subresource statuses for the proxy status
+	// until we make the full move.
+	equal, ok := hashutils.HashableEqual(original, desired)
+	if ok && equal {
+		desired.Status = original.Status
+	}
 }
