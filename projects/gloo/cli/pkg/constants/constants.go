de5b1203799c2e13f9a079d02a775242909960fa
Miles Holland <mlholland@users.noreply.github.com>
2020-10-06 12:59:03 -0400

    Remove dup helm values 1.4 backport 2 (#3713)
    * Remove duplicate resources in helm charts (#3630)
    
    * initial removal
    * changelog
    * Merge branch 'master' into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Adding changelog file to new location
    * Deleting changelog file from old location
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Adding changelog file to new location
    * Deleting changelog file from old location
    * test and code cleanup
    * rewrite test to not need new go-utils function
    * Merge branch 'remove-cleanup-hook-configs' of https://github.com/solo-io/gloo into remove-cleanup-hook-configs
    * generate code changes
    * remove test focus
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * remove multiple render manifest functions
    * generate code changes
    * remove cleanup hook test
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Adding changelog file to new location
    * Deleting changelog file from old location
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Adding changelog file to new location
    * Deleting changelog file from old location
    * comments
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Adding changelog file to new location
    * Deleting changelog file from old location
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * comment nit
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Adding changelog file to new location
    * Deleting changelog file from old location
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * move changelog file to 1.6 beta
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * Merge refs/heads/master into remove-cleanup-hook-configs
    * move CL

--- projects/gloo/cli/pkg/constants/constants.go
+++ projects/gloo/cli/pkg/constants/constants.go
@@ -5,7 +5,4 @@ const (
 	GlooWithUiHelmRepoTemplate = "https://storage.googleapis.com/gloo-os-ui-helm/charts/gloo-os-with-ui-%s.tgz"
 	GlooReleaseName            = "gloo"
 	KnativeServingNamespace    = "knative-serving"
-	// This annotation is present on resources that are included in the chart only to clean up hooks.
-	// We use it to filter out those resources wherever that it necessary.
-	HookCleanupResourceAnnotation = "solo.io/hook-cleanup"
 )
