efdfbd804faa7928158398808f3a683ff221fb6f
Sam Heilbron <SamHeilbron@gmail.com>
2021-01-27 13:10:31 -0500

    Backport expose listener socket options (#4200)
    * backport socket-options
    * add changelog
    * fixup test. bad backport
    * rename suite

--- projects/gloo/pkg/plugins/listener/plugin.go
+++ projects/gloo/pkg/plugins/listener/plugin.go
@@ -2,9 +2,11 @@ package listener
 
 import (
 	envoy_api_v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	envoy_api_v2_core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	"github.com/solo-io/gloo/pkg/utils/gogoutils"
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
+	"github.com/solo-io/solo-kit/pkg/api/external/envoy/api/v2/core"
 )
 
 func NewPlugin() *Plugin {
@@ -23,11 +25,52 @@ func (p *Plugin) Init(_ plugins.InitParams) error {
 
 // Used to set config that are directly on the [Envoy listener](https://www.envoyproxy.io/docs/envoy/latest/api-v2/api/v2/listener.proto)
 func (p *Plugin) ProcessListener(_ plugins.Params, in *v1.Listener, out *envoy_api_v2.Listener) error {
-	if in.GetOptions().GetPerConnectionBufferLimitBytes() == nil || in.GetOptions().GetPerConnectionBufferLimitBytes().Value == 0 {
-		// Rely on default behavior
-		return nil
+	if in.GetOptions().GetPerConnectionBufferLimitBytes().GetValue() != 0 {
+		out.PerConnectionBufferLimitBytes = gogoutils.UInt32GogoToProto(in.GetOptions().GetPerConnectionBufferLimitBytes())
 	}
 
-	out.PerConnectionBufferLimitBytes = gogoutils.UInt32GogoToProto(in.GetOptions().PerConnectionBufferLimitBytes)
+	if in.GetOptions().GetSocketOptions() != nil {
+		out.SocketOptions = translateSocketOptions(in.GetOptions().GetSocketOptions())
+	}
+
 	return nil
+}
+
+func translateSocketOptions(sos []*core.SocketOption) []*envoy_api_v2_core.SocketOption {
+	var socketOptions []*envoy_api_v2_core.SocketOption
+	for _, so := range sos {
+		socketOptions = append(socketOptions, translateSocketOption(so))
+	}
+	return socketOptions
+}
+
+func translateSocketOption(so *core.SocketOption) *envoy_api_v2_core.SocketOption {
+
+	ret := &envoy_api_v2_core.SocketOption{
+		Description: so.GetDescription(),
+		Level:       so.GetLevel(),
+		Name:        so.GetName(),
+		State:       translateSocketState(so.GetState()),
+	}
+
+	switch typed := so.GetValue().(type) {
+	case *core.SocketOption_BufValue:
+		ret.Value = &envoy_api_v2_core.SocketOption_BufValue{BufValue: typed.BufValue}
+	case *core.SocketOption_IntValue:
+		ret.Value = &envoy_api_v2_core.SocketOption_IntValue{IntValue: typed.IntValue}
+	}
+
+	return ret
+}
+
+func translateSocketState(state core.SocketOption_SocketState) envoy_api_v2_core.SocketOption_SocketState {
+	switch stateStr := state.String(); stateStr {
+	case core.SocketOption_SocketState_name[int32(core.SocketOption_STATE_PREBIND)]:
+		return envoy_api_v2_core.SocketOption_STATE_PREBIND
+	case core.SocketOption_SocketState_name[int32(core.SocketOption_STATE_BOUND)]:
+		return envoy_api_v2_core.SocketOption_STATE_BOUND
+	case core.SocketOption_SocketState_name[int32(core.SocketOption_STATE_LISTENING)]:
+		return envoy_api_v2_core.SocketOption_STATE_LISTENING
+	}
+	return envoy_api_v2_core.SocketOption_STATE_PREBIND
 }
--- projects/gloo/pkg/plugins/listener/plugin_test.go
+++ projects/gloo/pkg/plugins/listener/plugin_test.go
@@ -2,12 +2,14 @@ package listener_test
 
 import (
 	envoyapi "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	envoy_api_v2_core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	"github.com/gogo/protobuf/types"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
 	. "github.com/solo-io/gloo/projects/gloo/pkg/plugins/listener"
+	"github.com/solo-io/solo-kit/pkg/api/external/envoy/api/v2/core"
 )
 
 var _ = Describe("Plugin", func() {
@@ -33,5 +35,32 @@ var _ = Describe("Plugin", func() {
 		err := plugin.ProcessListener(plugins.Params{}, in, out)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(out.PerConnectionBufferLimitBytes.Value).To(BeEquivalentTo(uint32(4096)))
+	})
+
+	It("should set socket options", func() {
+		in := &v1.Listener{
+			Options: &v1.ListenerOptions{
+				SocketOptions: []*core.SocketOption{
+					{
+						Description: "desc",
+						Level:       1,
+						Name:        2,
+						Value:       &core.SocketOption_IntValue{IntValue: 123},
+						State:       core.SocketOption_STATE_LISTENING,
+					},
+				},
+			},
+		}
+		err := plugin.ProcessListener(plugins.Params{}, in, out)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(out.SocketOptions).To(BeEquivalentTo([]*envoy_api_v2_core.SocketOption{
+			{
+				Description: "desc",
+				Level:       1,
+				Name:        2,
+				Value:       &envoy_api_v2_core.SocketOption_IntValue{IntValue: 123},
+				State:       envoy_api_v2_core.SocketOption_STATE_LISTENING,
+			},
+		}))
 	})
 })
