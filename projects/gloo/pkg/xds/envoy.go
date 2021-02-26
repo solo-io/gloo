614e513387606f62db5049065d60ae2429a47aba
Sam Heilbron <SamHeilbron@gmail.com>
2021-02-26 09:05:23 -0700

    Revert "Remove v2 server creation (#4281)" (#4346)
    This reverts commit ffe4178ae1d6ca06cd7b290ccf89e66bab7f45ae.

--- projects/gloo/pkg/xds/envoy.go
+++ projects/gloo/pkg/xds/envoy.go
@@ -3,8 +3,10 @@ package xds
 import (
 	"fmt"
 
+	envoy_api_v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 	envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	envoy_service_cluster_v3 "github.com/envoyproxy/go-control-plane/envoy/service/cluster/v3"
+	envoy_service_discovery_v2 "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v2"
 	envoy_service_discovery_v3 "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 	envoy_service_endpoint_v3 "github.com/envoyproxy/go-control-plane/envoy/service/endpoint/v3"
 	envoy_service_listener_v3 "github.com/envoyproxy/go-control-plane/envoy/service/listener/v3"
@@ -74,9 +76,16 @@ func GetValidKeys(proxies v1.ProxyList, extensionKeys map[string]struct{}) []str
 func SetupEnvoyXds(grpcServer *grpc.Server, xdsServer envoyserver.Server, envoyCache envoycache.SnapshotCache) {
 
 	// check if we need to register
-	if _, ok := grpcServer.GetServiceInfo()["envoy.service.endpoint.v3.EndpointDiscoveryService"]; ok {
+	if _, ok := grpcServer.GetServiceInfo()["envoy.api.v2.EndpointDiscoveryService"]; ok {
 		return
 	}
+
+	serverV2 := NewEnvoyServerV2(xdsServer)
+	envoy_api_v2.RegisterEndpointDiscoveryServiceServer(grpcServer, serverV2)
+	envoy_api_v2.RegisterClusterDiscoveryServiceServer(grpcServer, serverV2)
+	envoy_api_v2.RegisterRouteDiscoveryServiceServer(grpcServer, serverV2)
+	envoy_api_v2.RegisterListenerDiscoveryServiceServer(grpcServer, serverV2)
+	envoy_service_discovery_v2.RegisterAggregatedDiscoveryServiceServer(grpcServer, serverV2)
 
 	serverV3 := NewEnvoyServerV3(xdsServer)
 	envoy_service_endpoint_v3.RegisterEndpointDiscoveryServiceServer(grpcServer, serverV3)
