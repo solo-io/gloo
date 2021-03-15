b7594b4a501b9dfb453d398628393b464ccceb9f
Kevin Dorosh <kevin.dorosh@solo.io>
2021-03-15 16:42:27 -0400

    Bump rate limiter (#2134)
    * Bump ratelimit dep
    * add changelog
    * Fix test compilation error
    * Fix test compilation error
    * Merge refs/heads/master into bump_rate_limiter
    * Adding changelog file to new location
    * Deleting changelog file from old location
    * Merge refs/heads/master into bump_rate_limiter
    * Merge refs/heads/master into bump_rate_limiter
    * Bump ratelimit dep
    * Merge branch 'bump_rate_limiter' of github.com:solo-io/solo-projects into bump_rate_limiter
    * Bump ratelimit dep
    * Bump ratelimit dep

--- projects/rate-limit/pkg/xds/xds_test.go
+++ projects/rate-limit/pkg/xds/xds_test.go
@@ -252,11 +252,11 @@ func (rateLimitDomain) Dump() string {
 	panic("implement me")
 }
 
-func (rateLimitDomain) GetLimit(_ context.Context, _ *envoy_extensions_common_ratelimit_v3.RateLimitDescriptor) *config.KeyedRateLimits {
+func (rateLimitDomain) GetLimit(_ context.Context, _ *envoy_extensions_common_ratelimit_v3.RateLimitDescriptor, _ int64) *config.RateLimit {
 	panic("implement me")
 }
 
-func (rateLimitDomain) GetSetLimits(_ *envoy_extensions_common_ratelimit_v3.RateLimitDescriptor) []*config.KeyedRateLimits {
+func (rateLimitDomain) GetSetLimits(_ context.Context, _ *envoy_extensions_common_ratelimit_v3.RateLimitDescriptor, _ int64) []*config.RateLimit {
 	panic("implement me")
 }
 
