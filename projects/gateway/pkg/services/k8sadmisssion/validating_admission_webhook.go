1936e060161803c4606adf855a4a5820133b4c5f
Alexander Berger <alex-berger@users.noreply.github.com>
2021-05-19 17:30:48 +0200

    Reload webhook certificate if it changed (#4627)
    * Implement https://github.com/solo-io/gloo/issues/4624, reload webhook certificate and private key if they changed.
    * Add unit tests for certProvider

--- projects/gateway/pkg/services/k8sadmisssion/validating_admission_webhook.go
+++ projects/gateway/pkg/services/k8sadmisssion/validating_admission_webhook.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"time"
 
 	"github.com/hashicorp/go-multierror"
 
@@ -122,9 +123,9 @@ func NewGatewayValidatingWebhook(cfg WebhookConfig) (*http.Server, error) {
 	readGatewaysFromAllNamespaces := cfg.readGatewaysFromAllNamespaces
 	webhookNamespace := cfg.webhookNamespace
 
-	keyPair, err := tls.LoadX509KeyPair(serverCertPath, serverKeyPath)
+	certProvider, err := NewCertificateProvider(serverCertPath, serverKeyPath, log.New(&debugLogger{ctx: ctx}, "validation-webhook-certificate-watcher", log.LstdFlags), ctx, 10*time.Second)
 	if err != nil {
-		return nil, errors.Wrapf(err, "loading x509 key pair")
+		return nil, errors.Wrapf(err, "loading TLS certificate provider")
 	}
 
 	handler := NewGatewayValidationHandler(
@@ -141,7 +142,7 @@ func NewGatewayValidatingWebhook(cfg WebhookConfig) (*http.Server, error) {
 
 	return &http.Server{
 		Addr:      fmt.Sprintf(":%v", port),
-		TLSConfig: &tls.Config{Certificates: []tls.Certificate{keyPair}},
+		TLSConfig: &tls.Config{GetCertificate: certProvider.GetCertificateFunc()},
 		Handler:   mux,
 		ErrorLog:  log.New(&debugLogger{ctx: ctx}, "validation-webhook-server", log.LstdFlags),
 	}, nil
