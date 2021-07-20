14cd41f3d3603835fedda61ef92f907b95d0f330
Sam Heilbron <samheilbron@gmail.com>
2021-07-20 00:30:40 -0700

    update proxy transition function

--- projects/gateway/pkg/utils/proxy_transition_function.go
+++ projects/gateway/pkg/utils/proxy_transition_function.go
@@ -8,16 +8,8 @@ import (
 func TransitionFunction(original, desired *v1.Proxy) (bool, error) {
 	equal, ok := hashutils.HashableEqual(original, desired)
 	if ok && equal {
-		// we made an update to the proxy from the gateway's point of view.
-		// let's make sure we update the status for gloo if the hash hasn't changed.
-		// the proxy can change from the gateway's point of view but not from gloo's if,
-		// for example, the generation changes on a listener.
-		//
-		// this is sort of a hack around using subresource statuses for the proxy status
-		// until we make the full move.
 		desired.Status = original.Status
 		return true, nil
 	}
-
 	return false, nil
 }
