11a64a1997c6da2f07ccb99fbfa5e2e116f05fdc
Ben Taussig <85883594+ben-taussig-solo@users.noreply.github.com>
2021-08-28 18:04:21 -0400

    Create gloo.splitLogOutput helm value (#5250)
    * Create gloo.splitLogOutput helm value
    * Changelog
    * Update reference to go-utils
    * Relocate changelog to proper location
    * Codegen
    * Merge refs/heads/master into split-log-output
    * Convert gloo.splitLogOutput to a bool, instead of string
    * Remove questionable changelog formatting
    * bump reference to go-utils PR
    * Merge branch 'split-log-output' of github.com:solo-io/gloo into split-log-output
    * Update go.mod to point to go-utils v0.21.17
    * Merge refs/heads/master into split-log-output
    * Merge refs/heads/master into split-log-output
    * Merge refs/heads/master into split-log-output
    * Update install/helm/gloo/generate/values.go
    
    Co-authored-by: Jenny Shu <28537278+jenshu@users.noreply.github.com>
    * Adding changelog file to new location
    * Deleting changelog file from old location
    * update values.txt
    * Merge branch 'split-log-output' of github.com:solo-io/gloo into split-log-output

--- install/helm/gloo/generate/values.go
+++ install/helm/gloo/generate/values.go
@@ -201,10 +201,11 @@ type InvalidConfigPolicy struct {
 }
 
 type Gloo struct {
-	Deployment     *GlooDeployment       `json:"deployment,omitempty"`
-	GlooService    *KubeResourceOverride `json:"service,omitempty"`
+	Deployment     *GlooDeployment `json:"deployment,omitempty"`
 	ServiceAccount `json:"serviceAccount,omitempty" `
-	LogLevel       *string `json:"logLevel,omitempty" desc:"Level at which the pod should log. Options include \"info\", \"debug\", \"warn\", \"error\", \"panic\" and \"fatal\". Default level is info"`
+	SplitLogOutput *bool                 `json:"splitLogOutput,omitempty" desc:"Set to true to send debug/info/warning logs to stdout, error/fatal/panic to stderr. Set to false to send all logs to stdout"`
+	GlooService    *KubeResourceOverride `json:"service,omitempty"`
+	LogLevel       *string               `json:"logLevel,omitempty" desc:"Level at which the pod should log. Options include \"info\", \"debug\", \"warn\", \"error\", \"panic\" and \"fatal\". Default level is info"`
 }
 
 type GlooDeployment struct {
