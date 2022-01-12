f04d5aa4451707d0b3124831c534ee231a84b258
Jenny Shu <28537278+jenshu@users.noreply.github.com>
2022-01-12 16:40:05 -0500

    Update docker compose for graphql [1.10] (#5784)
    * Update docker-compose.yaml to reflect v1.10 config change (#5778)
    
    * Update docker-compose.yaml to reflect v1.10 config change
    
    * Add changelog
    
    Co-authored-by: Jenny Shu <28537278+jenshu@users.noreply.github.com>
    
    * move changelog
    
    Co-authored-by: laurentquillerou <laurentquillerou@users.noreply.github.com>

--- install/docker-compose-file/prepare-directories.sh
+++ install/docker-compose-file/prepare-directories.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir -p ./data/artifact/artifacts/gloo-system
-mkdir -p ./data/config/{authconfigs,gateways,proxies,upstreams,upstreamgroups,ratelimitconfigs,routeoptions,routetables,virtualhostoptions,virtualservices}/gloo-system
+mkdir -p ./data/config/{authconfigs,gateways,graphqlschemas,proxies,ratelimitconfigs,routeoptions,routetables,upstreamgroups,upstreams,virtualhostoptions,virtualservices}/gloo-system
 mkdir -p ./data/secret/secrets/{default,gloo-system}
