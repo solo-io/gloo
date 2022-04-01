08c3e740ade0b3daf48bc68c363102b03222c158
Bslabe123 <bslabe123@gmail.com>
2022-04-01 09:34:35 -0400

    Removed references to deprecated removed references to deprecated http2ProtocolOptions (revised) (#6187)
    * Added to tests
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    * Added to tests
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    * Update changelog/v1.11.0-rc6/http2_protocol_options_update.yaml
    
    Co-authored-by: Jenny Shu <28537278+jenshu@users.noreply.github.com>
    
    * Update plugin_test.go
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: changelog-bot <changelog-bot>
    Co-authored-by: Jenny Shu <28537278+jenshu@users.noreply.github.com>

--- projects/gloo/pkg/plugins/protocoloptions/plugin.go
+++ projects/gloo/pkg/plugins/protocoloptions/plugin.go
@@ -3,10 +3,12 @@ package protocoloptions
 import (
 	envoy_config_cluster_v3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
 	envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	envoy_extensions_upstreams_http_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/upstreams/http/v3"
 	"github.com/golang/protobuf/ptypes/wrappers"
 	errors "github.com/rotisserie/eris"
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
+	"github.com/solo-io/gloo/projects/gloo/pkg/plugins/pluginutils"
 )
 
 var (
@@ -15,9 +17,11 @@ var (
 )
 
 const (
-	ExtensionName = "protocol_options"
-	MinWindowSize = 65535
-	MaxWindowSize = 2147483647
+	ExtensionName        = "protocol_options"
+	MinWindowSize        = 65535
+	MaxWindowSize        = 2147483647
+	MinConcurrentStreams = 1
+	MaxConcurrentStreams = 2147483647
 )
 
 type plugin struct{}
@@ -39,33 +43,51 @@ func (p *plugin) ProcessUpstream(params plugins.Params, in *v1.Upstream, out *en
 	if in.GetUseHttp2() == nil || !in.GetUseHttp2().GetValue() {
 		return nil
 	}
-
-	if out.GetHttp2ProtocolOptions() == nil {
-		out.Http2ProtocolOptions = &envoy_config_core_v3.Http2ProtocolOptions{}
-	}
-
 	// Both these values default to 268435456 if unset.
 	sws := in.GetInitialStreamWindowSize()
 	if sws != nil {
-		if validateWindowSize(sws.GetValue()) {
-			out.GetHttp2ProtocolOptions().InitialStreamWindowSize = &wrappers.UInt32Value{Value: sws.GetValue()}
-		} else {
+		if !validateWindowSize(sws.GetValue()) {
 			return errors.Errorf("Invalid Initial Steam Window Size: %d", sws.GetValue())
+		} else {
+			sws = &wrappers.UInt32Value{Value: sws.GetValue()}
 		}
 	}
 
 	cws := in.GetInitialConnectionWindowSize()
 	if cws != nil {
-		if validateWindowSize(cws.GetValue()) {
-			out.GetHttp2ProtocolOptions().InitialConnectionWindowSize = &wrappers.UInt32Value{Value: cws.GetValue()}
-		} else {
+		if !validateWindowSize(cws.GetValue()) {
 			return errors.Errorf("Invalid Initial Connection Window Size: %d", cws.GetValue())
+		} else {
+			cws = &wrappers.UInt32Value{Value: cws.GetValue()}
 		}
 	}
 
 	mcs := in.GetMaxConcurrentStreams()
 	if mcs != nil {
-		out.GetHttp2ProtocolOptions().MaxConcurrentStreams = &wrappers.UInt32Value{Value: mcs.GetValue()}
+		if !validateConcurrentStreams(mcs.GetValue()) {
+			return errors.Errorf("Invalid Max Concurrent Streams Size: %d", mcs.GetValue())
+		} else {
+			mcs = &wrappers.UInt32Value{Value: mcs.GetValue()}
+		}
+	}
+
+	protobuf := &envoy_extensions_upstreams_http_v3.HttpProtocolOptions{
+		UpstreamProtocolOptions: &envoy_extensions_upstreams_http_v3.HttpProtocolOptions_ExplicitHttpConfig_{
+			ExplicitHttpConfig: &envoy_extensions_upstreams_http_v3.HttpProtocolOptions_ExplicitHttpConfig{
+				ProtocolConfig: &envoy_extensions_upstreams_http_v3.HttpProtocolOptions_ExplicitHttpConfig_Http2ProtocolOptions{
+					Http2ProtocolOptions: &envoy_config_core_v3.Http2ProtocolOptions{
+						InitialStreamWindowSize:     sws,
+						InitialConnectionWindowSize: cws,
+						MaxConcurrentStreams:        mcs,
+					},
+				},
+			},
+		},
+	}
+
+	err := pluginutils.SetExtensionProtocolOptions(out, "envoy.extensions.upstreams.http.v3.HttpProtocolOptions", protobuf)
+	if err != nil {
+		return errors.Wrapf(err, "converting protocol options to struct")
 	}
 
 	return nil
@@ -73,6 +95,13 @@ func (p *plugin) ProcessUpstream(params plugins.Params, in *v1.Upstream, out *en
 
 func validateWindowSize(size uint32) bool {
 	if size < MinWindowSize || size > MaxWindowSize {
+		return false
+	}
+	return true
+}
+
+func validateConcurrentStreams(size uint32) bool {
+	if size < MinConcurrentStreams || size > MaxConcurrentStreams {
 		return false
 	}
 	return true
