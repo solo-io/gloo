f4adafab968a611b15958f7eb90554c7f176d7b5
Nathan Fudenberg <nathan.fudenberg@solo.io>
2022-04-04 10:56:49 -0400

    utils/regexutils: Re-allow nil engine types in regex. (#6226)
    * utils/regexutils: Reallow nil engine types in regex.
    
    * Update changelog/v1.11.0-rc7/regex-rewrite-engine.yaml
    
    Co-authored-by: Jenny Shu <28537278+jenshu@users.noreply.github.com>
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: Jenny Shu <28537278+jenshu@users.noreply.github.com>

--- pkg/utils/regexutils/regex_test.go
+++ pkg/utils/regexutils/regex_test.go
@@ -9,6 +9,7 @@ import (
 
 	. "github.com/solo-io/gloo/pkg/utils/regexutils"
 	"github.com/solo-io/gloo/pkg/utils/settingsutil"
+	v32 "github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/type/matcher/v3"
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 )
 
@@ -37,5 +38,21 @@ var _ = Describe("Regex", func() {
 		regex := NewRegex(ctx, "foo")
 		Expect(regex.GetRegex()).To(Equal("foo"))
 		Expect(regex.GetGoogleRe2().GetMaxProgramSize().GetValue()).To(BeEquivalentTo(123))
+	})
+	It("should create regex even without engine", func() {
+		ctx := settingsutil.WithSettings(context.Background(), &v1.Settings{
+			Gloo: &v1.GlooOptions{RegexMaxProgramSize: &wrappers.UInt32Value{Value: 123}},
+		})
+		subPattern := v32.RegexMatcher{
+			Regex: "(.*)",
+		}
+		in := v32.RegexMatchAndSubstitute{
+			Substitution: "123",
+			Pattern:      &subPattern,
+		}
+		out, err := ConvertRegexMatchAndSubstitute(ctx, &in)
+		Expect(err).To(BeNil())
+		Expect(out.Pattern.Regex).To(Equal(in.Pattern.Regex))
+		Expect(out.Substitution).To(Equal(in.Substitution))
 	})
 })
