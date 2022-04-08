4f133dd2be0875463754fecc84c1eced7d4202fd
Sam Heilbron <SamHeilbron@gmail.com>
2022-04-08 07:09:01 -0400

    Resolve consul plugin data race (#6271)
    * use dependency injectio to handle fallback resolution
    
    * add changelog
    
    * fixup dns resolvers and test

--- projects/gloo/pkg/plugins/consul/dns_resolver.go
+++ projects/gloo/pkg/plugins/consul/dns_resolver.go
@@ -3,6 +3,7 @@ package consul
 import (
 	"context"
 	"net"
+	"sync"
 
 	"github.com/rotisserie/eris"
 )
@@ -15,11 +16,23 @@ type DnsResolver interface {
 	Resolve(ctx context.Context, address string) ([]net.IPAddr, error)
 }
 
-type ConsulDnsResolver struct {
+var (
+	_ DnsResolver = new(dnsResolver)
+	_ DnsResolver = new(dnsResolverWithFallback)
+)
+
+func NewConsulDnsResolver(address string) DnsResolver {
+	basicResolver := &dnsResolver{
+		DnsAddress: address,
+	}
+	return NewDnsResolverWithFallback(basicResolver)
+}
+
+type dnsResolver struct {
 	DnsAddress string
 }
 
-func (c *ConsulDnsResolver) Resolve(ctx context.Context, address string) ([]net.IPAddr, error) {
+func (c *dnsResolver) Resolve(ctx context.Context, address string) ([]net.IPAddr, error) {
 	res := net.Resolver{
 		PreferGo: true, // otherwise we may use cgo which doesn't resolve on my mac in testing
 		Dial: func(ctx context.Context, network, address string) (conn net.Conn, err error) {
@@ -37,4 +50,39 @@ func (c *ConsulDnsResolver) Resolve(ctx context.Context, address string) ([]net.
 			"resolved as a hostname at %s but the DNS server returned no results", address, c.DnsAddress)
 	}
 	return ipAddrs, nil
+}
+
+type dnsResolverWithFallback struct {
+	resolver DnsResolver
+
+	sync.RWMutex
+	previousResolutions map[string][]net.IPAddr
+}
+
+func NewDnsResolverWithFallback(resolver DnsResolver) *dnsResolverWithFallback {
+	return &dnsResolverWithFallback{
+		resolver:            resolver,
+		previousResolutions: make(map[string][]net.IPAddr),
+	}
+}
+
+func (d *dnsResolverWithFallback) Resolve(ctx context.Context, address string) ([]net.IPAddr, error) {
+	ipAddrs, err := d.resolver.Resolve(ctx, address)
+
+	// Synchronize access to previous resolutions
+	d.Lock()
+	defer d.Unlock()
+
+	// If we successfully resolved the addresses, update our last known state and return
+	if err == nil {
+		d.previousResolutions[address] = ipAddrs
+		return ipAddrs, nil
+	}
+
+	// If we did not successfully resolve the addresses, attempt to use the last known state
+	lastKnownIdAddrs, resolvedPreviously := d.previousResolutions[address]
+	if !resolvedPreviously {
+		return nil, err
+	}
+	return lastKnownIdAddrs, nil
 }
