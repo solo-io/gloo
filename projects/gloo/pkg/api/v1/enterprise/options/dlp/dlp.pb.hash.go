1123e386f5c8dc1b4dd5375170f368e13341feb0
Ben Taussig <85883594+ben-taussig-solo@users.noreply.github.com>
2022-05-17 13:03:16 -0400

    V1.11: API support for KeyValueMatcher (#6451)
    * API support for KeyValueMatcher (#6428)
    
    * API support for headerAction
    
    * Add shadow field to HeaderAction
    
    * Add changelog entry
    
    * relocate changelog entry
    
    * envoy-gloo-ee api changes
    
    * envoy-gloo-ee api changes
    
    * Update changelog entry
    
    * Update headerAction to keyValueAction in accordance with envoy-gloo-ee changes
    
    * Update changelog entry
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    
    * undo breaking change in dlp.proto
    
    * Change changelog type to FIX to make changelog-bot happy
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>

--- projects/gloo/pkg/api/v1/enterprise/options/dlp/dlp.pb.hash.go
+++ projects/gloo/pkg/api/v1/enterprise/options/dlp/dlp.pb.hash.go
@@ -213,6 +213,26 @@ func (m *Action) Hash(hasher hash.Hash64) (uint64, error) {
 		}
 	}
 
+	if h, ok := interface{}(m.GetKeyValueAction()).(safe_hasher.SafeHasher); ok {
+		if _, err = hasher.Write([]byte("KeyValueAction")); err != nil {
+			return 0, err
+		}
+		if _, err = h.Hash(hasher); err != nil {
+			return 0, err
+		}
+	} else {
+		if fieldValue, err := hashstructure.Hash(m.GetKeyValueAction(), nil); err != nil {
+			return 0, err
+		} else {
+			if _, err = hasher.Write([]byte("KeyValueAction")); err != nil {
+				return 0, err
+			}
+			if err := binary.Write(hasher, binary.LittleEndian, fieldValue); err != nil {
+				return 0, err
+			}
+		}
+	}
+
 	err = binary.Write(hasher, binary.LittleEndian, m.GetShadow())
 	if err != nil {
 		return 0, err
@@ -292,6 +312,54 @@ func (m *CustomAction) Hash(hasher hash.Hash64) (uint64, error) {
 			}
 		}
 
+	}
+
+	return hasher.Sum64(), nil
+}
+
+// Hash function
+func (m *KeyValueAction) Hash(hasher hash.Hash64) (uint64, error) {
+	if m == nil {
+		return 0, nil
+	}
+	if hasher == nil {
+		hasher = fnv.New64()
+	}
+	var err error
+	if _, err = hasher.Write([]byte("dlp.options.gloo.solo.io.github.com/solo-io/gloo/projects/gloo/pkg/api/v1/enterprise/options/dlp.KeyValueAction")); err != nil {
+		return 0, err
+	}
+
+	if _, err = hasher.Write([]byte(m.GetName())); err != nil {
+		return 0, err
+	}
+
+	if _, err = hasher.Write([]byte(m.GetMaskChar())); err != nil {
+		return 0, err
+	}
+
+	if h, ok := interface{}(m.GetPercent()).(safe_hasher.SafeHasher); ok {
+		if _, err = hasher.Write([]byte("Percent")); err != nil {
+			return 0, err
+		}
+		if _, err = h.Hash(hasher); err != nil {
+			return 0, err
+		}
+	} else {
+		if fieldValue, err := hashstructure.Hash(m.GetPercent(), nil); err != nil {
+			return 0, err
+		} else {
+			if _, err = hasher.Write([]byte("Percent")); err != nil {
+				return 0, err
+			}
+			if err := binary.Write(hasher, binary.LittleEndian, fieldValue); err != nil {
+				return 0, err
+			}
+		}
+	}
+
+	if _, err = hasher.Write([]byte(m.GetKeyToMask())); err != nil {
+		return 0, err
 	}
 
 	return hasher.Sum64(), nil
