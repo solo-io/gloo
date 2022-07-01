82e19fefd7d25d4e58a93f4ea06e597762e4d607
ianmacclancy <ianmacclancy@gmail.com>
2022-07-01 16:19:10 -0400

    Reject WeightedDestination when Total Weight < 1 (v1.10.x) (#6665)
    * Reject WeightedDestination when Total Weight < 1  (#6655)
    
    * Error when totalWeight < 1, not individual weight
    
    * fix error message, and tests
    
    * Update projects/gloo/pkg/translator/route_config.go
    
    Co-authored-by: Jenny Shu <28537278+jenshu@users.noreply.github.com>
    
    * update expected error in test to reflect actual error
    
    * actually fix test
    
    * add changelog to track work
    
    Co-authored-by: Jenny Shu <28537278+jenshu@users.noreply.github.com>
    
    * move changelog
    
    * revert an accidental typing
    
    Co-authored-by: Sam Heilbron <SamHeilbron@gmail.com>
    Co-authored-by: Jenny Shu <28537278+jenshu@users.noreply.github.com>

--- projects/gloo/pkg/translator/route_config.go
+++ projects/gloo/pkg/translator/route_config.go
@@ -415,17 +415,6 @@ func (h *httpRouteConfigurationTranslator) setWeightedClusters(params plugins.Ro
 			return err
 		}
 
-		//Catch when Customers pass weights less than 1 or forget to pass weights - this leads to envoy errors
-		if weightedDest.GetWeight() < 1 {
-			weightedDestUpstream := weightedDest.GetDestination().GetUpstream()
-
-			validation.AppendRouteError(routeReport,
-				validationapi.RouteReport_Error_ProcessingError,
-				fmt.Sprintf("Incorrect configuration for Weighted Destination for upstream: %s - Weighted Destinations require a weight that is greater than 0", weightedDestUpstream.GetName()),
-				routeName,
-			)
-		}
-
 		totalWeight += weightedDest.GetWeight()
 
 		weightedCluster := &envoy_config_route_v3.WeightedCluster_ClusterWeight{
@@ -452,10 +441,18 @@ func (h *httpRouteConfigurationTranslator) setWeightedClusters(params plugins.Ro
 		}
 	}
 
-	//Envoy has a default total weight of 100 and requires all weights to equal the current value of total weight
-	//This overrides the default of 100 to the sum of all passed weights both satisfying the requirements
+	// Envoy has a default total weight of 100 and requires all weights to equal the current value of total weight
+	// This overrides the default of 100 to the sum of all passed weights both satisfying the requirements
 	// - that all weights equal total weight
 	// - the passed weights are weighted proportional to each other
+	if totalWeight < 1 {
+		// Envoy errors with:`WeightedClusterValidationError.TotalWeight: value must be greater than or equal to 1`
+		validation.AppendRouteError(routeReport,
+			validationapi.RouteReport_Error_ProcessingError,
+			fmt.Sprintf("Incorrect configuration for Weighted Destination for route - Weighted Destinations require a total weight that is greater than or equal to 1"),
+			routeName,
+		)
+	}
 	clusterSpecifier.WeightedClusters.TotalWeight = &wrappers.UInt32Value{Value: totalWeight}
 
 	return nil
