c2dcf592ba57fb5a271c6e5351ddda06dbccb149
Fabian Gonzalez <fabiangonz98@gmail.com>
2022-08-02 14:39:35 -0400

    Add key_prefix to InternalSession - 1.11 backport (#6836)
    * Add key_prefix to InternalSession (#6735)
    
    * Add key_prefix to InternalSession
    
    * update field comment + generate code
    
    * Add changelog
    
    * empty commit due to Expected - Waiting for status to be reported
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    * empty commit due to Expected - Waiting for status to be reported
    
    * Move changelog file to 1.12.0-rc3
    
    * Update changelog type + issue link to public repo
    
    * Add link to set-cookie MDN article on key_prefix comment
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    * Codegen after merging master
    
    * codegen
    
    * codegen
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: changelog-bot <changelog-bot>
    
    * codegen
    
    * moved changelog
    
    * update proto description
    
    * codegen
    
    * update key_prefix proto to =2
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>

--- projects/gloo/pkg/api/v1/enterprise/options/extauth/v1/extauth.pb.clone.go
+++ projects/gloo/pkg/api/v1/enterprise/options/extauth/v1/extauth.pb.clone.go
@@ -1374,6 +1374,8 @@ func (m *UserSession_InternalSession) Clone() proto.Message {
 	}
 	target = &UserSession_InternalSession{}
 
+	target.KeyPrefix = m.GetKeyPrefix()
+
 	return target
 }
 
--- projects/gloo/pkg/api/v1/enterprise/options/extauth/v1/extauth.pb.hash.go
+++ projects/gloo/pkg/api/v1/enterprise/options/extauth/v1/extauth.pb.hash.go
@@ -2519,6 +2519,10 @@ func (m *UserSession_InternalSession) Hash(hasher hash.Hash64) (uint64, error) {
 		return 0, err
 	}
 
+	if _, err = hasher.Write([]byte(m.GetKeyPrefix())); err != nil {
+		return 0, err
+	}
+
 	return hasher.Sum64(), nil
 }
 
