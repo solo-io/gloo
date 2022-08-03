bb013c47b4cf7d0ee37e36098add6455eb6fef4e
JM Huibonhoa <jm.huibonhoa@solo.io>
2022-08-03 17:28:29 -0400

    add target domain field to extauth proto

--- projects/gloo/pkg/api/v1/enterprise/options/extauth/v1/extauth.pb.equal.go
+++ projects/gloo/pkg/api/v1/enterprise/options/extauth/v1/extauth.pb.equal.go
@@ -2375,6 +2375,10 @@ func (m *UserSession_InternalSession) Equal(that interface{}) bool {
 		return false
 	}
 
+	if strings.Compare(m.GetTargetDomain(), target.GetTargetDomain()) != 0 {
+		return false
+	}
+
 	return true
 }
 
@@ -2435,6 +2439,10 @@ func (m *UserSession_RedisSession) Equal(that interface{}) bool {
 		if !proto.Equal(m.GetPreExpiryBuffer(), target.GetPreExpiryBuffer()) {
 			return false
 		}
+	}
+
+	if strings.Compare(m.GetTargetDomain(), target.GetTargetDomain()) != 0 {
+		return false
 	}
 
 	return true
