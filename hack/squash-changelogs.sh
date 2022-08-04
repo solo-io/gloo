aa07c0b329b459a83d93961099bc45ae0783b6f7
gunnar-solo <92050522+gunnar-solo@users.noreply.github.com>
2022-08-04 11:35:17 -0400

    12 | build(changelog): archived changelogs from unsupported versions (#6867)

---
+++ hack/squash-changelogs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+CURRENT_MAJOR_VERSION=1         # major semvar version of supported product
+CURRENT_MINOR_VERSION=12        # minor semvar version of supported product
+SUPPORTED_VERSIONS=3            # how many versions of product are in active support
+LEGACY_CODE_FOLDER="_archive"   # intended destination of legacy changelogs
+
+cd ../changelog
+
+for folder in v*.*.*; do
+    folder="${folder#?}"
+    semver=( ${folder//./ } )
+    major="${semver[0]}"
+    minor="${semver[1]}"
+
+    # active version, supported version, legacy version
+    if   [ $CURRENT_MAJOR_VERSION = $major ] && [ $CURRENT_MINOR_VERSION = $minor ]; then
+        continue
+    elif [ $CURRENT_MAJOR_VERSION = $major ] && [ $(($minor+$SUPPORTED_VERSIONS+1)) -gt $CURRENT_MINOR_VERSION ]; then
+        dst="$major.$minor"
+    else
+        dst="$LEGACY_CODE_FOLDER/$major.$minor"
+    fi
+
+    mkdir -p $dst
+    mv "v$folder" $dst
+done
