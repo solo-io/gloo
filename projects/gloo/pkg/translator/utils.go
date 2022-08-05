4beb4e1e057e97278224ae4a1cfab06c997383ab
Nathan Fudenberg <nathan.fudenberg@solo.io>
2022-08-05 16:15:17 -0400

    Back1.12/ipv4 only compat (#6891)
    * Set Ipv4Compat only on listeners with Ipv6 bind address (#6861)
    
    * set ipv4 compat only for ipv6 address
    
    * translator: Update minor naming and tests for ipv4->pv6 work
    
    * changelog: update to have one for ipv4 work
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    * translator: ipv4 in ipv6 strictness
    
    * translator: ipv4 in ipv6 order of strictness
    
    * translator: ipv6 should map ipv4 to its space on the correct method
    
    * translator: go semantics around ipv4
    
    * translator: Update to clean up unused functionalities
    
    * translator: Update comments for clarity on ip mapping
    
    * Update projects/gloo/pkg/translator/utils.go
    
    Co-authored-by: Jenny Shu <28537278+jenshu@users.noreply.github.com>
    
    * translator: Rename ipv4 checker to have a better name
    
    * translator: Rename ipv4 checker to have a better name
    
    * translator: New tests for ipv4 compatibility
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: nfuden <nathan.fudenberg@solo.io>
    Co-authored-by: changelog-bot <changelog-bot>
    Co-authored-by: Jenny Shu <28537278+jenshu@users.noreply.github.com>
    
    * changelog: Update location
    
    Co-authored-by: Sam Heilbron <SamHeilbron@gmail.com>
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: Jenny Shu <28537278+jenshu@users.noreply.github.com>

--- projects/gloo/pkg/translator/utils.go
+++ projects/gloo/pkg/translator/utils.go
@@ -99,20 +99,36 @@ type typedConfigObject interface {
 	GetTypedConfig() *any.Any
 }
 
-// GetIpv6Address returns the IPv6 Address for a provided bindAddress
-// returns an error and the original address, if the bindAddress is not a valid IP address
-func GetIpv6Address(bindAddress string) (string, error) {
+// IsIpv4Address returns whether
+// the provided address is valid IPv4, is pure(unmapped) IPv4, and if there was an error in the bindaddr
+// This is used to distinguish between IPv4 and IPv6 addresses
+func IsIpv4Address(bindAddress string) (validIpv4, strictIPv4 bool, err error) {
 	bindIP := net.ParseIP(bindAddress)
 	if bindIP == nil {
 		// If bindAddress is not a valid textual representation of an IP address
-		return bindAddress, errors.Errorf("bindAddress %s is not a valid IP address", bindAddress)
+		return false, false, errors.Errorf("bindAddress %s is not a valid IP address", bindAddress)
 
-	} else if bindIP.To4() != nil {
+	} else if bindIP.To4() == nil {
 		// If bindIP is not an IPv4 address, To4 returns nil.
-		// Therefore, this is the case where the bindIP represents an IPv4 address and we need to convert
-		// it into an IPv4-mapped IPv6 address
-		return fmt.Sprintf("::ffff:%s", bindAddress), nil
+		// so this is not an acceptable ipv4
+		return false, false, nil
 	}
+	return true, isPureIPv4Address(bindAddress), nil
 
-	return bindAddress, nil
+}
+
+// isPureIPv4Address checks the string to see if it is
+// ipv4 and not ipv4 mapped into ipv6 space and not ipv6.
+// Used as the standard net.Parse smashes everything to ipv6.
+// Basically false if ::ffff:0.0.0.0 and true if 0.0.0.0
+func isPureIPv4Address(ipString string) bool {
+	for i := 0; i < len(ipString); i++ {
+		switch ipString[i] {
+		case '.':
+			return true
+		case ':':
+			return false
+		}
+	}
+	return false
 }
