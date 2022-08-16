9fe27254610b349707710b17cec480772a544ccf
gunnar-solo <92050522+gunnar-solo@users.noreply.github.com>
2022-08-16 12:37:19 -0400

    fix(tls_inspector): added AggregateListener support (#6944)
    "envoy.filters.listener.tls_inspector" is an essential listener_filter when attempting to use TLS.  For _most_ gateways, we would previously always consider logic on whether or not to add it based on the presence of a user-provided SslConfig field.  For the new _AggregateListener_, we neglected to add support, originally.  This caused TLS to be unable to function when using the "isolateVirtualHostsBySslConfig" feature flag.
    
    related https://github.com/solo-io/gloo/issues/6677
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>

--- projects/gloo/pkg/plugins/tls_inspector/plugin.go
+++ projects/gloo/pkg/plugins/tls_inspector/plugin.go
@@ -55,7 +55,19 @@ func (p *plugin) ProcessListener(params plugins.Params, in *v1.Listener, out *en
 func shouldIncludeTlsInspectorListenerFilter(in *v1.Listener) bool {
 	return includeTlsInspectorForListener(in) ||
 		includeTlsInspectorForTcpListener(in.GetTcpListener()) ||
-		includeTlsInspectorForHybridListener(in.GetHybridListener())
+		includeTlsInspectorForHybridListener(in.GetHybridListener()) ||
+		includeTlsInspectorForAggregateListener(in.GetAggregateListener())
+}
+
+func includeTlsInspectorForAggregateListener(in *v1.AggregateListener) bool {
+	// check all httpFilterChains for a matcher-specified ssl config
+	for _, filterChain := range in.GetHttpFilterChains() {
+		if filterChain.GetMatcher().GetSslConfig() != nil {
+			return true
+		}
+	}
+
+	return false
 }
 
 func includeTlsInspectorForListener(in *v1.Listener) bool {
