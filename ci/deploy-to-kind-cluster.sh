0e70361bd451c435ea761d88602fb5b9a188b61b
Ben Taussig <85883594+ben-taussig-solo@users.noreply.github.com>
2022-09-27 14:10:19 -0400

    V1.12.x: Deflake Glooctl Istio kube2e Tests (#7250) (#7251)
    * Deflake Glooctl Istio kube2e Tests (#7250)
    
    * debug updates to glooctl_test.go
    
    * use 0.0.0-kind1 instead of 0.0.0-kind in CI
    
    * add changelog entry
    
    * relocate changelog entry to proper version
    
    * remove redundant check
    
    * clean up debug changes in glooctl_test.go
    
    * update changelog location

--- ci/deploy-to-kind-cluster.sh
+++ ci/deploy-to-kind-cluster.sh
@@ -6,7 +6,7 @@ CLUSTER_NAME="${CLUSTER_NAME:-kind}"
 # The version of the Node Docker image to use for booting the cluster
 CLUSTER_NODE_VERSION="${CLUSTER_NODE_VERSION:-v1.22.4}"
 # The version used to tag images
-VERSION="${VERSION:-0.0.0-kind}"
+VERSION="${VERSION:-0.0.0-kind1}"
 # Automatically (lazily) determine OS type
 if [[ $OSTYPE == 'darwin'* ]]; then
   OS='darwin'
