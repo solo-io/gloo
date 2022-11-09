b9fc530522d8385169496f3b3504f874ebc1ae86
bryan_english <bryan.english@solo.io>
2022-11-09 09:36:10 -0600

    changed things according to some of the suggestions in the chat, worked with Ashish a bunch. Tests aren't done but want to get input before modifying the tests

--- projects/gloo/pkg/api/v1/options/retries/retries.pb.hash.go
+++ projects/gloo/pkg/api/v1/options/retries/retries.pb.hash.go
@@ -26,6 +26,62 @@ var (
 )
 
 // Hash function
+func (m *RetryPolicyInterval) Hash(hasher hash.Hash64) (uint64, error) {
+	if m == nil {
+		return 0, nil
+	}
+	if hasher == nil {
+		hasher = fnv.New64()
+	}
+	var err error
+	if _, err = hasher.Write([]byte("retries.options.gloo.solo.io.github.com/solo-io/gloo/projects/gloo/pkg/api/v1/options/retries.RetryPolicyInterval")); err != nil {
+		return 0, err
+	}
+
+	if h, ok := interface{}(m.GetMaxInterval()).(safe_hasher.SafeHasher); ok {
+		if _, err = hasher.Write([]byte("MaxInterval")); err != nil {
+			return 0, err
+		}
+		if _, err = h.Hash(hasher); err != nil {
+			return 0, err
+		}
+	} else {
+		if fieldValue, err := hashstructure.Hash(m.GetMaxInterval(), nil); err != nil {
+			return 0, err
+		} else {
+			if _, err = hasher.Write([]byte("MaxInterval")); err != nil {
+				return 0, err
+			}
+			if err := binary.Write(hasher, binary.LittleEndian, fieldValue); err != nil {
+				return 0, err
+			}
+		}
+	}
+
+	if h, ok := interface{}(m.GetBaseInterval()).(safe_hasher.SafeHasher); ok {
+		if _, err = hasher.Write([]byte("BaseInterval")); err != nil {
+			return 0, err
+		}
+		if _, err = h.Hash(hasher); err != nil {
+			return 0, err
+		}
+	} else {
+		if fieldValue, err := hashstructure.Hash(m.GetBaseInterval(), nil); err != nil {
+			return 0, err
+		} else {
+			if _, err = hasher.Write([]byte("BaseInterval")); err != nil {
+				return 0, err
+			}
+			if err := binary.Write(hasher, binary.LittleEndian, fieldValue); err != nil {
+				return 0, err
+			}
+		}
+	}
+
+	return hasher.Sum64(), nil
+}
+
+// Hash function
 func (m *RetryPolicy) Hash(hasher hash.Hash64) (uint64, error) {
 	if m == nil {
 		return 0, nil
@@ -67,14 +123,24 @@ func (m *RetryPolicy) Hash(hasher hash.Hash64) (uint64, error) {
 		}
 	}
 
-	err = binary.Write(hasher, binary.LittleEndian, m.GetBaseInterval())
-	if err != nil {
-		return 0, err
-	}
-
-	err = binary.Write(hasher, binary.LittleEndian, m.GetMaxInterval())
-	if err != nil {
-		return 0, err
+	if h, ok := interface{}(m.GetRetryPolicyInterval()).(safe_hasher.SafeHasher); ok {
+		if _, err = hasher.Write([]byte("RetryPolicyInterval")); err != nil {
+			return 0, err
+		}
+		if _, err = h.Hash(hasher); err != nil {
+			return 0, err
+		}
+	} else {
+		if fieldValue, err := hashstructure.Hash(m.GetRetryPolicyInterval(), nil); err != nil {
+			return 0, err
+		} else {
+			if _, err = hasher.Write([]byte("RetryPolicyInterval")); err != nil {
+				return 0, err
+			}
+			if err := binary.Write(hasher, binary.LittleEndian, fieldValue); err != nil {
+				return 0, err
+			}
+		}
 	}
 
 	return hasher.Sum64(), nil
