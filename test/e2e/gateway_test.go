b09ee0906153a22ae1f962755bbf533a3176318b
Jake Cukjati <jakecukjati@gmail.com>
2022-11-17 08:39:37 -0600

    Revert "update testing for ext-auth error"
    This reverts commit 3faf329c904a5891170133a3940232a7009470a4.

--- test/e2e/gateway_test.go
+++ test/e2e/gateway_test.go
@@ -297,8 +297,8 @@ var _ = Describe("Gateway", func() {
 				_, err = testClients.VirtualServiceClient.Write(vs2, clients.WriteOpts{})
 				Expect(err).NotTo(HaveOccurred())
 
-				// Check that virtualservice is not reporting an error because of missing authconfig: this is only a warning
-				gloohelpers.EventuallyResourceAccepted(func() (resources.InputResource, error) {
+				// Check that virtualservice is reporting an error because of missing authconfig:
+				gloohelpers.EventuallyResourceRejected(func() (resources.InputResource, error) {
 					return testClients.VirtualServiceClient.Read(writeNamespace, "vs2", clients.ReadOpts{})
 				})
 
