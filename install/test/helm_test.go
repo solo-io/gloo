23f29355f4d560b8a5946e7c10b35792fd42107b
ashish b <108897222+ashishb-solo@users.noreply.github.com>
2022-12-23 10:54:36 -0500

    7316 helm circuitbreakers v1.11.x (#7591)
    * Enable circuit breaker setting on helm template (#7317)
    
    * expose circuitBreaker settings on helm templates
    - add values documentation
    - add circuitBreakers variable on gloo helm settings template
    - add helm test
    - add circuitBrekaers on helm template
    
    * add gateway circuit breakers yaml fixtures
    - remove unneeded helm test
    
    * add gateway circuit breakers changelog
    
    * expose circuitBreaker settings on helm templates
    - add values documentation
    - add circuitBreakers variable on gloo helm settings template
    - add helm test
    - add circuitBrekaers on helm template
    
    * add gateway circuit breakers yaml fixtures
    - remove unneeded helm test
    
    * add gateway circuit breakers changelog
    
    * move gateway circuit breakers changelog to another release version
    
    * remove gateway circuit breakers changelog from v1.13.0-beta22
    
    * address some PR reviews
    - remove changes from value default templates
    - run make -B generated-code
    - add some descriptions
    - add missing values on test
    
    * Run `go mod tidy` again
    
    * Update generated code
    
    Co-authored-by: daniel.fablius <daniel.fablius@vidio.com>
    Co-authored-by: Ashish Banerjee <ashish.banerjee@solo.io>
    
    * Move changelog to the right directory
    
    * Change changelog type to FIX
    
    * Fix broken unit test
    
    Removed some fields in the expected helm yaml that are not present in v1.11.x
    
    * unfocus
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    Co-authored-by: danielfablius <danielfablius34@gmail.com>
    Co-authored-by: daniel.fablius <daniel.fablius@vidio.com>
    Co-authored-by: Jenny Shu <jenny.shu@solo.io>
    Co-authored-by: changelog-bot <changelog-bot>

--- install/test/helm_test.go
+++ install/test/helm_test.go
@@ -3365,7 +3365,19 @@ spec:
 						})
 						testManifest.ExpectUnstructured(settings.GetKind(), settings.GetNamespace(), settings.GetName()).To(BeEquivalentTo(settings))
 					})
+					It("correctly sets the gateway circuitBreakers fields in the settings", func() {
+						settings := makeUnstructureFromTemplateFile("fixtures/settings/gateway_circuit_breakers.yaml", namespace)
 
+						prepareMakefile(namespace, helmValues{
+							valuesArgs: []string{
+								"settings.circuitBreakers.maxConnections=1024",
+								"settings.circuitBreakers.maxPendingRequests=1024",
+								"settings.circuitBreakers.maxRequests=1024",
+								"settings.circuitBreakers.maxRetries=3",
+							},
+						})
+						testManifest.ExpectUnstructured(settings.GetKind(), settings.GetNamespace(), settings.GetName()).To(BeEquivalentTo(settings))
+					})
 					It("correctly allows setting readGatewaysFromAllNamespaces field in the settings when validation disabled", func() {
 						settings := makeUnstructureFromTemplateFile("fixtures/settings/read_gateways_from_all_namespaces.yaml", namespace)
 
