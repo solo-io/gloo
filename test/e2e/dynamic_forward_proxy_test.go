b72d1b957c91361d76c4d4c29c7f20d52191fa58
gunnar-solo <92050522+gunnar-solo@users.noreply.github.com>
2023-01-27 12:29:37 -0500

    fix(replaceInvalidRoutes): exempt dfp from route replacement (#7736)
    * fix(replaceInvalidRoutes): exempt dfp from route replacement
    
    * removed testing logs
    
    * extracted magic string to const
    
    * repaired string assembly, per review catch

--- test/e2e/dynamic_forward_proxy_test.go
+++ test/e2e/dynamic_forward_proxy_test.go
@@ -41,9 +41,7 @@ var _ = Describe("dynamic forward proxy", func() {
 		testContext.SetRunSettings(&gloov1.Settings{
 			Gloo: &gloov1.GlooOptions{
 				InvalidConfigPolicy: &gloov1.GlooOptions_InvalidConfigPolicy{
-					// These tests fail when ReplaceInvalidRoutes is true
-					// https://github.com/solo-io/gloo/issues/7577
-					ReplaceInvalidRoutes:     false,
+					ReplaceInvalidRoutes:     true,
 					InvalidRouteResponseBody: "Dynamic Forward Proxy Response Body",
 				},
 			},
