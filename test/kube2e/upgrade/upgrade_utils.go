34f66f40257871889c9c6a842b938cb365a9ebdc
Nathan Fudenberg <nathan.fudenberg@solo.io>
2023-02-07 11:32:03 -0500

    1.13/cli safe testing (#7802)
    * cli/clients: Kube cache to respect context and therefore actually clean itself
    
    * cli: syntax cleanup
    
    * changelog: Add the cli flak issue
    
    * Update cli-leak-hardening.yaml

--- test/kube2e/upgrade/upgrade_utils.go
+++ test/kube2e/upgrade/upgrade_utils.go
@@ -48,7 +48,6 @@ func GetUpgradeVersions(ctx context.Context, repoName string) (lastMinorLatestPa
 func getLastReleaseOfCurrentMinor(repoName string) (*versionutils.Version, error) {
 	// pull out to const
 	_, filename, _, _ := runtime.Caller(0) //get info about what is calling the function
-	fmt.Printf(filename)
 	fParts := strings.Split(filename, string(os.PathSeparator))
 	splitIdx := 0
 	//we can end up in a situation where the path contains the repo_name twice when running in ci - keep going until we find the last use ex: /home/runner/work/gloo/gloo/test/kube2e/upgrade/junit.xml
