1f607ea88637b59c2e21d634feb68f0135dcd6f7
Fabian Gonzalez <fabian.gonzalez@solo.io>
2023-02-12 16:02:23 -0500

    [1.12] Fix Istio Crash When Using Headless Services (#7836)
    * Fix Istio Crash When Using Headless Services (#7804)
    
    * proposal fix
    
    * add changelog
    
    * move istioIntegration logic back out of eps loop
    
    * continue when setting istioIntegration ep
    
    * fix validation webhook start condition
    
    * add test for headless service curl-ing
    
    * remove focused test
    
    * empty commit
    
    * move headless service setup into beforeEach
    
    * add issue link
    
    * move changelog
    
    * Update changelog/v1.14.0-beta10/fix-istio-integration-headless-service-crash.yaml
    
    Co-authored-by: Kevin Dorosh <kevin.dorosh@solo.io>
    
    * add comment with issue link for clusterIP deprecation
    
    ---------
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: Kevin Dorosh <kevin.dorosh@solo.io>
    
    * move changelog
    
    ---------
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: Kevin Dorosh <kevin.dorosh@solo.io>

--- test/kube2e/istio/istio_integration_test.go
+++ test/kube2e/istio/istio_integration_test.go
@@ -33,115 +33,120 @@ var _ = Describe("Gloo + Istio integration tests", func() {
 		virtualServiceRef = core.ResourceRef{Name: helper.TestrunnerName, Namespace: "gloo-system"}
 	)
 
-	// Sets up services
-	setupServices := func(port int32, targetPort int) {
-		// A Service's TargetPort defaults to the Port if not set
-		tPort := intstr.FromInt(int(port))
-		if targetPort != -1 {
-			tPort = intstr.FromInt(targetPort)
-		}
-		service := corev1.Service{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      serviceRef.Name,
-				Namespace: serviceRef.Namespace,
-				Labels:    map[string]string{"gloo": helper.TestrunnerName},
-			},
-			Spec: corev1.ServiceSpec{
-				Ports: []corev1.ServicePort{
-					{
-						Name:       "http",
-						Port:       port,
-						TargetPort: tPort,
-						Protocol:   corev1.ProtocolTCP,
-					},
-				},
-				Selector: map[string]string{"gloo": helper.TestrunnerName},
-			},
-		}
+	AfterEach(func() {
 		var err error
-		_, err = resourceClientSet.ServiceClient().Write(
-			&kubernetes.Service{Service: kubeService.Service{Service: service}},
-			clients.WriteOpts{},
-		)
+		err = resourceClientSet.VirtualServiceClient().Delete(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.DeleteOpts{
+			IgnoreNotExist: true,
+		})
 		Expect(err).NotTo(HaveOccurred())
-		Eventually(func() error {
-			_, err := resourceClientSet.ServiceClient().Read(serviceRef.Namespace, service.Name, clients.ReadOpts{})
-			return err
-		}, "5s", "1s").Should(BeNil())
-
-		// the upstream should be created by discovery service
-		upstreamRef = core.ResourceRef{
-			Name:      fmt.Sprintf("%s-%s-%d", "gloo-system", helper.TestrunnerName, port),
-			Namespace: "gloo-system",
-		}
-		helpers.EventuallyResourceAccepted(func() (resources.InputResource, error) {
-			return resourceClientSet.UpstreamClient().Read(upstreamRef.Namespace, upstreamRef.Name, clients.ReadOpts{})
+		helpers.EventuallyResourceDeleted(func() (resources.InputResource, error) {
+			return resourceClientSet.VirtualServiceClient().Read(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.ReadOpts{})
 		})
 
-		virtualService := &v1.VirtualService{
-			Metadata: &core.Metadata{
-				Name:      virtualServiceRef.Name,
-				Namespace: virtualServiceRef.Namespace,
-			},
-			VirtualHost: &v1.VirtualHost{
-				Domains: []string{helper.TestrunnerName},
-				Routes: []*v1.Route{{
-					Action: &v1.Route_RouteAction{
-						RouteAction: &gloov1.RouteAction{
-							Destination: &gloov1.RouteAction_Single{
-								Single: &gloov1.Destination{
-									DestinationType: &gloov1.Destination_Upstream{
-										Upstream: &upstreamRef,
+		err = resourceClientSet.ServiceClient().Delete(serviceRef.Namespace, serviceRef.Name, clients.DeleteOpts{
+			IgnoreNotExist: true,
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(func() bool {
+			_, err := resourceClientSet.ServiceClient().Read(serviceRef.Namespace, serviceRef.Name, clients.ReadOpts{})
+			// we should receive a DNE error, meaning it's now deleted
+			return err != nil && skerrors.IsNotExist(err)
+		}, "5s", "1s").Should(BeTrue())
+
+		err = resourceClientSet.UpstreamClient().Delete(upstreamRef.Namespace, upstreamRef.Name, clients.DeleteOpts{
+			IgnoreNotExist: true,
+		})
+		helpers.EventuallyResourceDeleted(func() (resources.InputResource, error) {
+			return resourceClientSet.UpstreamClient().Read(upstreamRef.Namespace, upstreamRef.Name, clients.ReadOpts{})
+		})
+	})
+
+	Context("port settings", func() {
+		BeforeEach(func() {
+			serviceRef = core.ResourceRef{Name: helper.TestrunnerName, Namespace: "gloo-system"}
+			virtualServiceRef = core.ResourceRef{Name: helper.TestrunnerName, Namespace: "gloo-system"}
+		})
+
+		// Sets up services
+		setupServices := func(port int32, targetPort int) {
+			// A Service's TargetPort defaults to the Port if not set
+			tPort := intstr.FromInt(int(port))
+			if targetPort != -1 {
+				tPort = intstr.FromInt(targetPort)
+			}
+			service := corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      serviceRef.Name,
+					Namespace: serviceRef.Namespace,
+					Labels:    map[string]string{"gloo": helper.TestrunnerName},
+				},
+				Spec: corev1.ServiceSpec{
+					Ports: []corev1.ServicePort{
+						{
+							Name:       "http",
+							Port:       port,
+							TargetPort: tPort,
+							Protocol:   corev1.ProtocolTCP,
+						},
+					},
+					Selector: map[string]string{"gloo": helper.TestrunnerName},
+				},
+			}
+			var err error
+			_, err = resourceClientSet.ServiceClient().Write(
+				&kubernetes.Service{Service: kubeService.Service{Service: service}},
+				clients.WriteOpts{},
+			)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(func() error {
+				_, err := resourceClientSet.ServiceClient().Read(serviceRef.Namespace, service.Name, clients.ReadOpts{})
+				return err
+			}, "5s", "1s").Should(BeNil())
+
+			// the upstream should be created by discovery service
+			upstreamRef = core.ResourceRef{
+				Name:      fmt.Sprintf("%s-%s-%d", "gloo-system", helper.TestrunnerName, port),
+				Namespace: "gloo-system",
+			}
+			helpers.EventuallyResourceAccepted(func() (resources.InputResource, error) {
+				return resourceClientSet.UpstreamClient().Read(upstreamRef.Namespace, upstreamRef.Name, clients.ReadOpts{})
+			})
+
+			virtualService := &v1.VirtualService{
+				Metadata: &core.Metadata{
+					Name:      virtualServiceRef.Name,
+					Namespace: virtualServiceRef.Namespace,
+				},
+				VirtualHost: &v1.VirtualHost{
+					Domains: []string{helper.TestrunnerName},
+					Routes: []*v1.Route{{
+						Action: &v1.Route_RouteAction{
+							RouteAction: &gloov1.RouteAction{
+								Destination: &gloov1.RouteAction_Single{
+									Single: &gloov1.Destination{
+										DestinationType: &gloov1.Destination_Upstream{
+											Upstream: &upstreamRef,
+										},
 									},
 								},
 							},
 						},
-					},
-					Matchers: []*matchers.Matcher{
-						{
-							PathSpecifier: &matchers.Matcher_Prefix{
-								Prefix: "/",
+						Matchers: []*matchers.Matcher{
+							{
+								PathSpecifier: &matchers.Matcher_Prefix{
+									Prefix: "/",
+								},
 							},
 						},
-					},
-				}},
-			},
-		}
-		_, err = resourceClientSet.VirtualServiceClient().Write(virtualService, clients.WriteOpts{})
-		Expect(err).NotTo(HaveOccurred())
-		helpers.EventuallyResourceAccepted(func() (resources.InputResource, error) {
-			return resourceClientSet.VirtualServiceClient().Read(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.ReadOpts{})
-		})
-	}
-
-	Context("port settings", func() {
-		AfterEach(func() {
-			var err error
-			err = resourceClientSet.VirtualServiceClient().Delete(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.DeleteOpts{
-				IgnoreNotExist: true,
-			})
+					}},
+				},
+			}
+			_, err = resourceClientSet.VirtualServiceClient().Write(virtualService, clients.WriteOpts{})
 			Expect(err).NotTo(HaveOccurred())
-			helpers.EventuallyResourceDeleted(func() (resources.InputResource, error) {
+			helpers.EventuallyResourceAccepted(func() (resources.InputResource, error) {
 				return resourceClientSet.VirtualServiceClient().Read(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.ReadOpts{})
 			})
-
-			err = resourceClientSet.ServiceClient().Delete(serviceRef.Namespace, serviceRef.Name, clients.DeleteOpts{
-				IgnoreNotExist: true,
-			})
-			Expect(err).NotTo(HaveOccurred())
-			Eventually(func() bool {
-				_, err := resourceClientSet.ServiceClient().Read(serviceRef.Namespace, serviceRef.Name, clients.ReadOpts{})
-				// we should receive a DNE error, meaning it's now deleted
-				return err != nil && skerrors.IsNotExist(err)
-			}, "5s", "1s").Should(BeTrue())
-
-			err = resourceClientSet.UpstreamClient().Delete(upstreamRef.Namespace, upstreamRef.Name, clients.DeleteOpts{
-				IgnoreNotExist: true,
-			})
-			helpers.EventuallyResourceDeleted(func() (resources.InputResource, error) {
-				return resourceClientSet.UpstreamClient().Read(upstreamRef.Namespace, upstreamRef.Name, clients.ReadOpts{})
-			})
-		})
+		}
 
 		table.DescribeTable("should act as expected with varied ports", func(port int32, targetPort int, expected int) {
 			setupServices(port, targetPort)
@@ -165,5 +170,100 @@ var _ = Describe("Gloo + Istio integration tests", func() {
 			table.Entry("without target port, and port not matching app's port", int32(helper.TestRunnerPort+1), -1, http.StatusServiceUnavailable),
 			table.Entry("pointing to the wrong target port", int32(8000), helper.TestRunnerPort+1, http.StatusServiceUnavailable), // or maybe 404?
 		)
+	})
+
+	Context("headless services", func() {
+		BeforeEach(func() {
+			serviceRef = core.ResourceRef{Name: "headless-svc", Namespace: "gloo-system"}
+			virtualServiceRef = core.ResourceRef{Name: "headless-vs", Namespace: "gloo-system"}
+
+			// create a headless service routed to testrunner
+			service := corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      serviceRef.Name,
+					Namespace: serviceRef.Namespace,
+				},
+				Spec: corev1.ServiceSpec{
+					ClusterIP: "None",
+					Ports: []corev1.ServicePort{
+						{
+							Port:     helper.TestRunnerPort,
+							Protocol: corev1.ProtocolTCP,
+						},
+					},
+					Selector: map[string]string{"gloo": "testrunner"},
+				},
+			}
+			var err error
+			_, err = resourceClientSet.ServiceClient().Write(
+				&kubernetes.Service{Service: kubeService.Service{Service: service}},
+				clients.WriteOpts{},
+			)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(func() error {
+				_, err := resourceClientSet.ServiceClient().Read(serviceRef.Namespace, serviceRef.Name, clients.ReadOpts{})
+				return err
+			}, "5s", "1s").Should(BeNil())
+
+			// the upstream should be created by discovery service
+			upstreamRef = core.ResourceRef{
+				Name:      fmt.Sprintf("%s-%s-%d", serviceRef.Namespace, serviceRef.Name, helper.TestRunnerPort),
+				Namespace: "gloo-system",
+			}
+			helpers.EventuallyResourceAccepted(func() (resources.InputResource, error) {
+				return resourceClientSet.UpstreamClient().Read(upstreamRef.Namespace, upstreamRef.Name, clients.ReadOpts{})
+			})
+
+			// create virtual service routing to the headless service's upstream
+			virtualService := &v1.VirtualService{
+				Metadata: &core.Metadata{
+					Name:      virtualServiceRef.Name,
+					Namespace: virtualServiceRef.Namespace,
+				},
+				VirtualHost: &v1.VirtualHost{
+					Domains: []string{"headless.local"},
+					Routes: []*v1.Route{{
+						Action: &v1.Route_RouteAction{
+							RouteAction: &gloov1.RouteAction{
+								Destination: &gloov1.RouteAction_Single{
+									Single: &gloov1.Destination{
+										DestinationType: &gloov1.Destination_Upstream{
+											Upstream: &upstreamRef,
+										},
+									},
+								},
+							},
+						},
+						Matchers: []*matchers.Matcher{
+							{
+								PathSpecifier: &matchers.Matcher_Prefix{
+									Prefix: "/",
+								},
+							},
+						},
+					}},
+				},
+			}
+			_, err = resourceClientSet.VirtualServiceClient().Write(virtualService, clients.WriteOpts{})
+			Expect(err).NotTo(HaveOccurred())
+			helpers.EventuallyResourceAccepted(func() (resources.InputResource, error) {
+				return resourceClientSet.VirtualServiceClient().Read(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.ReadOpts{})
+			})
+		})
+
+		It("routes to headless services", func() {
+			testHelper.CurlEventuallyShouldRespond(helper.CurlOpts{
+				Protocol:          "http",
+				Path:              "/",
+				Method:            "GET",
+				Host:              "headless.local",
+				Service:           gatewayProxy,
+				Port:              gatewayPort,
+				ConnectionTimeout: 10,
+				Verbose:           false,
+				WithoutStats:      true,
+				ReturnHeaders:     true,
+			}, fmt.Sprintf("HTTP/1.1 %d", http.StatusOK), 1, time.Minute*1)
+		})
 	})
 })
