ab4b67b24b93b068172930c010ad8b73b4acc7cc
Fabian Gonzalez <fabian.gonzalez@solo.io>
2023-02-12 19:58:30 -0500

    [1.13] Fix Istio Crash When Using Headless Services (#7835)
    * Fix Istio Crash When Using Headless Services (#7804)
    
    * proposal fix
    
    * add changelog
    
    * move istioIntegration logic back out of eps loop
    
    * continue when setting istioIntegration ep
    
    * fix validation webhook start condition
    
    * add test for headless service curl-ing
    
    * remove focused test
    
    * empty commit
    
    * move headless service setup into beforeEach
    
    * add issue link
    
    * move changelog
    
    * Update changelog/v1.14.0-beta10/fix-istio-integration-headless-service-crash.yaml
    
    Co-authored-by: Kevin Dorosh <kevin.dorosh@solo.io>
    
    * add comment with issue link for clusterIP deprecation
    
    ---------
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: Kevin Dorosh <kevin.dorosh@solo.io>
    
    * move changelog
    
    ---------
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: Kevin Dorosh <kevin.dorosh@solo.io>

--- projects/gloo/pkg/plugins/kubernetes/eds.go
+++ projects/gloo/pkg/plugins/kubernetes/eds.go
@@ -269,6 +269,8 @@ type Epkey struct {
 	Name        string
 	Namespace   string
 	UpstreamRef *core.ResourceRef
+	// While we can use the upstream ref to get the upstream and service, if there are too many upstreams that could be slow.
+	IsHeadless bool
 }
 
 // Returns first matching port in the namespace and boolean value of true if the
@@ -286,6 +288,15 @@ func findPortForService(services []*kubev1.Service, spec *kubeplugin.UpstreamSpe
 		}
 	}
 	return nil, false
+}
+
+func getServiceFromUpstreamSpec(spec *kubeplugin.UpstreamSpec, services []*kubev1.Service) *kubev1.Service {
+	for _, svc := range services {
+		if svc.Namespace == spec.GetServiceNamespace() && svc.Name == spec.GetServiceName() {
+			return svc
+		}
+	}
+	return nil
 }
 
 func filterEndpoints(
@@ -312,8 +323,12 @@ func filterEndpoints(
 			continue
 		}
 
+		svc := getServiceFromUpstreamSpec(spec, services)
+		// TODO: Investigate possible deprecation of ClusterIPs in newer k8s versions https://github.com/solo-io/gloo/issues/7830
+		isHeadlessSvc := svc.Spec.ClusterIP == "None"
 		// Istio uses the service's port for routing requests
-		if istioIntegrationEnabled {
+		// Headless services don't have a cluster IP, so we'll resort to pod IP endpoints
+		if istioIntegrationEnabled && !isHeadlessSvc {
 			hostname := fmt.Sprintf("%v.%v", spec.GetServiceName(), spec.GetServiceNamespace())
 			copyRef := *usRef
 			key := Epkey{
@@ -322,6 +337,7 @@ func filterEndpoints(
 				Name:        spec.GetServiceName(),
 				Namespace:   spec.GetServiceNamespace(),
 				UpstreamRef: &copyRef,
+				IsHeadless:  isHeadlessSvc,
 			}
 			endpointsMap[key] = append(endpointsMap[key], &copyRef)
 			continue
@@ -339,7 +355,7 @@ func filterEndpoints(
 					continue
 				}
 
-				warnings := processSubsetAddresses(subset, spec, podMap, usRef, port, endpointsMap)
+				warnings := processSubsetAddresses(subset, spec, podMap, usRef, port, endpointsMap, isHeadlessSvc)
 				warnsToLog = append(warnsToLog, warnings...)
 			}
 		}
@@ -350,7 +366,7 @@ func filterEndpoints(
 	return endpoints, warnsToLog, errorsToLog
 }
 
-func processSubsetAddresses(subset kubev1.EndpointSubset, spec *kubeplugin.UpstreamSpec, pods *podMap, usRef *core.ResourceRef, port uint32, endpointsMap map[Epkey][]*core.ResourceRef) []string {
+func processSubsetAddresses(subset kubev1.EndpointSubset, spec *kubeplugin.UpstreamSpec, pods *podMap, usRef *core.ResourceRef, port uint32, endpointsMap map[Epkey][]*core.ResourceRef, isHeadlessService bool) []string {
 	var warnings []string
 	for _, addr := range subset.Addresses {
 		var podName, podNamespace string
@@ -377,7 +393,7 @@ func processSubsetAddresses(subset kubev1.EndpointSubset, spec *kubeplugin.Upstr
 				continue
 			}
 		}
-		key := Epkey{addr.IP, port, podName, podNamespace, usRef}
+		key := Epkey{addr.IP, port, podName, podNamespace, usRef, isHeadlessService}
 		copyRef := *usRef
 		endpointsMap[key] = append(endpointsMap[key], &copyRef)
 	}
@@ -425,7 +441,8 @@ func generateFilteredEndpointList(
 		endpointName := fmt.Sprintf("ep-%v-%v-%x", dnsname, addr.Port, hasher.Sum64())
 
 		var ep *v1.Endpoint
-		if istioIntegrationEnabled {
+		// While istio integration requires the Service VIP, headless services require the pod IP, as there is no Cluster IP
+		if istioIntegrationEnabled && !addr.IsHeadless {
 			// Istio integration requires assigning endpoints the Kub service VIP rather than pod address
 			service, _ := getServiceForHostname(addr.Address, addr.Name, addr.Namespace, services)
 			ep = createEndpoint(writeNamespace, endpointName, refs, service.Spec.ClusterIP, addr.Port, service.GetObjectMeta().GetLabels()) // TODO: labels may be nil
