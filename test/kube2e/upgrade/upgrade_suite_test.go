cf8ff9b58e4354cd73a0e18bdb4bea1494e77205
ianmacclancy <ianmacclancy@gmail.com>
2023-03-09 09:13:15 -0500

    1.13 - backport upgrade tests (#7929)
    * backport upgrade tests
    
    * backport upgrade tests - add changelog
    
    * cleanup go sum
    
    * remove anachronistic env variables

--- test/kube2e/upgrade/upgrade_suite_test.go
+++ test/kube2e/upgrade/upgrade_suite_test.go
@@ -1,24 +1,72 @@
 package upgrade_test
 
 import (
-	"os"
+	"context"
+	"fmt"
+	"path/filepath"
+	"strings"
 	"testing"
 
+	. "github.com/onsi/gomega"
+	"github.com/solo-io/gloo/test/kube2e"
+	"github.com/solo-io/gloo/test/kube2e/upgrade"
+	"github.com/solo-io/go-utils/versionutils"
+	"github.com/solo-io/skv2/codegen/util"
+
 	. "github.com/onsi/ginkgo"
-	"github.com/onsi/ginkgo/reporters"
 	"github.com/solo-io/gloo/test/helpers"
-	"github.com/solo-io/go-utils/log"
 	skhelpers "github.com/solo-io/solo-kit/test/helpers"
 )
 
 func TestUpgrade(t *testing.T) {
-	if os.Getenv("KUBE2E_TESTS") != "upgrade" {
-		log.Warnf("This test is disabled. To enable, set KUBE2E_TESTS to 'upgrade' in your env.")
-		return
-	}
 	helpers.RegisterGlooDebugLogPrintHandlerAndClearLogs()
 	skhelpers.RegisterCommonFailHandlers()
 	skhelpers.SetupLog()
-	junitReporter := reporters.NewJUnitReporter("junit.xml")
-	RunSpecsWithDefaultAndCustomReporters(t, "Upgrade Suite", []Reporter{junitReporter})
+	RunSpecs(t, "Upgrade Suite")
 }
+
+var (
+	suiteCtx    context.Context
+	suiteCancel context.CancelFunc
+
+	crdDir                string
+	chartUri              string
+	targetReleasedVersion string
+
+	// Versions to upgrade from
+	// ex: current branch is 1.13.10 - this would be the latest patch release of 1.12
+	LastPatchMostRecentMinorVersion *versionutils.Version
+
+	// ex:current branch is 1.13.10 - this would be 1.13.9
+	CurrentPatchMostRecentMinorVersion *versionutils.Version
+	firstReleaseOfMinor                bool
+)
+
+var _ = BeforeSuite(func() {
+	suiteCtx, suiteCancel = context.WithCancel(context.Background())
+
+	testHelper, err := kube2e.GetTestHelper(suiteCtx, namespace)
+	Expect(err).NotTo(HaveOccurred())
+
+	crdDir = filepath.Join(util.GetModuleRoot(), "install", "helm", "gloo", "crds")
+	targetReleasedVersion = kube2e.GetTestReleasedVersion(suiteCtx, "gloo")
+	if targetReleasedVersion != "" {
+		chartUri = "gloo/gloo"
+	} else {
+		chartUri = filepath.Join(testHelper.RootDir, testHelper.TestAssetDir, testHelper.HelmChartName+"-"+testHelper.ChartVersion()+".tgz")
+	}
+
+	LastPatchMostRecentMinorVersion, CurrentPatchMostRecentMinorVersion, err = upgrade.GetUpgradeVersions(suiteCtx, "gloo")
+	if err != nil {
+		if strings.Contains(err.Error(), upgrade.FirstReleaseError) {
+			firstReleaseOfMinor = true
+		} else {
+			fmt.Println(err.Error())
+			Fail(err.Error())
+		}
+	}
+})
+
+var _ = AfterSuite(func() {
+	suiteCancel()
+})
