be0711dee33d80d6056c2e530f5bf96d60f8d5e5
Jacob Bohanon <57016439+jbohanon@users.noreply.github.com>
2023-04-13 15:49:43 -0400

    Graphql per resolver timeout API (#8085)
    * add per-resolution timeout to graphql API
    
    * codegen
    
    * add delay endpoint to test upstream
    
    * add capability for test upstream server to sleep, added response chan to test upstream
    
    * revise API
    
    * codegen
    
    * Update graphql.proto
    
    * Update test/v1helpers/test_upstream.go
    
    * codegen
    
    * changelog & codegen
    
    ---------
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>

--- projects/gloo/pkg/api/v1/enterprise/options/graphql/v1beta1/graphql.pb.hash.go
+++ projects/gloo/pkg/api/v1/enterprise/options/graphql/v1beta1/graphql.pb.hash.go
@@ -294,6 +294,26 @@ func (m *RESTResolver) Hash(hasher hash.Hash64) (uint64, error) {
 		return 0, err
 	}
 
+	if h, ok := interface{}(m.GetTimeout()).(safe_hasher.SafeHasher); ok {
+		if _, err = hasher.Write([]byte("Timeout")); err != nil {
+			return 0, err
+		}
+		if _, err = h.Hash(hasher); err != nil {
+			return 0, err
+		}
+	} else {
+		if fieldValue, err := hashstructure.Hash(m.GetTimeout(), nil); err != nil {
+			return 0, err
+		} else {
+			if _, err = hasher.Write([]byte("Timeout")); err != nil {
+				return 0, err
+			}
+			if err := binary.Write(hasher, binary.LittleEndian, fieldValue); err != nil {
+				return 0, err
+			}
+		}
+	}
+
 	return hasher.Sum64(), nil
 }
 
@@ -406,6 +426,26 @@ func (m *GrpcResolver) Hash(hasher hash.Hash64) (uint64, error) {
 
 	if _, err = hasher.Write([]byte(m.GetSpanName())); err != nil {
 		return 0, err
+	}
+
+	if h, ok := interface{}(m.GetTimeout()).(safe_hasher.SafeHasher); ok {
+		if _, err = hasher.Write([]byte("Timeout")); err != nil {
+			return 0, err
+		}
+		if _, err = h.Hash(hasher); err != nil {
+			return 0, err
+		}
+	} else {
+		if fieldValue, err := hashstructure.Hash(m.GetTimeout(), nil); err != nil {
+			return 0, err
+		} else {
+			if _, err = hasher.Write([]byte("Timeout")); err != nil {
+				return 0, err
+			}
+			if err := binary.Write(hasher, binary.LittleEndian, fieldValue); err != nil {
+				return 0, err
+			}
+		}
 	}
 
 	return hasher.Sum64(), nil
