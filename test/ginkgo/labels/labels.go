708daa485547ab40d015a68815a953a3b8156806
Bernie Birnbaum <bewebi@earthlink.net>
2023-05-30 16:48:35 -0400

    Performance tests job in nightly action (#8298)
    * nightly job first pass
    
    * add setup step
    
    * fix Make target
    
    * fix artifact location
    
    * experimentation
    
    * fix targets
    
    * comment pkg-specified steps
    
    * Revert "comment pkg-specified steps"
    
    This reverts commit 1849b3e5c8cf727fe9921f8694f72d54793aa5b2.
    
    * Revert "fix targets"
    
    This reverts commit fc89b0c8ed9bbd3042b993eff6f70c76d2f6c5d9.
    
    * Revert "experimentation"
    
    This reverts commit 78c8890a663d76603956b6d2f0ec6c952162da81.
    
    * changelog
    
    * dev -> main
    
    * remove Nightly label
    
    * newline
    
    * do not skip tests
    
    ---------
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>

--- test/ginkgo/labels/labels.go
+++ test/ginkgo/labels/labels.go
@@ -3,9 +3,6 @@ package labels
 // Ginkgo Spec Labels (https://onsi.github.io/ginkgo/#spec-labels)
 
 const (
-	// Nightly is a label applied to any tests which should run during our nightly tests and not during PRs
-	Nightly = "nightly"
-
 	// Performance is a label applied to any tests which run performance tests
 	// These often require more resources/time to complete, and likely report their findings to a remote location
 	Performance = "performance"
