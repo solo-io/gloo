18f3679cf736289816ed97b23068b2f170b58d8f
nfuden <nathan.fudenberg@solo.io>
2023-06-20 10:47:38 -0400

    test/translator: update to have correct import

--- projects/gloo/pkg/translator/performance_linux_test.go
+++ projects/gloo/pkg/translator/performance_linux_test.go
@@ -32,11 +32,11 @@ import (
 	"github.com/onsi/gomega/gmeasure"
 	. "github.com/solo-io/gloo/projects/gloo/pkg/translator"
 
-	"time"
-
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	v1snap "github.com/solo-io/gloo/projects/gloo/pkg/api/v1/gloosnapshot"
+	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/ssl"
 	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
+	"time"
 )
 
 // benchmarkConfig allows configuration for benchmarking tests to be reused for similar cases
