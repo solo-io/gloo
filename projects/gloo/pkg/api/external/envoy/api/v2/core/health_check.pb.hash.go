cdfdb14afe9cd79fbf5adf73e07c09c48fd181b4
David Jumani <dj.davidjumani1994@gmail.com>
2023-08-01 17:03:23 -0400

    backport: Expose initial_metadata in GrpcHealthCheck [1.14] (#8530)
    * feat: Expose initial_metadata in GrpcHealthCheck (#8511)
    
    * feat: Expose initial_metadata in GrpcHealthCheck
    
    * Trigger CI
    
    * Fix uneven whitespace
    
    Co-authored-by: Nathan Fudenberg <nathan.fudenberg@solo.io>
    
    * Adding changelog file to new location
    
    * Avoid code duplcation
    
    * Removing duplicate changelog
    
    * fix test
    
    * Add check for forbidden custom headers
    
    ---------
    
    Co-authored-by: David Jumani <djumani@Solo-System-DJumani.local>
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: Nathan Fudenberg <nathan.fudenberg@solo.io>
    Co-authored-by: changelog-bot <changelog-bot>
    
    * Move changelog
    
    * Update changelog
    
    ---------
    
    Co-authored-by: David Jumani <djumani@Solo-System-DJumani.local>
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: Nathan Fudenberg <nathan.fudenberg@solo.io>

--- projects/gloo/pkg/api/external/envoy/api/v2/core/health_check.pb.hash.go
+++ projects/gloo/pkg/api/external/envoy/api/v2/core/health_check.pb.hash.go
@@ -612,6 +612,30 @@ func (m *HealthCheck_GrpcHealthCheck) Hash(hasher hash.Hash64) (uint64, error) {
 		return 0, err
 	}
 
+	for _, v := range m.GetInitialMetadata() {
+
+		if h, ok := interface{}(v).(safe_hasher.SafeHasher); ok {
+			if _, err = hasher.Write([]byte("")); err != nil {
+				return 0, err
+			}
+			if _, err = h.Hash(hasher); err != nil {
+				return 0, err
+			}
+		} else {
+			if fieldValue, err := hashstructure.Hash(v, nil); err != nil {
+				return 0, err
+			} else {
+				if _, err = hasher.Write([]byte("")); err != nil {
+					return 0, err
+				}
+				if err := binary.Write(hasher, binary.LittleEndian, fieldValue); err != nil {
+					return 0, err
+				}
+			}
+		}
+
+	}
+
 	return hasher.Sum64(), nil
 }
 
