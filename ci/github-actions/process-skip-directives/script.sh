1d5a5a929b1b1ec6347cbe7ed61bae1e0e60c800
David Jumani <dj.davidjumani1994@gmail.com>
2023-08-18 14:16:46 -0400

    ci: Fix skip directives script failing on merge (#8597)
    * ci: Fix skip directives script failing on merge
    
    * remove double space
    
    Co-authored-by: Fabian Gonzalez <fabian.gonzalez@solo.io>
    
    * Selectively do not error on exit
    
    * Applying suggestions
    
    * Add comment on missing githubbaseref
    
    ---------
    
    Co-authored-by: Fabian Gonzalez <fabian.gonzalez@solo.io>
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>

--- ci/github-actions/process-skip-directives/script.sh
+++ ci/github-actions/process-skip-directives/script.sh
@@ -22,19 +22,25 @@ skipDocsBuildDirective="skipCI-docs-build:true"
 shouldSkipDocsBuild=false
 
 githubBaseRef=$1
-
-changelog=$(git diff origin/$githubBaseRef HEAD --name-only | grep "changelog/")
-if [[ $(echo $changelog | wc -l | tr -d ' ') = "1" ]]; then
-    echo "exactly one changelog added since main"
-    echo "changelog file name == $changelog"
-    if [[ $(cat $changelog | grep $skipKubeTestsDirective) ]]; then
-        shouldSkipKubeTests=true
+# If `githubBaseRef` is not present, it means that this script is not running as part of a PR (probably running on a push to main or an LTS branch).
+# In that case we ignore the skip directives since we need to run CI
+if [ ! -z "$githubBaseRef" ]; then
+    # If there is no changelog found, the grep command fails and in turn the entire script exits since the error on exit flag has been set
+    # To avoid that, we are using `|| true` to ensure that even if there is no changelog, it doesn't exit
+    changelog=$(git diff origin/$githubBaseRef HEAD --name-only | grep "changelog/" || true)
+    # An empty string is also one line in bash. Hence adding the first check
+    if [ ! -z "$changelog" ] && [[ $(echo $changelog | wc -l | tr -d ' ') = "1" ]]; then
+        echo "exactly one changelog added since main"
+        echo "changelog file name == $changelog"
+        if [[ $(cat $changelog | grep $skipKubeTestsDirective) ]]; then
+            shouldSkipKubeTests=true
+        fi
+        if [[ $(cat $changelog | grep $skipDocsBuildDirective) ]]; then
+            shouldSkipDocsBuild=true
+        fi
+    else
+        echo "no changelog found (or more than one changelog found) - not skipping CI"
     fi
-    if [[ $(cat $changelog | grep $skipDocsBuildDirective) ]]; then
-        shouldSkipDocsBuild=true
-    fi
-else
-    echo "no changelog found (or more than one changelog found) - not skipping CI"
 fi
 
 echo "skip-kube-tests=${shouldSkipKubeTests}" >> $GITHUB_OUTPUT
