f2a6d053fd69ef395fc805bcd86b8973aafb0613
nfuden <nathan.fudenberg@solo.io>
2023-09-01 08:42:05 -0400

    extproc use our go control-plane fork for now

--- docs/examples/grpc-ext-proc/basic-sink/go.mod
+++ docs/examples/grpc-ext-proc/basic-sink/go.mod
@@ -4,9 +4,24 @@ go 1.20
 
 require (
 	github.com/envoyproxy/go-control-plane v0.11.1
-	github.com/solo-io/gloo v1.15.0-rc2
 	github.com/solo-io/go-utils v0.24.6
-	google.golang.org/grpc v1.57.0
+	google.golang.org/grpc v1.56.0-dev
+)
+
+require (
+	github.com/cncf/xds/go v0.0.0-20230428030218-4003588d1b74 // indirect
+	github.com/envoyproxy/protoc-gen-validate v1.0.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/k0kubun/pp v2.3.0+incompatible // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230526203410-71b5a4ffd15e // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 )
 
 replace github.com/solo-io/gloo => ./../../../..
+
+replace github.com/envoyproxy/go-control-plane => github.com/solo-io/go-control-plane-fork-v2 v0.0.0-20230820180611-2cf7e0da27c8
--- docs/examples/grpc-ext-proc/basic-sink/main.go
+++ docs/examples/grpc-ext-proc/basic-sink/main.go
@@ -7,30 +7,28 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"flag"
 	"io"
 	"net"
 	"os"
 	"os/signal"
-	"strconv"
-	"strings"
 	"syscall"
 	"time"
 
 	"github.com/solo-io/go-utils/log"
 
-	core "github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/config/core/v3"
+	// core "github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/config/core/v3"
 
+	pb "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
+	_struct "github.com/golang/protobuf/ptypes/struct"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/health"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/status"
-
-	v3 "github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/extensions/filters/http/ext_proc/v3"
-	// pb "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
-	pb "github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/service/ext_proc/v3"
+	structpb "google.golang.org/protobuf/types/known/structpb"
+	// v3 "github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/extensions/filters/http/ext_proc/v3"
+	// pb "github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/service/ext_proc/v3"
 )
 
 var (
@@ -75,154 +73,161 @@ func (s *server) Process(srv pb.ExternalProcessor_ProcessServer) error {
 
 		// clean the response struct
 		resp := &pb.ProcessingResponse{}
-
+		log.Printf("%v", req.Request)
+		log.Printf("%v", req.GetMetadataContext().FilterMetadata)
 		switch v := req.Request.(type) {
 		case *pb.ProcessingRequest_RequestHeaders:
 			log.Printf("pb.ProcessingRequest_RequestHeaders %v \n", v)
-			r := req.Request
-			h := r.(*pb.ProcessingRequest_RequestHeaders)
 
-			instructionString := ""
-			for _, n := range h.RequestHeaders.Headers.Headers {
-				if n.Key == "instructions" {
-					instructionString = n.Value
-				}
-			}
+			// respHeaders := &pb.HeadersResponse{Response: &pb.CommonResponse{HeaderMutation: mutates}}
 
-			if instructionString == "skip" {
-				log.Printf("  Skipping request \n")
-				return nil
-			}
-
-			instructionsOnHeaders := map[string]string{}
-			err := json.Unmarshal([]byte(instructionString), &instructionsOnHeaders)
-			if err == nil {
-
-				mutates := &pb.HeaderMutation{}
-				log.Printf("  Instructions: %v \n", instructionsOnHeaders)
-				for k, v := range instructionsOnHeaders {
-					if strings.HasPrefix(k, "scrub-header") {
-						mutates.RemoveHeaders = append(mutates.RemoveHeaders, v)
-					}
-				}
-				respHeaders := &pb.HeadersResponse{Response: &pb.CommonResponse{HeaderMutation: mutates}}
-
-				resp = &pb.ProcessingResponse{
-					Response: &pb.ProcessingResponse_RequestHeaders{
-						RequestHeaders: respHeaders,
-					},
-					ModeOverride: &v3.ProcessingMode{
-						RequestBodyMode:    v3.ProcessingMode_BUFFERED,
-						ResponseHeaderMode: v3.ProcessingMode_SKIP,
-						ResponseBodyMode:   v3.ProcessingMode_NONE,
-					},
-				}
-			}
-			break
-		case *pb.ProcessingRequest_RequestBody:
-
-			r := req.Request
-			b := r.(*pb.ProcessingRequest_RequestBody)
-			log.Printf("   RequestBody: %s", string(b.RequestBody.Body))
-			log.Printf("   EndOfStream: %T", b.RequestBody.EndOfStream)
-			if b.RequestBody.EndOfStream {
-
-				bytesToSend := append(b.RequestBody.Body, []byte(` baaar `)...)
-				resp = &pb.ProcessingResponse{
-					Response: &pb.ProcessingResponse_RequestBody{
-						RequestBody: &pb.BodyResponse{
-							Response: &pb.CommonResponse{
-								HeaderMutation: &pb.HeaderMutation{
-									SetHeaders: []*core.HeaderValueOption{
-										{
-											Header: &core.HeaderValue{
-												Key:   "Content-Length",
-												Value: strconv.Itoa(len(bytesToSend)),
-											},
-										},
-									},
-								},
-								BodyMutation: &pb.BodyMutation{
-									Mutation: &pb.BodyMutation_Body{
-										Body: bytesToSend,
-									},
-								},
-							},
-						},
-					},
-					ModeOverride: &v3.ProcessingMode{
-						ResponseHeaderMode: v3.ProcessingMode_SEND,
-						ResponseBodyMode:   v3.ProcessingMode_NONE,
-					},
-				}
-			}
-			break
-		case *pb.ProcessingRequest_ResponseHeaders:
-			log.Printf("pb.ProcessingRequest_ResponseHeaders %v \n", v)
-			r := req.Request
-			h := r.(*pb.ProcessingRequest_ResponseHeaders)
-
-			responseSize := 0
-			for _, n := range h.ResponseHeaders.Headers.Headers {
-				if n.Key == "content-length" {
-					responseSize, _ = strconv.Atoi(n.Value)
-					break
-				}
-			}
-
-			log.Printf("  Removing access-control-allow-* headers \n")
-			rhq := &pb.HeadersResponse{
-				Response: &pb.CommonResponse{
-					HeaderMutation: &pb.HeaderMutation{
-						RemoveHeaders: []string{"access-control-allow-origin", "access-control-allow-credentials"},
-						SetHeaders: []*core.HeaderValueOption{
-							{
-								Header: &core.HeaderValue{
-									Key:   "content-type",
-									Value: "text/plain",
-								},
-							},
-							{
-								Header: &core.HeaderValue{
-									Key:   "content-length",
-									Value: strconv.Itoa(responseSize + len([]byte(` qux`))),
-								},
-							},
-						},
-					},
-				},
-			}
 			resp = &pb.ProcessingResponse{
-				Response: &pb.ProcessingResponse_ResponseHeaders{
-					ResponseHeaders: rhq,
-				},
-				ModeOverride: &v3.ProcessingMode{
-					ResponseBodyMode: v3.ProcessingMode_BUFFERED,
-				},
-			}
-			break
-		case *pb.ProcessingRequest_ResponseBody:
-			log.Printf("pb.ProcessingRequest_ResponseBody %v \n", v)
-			r := req.Request
-			b := r.(*pb.ProcessingRequest_ResponseBody)
-			if b.ResponseBody.EndOfStream {
-				bytesToSend := append(b.ResponseBody.Body, []byte(` qux`)...)
-				resp = &pb.ProcessingResponse{
-					Response: &pb.ProcessingResponse_ResponseBody{
-						ResponseBody: &pb.BodyResponse{
-							Response: &pb.CommonResponse{
-								BodyMutation: &pb.BodyMutation{
-									Mutation: &pb.BodyMutation_Body{
-										Body: bytesToSend,
-									},
-								},
-							},
-						},
-					},
-				}
+				DynamicMetadata: &_struct.Struct{Fields: map[string]*structpb.Value{
+					"metaKey": &structpb.Value{Kind: &structpb.Value_StringValue{StringValue: "hello from meta"}},
+				}},
+				Response: &pb.ProcessingResponse_RequestHeaders{},
 			}
 
-			break
+			// instructionString := ""
+			// for _, n := range h.RequestHeaders.Headers.Headers {
+			// 	if n.Key == "instructions" {
+			// 		instructionString = n.Value
+			// 	}
+			// }
+
+			// if instructionString == "skip" {
+			// 	log.Printf("  Skipping request \n")
+			// 	return nil
+			// }
+
+			// instructionsOnHeaders := map[string]string{}
+			// err := json.Unmarshal([]byte(instructionString), &instructionsOnHeaders)
+			// if err == nil {
+
+			// 	mutates := &pb.HeaderMutation{}
+			// 	log.Printf("  Instructions: %v \n", instructionsOnHeaders)
+			// 	for k, v := range instructionsOnHeaders {
+			// 		if strings.HasPrefix(k, "scrub-header") {
+			// 			mutates.RemoveHeaders = append(mutates.RemoveHeaders, v)
+			// 		}
+			// 	}
+			// 	respHeaders := &pb.HeadersResponse{Response: &pb.CommonResponse{HeaderMutation: mutates}}
+
+			// 	resp = &pb.ProcessingResponse{
+			// 		Response: &pb.ProcessingResponse_RequestHeaders{
+			// 			RequestHeaders: respHeaders,
+			// 		},
+			// 		ModeOverride: &v3.ProcessingMode{
+			// 			RequestBodyMode:    v3.ProcessingMode_BUFFERED,
+			// 			ResponseHeaderMode: v3.ProcessingMode_SKIP,
+			// 			ResponseBodyMode:   v3.ProcessingMode_NONE,
+			// 		},
+			// 	}
+			// }
+		// case *pb.ProcessingRequest_RequestBody:
+
+		// 	r := req.Request
+		// 	b := r.(*pb.ProcessingRequest_RequestBody)
+		// 	log.Printf("   RequestBody: %s", string(b.RequestBody.Body))
+		// 	log.Printf("   EndOfStream: %T", b.RequestBody.EndOfStream)
+		// 	if b.RequestBody.EndOfStream {
+
+		// 		bytesToSend := append(b.RequestBody.Body, []byte(` baaar `)...)
+		// 		resp = &pb.ProcessingResponse{
+		// 			Response: &pb.ProcessingResponse_RequestBody{
+		// 				RequestBody: &pb.BodyResponse{
+		// 					Response: &pb.CommonResponse{
+		// 						HeaderMutation: &pb.HeaderMutation{
+		// 							SetHeaders: []*core.HeaderValueOption{
+		// 								{
+		// 									Header: &core.HeaderValue{
+		// 										Key:   "Content-Length",
+		// 										Value: strconv.Itoa(len(bytesToSend)),
+		// 									},
+		// 								},
+		// 							},
+		// 						},
+		// 						BodyMutation: &pb.BodyMutation{
+		// 							Mutation: &pb.BodyMutation_Body{
+		// 								Body: bytesToSend,
+		// 							},
+		// 						},
+		// 					},
+		// 				},
+		// 			},
+		// 			ModeOverride: &v3.ProcessingMode{
+		// 				ResponseHeaderMode: v3.ProcessingMode_SEND,
+		// 				ResponseBodyMode:   v3.ProcessingMode_NONE,
+		// 			},
+		// 		}
+		// 	}
+		// 	break
+		// case *pb.ProcessingRequest_ResponseHeaders:
+		// 	log.Printf("pb.ProcessingRequest_ResponseHeaders %v \n", v)
+		// 	r := req.Request
+		// 	h := r.(*pb.ProcessingRequest_ResponseHeaders)
+
+		// 	responseSize := 0
+		// 	for _, n := range h.ResponseHeaders.Headers.Headers {
+		// 		if n.Key == "content-length" {
+		// 			responseSize, _ = strconv.Atoi(n.Value)
+		// 			break
+		// 		}
+		// 	}
+
+		// 	log.Printf("  Removing access-control-allow-* headers \n")
+		// 	rhq := &pb.HeadersResponse{
+		// 		Response: &pb.CommonResponse{
+		// 			HeaderMutation: &pb.HeaderMutation{
+		// 				RemoveHeaders: []string{"access-control-allow-origin", "access-control-allow-credentials"},
+		// 				SetHeaders: []*core.HeaderValueOption{
+		// 					{
+		// 						Header: &core.HeaderValue{
+		// 							Key:   "content-type",
+		// 							Value: "text/plain",
+		// 						},
+		// 					},
+		// 					{
+		// 						Header: &core.HeaderValue{
+		// 							Key:   "content-length",
+		// 							Value: strconv.Itoa(responseSize + len([]byte(` qux`))),
+		// 						},
+		// 					},
+		// 				},
+		// 			},
+		// 		},
+		// 	}
+		// 	resp = &pb.ProcessingResponse{
+		// 		Response: &pb.ProcessingResponse_ResponseHeaders{
+		// 			ResponseHeaders: rhq,
+		// 		},
+		// 		ModeOverride: &v3.ProcessingMode{
+		// 			ResponseBodyMode: v3.ProcessingMode_BUFFERED,
+		// 		},
+		// 	}
+		// 	break
+		// case *pb.ProcessingRequest_ResponseBody:
+		// 	log.Printf("pb.ProcessingRequest_ResponseBody %v \n", v)
+		// 	r := req.Request
+		// 	b := r.(*pb.ProcessingRequest_ResponseBody)
+		// 	if b.ResponseBody.EndOfStream {
+		// 		bytesToSend := append(b.ResponseBody.Body, []byte(` qux`)...)
+		// 		resp = &pb.ProcessingResponse{
+		// 			Response: &pb.ProcessingResponse_ResponseBody{
+		// 				ResponseBody: &pb.BodyResponse{
+		// 					Response: &pb.CommonResponse{
+		// 						BodyMutation: &pb.BodyMutation{
+		// 							Mutation: &pb.BodyMutation_Body{
+		// 								Body: bytesToSend,
+		// 							},
+		// 						},
+		// 					},
+		// 				},
+		// 			},
+		// 		}
+		// 	}
+
+		// 	break
 		default:
 			log.Printf("Unknown Request type %v\n", v)
 		}
