47cb35f1c856ce11bbad92f33e877ed9f43f8afd
Fabian Gonzalez <fabian.gonzalez@solo.io>
2023-09-18 10:41:50 -0400

    [1.15] Add Vault + AWS IRSA Integration Docs (#8697)
    * make vault's aws key id + secret optional
    
    * update vault's aws proto secret + id description
    
    * expose helm secretOptions values in gloo templates
    
    * add helm test for secretOptions
    
    * add changelog
    
    * add issue link
    
    * remove unecessary comments
    
    * reformat vault+aws tests to prepare for sts e2e test
    
    * uncomment sts test vault + aws
    
    * add skipCI-kube-tests:true directive
    
    * noop
    
    * update sts vault secret settings
    
    * remove local aws creds profile
    
    * uncomment vaultRole for sts test
    
    * updates
    
    * Skip() sts tests
    
    * remove comment in tests
    
    * move changelog
    
    * review comment fixes
    
    * update helm secret options logic + generate code
    
    * add section on temporary credentials in aws test readme
    
    * add initial kube2e tests
    
    * add comment to vault_test kube2e
    
    * update comment in test
    
    * update role used in vault kube2e tests
    
    * noop
    
    * noop
    
    * remove lingering changelog
    
    * only run vault kube2e test
    
    * update setup
    
    * update vault_test to follow setup
    
    * fix vault test helm template
    
    * update vault_test to be closer to setup key creation + enable secret engine
    
    * add vault integration (irsa) docs
    
    * Update vault integration docs
    
    * update vault integration md
    
    * Update Vault main page with information regarding Vault secret managing benefits
    
    * Apply suggestions from code review
    
    
    
    * Update docs/content/guides/integrations/vault/aws-irsa-auth/_index.md
    
    
    
    * Update docs/content/guides/integrations/vault/aws-irsa-auth/_index.md
    
    
    
    * Apply suggestions from code review
    
    
    
    * Update irsa docs step headings
    
    * Update irsa doc intro + summary
    
    * remove unused test
    
    * re-add kube2e tests
    
    * remove unused updates
    
    * add docs
    
    * codegen
    
    * revert/remove non-working vault irsa e2e tests
    
    * revert more unused changes
    
    ---------
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: Rachael Graham <rachael.graham@solo.io>

--- test/e2e/vault_aws_test.go
+++ test/e2e/vault_aws_test.go
@@ -23,10 +23,6 @@ const (
 	vaultAwsRegion = "us-east-1"
 
 	vaultRole = "vault-role"
-
-	// The x-vault-awsiam-server-id is used to validate between Vault and AWS.
-	// The value used in our Vault AWS auth settings and in Vault's client config must match.
-	iamServerIdHeader = "vault.gloo.example.com"
 )
 
 var _ = Describe("Vault Secret Store (AWS Auth)", decorators.Vault, func() {
@@ -35,7 +31,6 @@ var _ = Describe("Vault Secret Store (AWS Auth)", decorators.Vault, func() {
 		testContext         *e2e.TestContextWithVault
 		vaultSecretSettings *gloov1.Settings_VaultSecrets
 		oauthSecret         *gloov1.Secret
-		useCredentials      bool
 	)
 
 	BeforeEach(func() {
@@ -72,12 +67,7 @@ var _ = Describe("Vault Secret Store (AWS Auth)", decorators.Vault, func() {
 		testContext.RunVault()
 
 		// We need to turn on Vault AWS Auth after it has started running
-		var err error
-		if useCredentials {
-			err = testContext.VaultInstance().EnableAWSCredentialsAuthMethod(vaultSecretSettings, vaultAwsRole)
-		} else {
-			err = testContext.VaultInstance().EnableAWSSTSAuthMethod(vaultAwsRole, iamServerIdHeader, vaultAwsRegion)
-		}
+		err := testContext.VaultInstance().EnableAWSCredentialsAuthMethod(vaultSecretSettings, vaultAwsRole)
 		Expect(err).NotTo(HaveOccurred())
 
 		testContext.JustBeforeEach()
@@ -89,7 +79,6 @@ var _ = Describe("Vault Secret Store (AWS Auth)", decorators.Vault, func() {
 
 	Context("Vault Credentials", func() {
 		BeforeEach(func() {
-			useCredentials = true
 			localAwsCredentials := credentials.NewSharedCredentials("", "")
 			v, err := localAwsCredentials.Get()
 			Expect(err).NotTo(HaveOccurred(), "can load AWS shared credentials")
@@ -150,77 +139,6 @@ var _ = Describe("Vault Secret Store (AWS Auth)", decorators.Vault, func() {
 			}, "5s", ".5s").Should(Succeed())
 		})
 
-	})
-
-	// TODO(fabiangonz98): Fix these tests
-	Context("STS", func() {
-		BeforeEach(func() {
-			useCredentials = false
-			vaultSecretSettings = &gloov1.Settings_VaultSecrets{
-				Address: testContext.VaultInstance().Address(),
-				AuthMethod: &gloov1.Settings_VaultSecrets_Aws{
-					Aws: &gloov1.Settings_VaultAwsAuth{
-						IamServerIdHeader: iamServerIdHeader,
-						// TODO(fabiangonz98): Example didn't have this set
-						VaultRole: vaultRole,
-						Region:    vaultAwsRegion,
-						MountPath: "aws",
-					},
-				},
-				PathPrefix: bootstrap.DefaultPathPrefix,
-				RootKey:    bootstrap.DefaultRootKey,
-			}
-		})
-
-		// add the extra annotation
-		JustBeforeEach(func() {
-			// can't find helm settings on test start...
-			// need to patch deployment(?) to add the annotation and restart?
-			// gloo.gloo.serviceAccount.extraAnnotations: eks.amazonaws.com/role-arn: arn:aws:iam::802411188784:role/vault-role
-		})
-
-		It("can read secret using resource client", func() {
-			Skip("Need to fix permissions and complete setup")
-			Eventually(func(g Gomega) {
-				secret, err := testContext.TestClients().SecretClient.Read(
-					oauthSecret.GetMetadata().GetNamespace(),
-					oauthSecret.GetMetadata().GetName(),
-					clients.ReadOpts{
-						Ctx: testContext.Ctx(),
-					})
-				g.Expect(err).NotTo(HaveOccurred())
-				g.Expect(secret.GetOauth().GetClientSecret()).To(Equal("test"))
-			}, "5s", ".5s").Should(Succeed())
-		})
-
-		It("can pick up new secrets created by vault client ", func() {
-			Skip("Need to fix permissions and complete setup")
-			newSecret := &gloov1.Secret{
-				Metadata: &core.Metadata{
-					Name:      "new-secret",
-					Namespace: writeNamespace,
-				},
-				Kind: &gloov1.Secret_Oauth{
-					Oauth: &v1.OauthSecret{
-						ClientSecret: "new-secret",
-					},
-				},
-			}
-
-			err := testContext.VaultInstance().WriteSecret(newSecret)
-			Expect(err).NotTo(HaveOccurred())
-
-			Eventually(func(g Gomega) {
-				secret, err := testContext.TestClients().SecretClient.Read(
-					newSecret.GetMetadata().GetNamespace(),
-					newSecret.GetMetadata().GetName(),
-					clients.ReadOpts{
-						Ctx: testContext.Ctx(),
-					})
-				g.Expect(err).NotTo(HaveOccurred())
-				g.Expect(secret.GetOauth().GetClientSecret()).To(Equal("new-secret"))
-			}, "5s", ".5s").Should(Succeed())
-		})
 	})
 
 })
