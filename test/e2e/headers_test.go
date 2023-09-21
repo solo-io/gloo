6de8d8fabbf7b8959a515452efd30bde50b473d3
Fabian Gonzalez <fabian.gonzalez@solo.io>
2023-09-21 10:34:21 -0400

    Expose most_specific_header_mutations_wins (#8692)
    * add most_specific_header_mutations_wins route config option
    
    * translate and add MostSpecificHeaderMutationsWins to translator_test
    
    * add changelog
    
    * add skipCI-kube-tests:true directive
    
    * add header manipulation order e2e tests and expose routeTable in testContext's testClients
    
    * codegen
    
    * Add section in docs to include mostSpecificHeaderMutationsWins
    
    * remove skipCI-kube-tests:true directive
    
    * review comment updates
    
    * Apply suggestions from code review
    
    Co-authored-by: Art <artberger@users.noreply.github.com>
    
    * review comment updates pt 2
    
    ---------
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: Art <artberger@users.noreply.github.com>

--- test/e2e/headers_test.go
+++ test/e2e/headers_test.go
@@ -1,21 +1,27 @@
 package e2e_test
 
 import (
+	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/golang/protobuf/ptypes/wrappers"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	"github.com/solo-io/gloo/pkg/utils/api_conversion"
 	v1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1"
+	"github.com/solo-io/gloo/projects/gateway/pkg/defaults"
 	gloov1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/options/headers"
+	"github.com/solo-io/gloo/test/e2e"
+	testmatchers "github.com/solo-io/gloo/test/gomega/matchers"
+	"github.com/solo-io/gloo/test/helpers"
+	"github.com/solo-io/gloo/test/testutils"
 	envoycore_sk "github.com/solo-io/solo-kit/pkg/api/external/envoy/api/v2/core"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources"
 	coreV1 "github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
-
-	. "github.com/onsi/ginkgo/v2"
-	"github.com/solo-io/gloo/test/e2e"
-	"github.com/solo-io/gloo/test/helpers"
 )
 
 var _ = Describe("HeaderManipulation", func() {
@@ -186,6 +192,165 @@ var _ = Describe("HeaderManipulation", func() {
 				vs, err := testContext.TestClients().VirtualServiceClient.Read(writeNamespace, "forbidden-header-manipulation", clients.ReadOpts{})
 				return vs, err
 			})
+		})
+	})
+
+	Context("Header mutation order (most_specific_header_mutations_wins)", func() {
+		const (
+			responseHeader = "Response-Header"
+			requestHeader  = "Request-Header"
+		)
+
+		rtWithOptions := func(append bool) *v1.RouteTable {
+			rtName := "append"
+			if !append {
+				rtName = "overwrite"
+			}
+
+			opts := &gloov1.RouteOptions{
+				HeaderManipulation: &headers.HeaderManipulation{
+					RequestHeadersToAdd: []*envoycore_sk.HeaderValueOption{{
+						HeaderOption: &envoycore_sk.HeaderValueOption_Header{
+							Header: &envoycore_sk.HeaderValue{Key: requestHeader, Value: "route-header"},
+						},
+						Append: &wrappers.BoolValue{Value: append},
+					}},
+					ResponseHeadersToAdd: []*headers.HeaderValueOption{{
+						Header: &headers.HeaderValue{Key: responseHeader, Value: "route-header"},
+						Append: &wrappers.BoolValue{Value: append},
+					}},
+				},
+			}
+			route := helpers.NewRouteBuilder().
+				WithName(fmt.Sprintf("%s-route", rtName)).
+				WithPrefixMatcher(fmt.Sprintf("/%s", rtName)).
+				WithRouteOptions(opts).
+				WithRouteActionToUpstreamRef(testContext.TestUpstream().Upstream.GetMetadata().Ref()).
+				Build()
+
+			return helpers.NewRouteTableBuilder().
+				WithName(fmt.Sprintf("%s-rt", rtName)).
+				WithNamespace(writeNamespace).
+				WithRoute("route", route).
+				Build()
+		}
+
+		vsWithOptions := func(append bool, rtRef []*coreV1.ResourceRef) *v1.VirtualService {
+			vsName := "append"
+			if !append {
+				vsName = "overwrite"
+			}
+
+			opts := &gloov1.VirtualHostOptions{HeaderManipulation: &headers.HeaderManipulation{
+				RequestHeadersToAdd: []*envoycore_sk.HeaderValueOption{{
+					HeaderOption: &envoycore_sk.HeaderValueOption_Header{
+						Header: &envoycore_sk.HeaderValue{Key: requestHeader, Value: "vs-header"},
+					},
+					Append: &wrappers.BoolValue{Value: append},
+				}},
+				ResponseHeadersToAdd: []*headers.HeaderValueOption{{
+					Header: &headers.HeaderValue{Key: responseHeader, Value: "vs-header"},
+					Append: &wrappers.BoolValue{Value: append},
+				}},
+			}}
+			vsBuilder := helpers.NewVirtualServiceBuilder().
+				WithName(fmt.Sprintf("%s-vs", vsName)).
+				WithNamespace(writeNamespace).
+				WithDomain(fmt.Sprintf("%s.com", vsName)).
+				WithVirtualHostOptions(opts)
+
+			for _, ref := range rtRef {
+				routeRef := ref
+				vsBuilder.WithRouteDelegateActionRef(fmt.Sprintf("%s-route", ref.GetName()), routeRef)
+			}
+
+			return vsBuilder.Build()
+		}
+
+		// headerCheck checks that the request and response headers are as expected for a given host and path.
+		headerCheck := func(appendVs, appendRoute bool, expectedReqHeaders, expectedResHeaders []string) {
+			// The `append.com` host is configured to append headers, while the `overwrite.com` domain is configured to overwrite headers.
+			host := "append.com"
+			if !appendVs {
+				host = "overwrite.com"
+			}
+
+			// The `append` path/route is configured to append headers, while the `overwrite` route is configured to overwrite headers.
+			path := "append"
+			if !appendRoute {
+				path = "overwrite"
+			}
+
+			Eventually(func(g Gomega) {
+				requestBuilder := testContext.GetHttpRequestBuilder().WithHost(host).WithPath(path)
+				res, err := testutils.DefaultHttpClient.Do(requestBuilder.Build())
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(res).Should(testmatchers.HaveHttpResponse(&testmatchers.HttpResponse{
+					StatusCode: http.StatusOK,
+					Custom:     testmatchers.ConsistOfHeaders(http.Header{responseHeader: expectedResHeaders}),
+				}))
+
+				select {
+				case req := <-testContext.TestUpstream().C:
+					g.Expect(req.Headers).To(HaveKeyWithValue(requestHeader, ConsistOf(expectedReqHeaders)))
+				case <-time.After(time.Second * 5):
+					Fail("request didn't make it upstream")
+				}
+			}, "5s", "0.5s").Should(Succeed())
+		}
+
+		BeforeEach(func() {
+			appendRT := rtWithOptions(true)
+			overwriteRT := rtWithOptions(false)
+
+			overwriteVS := vsWithOptions(false, []*coreV1.ResourceRef{appendRT.Metadata.Ref(), overwriteRT.Metadata.Ref()})
+			appendVS := vsWithOptions(true, []*coreV1.ResourceRef{appendRT.Metadata.Ref(), overwriteRT.Metadata.Ref()})
+
+			testContext.ResourcesToCreate().VirtualServices = v1.VirtualServiceList{overwriteVS, appendVS}
+			testContext.ResourcesToCreate().RouteTables = v1.RouteTableList{overwriteRT, appendRT}
+		})
+
+		When("most_specific_header_mutations_wins is nil", func() {
+			DescribeTable("appends headers correctly", headerCheck,
+				Entry("appends route and vhost level headers", true, true, []string{"route-header", "vs-header"}, []string{"vs-header", "route-header"}),
+				Entry("appends route and vhost level headers when route is set to overwrite headers", true, false, []string{"route-header", "vs-header"}, []string{"route-header", "vs-header"}),
+				Entry("vhost level header overwrites route level header", false, true, []string{"vs-header"}, []string{"vs-header"}),
+				Entry("vhost level header overwrites route level header when route is set to overwrite headers", false, false, []string{"vs-header"}, []string{"vs-header"}),
+			)
+		})
+
+		When("most_specific_header_mutations_wins is false", func() {
+			BeforeEach(func() {
+				gw := defaults.DefaultGateway(writeNamespace)
+				gw.RouteOptions = &gloov1.RouteConfigurationOptions{
+					MostSpecificHeaderMutationsWins: &wrappers.BoolValue{Value: false},
+				}
+				testContext.ResourcesToCreate().Gateways = v1.GatewayList{gw}
+			})
+
+			DescribeTable("appends headers correctly", headerCheck,
+				Entry("appends route and vhost level headers", true, true, []string{"route-header", "vs-header"}, []string{"route-header", "vs-header"}),
+				Entry("appends route and vhost level headers when route is set to overwrite headers", true, false, []string{"route-header", "vs-header"}, []string{"route-header", "vs-header"}),
+				Entry("vhost level header overwrites route level header", false, true, []string{"vs-header"}, []string{"vs-header"}),
+				Entry("vhost level header overwrites route level header when route is set to overwrite headers", false, false, []string{"vs-header"}, []string{"vs-header"}),
+			)
+		})
+
+		When("most_specific_header_mutations_wins is true", func() {
+			BeforeEach(func() {
+				gw := defaults.DefaultGateway(writeNamespace)
+				gw.RouteOptions = &gloov1.RouteConfigurationOptions{
+					MostSpecificHeaderMutationsWins: &wrappers.BoolValue{Value: true},
+				}
+				testContext.ResourcesToCreate().Gateways = v1.GatewayList{gw}
+			})
+
+			DescribeTable("appends headers correctly", headerCheck,
+				Entry("appends route and vhost level headers", true, true, []string{"route-header", "vs-header"}, []string{"route-header", "vs-header"}),
+				Entry("appends route and vhost level headers when vhost is set to overwrite headers", false, true, []string{"route-header", "vs-header"}, []string{"route-header", "vs-header"}),
+				Entry("route level header overwrites vhost level header", true, false, []string{"route-header"}, []string{"route-header"}),
+				Entry("route level header overwrites vhost level header when vhost is set to overwrite headers", false, false, []string{"route-header"}, []string{"route-header"}),
+			)
 		})
 	})
 })
