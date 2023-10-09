55aadf668db46012d90f9f27fb953c4a3bad7db5
Bernie Birnbaum <bewebi@earthlink.net>
2023-10-09 15:12:05 -0400

    Istio fix backport 1.14 (#8757)
    * Istio 1.18 support (#8676)
    
    * use istio 1.18 is CI
    
    * bump tests
    
    * guide updates
    
    * more fixes
    
    * istio-sds suite
    
    * make https requests
    
    * update beforesuite setup and assertions
    
    * WIP
    
    * passing test with cert assertion
    
    * add istio-sds to action
    
    * bump readiness timeout
    
    * install istio on istio-sds action
    
    * add spiffe volumes to chart
    
    * substring
    
    * remove single quotes from substring
    
    * remove single quotes from substring
    
    * substring
    
    * add peerauthentication
    
    * dump more
    
    * reorder
    
    * stricter istio inject assertions
    
    * cleanup peerauth
    
    * remove spiffe volumes
    
    * fmt
    
    * remove additional dumps
    
    * cleanup
    
    * fmt
    
    * changelog
    
    * typo
    
    * renaming, remove unecessary resource, cleanup
    
    * bump default istio version
    
    * review feedback/cleanup
    
    * cleanup
    
    * use public tag
    
    * comment on values
    
    * do not label gloo-system for injection, httpbin in separate ns
    
    * add required volumes
    
    * update/add tests for injection-free case
    
    * fmt
    
    * update changelog
    
    * update comments
    
    * cleanup
    
    * cleanup, helm test
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    * unfocus
    
    * cleanup
    
    * cleanup
    
    * docs wip
    
    * docs without injection first pass complete
    
    * additional content
    
    * docs review
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    * update guide to use httpbin
    
    * neaten eventually
    
    * combine istio suites
    
    * cleanup
    
    * add production values
    
    * fmt
    
    * move afterEach into contexts
    
    * fmt
    
    * fix helm tests
    
    * unfocus
    
    * update value description
    
    * bookinfo -> httpbin
    
    ---------
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: changelog-bot <changelog-bot>
    
    * modify istio version
    
    * use LTS SDS version
    
    * fix test
    
    * update docs with LTS version
    
    ---------
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>

--- test/kube2e/istio/istio_integration_test.go
+++ test/kube2e/istio/istio_integration_test.go
@@ -3,7 +3,15 @@ package istio_test
 import (
 	"fmt"
 	"net/http"
+	"path/filepath"
 	"time"
+
+	skerrors "github.com/solo-io/solo-kit/pkg/errors"
+
+	"github.com/solo-io/gloo/projects/gloo/cli/pkg/testutils"
+	"github.com/solo-io/go-utils/testutils/exec"
+
+	kubernetesplugin "github.com/solo-io/gloo/projects/gloo/pkg/plugins/kubernetes"
 
 	"k8s.io/apimachinery/pkg/util/intstr"
 
@@ -20,9 +28,13 @@ import (
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources"
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources/common/kubernetes"
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
-	skerrors "github.com/solo-io/solo-kit/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	httpbinName = "httpbin"
+	httpbinPort = 8000
 )
 
 var _ = Describe("Gloo + Istio integration tests", func() {
@@ -32,38 +44,38 @@ var _ = Describe("Gloo + Istio integration tests", func() {
 		virtualServiceRef = core.ResourceRef{Name: helper.TestrunnerName, Namespace: "gloo-system"}
 	)
 
-	AfterEach(func() {
-		var err error
-		err = resourceClientSet.VirtualServiceClient().Delete(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.DeleteOpts{
-			IgnoreNotExist: true,
-		})
-		Expect(err).NotTo(HaveOccurred())
-		helpers.EventuallyResourceDeleted(func() (resources.InputResource, error) {
-			return resourceClientSet.VirtualServiceClient().Read(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.ReadOpts{})
-		})
-
-		err = resourceClientSet.ServiceClient().Delete(serviceRef.Namespace, serviceRef.Name, clients.DeleteOpts{
-			IgnoreNotExist: true,
-		})
-		Expect(err).NotTo(HaveOccurred())
-		Eventually(func() bool {
-			_, err := resourceClientSet.ServiceClient().Read(serviceRef.Namespace, serviceRef.Name, clients.ReadOpts{})
-			// we should receive a DNE error, meaning it's now deleted
-			return err != nil && skerrors.IsNotExist(err)
-		}, "5s", "1s").Should(BeTrue())
-
-		err = resourceClientSet.UpstreamClient().Delete(upstreamRef.Namespace, upstreamRef.Name, clients.DeleteOpts{
-			IgnoreNotExist: true,
-		})
-		helpers.EventuallyResourceDeleted(func() (resources.InputResource, error) {
-			return resourceClientSet.UpstreamClient().Read(upstreamRef.Namespace, upstreamRef.Name, clients.ReadOpts{})
-		})
-	})
-
 	Context("port settings", func() {
 		BeforeEach(func() {
 			serviceRef = core.ResourceRef{Name: helper.TestrunnerName, Namespace: "gloo-system"}
 			virtualServiceRef = core.ResourceRef{Name: helper.TestrunnerName, Namespace: "gloo-system"}
+		})
+
+		AfterEach(func() {
+			var err error
+			err = resourceClientSet.VirtualServiceClient().Delete(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.DeleteOpts{
+				IgnoreNotExist: true,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			helpers.EventuallyResourceDeleted(func() (resources.InputResource, error) {
+				return resourceClientSet.VirtualServiceClient().Read(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.ReadOpts{})
+			})
+
+			err = resourceClientSet.ServiceClient().Delete(serviceRef.Namespace, serviceRef.Name, clients.DeleteOpts{
+				IgnoreNotExist: true,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(func() bool {
+				_, err := resourceClientSet.ServiceClient().Read(serviceRef.Namespace, serviceRef.Name, clients.ReadOpts{})
+				// we should receive a DNE error, meaning it's now deleted
+				return err != nil && skerrors.IsNotExist(err)
+			}, "5s", "1s").Should(BeTrue())
+
+			err = resourceClientSet.UpstreamClient().Delete(upstreamRef.Namespace, upstreamRef.Name, clients.DeleteOpts{
+				IgnoreNotExist: true,
+			})
+			helpers.EventuallyResourceDeleted(func() (resources.InputResource, error) {
+				return resourceClientSet.UpstreamClient().Read(upstreamRef.Namespace, upstreamRef.Name, clients.ReadOpts{})
+			})
 		})
 
 		// Sets up services
@@ -167,7 +179,7 @@ var _ = Describe("Gloo + Istio integration tests", func() {
 			Entry("with matching port and target port", int32(helper.TestRunnerPort), helper.TestRunnerPort, http.StatusOK),
 			Entry("without target port, and port matching pod's port", int32(helper.TestRunnerPort), -1, http.StatusOK),
 			Entry("without target port, and port not matching app's port", int32(helper.TestRunnerPort+1), -1, http.StatusServiceUnavailable),
-			Entry("pointing to the wrong target port", int32(8000), helper.TestRunnerPort+1, http.StatusServiceUnavailable), // or maybe 404?
+			Entry("pointing to the wrong target port", int32(8000), helper.TestRunnerPort+1, http.StatusServiceUnavailable),
 		)
 	})
 
@@ -250,6 +262,34 @@ var _ = Describe("Gloo + Istio integration tests", func() {
 			})
 		})
 
+		AfterEach(func() {
+			var err error
+			err = resourceClientSet.VirtualServiceClient().Delete(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.DeleteOpts{
+				IgnoreNotExist: true,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			helpers.EventuallyResourceDeleted(func() (resources.InputResource, error) {
+				return resourceClientSet.VirtualServiceClient().Read(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.ReadOpts{})
+			})
+
+			err = resourceClientSet.ServiceClient().Delete(serviceRef.Namespace, serviceRef.Name, clients.DeleteOpts{
+				IgnoreNotExist: true,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(func() bool {
+				_, err := resourceClientSet.ServiceClient().Read(serviceRef.Namespace, serviceRef.Name, clients.ReadOpts{})
+				// we should receive a DNE error, meaning it's now deleted
+				return err != nil && skerrors.IsNotExist(err)
+			}, "5s", "1s").Should(BeTrue())
+
+			err = resourceClientSet.UpstreamClient().Delete(upstreamRef.Namespace, upstreamRef.Name, clients.DeleteOpts{
+				IgnoreNotExist: true,
+			})
+			helpers.EventuallyResourceDeleted(func() (resources.InputResource, error) {
+				return resourceClientSet.UpstreamClient().Read(upstreamRef.Namespace, upstreamRef.Name, clients.ReadOpts{})
+			})
+		})
+
 		It("routes to headless services", func() {
 			testHelper.CurlEventuallyShouldRespond(helper.CurlOpts{
 				Protocol:          "http",
@@ -263,6 +303,194 @@ var _ = Describe("Gloo + Istio integration tests", func() {
 				WithoutStats:      true,
 				ReturnHeaders:     true,
 			}, fmt.Sprintf("HTTP/1.1 %d", http.StatusOK), 1, time.Minute*1)
+		})
+	})
+
+	Context("Istio mTLS", func() {
+		var (
+			upstreamRef, virtualServiceRef core.ResourceRef
+		)
+
+		BeforeEach(func() {
+			virtualServiceRef = core.ResourceRef{Name: httpbinName, Namespace: installNamespace}
+
+			// the upstream should be created by discovery service
+			upstreamRef = core.ResourceRef{
+				Name:      kubernetesplugin.UpstreamName(httpbinNamespace, httpbinName, httpbinPort),
+				Namespace: installNamespace,
+			}
+			helpers.EventuallyResourceAccepted(func() (resources.InputResource, error) {
+				return resourceClientSet.UpstreamClient().Read(upstreamRef.Namespace, upstreamRef.Name, clients.ReadOpts{})
+			})
+
+			route := helpers.NewRouteBuilder().
+				WithRouteActionToUpstreamRef(&upstreamRef).
+				WithMatcher(&matchers.Matcher{
+					PathSpecifier: &matchers.Matcher_Prefix{
+						Prefix: "/",
+					},
+				}).
+				Build()
+
+			vs := helpers.NewVirtualServiceBuilder().
+				WithName(virtualServiceRef.Name).
+				WithNamespace(virtualServiceRef.Namespace).
+				WithDomain(httpbinName).
+				WithRoute("default-route", route).
+				Build()
+
+			_, err := resourceClientSet.VirtualServiceClient().Write(vs, clients.WriteOpts{})
+			Expect(err).NotTo(HaveOccurred())
+			helpers.EventuallyResourceAccepted(func() (resources.InputResource, error) {
+				return resourceClientSet.VirtualServiceClient().Read(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.ReadOpts{})
+			})
+		})
+
+		AfterEach(func() {
+			var err error
+			err = resourceClientSet.VirtualServiceClient().Delete(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.DeleteOpts{
+				IgnoreNotExist: true,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			helpers.EventuallyResourceDeleted(func() (resources.InputResource, error) {
+				return resourceClientSet.VirtualServiceClient().Read(virtualServiceRef.Namespace, virtualServiceRef.Name, clients.ReadOpts{})
+			})
+
+			err = resourceClientSet.UpstreamClient().Delete(upstreamRef.Namespace, upstreamRef.Name, clients.DeleteOpts{
+				IgnoreNotExist: true,
+			})
+			helpers.EventuallyResourceDeleted(func() (resources.InputResource, error) {
+				return resourceClientSet.UpstreamClient().Read(upstreamRef.Namespace, upstreamRef.Name, clients.ReadOpts{})
+			})
+		})
+
+		Context("permissive peer auth", func() {
+			BeforeEach(func() {
+				err := exec.RunCommand(testHelper.RootDir, false, "kubectl", "apply", "-f", filepath.Join(cwd, "artifacts", "peerauth_permissive.yaml"))
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			AfterEach(func() {
+				err := exec.RunCommand(testHelper.RootDir, false, "kubectl", "delete", "-n", "istio-system", "peerauthentication", "test")
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			When("mtls is not enabled for the upstream", func() {
+
+				It("should be able to complete the request without mTLS header", func() {
+					testHelper.CurlEventuallyShouldRespond(helper.CurlOpts{
+						Protocol:          "http",
+						Path:              "/",
+						Method:            "GET",
+						Host:              httpbinName,
+						Service:           gatewayProxy,
+						Port:              gatewayPort,
+						ConnectionTimeout: 10,
+						Verbose:           false,
+						WithoutStats:      true,
+						ReturnHeaders:     false,
+					}, "200", 1, time.Minute)
+				})
+			})
+
+			When("mtls is enabled for the upstream", func() {
+				BeforeEach(func() {
+					err := testutils.Glooctl(fmt.Sprintf("istio enable-mtls --upstream %s", upstreamRef.Name))
+					Expect(err).NotTo(HaveOccurred())
+				})
+
+				AfterEach(func() {
+					// It seems to sometimes take multiple calls before the disable command is registered
+					EventuallyWithOffset(1, func(g Gomega) {
+						err := testutils.Glooctl(fmt.Sprintf("istio disable-mtls --upstream %s", upstreamRef.Name))
+						g.Expect(err).NotTo(HaveOccurred())
+						us, err := resourceClientSet.UpstreamClient().Read(upstreamRef.Namespace, upstreamRef.Name, clients.ReadOpts{})
+						g.Expect(err).NotTo(HaveOccurred())
+						g.Expect(us.SslConfig).To(BeNil())
+					}, 30*time.Second).ShouldNot(HaveOccurred())
+				})
+
+				It("should make a request with the expected cert header", func() {
+					// the /headers endpoint will respond with the headers the request to the client contains
+					testHelper.CurlEventuallyShouldRespond(helper.CurlOpts{
+						Protocol:          "http",
+						Path:              "/headers",
+						Method:            "GET",
+						Host:              httpbinName,
+						Service:           gatewayProxy,
+						Port:              gatewayPort,
+						ConnectionTimeout: 10,
+						Verbose:           false,
+						WithoutStats:      true,
+						ReturnHeaders:     false,
+					}, "\"X-Forwarded-Client-Cert\"", 1, time.Minute)
+				})
+			})
+		})
+
+		Context("strict peer auth", func() {
+			BeforeEach(func() {
+				err := exec.RunCommand(testHelper.RootDir, false, "kubectl", "apply", "-f", filepath.Join(cwd, "artifacts", "peerauth_strict.yaml"))
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			AfterEach(func() {
+				err := exec.RunCommand(testHelper.RootDir, false, "kubectl", "delete", "-n", "istio-system", "peerauthentication", "test")
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			When("mtls is not enabled for the upstream", func() {
+
+				It("should not be able to complete the request", func() {
+					// the /headers endpoint will respond with the headers the request to the client contains
+					testHelper.CurlEventuallyShouldRespond(helper.CurlOpts{
+						Protocol:          "http",
+						Path:              "/headers",
+						Method:            "GET",
+						Host:              httpbinName,
+						Service:           gatewayProxy,
+						Port:              gatewayPort,
+						ConnectionTimeout: 10,
+						Verbose:           false,
+						WithoutStats:      true,
+						ReturnHeaders:     false,
+					}, "upstream connect error or disconnect/reset before headers. reset reason: connection termination", 1, time.Minute*1)
+				})
+			})
+
+			When("mtls is enabled for the upstream", func() {
+				BeforeEach(func() {
+					err := testutils.Glooctl(fmt.Sprintf("istio enable-mtls --upstream %s", upstreamRef.Name))
+					Expect(err).NotTo(HaveOccurred())
+				})
+
+				AfterEach(func() {
+					// It seems to sometimes take multiple calls before the disable command is registered
+					EventuallyWithOffset(1, func(g Gomega) {
+						err := testutils.Glooctl(fmt.Sprintf("istio disable-mtls --upstream %s", upstreamRef.Name))
+						g.Expect(err).NotTo(HaveOccurred())
+						us, err := resourceClientSet.UpstreamClient().Read(upstreamRef.Namespace, upstreamRef.Name, clients.ReadOpts{})
+						g.Expect(err).NotTo(HaveOccurred())
+						g.Expect(us.SslConfig).To(BeNil())
+					}, 30*time.Second).ShouldNot(HaveOccurred())
+				})
+
+				It("should make a request with the expected cert header", func() {
+					// the /headers endpoint will respond with the headers the request to the client contains
+					testHelper.CurlEventuallyShouldRespond(helper.CurlOpts{
+						Protocol:          "http",
+						Path:              "/headers",
+						Method:            "GET",
+						Host:              httpbinName,
+						Service:           gatewayProxy,
+						Port:              gatewayPort,
+						ConnectionTimeout: 10,
+						Verbose:           false,
+						WithoutStats:      true,
+						ReturnHeaders:     false,
+					}, "\"X-Forwarded-Client-Cert\"", 1, time.Minute*1)
+				})
+			})
 		})
 	})
 })
