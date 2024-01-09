ab406328303f0c8a76aef94526da35e9b6f6ce32
sheidkamp <seth.heidkamp@gmail.com>
2024-01-08 19:44:56 -0500

    Update make_suite_test.go

--- test/make/make_suite_test.go
+++ test/make/make_suite_test.go
@@ -9,12 +9,23 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/types"
+	"github.com/solo-io/skv2/codegen/util"
+)
+
+const (
+	StandardGlooImage = "quay.io/solo-io/gloo:1.16.0-beta1"
+	SdsGlooImage      = "quay.io/solo-io/sds:1.16.0-beta1-fips"
 )
 
 func TestMake(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Make Suite")
 }
+
+var (
+	moduleRoot = util.GetModuleRoot()
+)
 
 type EnvVar struct {
 	Name, Value string
@@ -41,4 +52,25 @@ func ExpectMakeVarsWithEnvVars(envVars []*EnvVar, makeVars []*MakeVar) {
 		err := os.Unsetenv(envVar.Name)
 		ExpectWithOffset(1, err).NotTo(HaveOccurred())
 	}
+}
+
+func ExpectMakeOutputWithOffset(offset int, target string, outputMatcher types.GomegaMatcher) {
+	makeArgs := append([]string{
+		"--directory",
+		moduleRoot,
+	}, strings.Split(target, " ")...)
+
+	cmd := exec.Command("make", makeArgs...)
+	out, err := cmd.CombinedOutput()
+	ExpectWithOffset(offset+1, err).NotTo(HaveOccurred(), "make command should succeed")
+	ExpectWithOffset(offset+1, out).To(WithTransform(getRelevantOutput, outputMatcher), "make command should produce expected output")
+}
+
+func getRelevantOutput(rawOutput []byte) string {
+	// We trim lines that are produced in our CI pipeline
+	// These are not present locally, so the trim is a no-op
+	relevantOutput := strings.TrimSpace(string(rawOutput))
+	relevantOutput = strings.TrimPrefix(relevantOutput, "make[1]: Entering directory '/workspace/solo-projects'")
+	relevantOutput = strings.TrimSuffix(relevantOutput, "make[1]: Leaving directory '/workspace/solo-projects'")
+	return strings.TrimSpace(relevantOutput)
 }
