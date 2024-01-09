1c9a1ed7e063a8c82e413a9a120f155a404c3d0d
sheidkamp <seth.heidkamp@gmail.com>
2024-01-09 13:27:05 -0500

    Update fips_test.go

--- test/make/fips_test.go
+++ test/make/fips_test.go
@@ -5,65 +5,101 @@ import (
 	"os"
 	"path/filepath"
 
-	. "github.com/onsi/ginkgo/v2"
+	//. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/solo-io/gloo/test/testutils"
 )
 
-var _ = Describe("FIPS", func() {
+const (
+	ExpectStandardCrypto = false
+	ExpectBoringCrypto   = true
+)
 
-	Context("validate-%-crypto", Ordered, func() {
+func ValidateCrypto(imageName string, binaryPath string, binaryLocalPath string, expectFips bool) {
+	pwd, err := os.Getwd()
+	ExpectWithOffset(1, err).NotTo(HaveOccurred(), "can get working directory")
 
-		const (
-			standardBinaryLocalPath = "gloo-standard"
-			fipsBinaryLocalPath     = "gloo-fips"
+	err = testutils.CopyImageFileToLocal(imageName, binaryPath, binaryLocalPath)
+	ExpectWithOffset(1, err).NotTo(HaveOccurred(), "can copy binary from image to local filesystem")
 
-			glooContainerBinaryPath    = "/usr/local/bin/gloo"
-			StandardSDSImage           = "quay.io/solo-io/sds:1.16.0-beta1"
-			sdsContainerBinaryPath     = "/usr/local/bin/sds"
-			sdsStandardBinaryLocalPath = "gloo-standard"
-		)
+	expectedCrypto := "standard"
+	unexpectedCrypto := "boring"
 
-		AfterAll(func() {
-			for _, binaryPath := range []string{standardBinaryLocalPath, fipsBinaryLocalPath} {
-				_ = os.Remove(binaryPath)
-			}
-		})
+	if expectFips {
+		expectedCrypto = "boring"
+		unexpectedCrypto = "standard"
+	}
 
-		It("successfully validates standard-crypto binary", func() {
-			pwd, err := os.Getwd()
-			Expect(err).NotTo(HaveOccurred(), "can get working directory")
+	// Expected crypto
+	target := fmt.Sprintf("BINARY=%s validate-%s-crypto --ignore-errors", filepath.Join(pwd, binaryLocalPath), unexpectedCrypto)
+	testutils.ExpectMakeOutputWithOffset(1, target, ContainSubstring(fmt.Sprintf("validate-%s-crypto] Error 1 (ignored)", unexpectedCrypto)))
 
-			err = testutils.CopyImageFileToLocal(StandardSDSImage, sdsContainerBinaryPath, sdsStandardBinaryLocalPath)
-			Expect(err).NotTo(HaveOccurred(), "can copy binary from image to local filesystem")
+	// Fail this one
+	target = fmt.Sprintf("BINARY=%s validate-%s-crypto --ignore-errors", filepath.Join(pwd, binaryLocalPath), expectedCrypto)
+	testutils.ExpectMakeOutputWithOffset(1, target, And(
+		ContainSubstring("goversion -crypto"),
+		Not(ContainSubstring("Error 1 (ignored)")),
+	))
+}
 
-			target := fmt.Sprintf("BINARY=%s validate-boring-crypto --ignore-errors", filepath.Join(pwd, standardBinaryLocalPath))
-			testutils.ExpectMakeOutputWithOffset(0, target, ContainSubstring("validate-boring-crypto] Error 1 (ignored)"))
+// var _ = Describe("FIPS", func() {
 
-			target = fmt.Sprintf("BINARY=%s validate-standard-crypto --ignore-errors", filepath.Join(pwd, standardBinaryLocalPath))
-			testutils.ExpectMakeOutputWithOffset(0, target, And(
-				ContainSubstring("goversion -crypto"),
-				Not(ContainSubstring("Error 1 (ignored)")),
-			))
-		})
+// FContext("validate-%-crypto", Ordered, func() {
 
-		// It("successfully validates boring-crypto binary", func() {
-		// 	pwd, err := os.Getwd()
-		// 	Expect(err).NotTo(HaveOccurred(), "can get working directory")
+// 	const (
+// 		standardBinaryLocalPath = "gloo-standard"
+// 		fipsBinaryLocalPath     = "gloo-fips"
 
-		// 	err = testutils.CopyImageFileToLocal(SdsGlooImage, glooContainerBinaryPath, fipsBinaryLocalPath)
-		// 	Expect(err).NotTo(HaveOccurred(), "can copy binary from image to local filesystem")
+// 		glooContainerBinaryPath    = "/usr/local/bin/gloo"
+// 		StandardSDSImage           = "quay.io/solo-io/sds:1.16.0-beta1"
+// 		sdsContainerBinaryPath     = "/usr/local/bin/sds"
+// 		sdsStandardBinaryLocalPath = "gloo-standard"
+// 	)
 
-		// 	target := fmt.Sprintf("BINARY=%s validate-standard-crypto --ignore-errors", filepath.Join(pwd, fipsBinaryLocalPath))
-		// 	testutils.ExpectMakeOutputWithOffset(0, target, ContainSubstring("validate-standard-crypto] Error 1 (ignored)"))
+// 	AfterAll(func() {
+// 		for _, binaryPath := range []string{standardBinaryLocalPath, fipsBinaryLocalPath} {
+// 			_ = os.Remove(binaryPath)
+// 		}
+// 	})
 
-		// 	target = fmt.Sprintf("BINARY=%s validate-boring-crypto --ignore-errors", filepath.Join(pwd, fipsBinaryLocalPath))
-		// 	testutils.ExpectMakeOutputWithOffset(0, target, And(
-		// 		ContainSubstring("goversion -crypto"),
-		// 		Not(ContainSubstring("Error 1 (ignored)")),
-		// 	))
-		// })
+// 	It("successfully validates standard-crypto binary", func() {
+// 		ValidateCrypto(StandardSDSImage, sdsContainerBinaryPath, sdsStandardBinaryLocalPath, ExpectStandardCrypto)
+// 	})
 
-	})
+// It("successfully validates standard-crypto binary", func() {
+// 	pwd, err := os.Getwd()
+// 	Expect(err).NotTo(HaveOccurred(), "can get working directory")
 
-})
+// 	err = testutils.CopyImageFileToLocal(StandardSDSImage, sdsContainerBinaryPath, sdsStandardBinaryLocalPath)
+// 	Expect(err).NotTo(HaveOccurred(), "can copy binary from image to local filesystem")
+
+// 	target := fmt.Sprintf("BINARY=%s validate-boring-crypto --ignore-errors", filepath.Join(pwd, standardBinaryLocalPath))
+// 	testutils.ExpectMakeOutputWithOffset(0, target, ContainSubstring("validate-boring-crypto] Error 1 (ignored)"))
+
+// 	target = fmt.Sprintf("BINARY=%s validate-standard-crypto --ignore-errors", filepath.Join(pwd, standardBinaryLocalPath))
+// 	testutils.ExpectMakeOutputWithOffset(0, target, And(
+// 		ContainSubstring("goversion -crypto"),
+// 		Not(ContainSubstring("Error 1 (ignored)")),
+// 	))
+// })
+
+// It("successfully validates boring-crypto binary", func() {
+// 	pwd, err := os.Getwd()
+// 	Expect(err).NotTo(HaveOccurred(), "can get working directory")
+
+// 	err = testutils.CopyImageFileToLocal(SdsGlooImage, glooContainerBinaryPath, fipsBinaryLocalPath)
+// 	Expect(err).NotTo(HaveOccurred(), "can copy binary from image to local filesystem")
+
+// 	target := fmt.Sprintf("BINARY=%s validate-standard-crypto --ignore-errors", filepath.Join(pwd, fipsBinaryLocalPath))
+// 	testutils.ExpectMakeOutputWithOffset(0, target, ContainSubstring("validate-standard-crypto] Error 1 (ignored)"))
+
+// 	target = fmt.Sprintf("BINARY=%s validate-boring-crypto --ignore-errors", filepath.Join(pwd, fipsBinaryLocalPath))
+// 	testutils.ExpectMakeOutputWithOffset(0, target, And(
+// 		ContainSubstring("goversion -crypto"),
+// 		Not(ContainSubstring("Error 1 (ignored)")),
+// 	))
+// })
+
+// 	})
+
+// })
