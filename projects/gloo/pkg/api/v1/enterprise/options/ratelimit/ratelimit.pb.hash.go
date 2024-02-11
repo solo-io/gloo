1b5ef13563444e09bf2ff905959299379116a8d7
Jenny Shu <28537278+jenshu@users.noreply.github.com>
2024-02-11 15:03:06 -0500

    Expose ratelimit grpc authority field [1.14] (#9155)
    * expose ratelimit grpc authority field
    
    * Update changelog/v1.17.0-beta8/ratelimit-grpc-authority.yaml
    
    
    
    * move into oneof
    
    ---------
    
    Co-authored-by: Nathan Fudenberg <nathan.fudenberg@solo.io>
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>

--- projects/gloo/pkg/api/v1/enterprise/options/ratelimit/ratelimit.pb.hash.go
+++ projects/gloo/pkg/api/v1/enterprise/options/ratelimit/ratelimit.pb.hash.go
@@ -149,6 +149,52 @@ func (m *Settings) Hash(hasher hash.Hash64) (uint64, error) {
 		return 0, err
 	}
 
+	switch m.ServiceType.(type) {
+
+	case *Settings_GrpcService:
+
+		if h, ok := interface{}(m.GetGrpcService()).(safe_hasher.SafeHasher); ok {
+			if _, err = hasher.Write([]byte("GrpcService")); err != nil {
+				return 0, err
+			}
+			if _, err = h.Hash(hasher); err != nil {
+				return 0, err
+			}
+		} else {
+			if fieldValue, err := hashstructure.Hash(m.GetGrpcService(), nil); err != nil {
+				return 0, err
+			} else {
+				if _, err = hasher.Write([]byte("GrpcService")); err != nil {
+					return 0, err
+				}
+				if err := binary.Write(hasher, binary.LittleEndian, fieldValue); err != nil {
+					return 0, err
+				}
+			}
+		}
+
+	}
+
+	return hasher.Sum64(), nil
+}
+
+// Hash function
+func (m *GrpcService) Hash(hasher hash.Hash64) (uint64, error) {
+	if m == nil {
+		return 0, nil
+	}
+	if hasher == nil {
+		hasher = fnv.New64()
+	}
+	var err error
+	if _, err = hasher.Write([]byte("ratelimit.options.gloo.solo.io.github.com/solo-io/gloo/projects/gloo/pkg/api/v1/enterprise/options/ratelimit.GrpcService")); err != nil {
+		return 0, err
+	}
+
+	if _, err = hasher.Write([]byte(m.GetAuthority())); err != nil {
+		return 0, err
+	}
+
 	return hasher.Sum64(), nil
 }
 
