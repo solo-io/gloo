787e10e8fde85a9062e7ba0b1603d5a88003ff87
nfuden <nathan.fudenberg@solo.io>
2024-02-29 13:43:52 -0500

    using ci to test a hypothesis

--- test/kube2e/helm/helm_test.go
+++ test/kube2e/helm/helm_test.go
@@ -259,7 +259,8 @@ var _ = Describe("Kube2e: helm", func() {
 			additionalInstallArgs = []string{
 				// Setting `settings.disableKubernetesDestinations` && `global.glooRbac.namespaced` leads to panic in gloo
 				// Ref: https://github.com/solo-io/gloo/issues/8801
-				"--set", "global.glooRbac.namespaced=false",
+				// "--set", "global.glooRbac.namespaced=false",
+				"--set", "settings.watchNamespaces=gloo-system",
 			}
 			additionalInstallArgs = append(additionalInstallArgs, valuesForProductionRecommendations...)
 
