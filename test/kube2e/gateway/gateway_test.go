cdb8a02790a737f8296443115528543938414a4d
Seth Heidkamp <61526534+sheidkamp@users.noreply.github.com>
2024-03-21 15:59:13 -0400

    logging and verifyGlooValidationWorks (#9250)
    * logging and verifyGlooValidationWorks
    
    * Flake Debugging
    
    * PR feedback
    
    * Update delete-secret-flake.yaml
    
    * Update util.go
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    ---------
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: changelog-bot <changelog-bot>

--- test/kube2e/gateway/gateway_test.go
+++ test/kube2e/gateway/gateway_test.go
@@ -2113,7 +2113,9 @@ spec:
 				// There are times when the VirtualService + Proxy do not update Status with the error when deleting the referenced Secret, therefore the validation error doesn't occur.
 				// It isn't until later - either a few minutes and/or after forcing an update by updating the VS - that the error status appears.
 				// The reason is still unknown, so we retry on flakes in the meantime.
-				It("should act as expected with secret validation", FlakeAttempts(3), func() {
+				It("should act as expected with secret validation", func() {
+					verifyGlooValidationWorks()
+
 					By("waiting for the modified VS to be accepted")
 					helpers.EventuallyResourceAccepted(func() (resources.InputResource, error) {
 						return resourceClientset.VirtualServiceClient().Read(testHelper.InstallNamespace, testServerVs.GetMetadata().GetName(), clients.ReadOpts{Ctx: ctx})
@@ -2121,6 +2123,7 @@ spec:
 
 					By("failing to delete a secret that is in use")
 					err := resourceClientset.KubeClients().CoreV1().Secrets(testHelper.InstallNamespace).Delete(ctx, secretName, metav1.DeleteOptions{})
+
 					Expect(err).To(HaveOccurred())
 					Expect(err.Error()).To(matchers2.ContainSubstrings([]string{"admission webhook", "SSL secret not found", secretName}))
 
