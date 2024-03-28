5a0ca92ebc9cade2ce9f1a5b20c8f25e0db73544
Jacob Bohanon <jacob.bohanon@solo.io>
2024-03-28 12:18:35 -0400

    demangle version for PRs into semver for dry_run

--- ci/upload_github_release_assets.go
+++ ci/upload_github_release_assets.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"runtime"
 	"strconv"
+	"strings"
 
 	"github.com/solo-io/gloo/projects/gloo/pkg/api/grpc/version"
 	"github.com/solo-io/go-utils/githubutils"
@@ -68,7 +70,7 @@ func main() {
 	}
 }
 
-func validateReleaseVersionOfCli() {
+func validateReleaseVersionOfCli(dryRun bool) {
 	releaseVersion := getReleaseVersionOrExitGracefully().String()[1:]
 	name := fmt.Sprintf("_output/glooctl-%s-amd64", runtime.GOOS)
 
@@ -83,19 +85,33 @@ func validateReleaseVersionOfCli() {
 		log.Fatalf("Unexpected version output for glooctl: %s", string(bytes))
 	}
 
+	if dryRun {
+		ver.Client.Version, err = validVersionFromPrVersion(ver.Client.Version)
+		if err != nil {
+			log.Fatalf("unable to parse valid semver from PR version: %v", err)
+		}
+	}
+
 	if ver.Client.Version != releaseVersion {
 		log.Fatalf("Expected to release artifacts for version %s, glooctl binary reported version %s", releaseVersion, ver.Client.Version)
 	}
 }
 
 // stolen from "github.com/solo-io/go-utils/versionutils", but changed the hardcoding of "TAGGED_VERSION" to "VERSION"
-func getReleaseVersionOrExitGracefully() *versionutils.Version {
+func getReleaseVersionOrExitGracefully(dryRun bool) *versionutils.Version {
 	versionStr, present := os.LookupEnv("VERSION")
 	if !present || versionStr == "" {
 		fmt.Printf("VERSION not found in environment.\n")
 		os.Exit(1)
 	}
 
+	if dryRun {
+		var err error
+		versionStr, err = validVersionFromPrVersion(versionStr)
+		if err != nil {
+			log.Fatalf("unable to parse valid semver from PR version: %v", err)
+		}
+	}
 	tag := "v" + versionStr
 
 	version, err := versionutils.ParseVersion(tag)
@@ -104,4 +120,19 @@ func getReleaseVersionOrExitGracefully() *versionutils.Version {
 		os.Exit(1)
 	}
 	return version
+}
+
+// we have to force the version into semver in order to test this. We do this by chopping the PR designation off
+// when running as dry_run
+func validVersionFromPrVersion(ver string) (string, error) {
+	expectedLen := 2
+	if strings.Contains(ver, "beta") || strings.Contains(ver, "rc") {
+		expectedLen = 3
+	}
+	splitVer := strings.Split(ver, "-")
+	if len(splitVer) != expectedLen {
+		return "", errors.New("invalid format for PR version; expected v<MAJOR>.<MINOR>.<PATCH>-(beta|rc)<N>-<PR> for beta or rc and v<MAJOR>.<MINOR>.<PATCH>-<PR> for LTS")
+	}
+	return strings.Join(splitVer[:expectedLen-1], ""), nil
+
 }
