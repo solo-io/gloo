6ec5bc0ecad0980952e158a63c0f168ea051d0e7
Lawrence Gadban <lawrence.gadban@solo.io>
2024-03-29 12:42:03 -0500

    cleanup

--- projects/gateway2/translator/plugins/routeoptions/route_options_plugin_test.go
+++ projects/gateway2/translator/plugins/routeoptions/route_options_plugin_test.go
@@ -12,6 +12,7 @@ import (
 	sologatewayv1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1"
 	solokubev1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1/kube/apis/gateway.solo.io/v1"
 	gwquery "github.com/solo-io/gloo/projects/gateway2/query"
+	"github.com/solo-io/gloo/projects/gateway2/reports"
 	"github.com/solo-io/gloo/projects/gateway2/translator/plugins"
 	rtoptquery "github.com/solo-io/gloo/projects/gateway2/translator/plugins/routeoptions/query"
 	"github.com/solo-io/gloo/projects/gateway2/translator/testutils"
@@ -40,7 +41,7 @@ var _ = Describe("RouteOptionsPlugin", func() {
 						ExtensionRef: &gwv1.LocalObjectReference{
 							Group: gwv1.Group(sologatewayv1.RouteOptionGVK.Group),
 							Kind:  gwv1.Kind(sologatewayv1.RouteOptionGVK.Kind),
-							Name:  "policy",
+							Name:  "filter-policy",
 						}}}}}
 
 			outputRoute := &v1.Route{
@@ -52,7 +53,7 @@ var _ = Describe("RouteOptionsPlugin", func() {
 				Options: &v1.RouteOptions{
 					Faults: &faultinjection.RouteFaults{
 						Abort: &faultinjection.RouteAbort{
-							Percentage: 1,
+							Percentage: 1.00,
 							HttpStatus: 500,
 						},
 					},
@@ -60,9 +61,35 @@ var _ = Describe("RouteOptionsPlugin", func() {
 			}
 			Expect(proto.Equal(outputRoute, expectedRoute)).To(BeTrue())
 		})
+
+		It("reports an error and does not apply any RouteOptions when the referenced obj doesn't exist", func() {
+			deps := []client.Object{}
+			fakeClient := testutils.BuildIndexedFakeClient(deps, gwquery.IterateIndices, rtoptquery.IterateIndices)
+			gwQueries := testutils.BuildGatewayQueriesWithClient(fakeClient)
+			plugin := NewPlugin(gwQueries, fakeClient)
+
+			route := routeWithFilter()
+			reportsMap := reports.NewReportMap()
+			reporter := reports.NewReporter(&reportsMap)
+			parentRefReporter := reporter.Route(route).ParentRef(parentRef())
+
+			rtCtx := &plugins.RouteContext{
+				Route:    route,
+				Rule:     routeRuleWithExtRef(),
+				Reporter: parentRefReporter,
+			}
+
+			outputRoute := &v1.Route{
+				Options: &v1.RouteOptions{},
+			}
+			err := plugin.ApplyRoutePlugin(context.Background(), rtCtx, outputRoute)
+
+			Expect(err).ToNot(BeNil())
+			Expect(proto.Equal(outputRoute.GetOptions(), &v1.RouteOptions{})).To(BeTrue())
+		})
 	})
 
-	Describe("Attaching RouteOptions via policy attachemnt as a RoutePlugin", func() {
+	Describe("Attaching RouteOptions via policy attachemnt", func() {
 		When("RouteOptions exist in the same namespace and are attached correctly", func() {
 			It("correctly adds faultinjection", func() {
 				deps := []client.Object{attachedRouteOption()}
@@ -74,76 +101,8 @@ var _ = Describe("RouteOptionsPlugin", func() {
 				routeCtx := &plugins.RouteContext{
 					Route: &gwv1.HTTPRoute{
 						ObjectMeta: metav1.ObjectMeta{
-							Name:      "ghostface",
-							Namespace: "wu-tang",
-						},
-					},
-				}
-
-				outputRoute := &v1.Route{
-					Options: &v1.RouteOptions{},
-				}
-				plugin.ApplyRoutePlugin(ctx, routeCtx, outputRoute)
-
-				expectedOptions := &v1.RouteOptions{
-					Faults: &faultinjection.RouteFaults{
-						Abort: &faultinjection.RouteAbort{
-							Percentage: 1.00,
-							HttpStatus: 500,
-						},
-					},
-				}
-				Expect(proto.Equal(outputRoute.GetOptions(), expectedOptions)).To(BeTrue())
-			})
-		})
-
-		When("RouteOptions exist in the same namespace and are attached correctly but omit the namespace in targetRef", func() {
-			It("correctly adds faultinjection", func() {
-				deps := []client.Object{attachedRouteOptionOmitNamespace()}
-				fakeClient := testutils.BuildIndexedFakeClient(deps, gwquery.IterateIndices, rtoptquery.IterateIndices)
-				gwQueries := testutils.BuildGatewayQueriesWithClient(fakeClient)
-				plugin := NewPlugin(gwQueries, fakeClient)
-
-				ctx := context.Background()
-				routeCtx := &plugins.RouteContext{
-					Route: &gwv1.HTTPRoute{
-						ObjectMeta: metav1.ObjectMeta{
-							Name:      "ghostface",
-							Namespace: "wu-tang",
-						},
-					},
-				}
-
-				outputRoute := &v1.Route{
-					Options: &v1.RouteOptions{},
-				}
-				plugin.ApplyRoutePlugin(ctx, routeCtx, outputRoute)
-
-				expectedOptions := &v1.RouteOptions{
-					Faults: &faultinjection.RouteFaults{
-						Abort: &faultinjection.RouteAbort{
-							Percentage: 1.00,
-							HttpStatus: 500,
-						},
-					},
-				}
-				Expect(proto.Equal(outputRoute.GetOptions(), expectedOptions)).To(BeTrue())
-			})
-		})
-
-		When("Two RouteOptions are attached correctly with different creation timestamps", func() {
-			It("correctly adds faultinjection from the earliest created object", func() {
-				deps := []client.Object{attachedRouteOption(), attachedRouteOptionBefore()}
-				fakeClient := testutils.BuildIndexedFakeClient(deps, gwquery.IterateIndices, rtoptquery.IterateIndices)
-				gwQueries := testutils.BuildGatewayQueriesWithClient(fakeClient)
-				plugin := NewPlugin(gwQueries, fakeClient)
-
-				ctx := context.Background()
-				routeCtx := &plugins.RouteContext{
-					Route: &gwv1.HTTPRoute{
-						ObjectMeta: metav1.ObjectMeta{
-							Name:      "ghostface",
-							Namespace: "wu-tang",
+							Name:      "route",
+							Namespace: "default",
 						},
 					},
 				}
@@ -165,6 +124,74 @@ var _ = Describe("RouteOptionsPlugin", func() {
 			})
 		})
 
+		When("RouteOptions exist in the same namespace and are attached correctly but omit the namespace in targetRef", func() {
+			It("correctly adds faultinjection", func() {
+				deps := []client.Object{attachedRouteOptionOmitNamespace()}
+				fakeClient := testutils.BuildIndexedFakeClient(deps, gwquery.IterateIndices, rtoptquery.IterateIndices)
+				gwQueries := testutils.BuildGatewayQueriesWithClient(fakeClient)
+				plugin := NewPlugin(gwQueries, fakeClient)
+
+				ctx := context.Background()
+				routeCtx := &plugins.RouteContext{
+					Route: &gwv1.HTTPRoute{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "route",
+							Namespace: "default",
+						},
+					},
+				}
+
+				outputRoute := &v1.Route{
+					Options: &v1.RouteOptions{},
+				}
+				plugin.ApplyRoutePlugin(ctx, routeCtx, outputRoute)
+
+				expectedOptions := &v1.RouteOptions{
+					Faults: &faultinjection.RouteFaults{
+						Abort: &faultinjection.RouteAbort{
+							Percentage: 4.19,
+							HttpStatus: 500,
+						},
+					},
+				}
+				Expect(proto.Equal(outputRoute.GetOptions(), expectedOptions)).To(BeTrue())
+			})
+		})
+
+		When("Two RouteOptions are attached correctly with different creation timestamps", func() {
+			It("correctly adds faultinjection from the earliest created object", func() {
+				deps := []client.Object{attachedRouteOption(), attachedRouteOptionBefore()}
+				fakeClient := testutils.BuildIndexedFakeClient(deps, gwquery.IterateIndices, rtoptquery.IterateIndices)
+				gwQueries := testutils.BuildGatewayQueriesWithClient(fakeClient)
+				plugin := NewPlugin(gwQueries, fakeClient)
+
+				ctx := context.Background()
+				routeCtx := &plugins.RouteContext{
+					Route: &gwv1.HTTPRoute{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      "route",
+							Namespace: "default",
+						},
+					},
+				}
+
+				outputRoute := &v1.Route{
+					Options: &v1.RouteOptions{},
+				}
+				plugin.ApplyRoutePlugin(ctx, routeCtx, outputRoute)
+
+				expectedOptions := &v1.RouteOptions{
+					Faults: &faultinjection.RouteFaults{
+						Abort: &faultinjection.RouteAbort{
+							Percentage: 6.55,
+							HttpStatus: 500,
+						},
+					},
+				}
+				Expect(proto.Equal(outputRoute.GetOptions(), expectedOptions)).To(BeTrue())
+			})
+		})
+
 		When("RouteOptions exist in the same namespace but are not attached correctly", func() {
 			It("does not add faultinjection", func() {
 				deps := []client.Object{nonAttachedRouteOption()}
@@ -176,8 +203,8 @@ var _ = Describe("RouteOptionsPlugin", func() {
 				routeCtx := &plugins.RouteContext{
 					Route: &gwv1.HTTPRoute{
 						ObjectMeta: metav1.ObjectMeta{
-							Name:      "ghostface",
-							Namespace: "wu-tang",
+							Name:      "route",
+							Namespace: "default",
 						},
 					},
 				}
@@ -202,8 +229,8 @@ var _ = Describe("RouteOptionsPlugin", func() {
 				routeCtx := &plugins.RouteContext{
 					Route: &gwv1.HTTPRoute{
 						ObjectMeta: metav1.ObjectMeta{
-							Name:      "ghostface",
-							Namespace: "default",
+							Name:      "route",
+							Namespace: "non-default",
 						},
 					},
 				}
@@ -217,12 +244,42 @@ var _ = Describe("RouteOptionsPlugin", func() {
 			})
 		})
 	})
+
+	Describe("HTTPRoute with RouteOptions filter AND attached RouteOptions", func() {
+		It("Only applies RouteOptions from filter", func() {
+			deps := []client.Object{routeOption(), attachedRouteOption()}
+			fakeClient := testutils.BuildIndexedFakeClient(deps, gwquery.IterateIndices, rtoptquery.IterateIndices)
+			gwQueries := testutils.BuildGatewayQueriesWithClient(fakeClient)
+			plugin := NewPlugin(gwQueries, fakeClient)
+
+			ctx := context.Background()
+			routeCtx := &plugins.RouteContext{
+				Route: routeWithFilter(),
+				Rule:  routeRuleWithExtRef(),
+			}
+
+			outputRoute := &v1.Route{
+				Options: &v1.RouteOptions{},
+			}
+			plugin.ApplyRoutePlugin(ctx, routeCtx, outputRoute)
+
+			expectedOptions := &v1.RouteOptions{
+				Faults: &faultinjection.RouteFaults{
+					Abort: &faultinjection.RouteAbort{
+						Percentage: 1.00,
+						HttpStatus: 500,
+					},
+				},
+			}
+			Expect(proto.Equal(outputRoute.GetOptions(), expectedOptions)).To(BeTrue())
+		})
+	})
 })
 
 func routeOption() *solokubev1.RouteOption {
 	return &solokubev1.RouteOption{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "policy",
+			Name:      "filter-policy",
 			Namespace: "default",
 		},
 		Spec: sologatewayv1.RouteOption{
@@ -238,47 +295,63 @@ func routeOption() *solokubev1.RouteOption {
 	}
 }
 
-func attachedRouteOption() *solokubev1.RouteOption {
-	now := metav1.Now()
-	return &solokubev1.RouteOption{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:              "policy",
-			Namespace:         "wu-tang",
-			CreationTimestamp: now,
+func routeRuleWithExtRef() *gwv1.HTTPRouteRule {
+	return &gwv1.HTTPRouteRule{
+		Filters: []gwv1.HTTPRouteFilter{{
+			Type: "ExtensionRef",
+			ExtensionRef: &gwv1.LocalObjectReference{
+				Group: gwv1.Group(sologatewayv1.RouteOptionGVK.Group),
+				Kind:  gwv1.Kind(sologatewayv1.RouteOptionGVK.Kind),
+				Name:  "filter-policy",
+			}},
 		},
-		Spec: sologatewayv1.RouteOption{
-			TargetRef: &corev1.PolicyTargetReference{
-				Group:     gwv1.GroupVersion.Group,
-				Kind:      wellknown.HTTPRouteKind,
-				Name:      "ghostface",
-				Namespace: wrapperspb.String("wu-tang"),
-			},
-			Options: &v1.RouteOptions{
-				Faults: &faultinjection.RouteFaults{
-					Abort: &faultinjection.RouteAbort{
-						Percentage: 1.00,
-						HttpStatus: 500,
-					},
+	}
+}
+
+func parentRef() *gwv1.ParentReference {
+	return &gwv1.ParentReference{
+		Name: "my-gw",
+	}
+}
+
+func route() *gwv1.HTTPRoute {
+	return &gwv1.HTTPRoute{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "route",
+			Namespace: "default",
+		},
+		Spec: gwv1.HTTPRouteSpec{
+			CommonRouteSpec: gwv1.CommonRouteSpec{
+				ParentRefs: []gwv1.ParentReference{
+					*parentRef(),
 				},
 			},
 		},
 	}
 }
 
-func attachedRouteOptionBefore() *solokubev1.RouteOption {
-	anHourAgo := metav1.NewTime(time.Now().Add(-1 * time.Hour))
+func routeWithFilter() *gwv1.HTTPRoute {
+	rwf := route()
+	rwf.Spec.Rules = []gwv1.HTTPRouteRule{
+		*routeRuleWithExtRef(),
+	}
+	return rwf
+}
+
+func attachedRouteOption() *solokubev1.RouteOption {
+	now := metav1.Now()
 	return &solokubev1.RouteOption{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:              "policy-older",
-			Namespace:         "wu-tang",
-			CreationTimestamp: anHourAgo,
+			Name:              "policy",
+			Namespace:         "default",
+			CreationTimestamp: now,
 		},
 		Spec: sologatewayv1.RouteOption{
 			TargetRef: &corev1.PolicyTargetReference{
 				Group:     gwv1.GroupVersion.Group,
 				Kind:      wellknown.HTTPRouteKind,
-				Name:      "ghostface",
-				Namespace: wrapperspb.String("wu-tang"),
+				Name:      "route",
+				Namespace: wrapperspb.String("default"),
 			},
 			Options: &v1.RouteOptions{
 				Faults: &faultinjection.RouteFaults{
@@ -292,22 +365,49 @@ func attachedRouteOptionBefore() *solokubev1.RouteOption {
 	}
 }
 
+func attachedRouteOptionBefore() *solokubev1.RouteOption {
+	anHourAgo := metav1.NewTime(time.Now().Add(-1 * time.Hour))
+	return &solokubev1.RouteOption{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:              "policy-older",
+			Namespace:         "default",
+			CreationTimestamp: anHourAgo,
+		},
+		Spec: sologatewayv1.RouteOption{
+			TargetRef: &corev1.PolicyTargetReference{
+				Group:     gwv1.GroupVersion.Group,
+				Kind:      wellknown.HTTPRouteKind,
+				Name:      "route",
+				Namespace: wrapperspb.String("default"),
+			},
+			Options: &v1.RouteOptions{
+				Faults: &faultinjection.RouteFaults{
+					Abort: &faultinjection.RouteAbort{
+						Percentage: 6.55,
+						HttpStatus: 500,
+					},
+				},
+			},
+		},
+	}
+}
+
 func attachedRouteOptionOmitNamespace() *solokubev1.RouteOption {
 	return &solokubev1.RouteOption{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "policy",
-			Namespace: "wu-tang",
+			Namespace: "default",
 		},
 		Spec: sologatewayv1.RouteOption{
 			TargetRef: &corev1.PolicyTargetReference{
 				Group: gwv1.GroupVersion.Group,
 				Kind:  wellknown.HTTPRouteKind,
-				Name:  "ghostface",
+				Name:  "route",
 			},
 			Options: &v1.RouteOptions{
 				Faults: &faultinjection.RouteFaults{
 					Abort: &faultinjection.RouteAbort{
-						Percentage: 1.00,
+						Percentage: 4.19,
 						HttpStatus: 500,
 					},
 				},
@@ -319,15 +419,15 @@ func attachedRouteOptionOmitNamespace() *solokubev1.RouteOption {
 func nonAttachedRouteOption() *solokubev1.RouteOption {
 	return &solokubev1.RouteOption{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "policy",
-			Namespace: "wu-tang",
+			Name:      "bad-policy",
+			Namespace: "default",
 		},
 		Spec: sologatewayv1.RouteOption{
 			TargetRef: &corev1.PolicyTargetReference{
 				Group:     gwv1.GroupVersion.Group,
 				Kind:      wellknown.HTTPRouteKind,
-				Name:      "my-route",
-				Namespace: wrapperspb.String("wu-tang"),
+				Name:      "bad-route",
+				Namespace: wrapperspb.String("default"),
 			},
 			Options: &v1.RouteOptions{
 				Faults: &faultinjection.RouteFaults{
