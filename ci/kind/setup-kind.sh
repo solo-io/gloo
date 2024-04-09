c27f4d58feb6470deb80498e36224a2dc343730c
Sam Heilbron <SamHeilbron@gmail.com>
2024-04-09 08:01:51 -0600

    glooctl: resolve test flake (#9338)
    * fix glooctl ttest, by resolving version issue, and create tests to prove it
    
    * add changelog
    
    * remove dupe test
    
    * fix conformance test ref
    
    * Update projects/gloo/cli/pkg/prerun/version_warning.go
    
    Co-authored-by: Jenny Shu <28537278+jenshu@users.noreply.github.com>
    
    * update all refs to 1.0.0-ci1
    
    * fix tests that fail once assertion is more explicit
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    ---------
    
    Co-authored-by: Jenny Shu <28537278+jenshu@users.noreply.github.com>
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: changelog-bot <changelog-bot>

--- ci/kind/setup-kind.sh
+++ ci/kind/setup-kind.sh
@@ -8,7 +8,7 @@ CLUSTER_NAME="${CLUSTER_NAME:-kind}"
 # The version of the Node Docker image to use for booting the cluster
 CLUSTER_NODE_VERSION="${CLUSTER_NODE_VERSION:-v1.28.0}"
 # The version used to tag images
-VERSION="${VERSION:-1.0.0-ci}"
+VERSION="${VERSION:-1.0.0-ci1}"
 # Skip building docker images if we are testing a released version
 SKIP_DOCKER="${SKIP_DOCKER:-false}"
 # Stop after creating the kind cluster
