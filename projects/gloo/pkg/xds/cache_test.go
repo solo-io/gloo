bdf4d6e790146b5586ee7014f3021d9e97f18b71
npolshakova <nina.polshakova@solo.io>
2024-04-09 16:57:00 -0400

    fix gateway translator tests, add ratelimit test, fix cache test

--- projects/gloo/pkg/xds/cache_test.go
+++ projects/gloo/pkg/xds/cache_test.go
@@ -6,6 +6,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
+	"github.com/solo-io/gloo/projects/gloo/pkg/utils"
 	"github.com/solo-io/gloo/projects/gloo/pkg/xds"
 )
 
@@ -13,12 +14,13 @@ var _ = Describe("Cache", func() {
 
 	It("SnapshotCacheKeys returns the keys formatted correctly", func() {
 		owner, namespace1, namespace2, name1, name2 := "owner", "namespace1", "namespace2", "name1", "name2"
-		proxies := []*v1.Proxy{
-			v1.NewProxy(namespace1, name1),
-			v1.NewProxy(namespace2, name2),
-		}
+		p1 := v1.NewProxy(namespace1, name1)
+		p1.Metadata.Labels = map[string]string{utils.TranslatorOwnerKey: owner}
+		p2 := v1.NewProxy(namespace2, name2)
+		p2.Metadata.Labels = map[string]string{utils.TranslatorOwnerKey: owner}
+		proxies := []*v1.Proxy{p1, p2}
 		expectedKeys := []string{fmt.Sprintf("%v~%v~%v", owner, namespace1, name1), fmt.Sprintf("%v~%v~%v", owner, namespace2, name2)}
-		actualKeys := xds.SnapshotCacheKeys(owner, proxies)
+		actualKeys := xds.SnapshotCacheKeys(proxies)
 		Expect(actualKeys).To(BeEquivalentTo(expectedKeys))
 	})
 })
