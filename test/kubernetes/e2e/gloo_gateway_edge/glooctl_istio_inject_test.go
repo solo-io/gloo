0ec17c591760ca0b7986346d438744ced8f84995
Nina Polshakova <nina.polshakova@solo.io>
2024-05-13 14:37:15 -0400

    Add Istio strict/permissive auth e2e tests for Gloo Edge Gateway apis using new testing framework (#9436)
    * wip
    
    * wip
    
    * fix istio install
    
    * use global.istioIntegration.enableAutoMtls
    
    * fix imports
    
    * check settings
    
    * fix istio uninstall
    
    * split istio setup
    
    * remove settings client
    
    * pr feedback
    
    * fixed assertions, cleanup
    
    * changelog, debug instructions
    
    * switch to sep headless svc test suites
    
    * classic edge automtls tests
    
    * k8s gw headless svc test specific suite
    
    * regen
    
    * add edge api tests
    
    * passing tests
    
    * passing glooctl tests
    
    * fix uninject
    
    * go mod
    
    * changelog
    
    * mimimize diff
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    * glooctl
    
    * use testInst.GeneratedFiles.TempDir
    
    * remove glooctl path
    
    * pr feedback
    
    * regen
    
    * pr feedback, regen
    
    * add TestGlooctlIstioInjectEdgeApiGateway to test to run in ci
    
    * add TestAutomtlsIstioEdgeApisGateway and TestIstioEdgeApiGateway to ci tests
    
    * remove TestCluster refs
    
    * fix filepath
    
    * remove --istio-namespace, not required for uninject, use default for inject
    
    * fix manifest file name
    
    * add missing istio installation to automtls test
    
    * use edge api headless svc test
    
    ---------
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: changelog-bot <changelog-bot>

---
+++ test/kubernetes/e2e/gloo_gateway_edge/glooctl_istio_inject_test.go
@@ -0,0 +1,78 @@
+package gloo_gateway_edge_test
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/solo-io/gloo/test/kube2e/helper"
+	"github.com/solo-io/gloo/test/kubernetes/e2e/features/glooctl"
+	"github.com/solo-io/gloo/test/kubernetes/e2e/features/istio"
+	"github.com/solo-io/skv2/codegen/util"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/solo-io/gloo/test/kubernetes/e2e"
+	"github.com/solo-io/gloo/test/kubernetes/testutils/gloogateway"
+)
+
+// TestGlooctlIstioInjectEdgeApiGateway is the function which executes a series of tests against a given installation where
+// the k8s Gateway controller is disabled and glooctl istio inject is used to inject istio into the installation
+func TestGlooctlIstioInjectEdgeApiGateway(t *testing.T) {
+	ctx := context.Background()
+	testInstallation := e2e.CreateTestInstallation(
+		t,
+		&gloogateway.Context{
+			InstallNamespace:   "glooctl-edge-api-test",
+			ValuesManifestFile: filepath.Join(util.MustGetThisDir(), "manifests", "gloo-gateway-test-helm.yaml"),
+		},
+	)
+
+	testHelper := e2e.MustTestHelper(ctx, testInstallation)
+	err := testInstallation.AddIstioctl(ctx)
+	if err != nil {
+		t.Fatalf("failed to get istioctl: %v", err)
+	}
+
+	// We register the cleanup function _before_ we actually perform the installation.
+	// This allows us to uninstall Gloo Gateway, in case the original installation only completed partially
+	t.Cleanup(func() {
+		if t.Failed() {
+			testInstallation.PreFailHandler(ctx)
+		}
+
+		testInstallation.UninstallGlooGateway(ctx, func(ctx context.Context) error {
+			return testHelper.UninstallGlooAll()
+		})
+
+		// Uninstall Istio
+		err = testInstallation.UninstallIstio()
+		if err != nil {
+			t.Fatalf("failed to uninstall istio: %v", err)
+		}
+	})
+
+	// Install Istio before Gloo Gateway to make sure istiod is present before istio-proxy
+	err = testInstallation.InstallMinimalIstio(ctx)
+	if err != nil {
+		t.Fatalf("failed to install istio: %v", err)
+	}
+
+	// Install Gloo Gateway with only Gloo Edge Gateway APIs enabled
+	testInstallation.InstallGlooGateway(ctx, func(ctx context.Context) error {
+		return testHelper.InstallGloo(ctx, helper.GATEWAY, 5*time.Minute, helper.ExtraArgs("--values", testInstallation.Metadata.ValuesManifestFile))
+	})
+
+	// NOTE: Order of tests is important here because the tests are dependent on each other (e.g. the inject test must run before the istio test)
+	t.Run("GlooctlIstioInject", func(t *testing.T) {
+		suite.Run(t, glooctl.NewIstioInjectTestingSuite(ctx, testInstallation))
+	})
+
+	t.Run("IstioIntegration", func(t *testing.T) {
+		suite.Run(t, istio.NewGlooTestingSuite(ctx, testInstallation))
+	})
+
+	t.Run("GlooctlIstioUninject", func(t *testing.T) {
+		suite.Run(t, glooctl.NewIstioUninjectTestingSuite(ctx, testInstallation))
+	})
+}
