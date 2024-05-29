3b54e27aea1384f081f97eada3d616e1587c6abd
Jacob Bohanon <jacob.bohanon@solo.io>
2024-05-29 15:56:39 -0400

    Default GatewayParameters use only valid fields for image definitions (#9531)
    * use only valid fields
    
    * helm, oh helm
    
    * add changelog
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    * helm tests
    
    * fix version
    
    * fix version
    
    * helm overrides test
    
    ---------
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: changelog-bot <changelog-bot>
    Co-authored-by: Sam Heilbron <SamHeilbron@gmail.com>

--- install/helm/gloo/templates/_gg-helpers.tpl
+++ install/helm/gloo/templates/_gg-helpers.tpl
@@ -36,3 +36,26 @@ We want this label to remain constant as it is used in glooctl version.
 {{- define "gloo-gateway.constLabels" -}}
 gloo: kube-gateway
 {{- end }}
+
+{{/*
+Images valid for the GatewayParameters
+ref Image api in projects/gateway2/api/v1alpha1/kube/container.proto
+*/}}
+{{- define "gloo-gateway.gatewayParametersImage" -}}
+{{- $image := . -}}
+{{- if $image.registry }}
+registry: {{ $image.registry }}
+{{- end -}}{{/* if $image.registry */}}
+{{- if $image.repository }}
+repository: {{ $image.repository }}
+{{- end -}}{{/* if $image.repository */}}
+{{- if $image.tag }}
+tag: {{ $image.tag }}
+{{- end -}}{{/* if $image.tag */}}
+{{- if $image.digest }}
+digest: {{ $image.digest }}
+{{- end -}}{{/* if $image.digest */}}
+{{- if $image.pullPolicy }}
+pullPolicy: {{ $image.pullPolicy }}
+{{- end -}}{{/* if $image.pullPolicy */}}
+{{- end }}
--- install/test/k8sgateway_test.go
+++ install/test/k8sgateway_test.go
@@ -1,13 +1,17 @@
 package test
 
 import (
+	"encoding/json"
 	"fmt"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/solo-io/gloo/pkg/utils/kubeutils"
+	"github.com/solo-io/gloo/projects/gateway2/pkg/api/gateway.gloo.solo.io/v1alpha1"
+	"github.com/solo-io/gloo/projects/gateway2/pkg/api/gateway.gloo.solo.io/v1alpha1/kube"
 	"github.com/solo-io/gloo/projects/gateway2/wellknown"
 	"github.com/solo-io/gloo/projects/gloo/constants"
+	"github.com/solo-io/gloo/test/gomega/matchers"
 	. "github.com/solo-io/k8s-utils/manifesttestutils"
 )
 
@@ -15,6 +19,7 @@ var _ = Describe("Kubernetes Gateway API integration", func() {
 	var allTests = func(rendererTestCase renderTestCase) {
 		var (
 			testManifest TestManifest
+			valuesArgs   []string
 		)
 		prepareMakefile := func(namespace string, values helmValues) {
 			tm, err := rendererTestCase.renderer.RenderManifest(namespace, values)
@@ -22,52 +27,182 @@ var _ = Describe("Kubernetes Gateway API integration", func() {
 			testManifest = tm
 		}
 
-		It("when kube gateway integration is enabled, relevant resources are rendered", func() {
-			prepareMakefile(namespace, helmValues{
-				valuesArgs: []string{
-					"kubeGateway.enabled=true",
-				},
+		JustBeforeEach(func() {
+			prepareMakefile(namespace, helmValues{valuesArgs: valuesArgs})
+		})
+		When("kube gateway integration is enabled", func() {
+			BeforeEach(func() {
+				valuesArgs = append(valuesArgs, "kubeGateway.enabled=true")
 			})
 
-			// make sure the env variable that enables the controller is set
-			deployment := getDeployment(testManifest, namespace, kubeutils.GlooDeploymentName)
-			Expect(deployment.Spec.Template.Spec.Containers).To(HaveLen(1), "should have exactly 1 container")
-			expectEnvVarExists(deployment.Spec.Template.Spec.Containers[0], constants.GlooGatewayEnableK8sGwControllerEnv, "true")
+			It("relevant resources are rendered", func() {
+				// make sure the env variable that enables the controller is set
+				deployment := getDeployment(testManifest, namespace, kubeutils.GlooDeploymentName)
+				Expect(deployment.Spec.Template.Spec.Containers).To(HaveLen(1), "should have exactly 1 container")
+				expectEnvVarExists(deployment.Spec.Template.Spec.Containers[0], constants.GlooGatewayEnableK8sGwControllerEnv, "true")
 
-			// make sure the GatewayClass and RBAC resources exist (note, since they are all cluster-scoped, they do not have a namespace)
-			testManifest.ExpectUnstructured("GatewayClass", "", "gloo-gateway").NotTo(BeNil())
+				// make sure the GatewayClass and RBAC resources exist (note, since they are all cluster-scoped, they do not have a namespace)
+				testManifest.ExpectUnstructured("GatewayClass", "", "gloo-gateway").NotTo(BeNil())
 
-			testManifest.ExpectUnstructured("GatewayParameters", namespace, wellknown.DefaultGatewayParametersName).NotTo(BeNil())
+				testManifest.ExpectUnstructured("GatewayParameters", namespace, wellknown.DefaultGatewayParametersName).NotTo(BeNil())
 
-			controlPlaneRbacName := fmt.Sprintf("glood-%s.%s", releaseName, namespace)
-			testManifest.Expect("ClusterRole", "", controlPlaneRbacName).NotTo(BeNil())
-			testManifest.Expect("ClusterRoleBinding", "", controlPlaneRbacName+"-binding").NotTo(BeNil())
+				controlPlaneRbacName := fmt.Sprintf("glood-%s.%s", releaseName, namespace)
+				testManifest.Expect("ClusterRole", "", controlPlaneRbacName).NotTo(BeNil())
+				testManifest.Expect("ClusterRoleBinding", "", controlPlaneRbacName+"-binding").NotTo(BeNil())
 
-			deployerRbacName := fmt.Sprintf("glood-%s-deploy.%s", releaseName, namespace)
-			testManifest.Expect("ClusterRole", "", deployerRbacName).NotTo(BeNil())
-			testManifest.Expect("ClusterRoleBinding", "", deployerRbacName+"-binding").NotTo(BeNil())
+				deployerRbacName := fmt.Sprintf("glood-%s-deploy.%s", releaseName, namespace)
+				testManifest.Expect("ClusterRole", "", deployerRbacName).NotTo(BeNil())
+				testManifest.Expect("ClusterRoleBinding", "", deployerRbacName+"-binding").NotTo(BeNil())
+			})
+			It("renders default GatewayParameters", func() {
+
+				gwpUnstructured := testManifest.ExpectCustomResource("GatewayParameters", namespace, wellknown.DefaultGatewayParametersName)
+				Expect(gwpUnstructured).NotTo(BeNil())
+
+				var gwp v1alpha1.GatewayParameters
+				b, err := gwpUnstructured.MarshalJSON()
+				Expect(err).ToNot(HaveOccurred())
+				err = json.Unmarshal(b, &gwp)
+				Expect(err).ToNot(HaveOccurred())
+
+				gwpKube := gwp.Spec.GetKube()
+				Expect(gwpKube).ToNot(BeNil())
+
+				Expect(gwpKube.GetDeployment().GetReplicas().GetValue()).To(Equal(uint32(1)))
+
+				Expect(gwpKube.GetEnvoyContainer().GetImage().GetPullPolicy()).To(Equal(kube.Image_IfNotPresent))
+				Expect(gwpKube.GetEnvoyContainer().GetImage().GetRegistry().GetValue()).To(Equal("quay.io/solo-io"))
+				Expect(gwpKube.GetEnvoyContainer().GetImage().GetRepository().GetValue()).To(Equal("gloo-envoy-wrapper"))
+				Expect(gwpKube.GetEnvoyContainer().GetImage().GetTag().GetValue()).To(Equal(version))
+
+				Expect(gwpKube.GetIstio().GetEnabled().GetValue()).To(BeFalse())
+				Expect(gwpKube.GetIstio().GetIstioContainer().GetImage().GetPullPolicy()).To(Equal(kube.Image_IfNotPresent))
+				Expect(gwpKube.GetIstio().GetIstioContainer().GetImage().GetRegistry().GetValue()).To(Equal("docker.io/istio"))
+				Expect(gwpKube.GetIstio().GetIstioContainer().GetImage().GetRepository().GetValue()).To(Equal("proxyv2"))
+				Expect(gwpKube.GetIstio().GetIstioContainer().GetImage().GetTag().GetValue()).To(Equal("1.21.2"))
+				Expect(gwpKube.GetIstio().GetIstioContainer().GetLogLevel().GetValue()).To(Equal("warning"))
+				Expect(gwpKube.GetIstio().GetIstioDiscoveryAddress().GetValue()).To(Equal("istiod.istio-system.svc:15012"))
+				Expect(gwpKube.GetIstio().GetIstioMetaMeshId().GetValue()).To(Equal("cluster.local"))
+				Expect(gwpKube.GetIstio().GetIstioMetaClusterId().GetValue()).To(Equal("Kubernetes"))
+
+				Expect(gwpKube.GetPodTemplate().GetExtraLabels()).To(matchers.ContainMapElements(map[string]string{"gloo": "kube-gateway"}))
+
+				Expect(gwpKube.GetSdsContainer().GetImage().GetPullPolicy()).To(Equal(kube.Image_IfNotPresent))
+				Expect(gwpKube.GetSdsContainer().GetImage().GetRegistry().GetValue()).To(Equal("quay.io/solo-io"))
+				Expect(gwpKube.GetSdsContainer().GetImage().GetRepository().GetValue()).To(Equal("sds"))
+				Expect(gwpKube.GetSdsContainer().GetImage().GetTag().GetValue()).To(Equal(version))
+				Expect(gwpKube.GetSdsContainer().GetBootstrap().GetLogLevel().GetValue()).To(Equal("info"))
+
+				Expect(gwpKube.GetService().GetType()).To(Equal(kube.Service_LoadBalancer))
+			})
+
+			When("overrides are set", func() {
+				BeforeEach(func() {
+					sdsVals := []string{"101Mi", "201m", "301Mi", "401m"}
+					extraValuesArgs := []string{
+						"global.image.variant=standard",
+						"global.image.tag=global-override-tag",
+						"global.image.registry=global-override-registry",
+						"global.image.repository=global-override-repository",
+						"global.image.pullPolicy=Never",
+						"kubeGateway.gatewayParameters.glooGateway.image.tag=envoy-override-tag",
+						"kubeGateway.gatewayParameters.glooGateway.image.registry=envoy-override-registry",
+						"kubeGateway.gatewayParameters.glooGateway.image.repository=envoy-override-repository",
+						"kubeGateway.gatewayParameters.glooGateway.image.pullPolicy=Always",
+						"kubeGateway.gatewayParameters.glooGateway.proxyDeployment.replicas=5",
+						"kubeGateway.gatewayParameters.glooGateway.service.type=ClusterIP",
+						"global.glooMtls.enabled=false",
+						"global.glooMtls.sds.image.tag=sds-override-tag",
+						"global.glooMtls.sds.image.registry=sds-override-registry",
+						"global.glooMtls.sds.image.repository=sds-override-repository",
+						"global.glooMtls.sds.image.pullPolicy=Never",
+						"global.glooMtls.sds.logLevel=debug",
+						"global.glooMtls.sds.securityContext.runAsUser=999",
+						fmt.Sprintf("global.glooMtls.sdsResources.requests.memory=%s", sdsVals[0]),
+						fmt.Sprintf("global.glooMtls.sdsResources.requests.cpu=%s", sdsVals[1]),
+						fmt.Sprintf("global.glooMtls.sdsResources.limits.memory=%s", sdsVals[2]),
+						fmt.Sprintf("global.glooMtls.sdsResources.limits.cpu=%s", sdsVals[3]),
+						"global.glooMtls.istioProxy.image.tag=istio-override-tag",
+						"global.glooMtls.istioProxy.image.registry=istio-override-registry",
+						"global.glooMtls.istioProxy.image.repository=istio-override-repository",
+						"global.glooMtls.istioProxy.image.pullPolicy=Never",
+						"global.glooMtls.istioProxy.logLevel=debug",
+						"global.glooMtls.istioProxy.securityContext.runAsUser=888",
+						"global.istioSDS.enabled=true",
+					}
+					valuesArgs = append(valuesArgs, extraValuesArgs...)
+				})
+				It("passes overrides to default GatewayParameters", func() {
+
+					gwpUnstructured := testManifest.ExpectCustomResource("GatewayParameters", namespace, wellknown.DefaultGatewayParametersName)
+					Expect(gwpUnstructured).NotTo(BeNil())
+
+					var gwp v1alpha1.GatewayParameters
+					b, err := gwpUnstructured.MarshalJSON()
+					Expect(err).ToNot(HaveOccurred())
+					err = json.Unmarshal(b, &gwp)
+					Expect(err).ToNot(HaveOccurred())
+
+					gwpKube := gwp.Spec.GetKube()
+					Expect(gwpKube).ToNot(BeNil())
+
+					Expect(gwpKube.GetDeployment().GetReplicas().GetValue()).To(Equal(uint32(5)))
+
+					Expect(gwpKube.GetEnvoyContainer().GetImage().GetPullPolicy()).To(Equal(kube.Image_Always))
+					Expect(gwpKube.GetEnvoyContainer().GetImage().GetRegistry().GetValue()).To(Equal("envoy-override-registry"))
+					Expect(gwpKube.GetEnvoyContainer().GetImage().GetRepository().GetValue()).To(Equal("envoy-override-repository"))
+					Expect(gwpKube.GetEnvoyContainer().GetImage().GetTag().GetValue()).To(Equal("envoy-override-tag"))
+
+					Expect(gwpKube.GetIstio().GetEnabled().GetValue()).To(BeTrue())
+					Expect(gwpKube.GetIstio().GetIstioContainer().GetImage().GetPullPolicy()).To(Equal(kube.Image_Never))
+					Expect(gwpKube.GetIstio().GetIstioContainer().GetImage().GetRegistry().GetValue()).To(Equal("istio-override-registry"))
+					Expect(gwpKube.GetIstio().GetIstioContainer().GetImage().GetRepository().GetValue()).To(Equal("istio-override-repository"))
+					Expect(gwpKube.GetIstio().GetIstioContainer().GetImage().GetTag().GetValue()).To(Equal("istio-override-tag"))
+					Expect(gwpKube.GetIstio().GetIstioContainer().GetLogLevel().GetValue()).To(Equal("debug"))
+					Expect(gwpKube.GetIstio().GetIstioDiscoveryAddress().GetValue()).To(Equal("istiod.istio-system.svc:15012"))
+					Expect(gwpKube.GetIstio().GetIstioMetaMeshId().GetValue()).To(Equal("cluster.local"))
+					Expect(gwpKube.GetIstio().GetIstioMetaClusterId().GetValue()).To(Equal("Kubernetes"))
+
+					Expect(gwpKube.GetPodTemplate().GetExtraLabels()).To(matchers.ContainMapElements(map[string]string{"gloo": "kube-gateway"}))
+
+					Expect(gwpKube.GetSdsContainer().GetImage().GetPullPolicy()).To(Equal(kube.Image_Never))
+					Expect(gwpKube.GetSdsContainer().GetImage().GetRegistry().GetValue()).To(Equal("sds-override-registry"))
+					Expect(gwpKube.GetSdsContainer().GetImage().GetRepository().GetValue()).To(Equal("sds-override-repository"))
+					Expect(gwpKube.GetSdsContainer().GetImage().GetTag().GetValue()).To(Equal("sds-override-tag"))
+					Expect(gwpKube.GetSdsContainer().GetBootstrap().GetLogLevel().GetValue()).To(Equal("debug"))
+
+					Expect(gwpKube.GetService().GetType()).To(Equal(kube.Service_ClusterIP))
+				})
+				// set overrides for all fields that get translated into gwp and make sure they make it in
+				//
+				// this should include image fields that are not valid in gwp (e.g. variant)
+			})
 		})
 
-		It("by default, kube gateway integration is disabled, and relevant resources are not rendered", func() {
-			prepareMakefile(namespace, helmValues{
-				valuesArgs: []string{},
+		When("kube gateway integration is disabled (default)", func() {
+			BeforeEach(func() {
+				valuesArgs = append(valuesArgs, "kubeGateway.enabled=false")
 			})
 
-			// the env variable that enables the controller should not be set
-			deployment := getDeployment(testManifest, namespace, kubeutils.GlooDeploymentName)
-			Expect(deployment.Spec.Template.Spec.Containers).To(HaveLen(1), "should have exactly 1 container")
-			expectEnvVarDoesNotExist(deployment.Spec.Template.Spec.Containers[0], constants.GlooGatewayEnableK8sGwControllerEnv)
+			It("relevant resources are not rendered", func() {
+				// the env variable that enables the controller should not be set
+				deployment := getDeployment(testManifest, namespace, kubeutils.GlooDeploymentName)
+				Expect(deployment.Spec.Template.Spec.Containers).To(HaveLen(1), "should have exactly 1 container")
+				expectEnvVarDoesNotExist(deployment.Spec.Template.Spec.Containers[0], constants.GlooGatewayEnableK8sGwControllerEnv)
 
-			// the RBAC resources should not be rendered
-			testManifest.ExpectUnstructured("GatewayClass", "", "gloo-gateway").To(BeNil())
+				// the RBAC resources should not be rendered
+				testManifest.ExpectUnstructured("GatewayClass", "", "gloo-gateway").To(BeNil())
 
-			controlPlaneRbacName := fmt.Sprintf("glood-%s.%s", releaseName, namespace)
-			testManifest.Expect("ClusterRole", "", controlPlaneRbacName).To(BeNil())
-			testManifest.Expect("ClusterRoleBinding", "", controlPlaneRbacName+"-binding").To(BeNil())
+				testManifest.ExpectUnstructured("GatewayParameters", namespace, wellknown.DefaultGatewayParametersName).To(BeNil())
 
-			deployerRbacName := fmt.Sprintf("glood-%s-deploy.%s", releaseName, namespace)
-			testManifest.Expect("ClusterRole", "", deployerRbacName).To(BeNil())
-			testManifest.Expect("ClusterRoleBinding", "", deployerRbacName+"-binding").To(BeNil())
+				controlPlaneRbacName := fmt.Sprintf("glood-%s.%s", releaseName, namespace)
+				testManifest.Expect("ClusterRole", "", controlPlaneRbacName).To(BeNil())
+				testManifest.Expect("ClusterRoleBinding", "", controlPlaneRbacName+"-binding").To(BeNil())
+
+				deployerRbacName := fmt.Sprintf("glood-%s-deploy.%s", releaseName, namespace)
+				testManifest.Expect("ClusterRole", "", deployerRbacName).To(BeNil())
+				testManifest.Expect("ClusterRoleBinding", "", deployerRbacName+"-binding").To(BeNil())
+			})
 		})
 	}
 	runTests(allTests)
