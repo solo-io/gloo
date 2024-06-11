edbc19d585c48b79c541ff4ac3229f03793191cc
Jacob Bohanon <jacob.bohanon@solo.io>
2024-06-11 14:09:22 -0400

    change import back

--- test/kube2e/helper/install.go
+++ test/kube2e/helper/install.go
@@ -15,9 +15,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/rotisserie/eris"
-	"github.com/solo-io/gloo/test/testutils/hack/k8s-utils/testutils/kube"
 	"github.com/solo-io/go-utils/log"
 	"github.com/solo-io/go-utils/testutils/exec"
+	"github.com/solo-io/k8s-utils/testutils/kube"
 )
 
 const (
