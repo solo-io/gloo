3d5228ba8d4a7a7b817ae143a7b915882b3c1036
Lawrence G <lawrence.gadban@solo.io>
2024-06-13 08:53:17 -0500

    Add HttpListenerOption Policy Impl (#9605)
    * add HttpListenerOption plugin
    
    * cleanup, better LisOpt example (access logs)
    
    * add httpListenerOption e2e test
    
    * add "g" prefix to shortnames to match other gloo crds
    
    * cleanup and polish
    
    * cleanup naming boilerplate...
    
    * changelog
    
    * fix crd
    
    * fixup
    
    * pr feedback
    
    * add negative e2e test, only run in standard gw test suite
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    ---------
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: changelog-bot <changelog-bot>

--- projects/gateway2/translator/plugins/listeneroptions/listener_options_plugin.go
+++ projects/gateway2/translator/plugins/listeneroptions/listener_options_plugin.go
@@ -44,13 +44,9 @@ func (p *plugin) ApplyListenerPlugin(
 		return nil
 	}
 
+	// use the first option (highest in priority)
+	// see for more context: https://github.com/solo-io/solo-projects/issues/6313
 	optToUse := attachedOptions[0]
-
-	if optToUse == nil {
-		// unsure if this should be an error case
-		return nil
-	}
-
 	outListener.Options = optToUse.Spec.GetOptions()
 
 	return nil
