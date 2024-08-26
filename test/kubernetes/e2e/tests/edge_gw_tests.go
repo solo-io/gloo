6dc7e1751e4882887e1381427505f8ffb0fa5e86
sheidkamp <seth.heidkamp@gmail.com>
2024-08-26 16:00:39 -0400

    removing non-backported client_tls feature

--- test/kubernetes/e2e/tests/edge_gw_tests.go
+++ test/kubernetes/e2e/tests/edge_gw_tests.go
@@ -4,7 +4,6 @@ import (
 	"github.com/solo-io/gloo/test/kubernetes/e2e"
 	"github.com/solo-io/gloo/test/kubernetes/e2e/features/admin_server"
 	"github.com/solo-io/gloo/test/kubernetes/e2e/features/basicrouting"
-	"github.com/solo-io/gloo/test/kubernetes/e2e/features/client_tls"
 	"github.com/solo-io/gloo/test/kubernetes/e2e/features/headless_svc"
 	"github.com/solo-io/gloo/test/kubernetes/e2e/features/port_routing"
 	"github.com/solo-io/gloo/test/kubernetes/e2e/features/validation/validation_allow_warnings"
@@ -19,7 +18,6 @@ func EdgeGwSuiteRunner() e2e.SuiteRunner {
 	edgeGwSuiteRunner.Register("ValidationRejectInvalid", validation_reject_invalid.NewTestingSuite)
 	edgeGwSuiteRunner.Register("ValidationAllowWarnings", validation_allow_warnings.NewTestingSuite)
 	edgeGwSuiteRunner.Register("GlooAdminServer", admin_server.NewTestingSuite)
-	edgeGwSuiteRunner.Register("ClientTls", client_tls.NewTestingSuite)
 	edgeGwSuiteRunner.Register("BasicRouting", basicrouting.NewBasicEdgeRoutingSuite)
 
 	return edgeGwSuiteRunner
