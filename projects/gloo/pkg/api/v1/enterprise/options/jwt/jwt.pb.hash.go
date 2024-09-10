d0928bd1dc5e4adc163c0313b1e7576fe796e68a
Nina Polshakova <nina.polshakova@solo.io>
2024-09-10 12:49:14 -0400

    Separate `allow_missing` option for jwt (#9985)

--- projects/gloo/pkg/api/v1/enterprise/options/jwt/jwt.pb.hash.go
+++ projects/gloo/pkg/api/v1/enterprise/options/jwt/jwt.pb.hash.go
@@ -250,6 +250,11 @@ func (m *VhostExtension) Hash(hasher hash.Hash64) (uint64, error) {
 		return 0, err
 	}
 
+	err = binary.Write(hasher, binary.LittleEndian, m.GetValidationPolicy())
+	if err != nil {
+		return 0, err
+	}
+
 	return hasher.Sum64(), nil
 }
 
