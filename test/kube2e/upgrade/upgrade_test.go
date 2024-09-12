1742bf45344d78d9c109847c7d9e9ad126576c84
nfuden <nathan.fudenberg@solo.io>
2024-09-12 09:14:36 -0400

    test/kube2e/upgrade: Need to add some more linters locally

--- test/kube2e/upgrade/upgrade_test.go
+++ test/kube2e/upgrade/upgrade_test.go
@@ -458,7 +458,7 @@ func preUpgradeDataSetup(testHelper *helper.SoloTestHelper) {
 	//hello world example
 	resDirPath := filepath.Join(util.MustGetThisDir(), "testdata", "petstore")
 	resourceFiles, err := os.ReadDir(resDirPath)
-	Expect(err).To(BeNil())
+	Expect(err).ToNot(HaveOccurred())
 
 	// Note that this is really unclean, its not ordered and not our current standard.
 	for _, toApplyFile := range resourceFiles {
@@ -473,7 +473,7 @@ func postUpgradeDataStep(testHelper *helper.SoloTestHelper) {
 	//hello world example
 	resDirPath := filepath.Join(util.MustGetThisDir(), "testdata", "petstoreupdated")
 	resourceFiles, err := os.ReadDir(resDirPath)
-	Expect(err).To(BeNil())
+	Expect(err).ToNot(HaveOccurred())
 
 	// Note that this is really unclean, its not ordered and not our current standard.
 	for _, toApplyFile := range resourceFiles {
