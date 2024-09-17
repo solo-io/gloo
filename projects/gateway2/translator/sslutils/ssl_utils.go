2b268c2efe03f050dbd06ff5474cb467a47abf5d
Nathan Fudenberg <nathan.fudenberg@solo.io>
2024-09-17 12:26:45 -0400

    projects/utils/ssl: Add a check for ssl secrets that will be rejected by envoy but not go (#10047)
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: Bernie Birnbaum <bewebi@earthlink.net>

--- projects/gateway2/translator/sslutils/ssl_utils.go
+++ projects/gateway2/translator/sslutils/ssl_utils.go
@@ -3,9 +3,11 @@ package sslutils
 import (
 	"crypto/tls"
 	"fmt"
+	"strings"
 
 	"github.com/rotisserie/eris"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/util/cert"
 )
 
 var (
@@ -30,6 +32,10 @@ func validatedCertData(sslSecret *corev1.Secret) error {
 	privateKey := sslSecret.Data[corev1.TLSPrivateKeyKey]
 	rootCa := sslSecret.Data[corev1.ServiceAccountRootCAKey]
 
+	// we always return an error when the certChain and/or privateKey are invalid
+	// in theory we could propagate only the valid blocks of the certChain (ie the output of cert.ParseCertsPEM(certChain))º
+	// and this would be accepted by Envoy, however we choose to maintain consistency between the secret at rest and in
+	// Envoy, which also maintains consistency with existing UX
 	err := isValidSslKeyPair(certChain, privateKey, rootCa)
 	if err != nil {
 		return InvalidTlsSecretError(sslSecret, err)
@@ -38,12 +44,34 @@ func validatedCertData(sslSecret *corev1.Secret) error {
 	return nil
 }
 
+// isValidSslKeyPair validates that the cert and key are a valid pair
+// It previously only checked in go but now also checks that nothing is lost in cert encoding
 func isValidSslKeyPair(certChain, privateKey, rootCa []byte) error {
 
 	if len(certChain) == 0 || len(privateKey) == 0 {
 		return NoCertificateFoundError
 	}
 
-	_, err := tls.X509KeyPair([]byte(certChain), []byte(privateKey))
+	_, err := tls.X509KeyPair(certChain, privateKey)
+	if err != nil {
+		return err
+	}
+	// validate that the parsed piece is valid
+	// this is still faster than a call out to openssl despite this second parsing pass of the cert
+	// pem parsing in go is permissive while envoy is not
+	// this might not be needed once we have larger envoy validation
+	candidateCert, err := cert.ParseCertsPEM(certChain)
+	if err != nil {
+		return err
+	}
+	reencoded, err := cert.EncodeCertificates(candidateCert...)
+	if err != nil {
+		return err
+	}
+	trimmedEncoded := strings.TrimSpace(string(reencoded))
+	if trimmedEncoded != strings.TrimSpace(string(certChain)) {
+		return fmt.Errorf("certificate chain does not match parsed certificate")
+	}
+
 	return err
 }
