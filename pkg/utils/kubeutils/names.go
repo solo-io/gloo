f89eec59130f473fe66b519da91d1f4277a19f00
Jenny Shu <28537278+jenshu@users.noreply.github.com>
2024-09-24 14:47:28 -0400

    Set GOMEMLIMIT / GOMAXPROCS [1.18] (#10061)
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>

--- pkg/utils/kubeutils/names.go
+++ pkg/utils/kubeutils/names.go
@@ -7,4 +7,6 @@ const (
 	// GlooXdsPortName is the name of the port in the Gloo Gateway control plane Kubernetes Service that serves xDS config.
 	// See: install/helm/gloo/templates/2-gloo-service.yaml
 	GlooXdsPortName = "grpc-xds"
+
+	DiscoveryDeploymentName = "discovery"
 )
