e5c52524b6ae474aab8363cd78db330f429207aa
Ashish Banerjee <ashish.banerjee@solo.io>
2024-10-01 15:55:39 -0400

    Add test for span transformations

--- projects/gloo/pkg/plugins/transformation/plugin_test.go
+++ projects/gloo/pkg/plugins/transformation/plugin_test.go
@@ -39,6 +39,21 @@ var _ = Describe("Plugin", func() {
 		cancel()
 	})
 
+	It("Can write span transformations", func() {
+		outputTransformation := &envoytransformation.Transformation{
+			TransformationType: &envoytransformation.Transformation_TransformationTemplate{
+				TransformationTemplate: &envoytransformation.TransformationTemplate{
+					SpanTransformer: &envoytransformation.TransformationTemplate_SpanTransformer{
+						Name: &envoytransformation.InjaTemplate{
+							Text: `{{ header("host")}}`,
+						},
+					},
+				},
+			},
+		}
+		_ = outputTransformation
+	})
+
 	Context("translate transformations", func() {
 
 		BeforeEach(func() {
