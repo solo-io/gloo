2aff8d7d8ddc8730eefc00f8ee9c5b2191853bbf
Sam Heilbron <SamHeilbron@gmail.com>
2024-10-03 20:14:44 -0500

    test/kubernetes/e2e: migrate helm tests (#10165)
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>

--- test/kubernetes/e2e/tests/helm_test.go
+++ test/kubernetes/e2e/tests/helm_test.go
@@ -46,8 +46,46 @@ func TestHelm(t *testing.T) {
 		testInstallation.UninstallGlooGatewayWithTestHelper(ctx, testHelper)
 	})
 
-	// Install Gloo Gateway with correct validation settings
 	testInstallation.InstallGlooGatewayWithTestHelper(ctx, testHelper, 5*time.Minute)
 
 	HelmSuiteRunner().Run(ctx, t, testInstallation)
+}
+
+// TestHelmSettings is the function which executes a series tests for our features/helm_settings
+// See features/helm_settings/suite.go for the reason we split these tests from our standard helm tests
+func TestHelmSettings(t *testing.T) {
+	ctx := context.Background()
+	installNs, nsEnvPredefined := envutils.LookupOrDefault(testutils.InstallNamespace, "helm-settings-test")
+	testInstallation := e2e.CreateTestInstallation(
+		t,
+		&gloogateway.Context{
+			InstallNamespace:          installNs,
+			ProfileValuesManifestFile: e2e.EdgeGatewayProfilePath,
+			ValuesManifestFile:        e2e.ManifestPath("test-helm.yaml"),
+		},
+	)
+
+	testHelper := e2e.MustTestHelper(ctx, testInstallation)
+
+	// Set the env to the install namespace if it is not already set
+	if !nsEnvPredefined {
+		os.Setenv(testutils.InstallNamespace, installNs)
+	}
+
+	// We register the cleanup function _before_ we actually perform the installation.
+	// This allows us to uninstall Gloo Gateway, in case the original installation only completed partially
+	t.Cleanup(func() {
+		if !nsEnvPredefined {
+			os.Unsetenv(testutils.InstallNamespace)
+		}
+		if t.Failed() {
+			testInstallation.PreFailHandler(ctx)
+		}
+
+		testInstallation.UninstallGlooGatewayWithTestHelper(ctx, testHelper)
+	})
+
+	testInstallation.InstallGlooGatewayWithTestHelper(ctx, testHelper, 5*time.Minute)
+
+	HelmSettingsSuiteRunner().Run(ctx, t, testInstallation)
 }
