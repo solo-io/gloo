6b09a8c30efb414418f5559384d68e12c1c7277d
Benjamin Leggett <benjamin.leggett@solo.io>
2024-10-30 17:06:42 -0400

    Revert "This fixes tests..why?"
    This reverts commit fd47fe8c55b1debad478ddbad222ab79797baa1f.

--- projects/gloo/cli/pkg/cmd/gateway/dump.go
+++ projects/gloo/cli/pkg/cmd/gateway/dump.go
@@ -1,17 +1,10 @@
 package gateway
 
 import (
-	"context"
 	"archive/zip"
 	"fmt"
-	"strings"
-	"strconv"
 	"os"
 	"time"
-	"io"
-	"log"
-	"net/http"
-	"os/exec"
 
 	"github.com/solo-io/go-utils/cliutils"
 
@@ -20,7 +13,6 @@ import (
 	"github.com/solo-io/gloo/projects/gloo/cli/pkg/cmd/options"
 	"github.com/solo-io/gloo/projects/gloo/pkg/defaults"
 	"github.com/spf13/cobra"
-	"github.com/solo-io/solo-kit/pkg/errors"
 )
 
 func dumpCfgCmd(opts *options.Options, optionsFunc ...cliutils.OptionsFunc) *cobra.Command {
@@ -127,70 +119,4 @@ func createArchiveFile() (*os.File, error) {
 		fmt.Printf("error creating proxy snapshot archive: %f\n", err)
 	}
 	return f, err
-}
-// GetEnvoyAdminData returns the response from the envoy admin interface based on the `path` specified within the defined timeout.
-// Note that a `/` will be prepended to path if it does not exist.
-func GetEnvoyAdminData(ctx context.Context, proxyName, namespace, path string, timeout time.Duration) (string, error) {
-	if !strings.HasPrefix(path, "/") {
-		path = "/" + path
-	}
-	adminPort := strconv.Itoa(int(defaults.EnvoyAdminPort))
-	portFwd := exec.Command("kubectl", "port-forward", "-n", namespace,
-		"deployment/"+proxyName, adminPort)
-	portFwd.Stdout = os.Stderr
-	portFwd.Stderr = os.Stderr
-	if err := portFwd.Start(); err != nil {
-		return "", errors.Wrapf(err, "failed to start port-forward")
-	}
-	defer func() {
-		if portFwd.Process != nil {
-			portFwd.Process.Kill()
-		}
-	}()
-	result := make(chan string)
-	errs := make(chan error)
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-			}
-			res, err := http.Get("http://localhost:" + adminPort + path)
-			if err != nil {
-				errs <- err
-				time.Sleep(time.Millisecond * 250)
-				continue
-			}
-			if res.StatusCode != http.StatusOK {
-				errs <- errors.Errorf("invalid status code: %v %v", res.StatusCode, res.Status)
-				time.Sleep(time.Millisecond * 250)
-				continue
-			}
-			b, err := io.ReadAll(res.Body)
-			if err != nil {
-				errs <- err
-				time.Sleep(time.Millisecond * 250)
-				continue
-			}
-			res.Body.Close()
-			result <- string(b)
-			return
-		}
-	}()
-
-	timer := time.Tick(timeout)
-
-	for {
-		select {
-		case <-ctx.Done():
-			return "", errors.Errorf("cancelled")
-		case err := <-errs:
-			log.Printf("connecting to envoy failed with err %v", err.Error())
-		case res := <-result:
-			return res, nil
-		case <-timer:
-			return "", errors.Errorf("timed out trying to connect to Envoy admin port")
-		}
-	}
 }
