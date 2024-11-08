cf84b4acae30d1002219a76307e3273cf07e4854
Ryan Olds <ryanrolds@gmail.com>
2024-11-08 10:19:31 -0800

    [1.17]: Early Header Mutation (#10295)
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: Nathan Fudenberg <nathan.fudenberg@solo.io>
    Co-authored-by: David Jumani <dj.davidjumani1994@gmail.com>

--- projects/gloo/pkg/api/v1/options/headers/headers.pb.hash.go
+++ projects/gloo/pkg/api/v1/options/headers/headers.pb.hash.go
@@ -106,6 +106,54 @@ func (m *HeaderManipulation) Hash(hasher hash.Hash64) (uint64, error) {
 }
 
 // Hash function
+func (m *EarlyHeaderManipulation) Hash(hasher hash.Hash64) (uint64, error) {
+	if m == nil {
+		return 0, nil
+	}
+	if hasher == nil {
+		hasher = fnv.New64()
+	}
+	var err error
+	if _, err = hasher.Write([]byte("headers.options.gloo.solo.io.github.com/solo-io/gloo/projects/gloo/pkg/api/v1/options/headers.EarlyHeaderManipulation")); err != nil {
+		return 0, err
+	}
+
+	for _, v := range m.GetHeadersToAdd() {
+
+		if h, ok := interface{}(v).(safe_hasher.SafeHasher); ok {
+			if _, err = hasher.Write([]byte("")); err != nil {
+				return 0, err
+			}
+			if _, err = h.Hash(hasher); err != nil {
+				return 0, err
+			}
+		} else {
+			if fieldValue, err := hashstructure.Hash(v, nil); err != nil {
+				return 0, err
+			} else {
+				if _, err = hasher.Write([]byte("")); err != nil {
+					return 0, err
+				}
+				if err := binary.Write(hasher, binary.LittleEndian, fieldValue); err != nil {
+					return 0, err
+				}
+			}
+		}
+
+	}
+
+	for _, v := range m.GetHeadersToRemove() {
+
+		if _, err = hasher.Write([]byte(v)); err != nil {
+			return 0, err
+		}
+
+	}
+
+	return hasher.Sum64(), nil
+}
+
+// Hash function
 func (m *HeaderValueOption) Hash(hasher hash.Hash64) (uint64, error) {
 	if m == nil {
 		return 0, nil
