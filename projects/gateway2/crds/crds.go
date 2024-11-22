25aa23276726cd21282bc5f7709437a5b76f8b23
Seth Heidkamp <61526534+sheidkamp@users.noreply.github.com>
2024-11-22 12:40:29 -0500

    Remove symlink to crd file in kubernetes e2e test data (#10388)
    Co-authored-by: Sam Heilbron <SamHeilbron@gmail.com>

--- projects/gateway2/crds/crds.go
+++ projects/gateway2/crds/crds.go
@@ -2,7 +2,25 @@ package crds
 
 import (
 	_ "embed"
+	"path"
+	"path/filepath"
+	"runtime"
 )
 
 //go:embed gateway-crds.yaml
 var GatewayCrds []byte
+
+func getDirectory() string {
+	_, filename, _, _ := runtime.Caller(0)
+	return path.Dir(filename)
+}
+
+// directory is the absolute path to the directory containing the crd files
+// It can't change at runtime, so we can cache it
+var directory = getDirectory()
+
+// AbsPathToCrd returns the absolute path to directory in which crds are stored (currently the same directory as this file)
+// Used for tests to find the crd files if needed
+func AbsPathToCrd(crdFile string) string {
+	return filepath.Join(directory, crdFile)
+}
