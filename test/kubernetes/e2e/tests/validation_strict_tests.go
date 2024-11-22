a910a2822c8c2239aa60d23d0d702f00e1a6bec1
sheidkamp <seth.heidkamp@gmail.com>
2024-11-22 09:18:41 -0500

    Update validation_strict_tests.go

--- test/kubernetes/e2e/tests/validation_strict_tests.go
+++ test/kubernetes/e2e/tests/validation_strict_tests.go
@@ -8,16 +8,13 @@ import (
 )
 
 func ValidationStrictSuiteRunner() e2e.SuiteRunner {
-	validationSuiteRunner := e2e.NewSuiteRunner(false)
-
-	validationSuiteRunner.Register("ValidationStrictWarnings", validation_strict_warnings.NewTestingSuite)
-	validationSuiteRunner.Register("ValidationRejectInvalid", validation_reject_invalid.NewTestingSuite)
+	validationSuiteRunner := ValidationStrictSuiteRunnerForExport()
 	validationSuiteRunner.Register("ValidationSplitWebhook", split_webhook.NewTestingSuite)
 
 	return validationSuiteRunner
 }
 
-// This is a copy of the above function with the "ValidationSplitWebhook" test removed
+// ValidationStrictSuiteRunnerForExport is used to export the validation tests that can be run when the project is imported as a helm dependency
 // The "ValidationSplitWebhook" test has logic that depends on the helm chart/values/helpers
 // that are not valid when the project is imported as a helm dependency
 // https://github.com/k8sgateway/k8sgateway/issues/10374 has been created to create a fix for this
