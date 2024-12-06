1d860c4d1ce417a0e4eda5b19b1a30c67c27781e
arianaw66 <ariana.weinstock@solo.io>
2024-12-06 14:17:18 -0500

    basic CLI unit testing, not relying on k8s

--- projects/gloo/cli/pkg/cmd/debug/root_test.go
+++ projects/gloo/cli/pkg/cmd/debug/root_test.go
@@ -3,8 +3,16 @@ package debug_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
+	"os"
+
 	"github.com/solo-io/gloo/projects/gloo/cli/pkg/helpers"
 	"github.com/solo-io/gloo/projects/gloo/cli/pkg/testutils"
+)
+
+const (
+	defaultOutDir = "debug"
+	kubeStateFile = defaultOutDir + "/kube-state.log"
 )
 
 var _ = Describe("Debug", func() {
@@ -13,9 +21,38 @@ var _ = Describe("Debug", func() {
 		helpers.UseMemoryClients()
 	})
 
-	It("should not require a subcommand after debug", func() {
+	AfterEach(func() {
+		Expect(os.RemoveAll(defaultOutDir)).NotTo(HaveOccurred())
+	})
+
+	It("should support the top level debug command and should populate the kube-state.log file", func() {
 		err := testutils.Glooctl("debug")
+		Expect(err).NotTo(HaveOccurred())
+
+		checkDebugKube()
+	})
+
+	It("should support the debug kube subcommand and should populate the kube-state.log file", func() {
+		err := testutils.Glooctl("debug kube")
+		Expect(err).NotTo(HaveOccurred())
+
+		checkDebugKube()
+	})
+
+	It("should support the debug gloo subcommand", func() {
+		err := testutils.Glooctl("debug gloo")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should support the debug envoy subcommand", func() {
+		err := testutils.Glooctl("debug envoy")
 		Expect(err).NotTo(HaveOccurred())
 	})
 
 })
+
+func checkDebugKube() {
+	kubeStateBytes, err := os.ReadFile(kubeStateFile)
+	Expect(err).NotTo(HaveOccurred(), kubeStateFile+" file should be present")
+	Expect(kubeStateBytes).NotTo(BeEmpty())
+}
