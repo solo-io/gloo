b5aa13f810efe54a9ca3a90a7024c9440dc473c7
Jenny Shu <jenny.shu@solo.io>
2024-12-10 20:17:06 -0500

    refactor

--- test/kubernetes/e2e/features/upgrade/types.go
+++ test/kubernetes/e2e/features/upgrade/types.go
@@ -1,13 +1,15 @@
-package zero_downtime_rollout
+package upgrade
 
 import (
 	"path/filepath"
 
+	"github.com/solo-io/gloo/test/kubernetes/e2e/defaults"
+	"github.com/solo-io/gloo/test/kubernetes/e2e/tests/base"
 	"github.com/solo-io/skv2/codegen/util"
-
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var (
@@ -25,6 +27,15 @@ var (
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "hey",
 			Namespace: "hey",
+		},
+	}
+
+	testCases = map[string]*base.TestCase{
+		"TestZeroDowntimeUpgrade": {
+			SimpleTestCase: base.SimpleTestCase{
+				Manifests: []string{defaults.CurlPodManifest, serviceManifest, routeWithServiceManifest},
+				Resources: []client.Object{proxyDeployment, proxyService, defaults.CurlPod, heyPod},
+			},
 		},
 	}
 )
