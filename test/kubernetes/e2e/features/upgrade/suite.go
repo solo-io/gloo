b3c44e55cba9e03c5cced56a4f1f8b0c977d7bc0
David Jumani <dj.davidjumani1994@gmail.com>
2024-12-13 12:43:24 +0530

    fix

--- test/kubernetes/e2e/features/upgrade/suite.go
+++ test/kubernetes/e2e/features/upgrade/suite.go
@@ -38,7 +38,7 @@ func NewTestingSuite(ctx context.Context, testInst *e2e.TestInstallation) suite.
 	testInst.Metadata.ReleasedVersion = releaseVersion
 
 	return &testingSuite{
-		base.NewBaseTestingSuiteWithUpgrades(ctx, testInst, testHelper, base.SimpleTestCase{}, nil),
+		base.NewBaseTestingSuiteWithUpgrades(ctx, testInst, testHelper, base.SimpleTestCase{}, testCases),
 	}
 }
 
