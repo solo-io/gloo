1bed38058d42bf163538321fbcc88bf4313d9c86
Shashank Ram <21697719+shashankram@users.noreply.github.com>
2024-12-16 08:58:30 -0800

    gateway2/delegation: enable inherited policy overrides (#10470)
    Signed-off-by: Shashank Ram <shashank.ram@solo.io>

--- projects/gloo/pkg/utils/merge.go
+++ projects/gloo/pkg/utils/merge.go
@@ -2,8 +2,27 @@ package utils
 
 import (
 	"reflect"
+	"strings"
+
+	"k8s.io/apimachinery/pkg/util/sets"
 
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
+)
+
+const wildcardField = "*"
+
+// OptionsMergeResult is an enum indicating the result of the merge of options from src to dst.
+type OptionsMergeResult int
+
+const (
+	// OptionsMergedNone indicates that no fields were merged from src to dst.
+	OptionsMergedNone OptionsMergeResult = iota
+
+	// OptionsMergedPartial indicates that some but not all fields were merged from src to dst.
+	OptionsMergedPartial
+
+	// OptionsMergedFull indicates that all fields set in dst were overwritten by src.
+	OptionsMergedFull
 )
 
 // ShallowMerge sets dst to the value of src, if src is non-zero and dst is zero-valued or overwrite=true.
@@ -97,4 +116,64 @@ func ShallowMergeVirtualHostOptions(dst, src *v1.VirtualHostOptions) (*v1.Virtua
 	}
 
 	return dst, overwrote
+}
+
+// MergeRouteOptionsWithOverrides merges the top-level fields of src that are allowed to be merged into dst.
+// allowedOverrides is a set of field names in lowercase that are allowed to be merged, and may contain a wildcard field "*"
+// to allow all fields to be merged.
+// When allowedOverrides is empty, only fields unset in dst and set in src will be merged into dst.
+//
+// It returns an Enum indicating the result of the merge.
+func MergeRouteOptionsWithOverrides(dst, src *v1.RouteOptions, allowedOverrides sets.Set[string]) (*v1.RouteOptions, OptionsMergeResult) {
+	if src == nil {
+		return dst, OptionsMergedNone
+	}
+
+	if dst == nil {
+		return src.Clone().(*v1.RouteOptions), OptionsMergedFull
+	}
+
+	dstValue, srcValue := reflect.ValueOf(dst).Elem(), reflect.ValueOf(src).Elem()
+
+	// By default, do not allow fields in src to overwrite fields in dst.
+	// If allowedOverrides is non-empty, enable overwrites for the allowed fields.
+	overwriteByDefault := false
+	if allowedOverrides.Len() > 0 {
+		overwriteByDefault = true
+	}
+
+	var srcFieldsUsed int
+	var dstFieldsSet int
+	var dstFieldsOverwritten int
+	for i := range dstValue.NumField() {
+		dstField, srcField := dstValue.Field(i), srcValue.Field(i)
+		if overwriteByDefault && !(allowedOverrides.Has(wildcardField) ||
+			allowedOverrides.Has(strings.ToLower(dstValue.Type().Field(i).Name))) {
+			continue
+		}
+		// NOTE: important to pre-compute this for use in the conditional below
+		// because dstFieldsOverwritten needs to be incremented based on the original value of dstField
+		// and not the state of the field after the merge
+		dstOverridable := dstField.CanSet() && !isEmptyValue(dstField)
+		if dstOverridable {
+			dstFieldsSet++
+		}
+		if srcOverride := ShallowMerge(dstField, srcField, overwriteByDefault); srcOverride {
+			srcFieldsUsed++
+			if dstOverridable {
+				dstFieldsOverwritten++
+			}
+		}
+	}
+
+	var overrideState OptionsMergeResult
+	if srcFieldsUsed == 0 {
+		overrideState = OptionsMergedNone
+	} else if dstFieldsSet == dstFieldsOverwritten {
+		overrideState = OptionsMergedFull
+	} else {
+		overrideState = OptionsMergedPartial
+	}
+
+	return dst, overrideState
 }
