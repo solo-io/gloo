d8439a8dda6d6392dc4f0bd6c6429276e6a741c6
Kevin Shelaga <kevin.shelaga@solo.io>
2025-01-08 15:55:46 -0500

    comments fix

--- test/kubernetes/testutils/assertions/service.go
+++ test/kubernetes/testutils/assertions/service.go
@@ -17,8 +17,6 @@ func (p *Provider) EventuallyExternalTrafficPolicy(ctx context.Context, service 
 		innerG.Expect(service.Spec.ExternalTrafficPolicy).To(externalTrafficPolicyMatcher, "externalTrafficPolicy to match")
 	}).
 		WithContext(ctx).
-		// It may take some time for pods to initialize and pull images from remote registries.
-		// Therefore, we set a longer timeout, to account for latency that may exist.
 		WithTimeout(time.Second * 30).
 		WithPolling(time.Millisecond * 200).
 		Should(Succeed())
