44e2272dfc21eb89ee449b7b38d4c1d1ee90e19a
Kevin Shelaga <kevin.shelaga@solo.io>
2025-01-08 18:06:00 -0500

    add missing tests for external traffic policy gateway params

---
+++ pkg/utils/kubeutils/service.go
@@ -0,0 +1,26 @@
+package kubeutils
+
+import (
+	"context"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+// GetService gets the service from the provided name/namespace
+func GetService(
+	ctx context.Context,
+	kubeClient *kubernetes.Clientset,
+	serviceName string,
+	serviceNamespace string,
+) (*corev1.Service, error) {
+
+	service, err := kubeClient.CoreV1().Services(serviceNamespace).Get(ctx, serviceName, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	return service, nil
+}
