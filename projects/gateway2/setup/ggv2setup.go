6a3fe9866faa1528a86769cc603552bee638d3d8
sheidkamp <seth.heidkamp@gmail.com>
2025-01-23 12:25:11 -0500

    move logic

--- projects/gateway2/setup/ggv2setup.go
+++ projects/gateway2/setup/ggv2setup.go
@@ -81,7 +81,7 @@ func getInitialSettings(ctx context.Context, c istiokube.Client, nns types.Names
 	return out
 }
 
-func getInitialSecret(ctx context.Context, c istiokube.Client, nns types.NamespacedName) *corev1.Secret {
+func getInitialSecret(ctx context.Context, nns types.NamespacedName) *corev1.Secret {
 	// // get initial settings
 	// logger := contextutils.LoggerFrom(ctx)
 	// logger.Infof("getting  mtls secret. gvr: %v", secretsGVR)
@@ -105,7 +105,7 @@ func getInitialSecret(ctx context.Context, c istiokube.Client, nns types.Namespa
 	// logger.Info("Got getInitialSecret ", "secret", out)
 	// return out
 
-	nns = types.NamespacedName{Name: "gloo-mtls-certs", Namespace: "gloo-system"}
+	//nns = types.NamespacedName{Name: "gloo-mtls-certs", Namespace: "gloo-system"}
 	kubeClient := helpers.MustKubeClient()
 
 	secretClient := kubeClient.CoreV1().Secrets(nns.Namespace)
@@ -125,6 +125,37 @@ func getInitialSecret(ctx context.Context, c istiokube.Client, nns types.Namespa
 
 	return existing
 
+}
+
+func getGlooMtls(ctx context.Context) *deployer.GlooMtlsInfo {
+	logger := contextutils.LoggerFrom(ctx)
+	// DO_NOT_SUBMIT - Check if TLS is enabled first (check for sds container on gloo pod)
+	// DO_NOT_SUBMIT - Pass in namespace (whole nns?)
+	secretNns := types.NamespacedName{Name: "gloo-mtls-certs", Namespace: "gloo-system"}
+	initialSecret := getInitialSecret(ctx, secretNns)
+	glooMtls := &deployer.GlooMtlsInfo{}
+	if initialSecret != nil {
+
+		tlsCert := initialSecret.Data[corev1.TLSCertKey]
+		tlsKey := initialSecret.Data[corev1.TLSPrivateKeyKey]
+		caCert := initialSecret.Data[corev1.ServiceAccountRootCAKey]
+
+		logger.Info("Got initial certs", "caCert", caCert, "tlsCert", tlsCert, "tlsKey", tlsKey)
+
+		glooMtls = &deployer.GlooMtlsInfo{
+			Enabled: true,
+			TlsCert: &deployer.TlsCertInfo{
+				CaCert:  caCert,  //[]byte(base64.StdEncoding.EncodeToString(caCert)),
+				TlsCert: tlsCert, //[]byte(base64.StdEncoding.EncodeToString(tlsCert)),
+				TlsKey:  tlsKey,  //[]byte(base64.StdEncoding.EncodeToString(tlsKey)),
+			},
+		}
+
+	} else {
+		logger.Info("No TLS secret found")
+	}
+
+	return glooMtls
 }
 
 func StartGGv2(ctx context.Context,
@@ -185,37 +216,8 @@ func StartGGv2WithConfig(ctx context.Context,
 		return nil
 	}, krt.WithName("GlooSettingsSingleton"))
 
-	// DO_NOT_SUBMIT - Start secrets logic move to own function
-	initialSecret := getInitialSecret(ctx, kubeClient, settingsNns)
-
-	// Hardcoded defaults
-	caCert := []byte("LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSURNakNDQWhxZ0F3SUJBZ0lJU05GVDN2MXpoVmN3RFFZSktvWklodmNOQVFFTEJRQXdKREVpTUNBR0ExVUUKQXhNWmMzVndaWEpuYkc5dkxYZGxZbWh2YjJzdFkyVnlkQzFqWVRBZUZ3MHlOVEF4TWpJeU1qTXlNamxhRncwegpOVEF4TWpBeU1qTXlNamxhTUNReElqQWdCZ05WQkFNVEdYTjFjR1Z5WjJ4dmJ5MTNaV0pvYjI5ckxXTmxjblF0ClkyRXdnZ0VpTUEwR0NTcUdTSWIzRFFFQkFRVUFBNElCRHdBd2dnRUtBb0lCQVFEQjU2eW9zRFNnamxtam5jakQKRm53UkJwL0hKdHBGYzJTOW14bFF6QVRlc1Mzd1JhbS8wV1lXNjZkVGpPQUVSblRlZFc5dW94bisyTC94clBRaApWWmJXQ0tzTGJUZW1GWmc4bXpLeGhuM1BFditOL0xoWDdSQXAxUzEzdFRYMmVnM2hqQUI0U3d2RFFvTHBlbEl6CmRkSU1YeVRKM3FYZkN3c3JPSEFMalpaNXNyMFZTY0UxZXFqZ2VGT0NDTEJXSVV4V1dtWElyaTNZTGhvUDI2M1IKc0t0MkljVi9IaGRqSFdhclUxOFo1WXg4ZkNFZFlmaTNRYWtCL1NCclYvSFRWNVpoNkNFbHVrV3haWjJBZFZjbApLbVg3RDZJdE9MZ3VLbTRNR1o2MVhPa0Z4Ujgvak5Idm45MWJhK2Q4ZHVHNWxkVWZWYm5kVlV0ZEF1eFZUTHNXCm0vdmpBZ01CQUFHamFEQm1NQTRHQTFVZER3RUIvd1FFQXdJQ3BEQVBCZ05WSFJNQkFmOEVCVEFEQVFIL01CMEcKQTFVZERnUVdCQlRhMlJvcEhKT0NEZFR1Y2VhR3NRVHZHTUFhcHpBa0JnTlZIUkVFSFRBYmdobHpkWEJsY21kcwpiMjh0ZDJWaWFHOXZheTFqWlhKMExXTmhNQTBHQ1NxR1NJYjNEUUVCQ3dVQUE0SUJBUUErbndzNE8zNGRIU1YvCmFqNW9icDRFa0U1S0ZVdmh2dDBHZmg3amRxaWl6ajd6OFk4SUd0cmxtdFRVdVlCalJZY3ZhNnp5M3VWTW9Bb3kKeHZ1ZkwyWFN6aGg3c2VsaFlWaUlXdUlna0ozOWl2SUYwU2ZzYnBjWmR3WW4rVlEyNG0zWnN5Rjd2V2o0SGdKUwpyazNOZ3RVVkFsTzM0UDEvL2xieEpHRFNYTFoyMjFYajBaWVdTN25CNDluU01WSTZDSkhWUzlLUWxFY2N3Q0pXCjc2dmdCc3hVQXk1QVd2NmRyOWtuNWhtRWlBWTFRS3FmaEF4aVpEWDdLWklBNVprS1VXMS90dFpFYnphU3BhYVIKTnp2ZVR5YzdQcmNrVDNYMTRyL29DS2lWTWFWSUYvelVtVjV0d1hGMDQrTFlIaU5BcGFTSERmejRvTlJIQTFwNApKMTNLNlFVQwotLS0tLUVORCBDRVJUSUZJQ0FURS0tLS0tCg==")
-	tlsCert := []byte("LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSURpRENDQW5DZ0F3SUJBZ0lJWVpCc2FmRTJmN2d3RFFZSktvWklodmNOQVFFTEJRQXdKREVpTUNBR0ExVUUKQXhNWmMzVndaWEpuYkc5dkxYZGxZbWh2YjJzdFkyVnlkQzFqWVRBZUZ3MHlOVEF4TWpJeU1qTXlNamxhRncweQpOakF4TWpJeU1qTXlNamxhTURFeEVEQU9CZ05WQkFvVEIzTnZiRzh1YVc4eEhUQWJCZ05WQkFNVEZHZHNiMjh1CloyeHZieTF6ZVhOMFpXMHVjM1pqTUlJQklqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FROEFNSUlCQ2dLQ0FRRUEKeHBXdVZVbkYybm5SZWhRM1lXMUpXWTBoYzd4RFNUaWpUbGdlSjJITDRBNkV4aEpxSG1EWkRaNEt1OTZJYlp3WgpnSERRRitUS1UrcE5DRThqN3VnNVAybFc0Y2EzQTQzQkFsSlpvZ3pPUEpvYW1yeVBhTEN1NCtOL1hlU1VsM2FICmNEaS9OYzF5YkkzNmRDN29JcTRBaUl1VjQ4M3U5ZG56QWRVb05tc3BQbmtndURPUzgrUFN5SFNpbmx6S0YzZ1UKZk4yYVhBRndXczNnMGk5M1piRHZlNzIyZ2FYTEQrSm9EMWdUbEhORmJRN1FDL1hzZU1hZjdMYllmend5K3h5Qgo0ZWxZYTlDQmdrR0VWbXRJcGxnRU8vdU5lNXhHVCtlLzVPczBXSW9zc09PMzJtK0kvcitDUkZ3ZVN4NnBiWWhrCk9vZjVRdWVDUDREclA2SDJROUtuS1FJREFRQUJvNEd3TUlHdE1BNEdBMVVkRHdFQi93UUVBd0lGb0RBZEJnTlYKSFNVRUZqQVVCZ2dyQmdFRkJRY0RBUVlJS3dZQkJRVUhBd0l3SHdZRFZSMGpCQmd3Rm9BVTJ0a2FLUnlUZ2czVQo3bkhtaHJFRTd4akFHcWN3V3dZRFZSMFJCRlF3VW9JRVoyeHZiNElRWjJ4dmJ5NW5iRzl2TFhONWMzUmxiWUlVCloyeHZieTVuYkc5dkxYTjVjM1JsYlM1emRtT0NJbWRzYjI4dVoyeHZieTF6ZVhOMFpXMHVjM1pqTG1Oc2RYTjAKWlhJdWJHOWpZV3d3RFFZSktvWklodmNOQVFFTEJRQURnZ0VCQUhrMC9sc1lyc0tCcFQ5WFprQXdxV0lHeVM4LwpmVnhLR3JLbS9WSnR4akFFRk8vVWJFRWV4a3Q2N3FOa3lnWFFVWGpIa3FQTUxrSjBucFZ3bm05Yi9hL3c5K3BoCkprdWZSdHdYb2JleFRRMzRKb05yUUZQeVg0U3EvTnU4K2dlT1FJSERGYjI5bWE3OW5xTU9Oa2lzaThROFFsVmQKQVA0ckR3TGlFTFA1ajRHWXdaMmltTVp5MkxNY0tnYUFyVTBtTnJ5Wkk2N0xiNm9vVjVrNmpOaUpjTWtKSzY1WgpGYTBCZFVHZGNOUWo2UnNRL2Y5a25LM1dOVTkwYW1mcmNPbG1paGI3cDdwWjFMOXZId3hPRlAxQ1lVak9yZXRjCmUvUFE4amFwVU9RTytVVVRjL3EyYmtMQlhMa1hNY0hLMGdCQm9xTk05VmtXSWVrN29oZlBxb3AzYmRzPQotLS0tLUVORCBDRVJUSUZJQ0FURS0tLS0tCg==")
-	tlsKey := []byte("LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQpNSUlFcFFJQkFBS0NBUUVBeHBXdVZVbkYybm5SZWhRM1lXMUpXWTBoYzd4RFNUaWpUbGdlSjJITDRBNkV4aEpxCkhtRFpEWjRLdTk2SWJad1pnSERRRitUS1UrcE5DRThqN3VnNVAybFc0Y2EzQTQzQkFsSlpvZ3pPUEpvYW1yeVAKYUxDdTQrTi9YZVNVbDNhSGNEaS9OYzF5YkkzNmRDN29JcTRBaUl1VjQ4M3U5ZG56QWRVb05tc3BQbmtndURPUwo4K1BTeUhTaW5sektGM2dVZk4yYVhBRndXczNnMGk5M1piRHZlNzIyZ2FYTEQrSm9EMWdUbEhORmJRN1FDL1hzCmVNYWY3TGJZZnp3eSt4eUI0ZWxZYTlDQmdrR0VWbXRJcGxnRU8vdU5lNXhHVCtlLzVPczBXSW9zc09PMzJtK0kKL3IrQ1JGd2VTeDZwYlloa09vZjVRdWVDUDREclA2SDJROUtuS1FJREFRQUJBb0lCQVFDY1pMTklMZkpvN2psQQpHSDNJOThXbGhoVkxUWC84UVdPelJvaHc0WDhyZEtPeVRqeE9zbDBlY1ZIb3hRZlNzdllPaGtvTUZ6NFV1bGh5CmE4bFQxSVdKWUE5eGZnc1IvR2g5eUpjWW5WY3F1UDZzMEVWczRJRExycFhYUHphYTFsa3gweThiVXpLRE5ZbUMKU0pLL0JTUWNaVG5sajRCYkZJdGg3UnBmU1NQUk16UnZHRFcrUlNlNzkzdHVIRjc2dVRvS29Mdkczc0dFOTh6ZQovNmhXNTFiTW1PbXkwbTFxU2lKUUFyYlR1ams3b2l3NERiYjU0T2QyaGJtaDhwMVFqMUZUUnhDVkp6RStQY1RyCnNWRExucWZJZHhSYWI4UnhTT1podGZZTUJTeHE4cUtuQzR4cHhkTllYeUpqYllzZFNqei9McDBpOW1CNjNFNTMKSUJYeG5aMkZBb0dCQU1tOGd3NmJqM3MzSzhVUUovMmdMdHVZT2xaTUg2eVVaZ3gzMkZWemVWcmJkeDE2Y1hFVApiS3puNHhad3BLc0NlUk5sOEFuWk90S0JUVk1NejdvellYd0hqMXpmamoxcURmOGlHSUYxM01qM3EyaTkwam5kCklrcU9pK0JlaUtDT1lvTE5qYjhlMktMOUdTOVY4SW1JL3I5YUVSK1BvU1BYbTM0T0c3YlRQOFRYQW9HQkFQd0EKSlRZamM1WEQ0KzE3Nmk5ak5rQmVXTjB2RXBnTFRhQnRnTTRGTko3MVF2Mko2TmdxTHUzZWk0U1Z5WkdwcE91UQpYM28zcWlhaUFEd2E3dWREWERMRnlsZUxjays3eFpKSUZuWlJLbHlnakxzUHJaZ0Ftd01zMndWLzdMa2kzSS8wCklyWnJnU3EvcEp5WmFTZmIrdDFGRXh1OVhheHZhK3VHK082d0IzUC9Bb0dCQUlJYzJMWnFOSzkyMVA4anZYZEMKZ2haVjU0SlRWTFo5ZkJnY2orWUZOVWNaZDRrR3VQWUNYanhpeno5ZzVZUDZjMWJFajMzNm9vcTBwTWVrNHJHbwpnLzUzN2NvcjBkVGdleWlMdUJ4L2hTZ0ZQWU92c2xCcHhMMHJsU0hnTnVTL0VPQm1iVDdRU1U5T1NKa1VKN0M5Ckwva0F3VHlHNlpweGJETndMQVhOMkRvL0FvR0JBTVJVaktsQU40WWdCd3ozOUwwVXE2aThtTGxDT2xkUXZ4clYKRlh2dEhGRVh2aWh2OElPeFlieWJITkdnTFZtWjlNNCtQZFNuVjU0ZnF0VXBHcVg4bWZGSW5kdFUzaXQybkhmYQpSLzNJUUp2SHpielRleWlvbUJ5Q0x1VjdCQUE5UTkrM2tlL1RrOSt0VFY5Z09rZituOVVTUXMvaTJmOUZFNng5CkRLWlJhSTBiQW9HQUx4Z0Y5RFZDbzVQMFB5MzBsM3cvR0dGTit3RkpONGx0RWtVcFo4NTI3b29TZ0kyZy9Mb1kKRGg0QThFNXNXbDVRWXptdXVUQ29WSkJNUUl0S1VWMlhXZjJySlN5ejZuWXY5ZFh4eVpqaVhyZW5nNENtaTU1YQo3MC9RQ29mQnp1aDF0dGVMMUIxZ2hsOXA5cXBNS3E5cFREZWtQQ1V2bktObkJWam1HbXYvUlVvPQotLS0tLUVORCBSU0EgUFJJVkFURSBLRVktLS0tLQo=")
-
-	if initialSecret != nil {
-
-		tlsCert = initialSecret.Data[corev1.TLSCertKey]
-		tlsKey = initialSecret.Data[corev1.TLSPrivateKeyKey]
-		caCert = initialSecret.Data[corev1.ServiceAccountRootCAKey]
-
-		logger.Info("Got initial certs", "caCert", caCert, "tlsCert", tlsCert, "tlsKey", tlsKey)
-
-	} else {
-		logger.Info("No initial secret found, using hardcoded certs")
-	}
-
-	// Hardcode for now
-	glooMtls := &deployer.GlooMtlsInfo{
-		Enabled: true,
-		TlsCert: &deployer.TlsCertInfo{
-			CaCert:  caCert,  //[]byte(base64.StdEncoding.EncodeToString(caCert)),
-			TlsCert: tlsCert, //[]byte(base64.StdEncoding.EncodeToString(tlsCert)),
-			TlsKey:  tlsKey,  //[]byte(base64.StdEncoding.EncodeToString(tlsKey)),
-		},
-	}
-
-	logger.Info("Hardcoding glooMtls for now", "glooMtls", glooMtls)
+	glooMtls := getGlooMtls(ctx)
+	logger.Info("Got glooMtls", "glooMtls", glooMtls)
 
 	serviceClient := kclient.New[*corev1.Service](kubeClient)
 	services := krt.WrapClient(serviceClient, krt.WithName("Services"))
