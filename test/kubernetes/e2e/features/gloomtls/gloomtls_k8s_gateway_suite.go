3b54a17d18555694d87f2bb7d649b9025fe5b5af
David Jumani <dj.davidjumani1994@gmail.com>
2025-01-28 12:19:28 -0500

    cleanup tests

--- test/kubernetes/e2e/features/gloomtls/gloomtls_k8s_gateway_suite.go
+++ test/kubernetes/e2e/features/gloomtls/gloomtls_k8s_gateway_suite.go
@@ -2,6 +2,7 @@ package gloomtls
 
 import (
 	"context"
+	"encoding/json"
 	"path/filepath"
 	"time"
 
@@ -15,6 +16,7 @@ import (
 	"github.com/solo-io/gloo/test/kubernetes/e2e/tests/base"
 	"github.com/solo-io/skv2/codegen/util"
 	"github.com/stretchr/testify/suite"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -26,7 +28,7 @@ type gloomtlsK8sGatewayTestingSuite struct {
 
 func NewGloomtlsK8sGatewayApiTestingSuite(ctx context.Context, testInst *e2e.TestInstallation) suite.TestingSuite {
 	return &gloomtlsK8sGatewayTestingSuite{
-		base.NewBaseTestingSuiteWithUpgrades(ctx, testInst, e2e.MustTestHelper(ctx, testInst), base.SimpleTestCase{}, k8sGatewayTestCases),
+		base.NewBaseTestingSuite(ctx, testInst, base.SimpleTestCase{}, k8sGatewayTestCases),
 	}
 }
 
@@ -38,24 +40,9 @@ func (s *gloomtlsK8sGatewayTestingSuite) TestRouteSecureRequestToUpstream() {
 	matcher := gomega.And(
 		matchers.PodMatches(matchers.ExpectedPod{ContainerName: istio.SDSContainerName}),
 	)
-
 	s.TestInstallation.Assertions.EventuallyPodsMatches(s.Ctx, "default", listOpts, matcher, time.Minute*2)
 
-	// Check curl works
-	s.TestInstallation.Assertions.EventuallyRunningReplicas(s.Ctx, glooProxyObjectMeta, gomega.Equal(1))
-	s.TestInstallation.Assertions.AssertEventualCurlResponse(
-		s.Ctx,
-		testdefaults.CurlPodExecOpt,
-		[]curl.Option{
-			curl.WithHost(kubeutils.ServiceFQDN(proxyService.ObjectMeta)),
-			curl.WithHostHeader("example.com"),
-		},
-		expectedHealthyResponse)
-
-	// Upgrade to Rotate certs. This can be done by just running the certgen job ??????
-	s.UpgradeWithCustomValuesFile(filepath.Join(util.MustGetThisDir(), "../../tests/manifests", "gloomtls-edge-gateway-test-helm.yaml"))
-	// TODO : Check if certs have changed
-	// TODO : Add a new route and curl it
+	s.ensureGlooAndProxyCertsMatch()
 
 	// Check curl works
 	s.TestInstallation.Assertions.EventuallyRunningReplicas(s.Ctx, glooProxyObjectMeta, gomega.Equal(1))
@@ -68,4 +55,53 @@ func (s *gloomtlsK8sGatewayTestingSuite) TestRouteSecureRequestToUpstream() {
 		},
 		expectedHealthyResponse)
 
+	// Get the certs before the upgrade to ensure it was rotated
+	oldCerts := s.getMtlsCerts(s.TestInstallation.Metadata.InstallNamespace)
+
+	// Run the certgen job manually instead of upgrading - this simulates the cronjob
+	// Delete the job if it still exists after completion. This ensures that the job will run and the certs rotated
+	s.TestInstallation.Actions.Kubectl().DeleteFile(s.Ctx, filepath.Join(util.MustGetThisDir(), "testdata/certgen.yaml"), "-n", s.TestInstallation.Metadata.InstallNamespace)
+	s.TestInstallation.Actions.Kubectl().ApplyFile(s.Ctx, filepath.Join(util.MustGetThisDir(), "testdata/certgen.yaml"), "-n", s.TestInstallation.Metadata.InstallNamespace)
+
+	// Wait until the job has completed and the certs have been rotated
+	s.TestInstallation.Actions.Kubectl().RunCommand(s.Ctx, "-n", s.TestInstallation.Metadata.InstallNamespace, "wait", "--for=condition=complete", "job", "gloo-mtls-certgen", "--timeout=600s")
+
+	newCerts := s.getMtlsCerts(s.TestInstallation.Metadata.InstallNamespace)
+	s.NotEqual(oldCerts.Data, newCerts.Data)
+
+	s.ensureGlooAndProxyCertsMatch()
+
+	s.TestInstallation.Actions.Kubectl().ApplyFile(s.Ctx, filepath.Join(util.MustGetThisDir(), "testdata/hello-route.yaml"))
+	s.T().Cleanup(func() {
+		s.TestInstallation.Actions.Kubectl().DeleteFile(s.Ctx, filepath.Join(util.MustGetThisDir(), "testdata/hello-route.yaml"))
+	})
+
+	// Check curl works on the new route
+	s.TestInstallation.Assertions.EventuallyRunningReplicas(s.Ctx, glooProxyObjectMeta, gomega.Equal(1))
+	s.TestInstallation.Assertions.AssertEventualCurlResponse(
+		s.Ctx,
+		testdefaults.CurlPodExecOpt,
+		[]curl.Option{
+			curl.WithHost(kubeutils.ServiceFQDN(proxyService.ObjectMeta)),
+			curl.WithHostHeader("hello.com"),
+		},
+		expectedHealthyResponse)
+}
+
+// ensureGlooAndProxyCertsMatch checks that the gloo Mtls certs that exist in the installation namespace and the proxy namespace are the same
+func (s *gloomtlsK8sGatewayTestingSuite) ensureGlooAndProxyCertsMatch() {
+	glooSystemNsMtlsSecret := s.getMtlsCerts(s.TestInstallation.Metadata.InstallNamespace)
+	defaultNsMtlsSecret := s.getMtlsCerts("default")
+	s.TestInstallation.Assertions.Assert.Equal(defaultNsMtlsSecret.Data, glooSystemNsMtlsSecret.Data)
+}
+
+func (s *gloomtlsK8sGatewayTestingSuite) getMtlsCerts(namespace string) *corev1.Secret {
+	secretString, _, err := s.TestInstallation.Actions.Kubectl().Get(s.Ctx, "secret", "gloo-mtls-certs", "-n", namespace, "-o", "json")
+	s.NoError(err)
+
+	var mtlsSecret corev1.Secret
+	err = json.Unmarshal([]byte(secretString), &mtlsSecret)
+	s.NoError(err)
+
+	return &mtlsSecret
 }
