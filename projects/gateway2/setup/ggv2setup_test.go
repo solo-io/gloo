e5ec98a005cc843f54cb3239f28f40047a6e3281
Steven Landow <steven@landow.dev>
2025-02-14 09:34:21 -0800

    make EnvoySnapshot threadsafe

--- projects/gateway2/setup/ggv2setup_test.go
+++ projects/gateway2/setup/ggv2setup_test.go
@@ -25,7 +25,6 @@ import (
 	jsonpb "google.golang.org/protobuf/encoding/protojson"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
@@ -191,13 +190,6 @@ func testScenariosWithCRDs(
 	err = client.ApplyYAMLFiles("gwtest", "testdata/setupyaml/pods.yaml")
 	if err != nil {
 		t.Fatalf("failed to apply yaml: %v", err)
-	}
-
-	// there may be a slight delay in EndpointSlices being generated
-	// but if we query xDS before then, the test will fail
-	err = waitForEndpointSlices(client, "gwtest")
-	if err != nil {
-		t.Fatalf("pods did not become ready: %v", err)
 	}
 
 	// setup xDS server:
@@ -857,49 +849,4 @@ func generateKubeConfiguration(t *testing.T, restconfig *rest.Config) string {
 	}
 
 	return tmpfile
-}
-
-// waitForPodReady waits for a pod to be in the 'Running' state
-func waitForEndpointSlices(client istiokube.Client, namespace string) error {
-	timeout := 15 * time.Second
-	interval := 1 * time.Second
-
-	return wait.PollUntilContextTimeout(context.Background(), interval, timeout, true, func(ctx context.Context) (bool, error) {
-		services, err := client.Kube().CoreV1().Services(namespace).List(ctx, metav1.ListOptions{})
-		if err != nil {
-			return false, fmt.Errorf("failed to list services: %v", err)
-		}
-
-		endpointSlices, err := client.Kube().DiscoveryV1().EndpointSlices(namespace).List(ctx, metav1.ListOptions{})
-		if err != nil {
-			return false, fmt.Errorf("failed to list EndpointSlices: %v", err)
-		}
-
-		// Track which services have an EndpointSlice
-		serviceHasEndpoint := make(map[string]bool)
-		for _, es := range endpointSlices.Items {
-			for _, svc := range es.OwnerReferences {
-				if svc.Kind == "Service" {
-					serviceHasEndpoint[svc.Name] = true
-				}
-			}
-		}
-
-		// Collect all services missing an EndpointSlice
-		var missingServices []string
-		var foundServices []string
-		for _, svc := range services.Items {
-			if serviceHasEndpoint[svc.Name] {
-				foundServices = append(foundServices, svc.Name)
-			} else {
-				missingServices = append(missingServices, svc.Name)
-			}
-		}
-
-		if len(missingServices) > 0 {
-			return false, fmt.Errorf("services missing EndpointSlices: %s; services with EndpointSlices: %s", strings.Join(missingServices, ", "), strings.Join(foundServices, ", "))
-		}
-
-		return true, nil
-	})
 }
