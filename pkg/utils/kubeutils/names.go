189628759b60978eac801a11d69a4bf7930f84a5
Ryan Old <ryanrolds@gmail.com>
2025-02-26 11:46:09 -0800

    Discover Gloo service with labels

--- pkg/utils/kubeutils/names.go
+++ pkg/utils/kubeutils/names.go
@@ -1,8 +1,10 @@
 package kubeutils
 
 const (
-	GlooDeploymentName = "gloo"
-	GlooServiceName    = "gloo"
+	GlooDeploymentName   = "gloo"
+	GlooServiceName      = "gloo"
+	GlooServiceAppLabel  = "gloo"
+	GlooServiceGlooLabel = "gloo"
 
 	// GlooXdsPortName is the name of the port in the Gloo Gateway control plane Kubernetes Service that serves xDS config.
 	// See: install/helm/gloo/templates/2-gloo-service.yaml
