0304ef336cf49aa67118d2ce39ed0d7374da5cab
Yuval Kohavi <yuval.kohavi@gmail.com>
2025-03-06 17:29:36 -0500

    route: don't use specific backend gvk here (#10772)

--- internal/kgateway/translator/irtranslator/route.go
+++ internal/kgateway/translator/irtranslator/route.go
@@ -21,7 +21,6 @@ import (
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/reports"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/translator/routeutils"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/utils"
-	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/wellknown"
 	"github.com/kgateway-dev/kgateway/v2/pkg/utils/regexutils"
 )
 
@@ -249,8 +248,9 @@ func (h *httpRouteConfigurationTranslator) runBackendPolicies(ctx context.Contex
 func (h *httpRouteConfigurationTranslator) runBackend(ctx context.Context, in ir.HttpBackend, pCtx *ir.RouteBackendContext, outRoute *envoy_config_route_v3.Route) error {
 	var errs []error
 	if in.Backend.BackendObject != nil {
-		if in.Backend.BackendObject.GetGroupKind().Kind == wellknown.BackendGVK.Kind {
-			err := h.PluginPass[in.Backend.BackendObject.GetGroupKind()].ApplyForBackend(ctx, pCtx, in, outRoute)
+		backendPass := h.PluginPass[in.Backend.BackendObject.GetGroupKind()]
+		if backendPass != nil {
+			err := backendPass.ApplyForBackend(ctx, pCtx, in, outRoute)
 			if err != nil {
 				errs = append(errs, err)
 			}
