62ac501cac979965720c08b920fcd55bbde0a63f
Tim Flannagan <timflannagan@gmail.com>
2025-03-06 14:42:58 -0500

    Remove the test/mocks directory (#10770)
    Signed-off-by: timflannagan <timflannagan@gmail.com>

--- test/mocks/mocks.go
+++
@@ -1,7 +0,0 @@
-//go:build ignore
-
-package mocks
-
-//go:generate go run github.com/golang/mock/mockgen -destination ./cache/corecache.go github.com/solo-io/solo-kit/pkg/api/v1/clients/kube/cache KubeCoreCache
-//go:generate go run github.com/golang/mock/mockgen -destination ./kubernetes/kubeinterface.go k8s.io/client-go/kubernetes Interface
-//go:generate go run github.com/golang/mock/mockgen -destination ./gloo/validation_client.go github.com/kgateway-dev/kgateway/internal/gloo/pkg/api/grpc/validation GlooValidationServiceClient
