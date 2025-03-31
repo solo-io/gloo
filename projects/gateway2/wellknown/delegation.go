27268406b7d07e300b0d447b97fcc12386a1f5c1
Shashank Ram <shashank.ram@solo.io>
2025-03-31 09:05:49 -0700

    wildcard ns override

--- projects/gateway2/wellknown/delegation.go
+++ projects/gateway2/wellknown/delegation.go
@@ -1,12 +1,10 @@
 package wellknown
 
+import "github.com/solo-io/gloo/pkg/utils/envutils"
+
 const (
 	// RouteDelegationLabelSelector is the label used to select delegated HTTPRoutes
 	RouteDelegationLabelSelector = "delegation.gateway.solo.io/label"
-
-	// RouteDelegationLabelSelectorWildcard wildcards the namespace to select delegatee routes by label
-	// Note: this must be a valid RFC 1123 DNS label
-	RouteDelegationLabelSelectorWildcardNamespace = "all"
 
 	// InheritMatcherAnnotation is the annotation used on an child HTTPRoute that
 	// participates in a delegation chain to indicate that child route should inherit
@@ -17,3 +15,7 @@ const (
 	// all (wildcard *) or specific fields (comma separated field names) in RouteOptions inherited from the parent route.
 	PolicyOverrideAnnotation = "delegation.gateway.solo.io/enable-policy-overrides"
 )
+
+// RouteDelegationLabelSelectorWildcard wildcards the namespace to select delegatee routes by label
+// Note: this must be a valid RFC 1123 DNS label
+var RouteDelegationLabelSelectorWildcardNamespace = envutils.GetOrDefault("DELEGATION_WILDCARD_NAMESPACE", "all", false)
