c493cde92f70f0c08b119db2a16ac93d5dcfeefc
Ryan Old <ryanrolds@gmail.com>
2025-04-02 08:46:56 -0700

    Adding HTTP tunneling ggv2 golden yaml

--- projects/gateway2/setup/ggv2setup_test.go
+++ projects/gateway2/setup/ggv2setup_test.go
@@ -432,14 +432,21 @@ func (x xdsDumper) Dump(t *testing.T, ctx context.Context) xdsDump {
 				}
 			} else if dresp.GetTypeUrl() == "type.googleapis.com/envoy.config.listener.v3.Listener" {
 				needMoreListerners := false
+
 				for _, anyListener := range dresp.GetResources() {
 					var listener envoylistener.Listener
 					if err := anyListener.UnmarshalTo(&listener); err != nil {
 						t.Errorf("failed to unmarshal listener: %v", err)
 					}
 					listeners = append(listeners, &listener)
-					needMoreListerners = needMoreListerners || (len(getroutesnames(&listener)) == 0)
+					// ROLDS: Disabling this for now. It didn't like the pipe listener
+					// from the upstream-tunneling.yaml as it expects routes on all listeners.
+					// It looks like this was put in place to deflake. I've run the tests
+					// in a loop on my workstation and they consistently passed.
+					// for i in {1..30}; do go clean -testcache; go test ./projects/gateway2/setup/...; done
+					//needMoreListerners = needMoreListerners || (len(getRouteNames(&listener)) == 0)
 				}
+
 				if len(listeners) == 0 {
 					needMoreListerners = true
 				}
@@ -484,7 +491,7 @@ func (x xdsDumper) Dump(t *testing.T, ctx context.Context) xdsDump {
 
 	var routenames []string
 	for _, l := range listeners {
-		routenames = append(routenames, getroutesnames(l)...)
+		routenames = append(routenames, getRouteNames(l)...)
 	}
 
 	dr = proto.Clone(x.dr).(*discovery_v3.DiscoveryRequest)
@@ -790,10 +797,13 @@ func protoJsonRoundTrip(c proto.Message) (any, error) {
 	return roundtrip, nil
 }
 
-func getroutesnames(l *envoylistener.Listener) []string {
+func getRouteNames(l *envoylistener.Listener) []string {
 	var routes []string
 	for _, fc := range l.GetFilterChains() {
 		for _, filter := range fc.GetFilters() {
+			fmt.Printf("filter: %s\n", filter.GetName())
+
+			// check if the filter is a http connection manager
 			suffix := string((&envoyhttp.HttpConnectionManager{}).ProtoReflect().Descriptor().FullName())
 			if strings.HasSuffix(filter.GetTypedConfig().GetTypeUrl(), suffix) {
 				var hcm envoyhttp.HttpConnectionManager
