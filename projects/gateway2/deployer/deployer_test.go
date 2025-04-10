55b224ee91e89fe9d07c324e8b99617405949eb4
David Jumani <dj.davidjumani1994@gmail.com>
2025-04-10 16:18:35 -0400

    fix test

--- projects/gateway2/deployer/deployer_test.go
+++ projects/gateway2/deployer/deployer_test.go
@@ -1703,13 +1703,13 @@ var _ = Describe("Deployer", func() {
 			servicePorts := objs.findService(defaultNamespace, proxyName(gw.Name)).Spec.Ports
 			Expect(servicePorts[0].Name).To(Equal(listenerName))
 			Expect(servicePorts[0].Port).To(Equal(listenerPort))
-			Expect(servicePorts[1].Name).To(Equal(fmt.Sprintf("0-%s", listenerName)))
+			Expect(servicePorts[1].Name).To(Equal(fmt.Sprintf("1-%s", listenerName)))
 			Expect(servicePorts[1].Port).To(Equal(listenerSetPort))
 
 			deploymentPorts := objs.findDeployment(defaultNamespace, proxyName(gw.Name)).Spec.Template.Spec.Containers[0].Ports
 			Expect(deploymentPorts[0].Name).To(Equal(listenerName))
 			Expect(deploymentPorts[0].ContainerPort).To(Equal(listenerPort))
-			Expect(deploymentPorts[1].Name).To(Equal(fmt.Sprintf("0-%s", listenerName)))
+			Expect(deploymentPorts[1].Name).To(Equal(fmt.Sprintf("1-%s", listenerName)))
 			Expect(deploymentPorts[1].ContainerPort).To(Equal(listenerSetPort))
 		})
 	})
