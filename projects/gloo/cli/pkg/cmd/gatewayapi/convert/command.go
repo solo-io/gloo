7c9841a5a4af6444cc69baa4d3bd32357a1fffde
Nick <nick.nellis@solo.io>
2025-04-11 15:55:30 -0500

    Update projects/gloo/cli/pkg/cmd/gatewayapi/convert/command.go
    Co-authored-by: Nadine Spies <nadinekipsch@onlinehome.de>

--- projects/gloo/cli/pkg/cmd/gatewayapi/convert/command.go
+++ projects/gloo/cli/pkg/cmd/gatewayapi/convert/command.go
@@ -43,7 +43,7 @@ func RootCmd(op *options.Options) *cobra.Command {
 # To generate gateway api by a single kubernetes yaml file but place all the output configurations in the same file.
   glooctl gateway-api convert --input-file gloo-yamls.yaml --output-dir ./_output --retain-input-folder-structure
 
-# To load a bunch of *.yaml or *.yml files in nested directories. You can also retain the same file structure which is helpful in CI/CD pipelines.
+# To load a bunch of `*.yaml` or `*.yml` files in nested directories. You can also use the `--retain-input-folder-structure` option to keep the original file structure, which can be helpful in CI/CD pipelines.
   glooctl gateway-api convert --input-dir ./gloo-configs --output-dir ./_output --retain-input-folder-structure
 
 # Configuration can also be pulled an translated directly from a running Gloo Gateway Instance (version 1.17+).
