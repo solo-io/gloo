9e3c17afc971c56aa20e32777609f7ca7dd04709
Shashank Ram <shashank.ram@solo.io>
2025-04-14 09:37:51 -0700

    gateway2/delegation: ignore child routes with mismatched parentRef early
    After the refactor to decouple querying of routes from translation
    to enable KRT integration, the resolution of child routes for a
    delegating route became too permissive, leading to massive amount
    of memory usage when there are cyclic references to namespaces,
    i.e., a parent route delegating to wildcard routes in the parent
    route's namespace. This happens even if a child route does not
    attach to the parent because this check was omitted during the
    initial route chain construction. Without this check, the recursive
    nature of evaluating a delegation chain can consume massive amount
    of memory.
    
    This change prunes the routes in the chain early on to avoid
    this problem, as this is a regression from 1.17.x. While we could
    prune the child route list further, it would degrade the performance
    due to O(N^2) computation required to match all the rules in a child
    route with all the rules on the parent, which already happens later
    in the translation. To reduce the risk of introducing new bugs,
    we defer other optimizations for the future.
    
    Testing done:
    Verified that the memory usage remains stable with the
    repro config provided in solo-io/solo-projects#8119
    
    Signed-off-by: Shashank Ram <shashank.ram@solo.io>

--- projects/gateway2/query/httproute.go
+++ projects/gateway2/query/httproute.go
@@ -129,7 +129,7 @@ func (r *gatewayQueries) GetRouteChain(
 	switch typedRoute := route.(type) {
 	case *gwv1.HTTPRoute:
 		backends = r.resolveRouteBackends(ctx, typedRoute)
-		children = r.getDelegatedChildren(ctx, typedRoute, nil)
+		children = r.getDelegatedChildren(ctx, typedRoute, sets.New[types.NamespacedName]())
 	case *gwv1a2.TCPRoute:
 		backends = r.resolveRouteBackends(ctx, typedRoute)
 		// TODO (danehans): Should TCPRoute delegation support be added in the future?
@@ -255,10 +255,6 @@ func (r *gatewayQueries) getDelegatedChildren(
 	parent *gwv1.HTTPRoute,
 	visited sets.Set[types.NamespacedName],
 ) BackendMap[[]*RouteInfo] {
-	// Initialize the set of visited routes if it hasn't been initialized yet
-	if visited == nil {
-		visited = sets.New[types.NamespacedName]()
-	}
 	parentRef := namespacedName(parent)
 	// `visited` is used to detect cyclic references to routes in the delegation chain.
 	// It is important to remove the route from the set once all its children have been evaluated
@@ -277,7 +273,7 @@ func (r *gatewayQueries) getDelegatedChildren(
 				continue
 			}
 			// Fetch child routes based on the backend reference
-			referencedRoutes, err := r.fetchChildRoutes(ctx, parent.Namespace, backendRef)
+			referencedRoutes, err := r.fetchRoutesByRef(ctx, namespacedName(parent), backendRef)
 			if err != nil {
 				children.AddError(backendRef.BackendObjectReference, err)
 				continue
@@ -290,6 +286,15 @@ func (r *gatewayQueries) getDelegatedChildren(
 					// Don't resolve invalid child route
 					continue
 				}
+				// ignore reference to self
+				if childRef == parentRef {
+					continue
+				}
+				// ignore routes that are not attached to the parent
+				if !utils.ChildRouteCanAttachToParentRef(&childRoute, parentRef) {
+					continue
+				}
+
 				// Recursively get the route chain for each child route
 				routeInfo := &RouteInfo{
 					Object: &childRoute,
@@ -311,12 +316,14 @@ func (r *gatewayQueries) getDelegatedChildren(
 	return children
 }
 
-func (r *gatewayQueries) fetchChildRoutes(
+// fetchRoutesByRef fetches the child routes based on the given backendRef and parentRef.
+// NOTE: it does not check if the route attaches to the parent (checked in getDelegatedChildren)
+func (r *gatewayQueries) fetchRoutesByRef(
 	ctx context.Context,
-	parentNamespace string,
+	parentRef types.NamespacedName,
 	backendRef gwv1.HTTPBackendRef,
 ) ([]gwv1.HTTPRoute, error) {
-	delegatedNs := parentNamespace
+	delegatedNs := parentRef.Namespace
 	// Use the namespace specified in the backend reference if available
 	if backendRef.Namespace != nil {
 		delegatedNs = string(*backendRef.Namespace)
