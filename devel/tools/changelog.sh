0f87d1f8fd890042c8d0c02f3432c66f9fa6c0a8
Ashish Banerjee <ashish.banerjee@solo.io>
2025-05-05 12:23:28 -0400

    Bump envoy to 1.34.0-patch1

--- devel/tools/changelog.sh
+++ devel/tools/changelog.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-set -e
+# Create a changelog file. This script automatically creates a file in the
+# correct changelog directory. You can also pass the `-e` flag if you want to
+# edit the file after creation. You can also use `-t` to specify which kind of
+# changelog entry you would like to create (`-t FIX` or `-t DEPENDENCY_BUMP`
+# for example). This script can be called multiple times, and it will
+# automatically append additional changelog entries to the changelog file in
+# question. So you could run `devel/tools/changelog.sh -t FIX` and then after
+# that you could run `devel/tools/changelog.sh -t DEPENDENCY_BUMP` and both of
+# those entries would appear in the file.
+
+set -euo pipefail
 
 CURRENT_VERSION=$(git describe --tags --always --abbrev=0)
 OLD_VERSION=$(echo -n $CURRENT_VERSION | sed -E 's/.*[^0-9]([0-9]+)$/\1/')
@@ -14,24 +24,87 @@ mkdir -p "$CHANGELOG_DIR"
 
 CHANGELOG_FILE="$CHANGELOG_DIR/$BRANCH_NAME.yaml"
 
-if [[ ! -f $CHANGELOG_FILE ]]; then
-    echo "Creating $CHANGELOG_FILE"
+##################################################
+# OPTION FLAGS
+OPT_EDIT=false # should we open the changelog file in $EDITOR?
+OPT_CHANGELOG_TYPE="" # NEW_FEATURE, DEPENDENCY_BUMP, ENVOY_DEPENDENCY_BUMP, NON_USER_FACING
 
-    cat << EOF > "$CHANGELOG_FILE"
-changelog:
+##################################################
+# OPTION PARSING
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -e|--edit)
+      OPT_EDIT=true
+      shift
+      ;;
+    --no-edit)
+      OPT_EDIT=false
+      shift
+      ;;
+    -t|--type)
+      OPT_CHANGELOG_TYPE="$2"
+      shift
+      shift
+      ;;
+  esac
+done
+##################################################
+# if the file does not exist, create it by adding the initial "changelog": line
+test -f "${CHANGELOG_FILE}" || echo "changelog:" >> "${CHANGELOG_FILE}"
+
+##################################################
+# process the -t flag to add the templated changelog entries
+case "$OPT_CHANGELOG_TYPE" in
+  FIX)
+    echo "creating FIX changelog"
+    # make sure to append to the file so we don't delete existing entries!!
+    cat << EOF >> "${CHANGELOG_FILE}"
   - type: FIX
     issueLink:
     resolvesIssue: false
     description: >-
       "${DESCRIPTION}"
 EOF
+    ;;
+  NEW_FEATURE)
+    echo "creating NEW_FEATURE changelog"
+    # make sure to append to the file so we don't delete existing entries!!
+    cat << EOF >> "${CHANGELOG_FILE}"
+  - type: NEW_FEATURE
+    issueLink:
+    resolvesIssue: false
+    description: >-
+      "${DESCRIPTION}"
+EOF
+    ;;
+  DEPENDENCY_BUMP)
+    echo "creating DEPENDENCY_BUMP changelog"
+    # make sure to append to the file so we don't delete existing entries!!
+    cat << EOF >> "${CHANGELOG_FILE}"
+  - type: DEPENDENCY_BUMP
+    issueLink:
+    resolvesIssue: false
+    dependencyOwner: # solo-io
+    dependencyRepo: # gloo
+    dependencyTag: # v0.7.0
+EOF
+    ;;
+  ENVOY_DEPENDENCY_BUMP)
+    echo "creating ENVOY DEPENDENCY_BUMP changelog"
+    # make sure to append to the file so we don't delete existing entries!!
+    cat << EOF >> "${CHANGELOG_FILE}"
+  - type: DEPENDENCY_BUMP
+    issueLink:
+    resolvesIssue: false
+    dependencyOwner: solo-io
+    dependencyRepo: envoy-gloo-ee
+    dependencyTag: # 1.33.0-patch3
+EOF
+    ;;
+esac
 
-echo "Wrote to $CHANGELOG_FILE"
-fi
-
-# TODO this is a quick hack to avoid args parsing. If this script grows in complexity,
-# parsing args will be more appropriate than this check.
-if [[ $# > 0 ]] && [[ "$1" == "edit" ]]; then
-    echo "Editing..."
-    "$EDITOR" "$CHANGELOG_FILE"
+##################################################
+# Edit the file if `-e` was provided
+if $OPT_EDIT ; then
+  exec "$EDITOR" "${CHANGELOG_FILE}"
 fi
