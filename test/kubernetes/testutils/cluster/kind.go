f902335ca6af28e49413bd1a421aef2a477a962f
Andy Fong <andy.fong@solo.io>
2025-05-19 11:22:45 -0400

    Allow name of kube context to be changed via env variable (#10837)
    Co-authored-by: changelog-bot <changelog-bot>

--- test/kubernetes/testutils/cluster/kind.go
+++ test/kubernetes/testutils/cluster/kind.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/solo-io/gloo/pkg/schemes"
+	"github.com/solo-io/gloo/test/testutils"
 
 	"k8s.io/apimachinery/pkg/runtime"
 
@@ -29,7 +30,10 @@ func MustKindContextWithScheme(clusterName string, scheme *runtime.Scheme) *Cont
 		clusterName = "kind"
 	}
 
-	kubeCtx := fmt.Sprintf("kind-%s", clusterName)
+	kubeCtx := os.Getenv(testutils.KubeCtx)
+	if len(kubeCtx) == 0 {
+		kubeCtx = fmt.Sprintf("kind-%s", clusterName)
+	}
 
 	restCfg, err := kubeutils.GetRestConfigWithKubeContext(kubeCtx)
 	if err != nil {
