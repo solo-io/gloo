570948e1c9bf76e6d85790fcf274ddcb49999d46
Sam Heilbron <SamHeilbron@gmail.com>
2025-08-08 11:49:15 -0400

    ssl.go: improve oneWayTls in UpstreamSslConfig (#10945)

--- test/kubernetes/e2e/defaults/defaults.go
+++ test/kubernetes/e2e/defaults/defaults.go
@@ -6,8 +6,11 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/solo-io/gloo/pkg/utils/kubeutils/kubectl"
 	"github.com/solo-io/skv2/codegen/util"
+
+	"github.com/solo-io/gloo/pkg/utils/kubeutils/kubectl"
+
+	_ "embed"
 )
 
 var (
