947c5da215480799be7719dc7e4db40e3175dcc5
Sam Heilbron <samheilbron@gmail.com>
2025-08-18 08:54:10 -0600

    cleanup e2e test

--- test/kubernetes/e2e/features/basicrouting/edge_suite.go
+++ test/kubernetes/e2e/features/basicrouting/edge_suite.go
@@ -47,7 +47,7 @@ func NewBasicEdgeRoutingSuite(ctx context.Context, testInst *e2e.TestInstallatio
 	}
 }
 
-func (s *edgeBasicRoutingSuite) XTestBasicVirtualServiceRouting() {
+func (s *edgeBasicRoutingSuite) TestBasicVirtualServiceRouting() {
 	s.T().Cleanup(func() {
 		err := s.testInstallation.Actions.Kubectl().DeleteFile(s.ctx, testdefaults.NginxPodManifest)
 		s.Assertions.NoError(err, "can delete nginx pod")
@@ -65,14 +65,14 @@ func (s *edgeBasicRoutingSuite) XTestBasicVirtualServiceRouting() {
 	err := s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, testdefaults.NginxPodManifest)
 	s.Assert().NoError(err)
 	// Check that test resources are running
-	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, testdefaults.NginxPod.ObjectMeta.GetNamespace(), metav1.ListOptions{
+	s.testInstallation.AssertionsT(s.T()).EventuallyPodsRunning(s.ctx, testdefaults.NginxPod.ObjectMeta.GetNamespace(), metav1.ListOptions{
 		LabelSelector: "app.kubernetes.io/name=nginx",
 	})
 
 	err = s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, testdefaults.CurlPodManifest)
 	s.Assert().NoError(err)
 	// Check that test resources are running
-	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, testdefaults.CurlPod.ObjectMeta.GetNamespace(), metav1.ListOptions{
+	s.testInstallation.AssertionsT(s.T()).EventuallyPodsRunning(s.ctx, testdefaults.CurlPod.ObjectMeta.GetNamespace(), metav1.ListOptions{
 		LabelSelector: "app.kubernetes.io/name=curl",
 	})
 
@@ -81,13 +81,13 @@ func (s *edgeBasicRoutingSuite) XTestBasicVirtualServiceRouting() {
 	// other syncers that have translated them, so we can only detect that the objects exist here
 	err = s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, ossvalidation.ExampleUpstream, "-n", s.testInstallation.Metadata.InstallNamespace)
 	s.Assert().NoError(err, "can apply valid upstream")
-	s.testInstallation.Assertions.EventuallyResourceExists(
+	s.testInstallation.AssertionsT(s.T()).EventuallyResourceExists(
 		func() (resources.Resource, error) {
 			return s.testInstallation.ResourceClients.UpstreamClient().Read(s.testInstallation.Metadata.InstallNamespace, ossvalidation.ExampleUpstreamName, clients.ReadOpts{Ctx: s.ctx})
 		},
 	)
 	// we need to make sure Gloo has had a chance to process it
-	s.testInstallation.Assertions.ConsistentlyResourceExists(
+	s.testInstallation.AssertionsT(s.T()).ConsistentlyResourceExists(
 		s.ctx,
 		func() (resources.Resource, error) {
 			return s.testInstallation.ResourceClients.UpstreamClient().Read(s.testInstallation.Metadata.InstallNamespace, "nginx-upstream", clients.ReadOpts{Ctx: s.ctx})
@@ -95,7 +95,7 @@ func (s *edgeBasicRoutingSuite) XTestBasicVirtualServiceRouting() {
 	)
 	err = s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, ossvalidation.ExampleVS, "-n", s.testInstallation.Metadata.InstallNamespace)
 	s.Assert().NoError(err, "can apply valid virtual service")
-	s.testInstallation.Assertions.EventuallyResourceStatusMatchesState(
+	s.testInstallation.AssertionsT(s.T()).EventuallyResourceStatusMatchesState(
 		func() (resources.InputResource, error) {
 			return s.testInstallation.ResourceClients.VirtualServiceClient().Read(s.testInstallation.Metadata.InstallNamespace, ossvalidation.ExampleVsName, clients.ReadOpts{Ctx: s.ctx})
 		},
@@ -104,7 +104,7 @@ func (s *edgeBasicRoutingSuite) XTestBasicVirtualServiceRouting() {
 	)
 
 	// Should have a successful response
-	s.testInstallation.Assertions.AssertEventualCurlResponse(
+	s.testInstallation.AssertionsT(s.T()).AssertEventualCurlResponse(
 		s.ctx,
 		kubectl.PodExecOptions{
 			Name:      testdefaults.CurlPod.Name,
@@ -125,7 +125,10 @@ func (s *edgeBasicRoutingSuite) XTestBasicVirtualServiceRouting() {
 		})
 }
 
-func (s *edgeBasicRoutingSuite) TestVirtualServiceWithRetries() {
+// TestVirtualServiceWithRetriesIntegration asserts that the virtual service with retries can
+// be applied and accepted by the Proxy. It is an integration test that asserts that the properties
+// are correctly set in the envoy configuration, and not a true end-to-end test.
+func (s *edgeBasicRoutingSuite) TestVirtualServiceWithRetriesIntegration() {
 	s.T().Cleanup(func() {
 		err := s.testInstallation.Actions.Kubectl().Delete(s.ctx, VirtualServiceWithRetriesYaml, "-n", s.testInstallation.Metadata.InstallNamespace)
 		s.Assert().NoError(err)
@@ -144,13 +147,13 @@ func (s *edgeBasicRoutingSuite) TestVirtualServiceWithRetries() {
 	)
 }
 
+// routeRetriesAssertion asserts that the envoy configuration contains the properties defined in the vs-with-retries.yaml file
 func routeRetriesAssertion(testInstallation *e2e.TestInstallation, t *testing.T) func(ctx context.Context, adminClient *admincli.Client) {
 	return func(ctx context.Context, adminClient *admincli.Client) {
 		testInstallation.AssertionsT(t).Gomega.Eventually(func(g gomega.Gomega) {
 			routeConfig, err := adminClient.GetSingleRouteConfig(ctx, "listener-::-8080-routes")
 			g.Expect(err).NotTo(gomega.HaveOccurred(), "error getting route config")
 
-			// We minimally expect the envoy configuration to contain the properties defined in the virtual service
 			route := routeConfig.GetVirtualHosts()[0].GetRoutes()[0]
 			g.Expect(route.GetRoute().GetRetryPolicy().GetRetryOn()).To(gomega.Equal("5xx"))
 			g.Expect(route.GetRoute().GetRetryPolicy().GetRetryBackOff()).NotTo(gomega.BeNil())
