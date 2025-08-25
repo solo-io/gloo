d219ddaad35adfc042c6e49115ddf908cc19e5dc
Kasun Talwatta <kasun.talwatta@solo.io>
2025-08-25 14:14:23 +1200

    fixed a minor issue
    Signed-off-by: Kasun Talwatta <kasun.talwatta@solo.io>

--- projects/gloo/pkg/syncer/setup/setup_syncer.go
+++ projects/gloo/pkg/syncer/setup/setup_syncer.go
@@ -319,7 +319,7 @@ func (s *setupSyncer) Setup(ctx context.Context, kubeCache kube.SharedCache, mem
 	}
 
 	// if set to auto, hardcode to V4
-	if settings.GetUpstreamOptions().GetDnsLookupIpFamily() == v1.DnsIpFamily_AUTO {
+	if settings.GetUpstreamOptions() != nil && (settings.GetUpstreamOptions().GetDnsLookupIpFamily() == v1.DnsIpFamily_AUTO) {
 		settings.GetUpstreamOptions().DnsLookupIpFamily = v1.DnsIpFamily_V4_ONLY
 	}
 
