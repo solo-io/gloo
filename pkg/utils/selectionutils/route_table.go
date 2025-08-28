d54e4d80a24bcc010824b01ad98be5f2b73daa7e
sheidkamp <seth.heidkamp@gmail.com>
2025-08-28 12:29:54 -0400

    Update route_table.go

--- pkg/utils/selectionutils/route_table.go
+++ pkg/utils/selectionutils/route_table.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/rotisserie/eris"
 	"github.com/solo-io/gloo/projects/gloo/pkg/defaults"
-	"github.com/solo-io/go-utils/contextutils"
 
 	gatewayv1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
@@ -32,58 +31,26 @@ func NewRouteTableSelector(client gatewayv1.RouteTableClient, podNamespace strin
 }
 
 func (s *routeTableSelector) SelectOrBuildRouteTable(ctx context.Context, ref *core.ResourceRef) (*gatewayv1.RouteTable, error) {
-	logger := contextutils.LoggerFrom(ctx)
-	logger.Debugw("Selecting or building RouteTable",
-		"issue", "8539",
-		"ref", ref.String(),
-		"podNamespace", s.podNamespace)
-
 	// Read or build route table
 	// unlike virtual service, name must be provided as there is no "default" virtual service
 	name := ref.GetName()
 	if name == "" {
-		logger.Debugw("RouteTable name is empty",
-			"issue", "8539",
-			"ref", ref.String())
 		return nil, eris.New("must provide a name for the target route table")
 	}
 
 	ns := ref.GetNamespace()
 	if ns == "" {
 		ns = defaults.GlooSystem
-		logger.Debugw("RouteTable namespace was empty, using default",
-			"issue", "8539",
-			"originalRef", ref.String(),
-			"defaultNamespace", ns)
 	}
-
-	logger.Debugw("Attempting to read RouteTable from client",
-		"issue", "8539",
-		"namespace", ns,
-		"name", name,
-		"fullRef", ref.String())
 
 	found, err := s.client.Read(ref.GetNamespace(), ref.GetName(), clients.ReadOpts{Ctx: ctx})
 	if err != nil && !sk_errors.IsNotExist(err) {
-		logger.Debugw("RouteTable client read failed with error",
-			"issue", "8539",
-			"ref", ref.String(),
-			"error", err.Error(),
-			"isNotExistError", sk_errors.IsNotExist(err))
 		return nil, err
 	}
 	if found != nil {
-		logger.Debugw("RouteTable found successfully",
-			"issue", "8539",
-			"ref", ref.String(),
-			"foundNamespace", found.GetMetadata().GetNamespace(),
-			"foundName", found.GetMetadata().GetName())
 		return found, nil
 	}
 
-	logger.Debugw("RouteTable not found, building default",
-		"issue", "8539",
-		"ref", ref.String())
 	// Build a new default route table object
 	return s.build(ctx, ref)
 }
