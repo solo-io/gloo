9990b7598bec703ede90c7345397a69cc8dab54e
Kasun Talwatta <kasun.talwatta@gmail.com>
2025-09-03 22:49:26 +1200

    added a temp script to check the dns entries
    Signed-off-by: Kasun Talwatta <kasun.talwatta@gmail.com>

--- ci/kind/setup-kind.sh
+++ ci/kind/setup-kind.sh
@@ -50,6 +50,10 @@ function create_kind_cluster_or_skip() {
     # instead of forwarding queries to whatever /etc/resolv.conf has, CoreDNS will always forward external DNS queries directly to Google Public DNS (8.8.8.8 and 8.8.4.4) but wrapped in NAT64 IPv6 addresses (64:ff9b::/96).
     new_core_file=$(kubectl get cm -n kube-system coredns -o jsonpath='{.data.Corefile}' | sed -E 's,forward . /etc/resolv.conf( ?\{)?,forward . [64:ff9b::8.8.8.8]:53 [64:ff9b::8.8.4.4]:53\1,' | sed -z 's/\n/\\n/g')
     kubectl patch configmap/coredns -n kube-system --type merge -p '{"data":{"Corefile": "'"$new_core_file"'"}}'
+
+    sleep 5
+
+    . $SCRIPT_DIR/verify-ipv6-dns.sh
   elif [[ "$ip_family" = "dual" ]]; then
     echo "creating dual stack based cluster ${CLUSTER_NAME}"
     kind create cluster \
