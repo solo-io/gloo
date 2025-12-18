d21a38fefedff0cea2462792a0c09d5da44cabba
omar <omar.hammami@solo.io>
2025-12-17 23:48:57 -0600

    fix: hardcode known object Kind
    Signed-off-by: omar <omar.hammami@solo.io>

--- projects/gateway2/translator/vhostutils/utils.go
+++ projects/gateway2/translator/vhostutils/utils.go
@@ -1,11 +1,13 @@
 package vhostutils
 
 import (
+	sologatewayv1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1"
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+// AppendSourceToVirtualHost appends a source VirtualHostOption to the VirtualHost's metadata static
 func AppendSourceToVirtualHost(vh *v1.VirtualHost, source client.Object) {
 	meta := vh.GetMetadataStatic()
 	if meta == nil {
@@ -17,7 +19,7 @@ func AppendSourceToVirtualHost(vh *v1.VirtualHost, source client.Object) {
 			Name:      source.GetName(),
 			Namespace: source.GetNamespace(),
 		},
-		ResourceKind:       source.GetObjectKind().GroupVersionKind().Kind,
+		ResourceKind:       sologatewayv1.VirtualHostOptionGVK.Kind,
 		ObservedGeneration: source.GetGeneration(),
 	})
 	vh.OpaqueMetadata = &v1.VirtualHost_MetadataStatic{
