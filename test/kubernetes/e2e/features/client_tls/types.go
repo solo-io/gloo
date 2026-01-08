558d6e56b2d597b3a8aaa100a7c94211a7f58cf7
Omar Hammami <58956785+puertomontt@users.noreply.github.com>
2026-01-08 13:18:32 -0600

    fix oneWayTls (#11093)
    Signed-off-by: omar <omar.hammami@solo.io>

--- test/kubernetes/e2e/features/client_tls/types.go
+++ test/kubernetes/e2e/features/client_tls/types.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/onsi/gomega"
 
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	kubev1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1/kube/apis/gateway.solo.io/v1"
 	"github.com/solo-io/gloo/test/gomega/matchers"
@@ -23,6 +25,9 @@ var VSTargetingUpstreamYaml []byte
 
 //go:embed testdata/vs-targeting-kube.yaml
 var VSTargetingKubeYaml []byte
+
+//go:embed testdata/vs-oneway-downstream-tls.yaml
+var VSOnewayDownstreamTlsYaml []byte
 
 var (
 	vSTargetingUpstreamObject = func(ns string) *kubev1.VirtualService {
@@ -43,6 +48,15 @@ var (
 		}
 	}
 
+	vSOnewayDownstreamTlsObject = func(ns string) *kubev1.VirtualService {
+		return &kubev1.VirtualService{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "vs-oneway-downstream-tls",
+				Namespace: ns,
+			},
+		}
+	}
+
 	expectedHealthyResponse = &matchers.HttpResponse{
 		StatusCode: http.StatusOK,
 		Body:       gomega.ContainSubstring("Welcome to nginx!"),
@@ -51,5 +65,20 @@ var (
 	expectedCertVerifyFailedResponse = &matchers.HttpResponse{
 		StatusCode: http.StatusServiceUnavailable,
 		Body:       gomega.ContainSubstring("CERTIFICATE_VERIFY_FAILED"),
+	}
+
+	nginxTlsSecret = func(ns string) *corev1.Secret {
+		return &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "nginx-tls",
+				Namespace: ns,
+			},
+		}
+	}
+
+	coreSecretGVK = schema.GroupVersionKind{
+		Version: "v1",
+		Group:   "",
+		Kind:    "Secret",
 	}
 )
