384dc7b9b103fc6f51b0ba046043060cefee1a4a
= <jdcigan@gmail.com>
2026-02-27 13:25:14 -0800

    test cleanup

--- test/e2e/dynamic_forward_proxy_test.go
+++ test/e2e/dynamic_forward_proxy_test.go
@@ -1,11 +1,7 @@
 package e2e_test
 
 import (
-	"bufio"
-	"fmt"
-	"net"
 	"net/http"
-	"strings"
 
 	"github.com/solo-io/gloo/test/testutils"
 
@@ -22,15 +18,9 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"google.golang.org/protobuf/types/known/durationpb"
-	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	gatewayv1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1"
 	gloov1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
-	gloomatchers "github.com/solo-io/gloo/projects/gloo/pkg/api/v1/core/matchers"
-	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/options/hcm"
-	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/options/protocol_upgrade"
-	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/ssl"
 )
 
 var _ = Describe("dynamic forward proxy", func() {
@@ -165,116 +155,6 @@ var _ = Describe("dynamic forward proxy", func() {
 					StatusCode: http.StatusOK,
 					Body:       MatchRegexp(`"host":\s*"postman-echo.com"`),
 				}))
-			}, "10s", ".1s").Should(Succeed())
-		})
-	})
-
-	Context("with connect_terminate for HTTPS tunneling", func() {
-
-		BeforeEach(func() {
-			// Replicates customer reproducer gateway configuration:
-			// /Users/jasoncigan/Git/customer-success-reproducer-agent/reproductions/7973-https-connect-tunnel/fix/gateway-ssl-fixed-with-connect-terminate.yaml
-			gw := defaults2.DefaultGateway(writeNamespace)
-			gw.GetHttpGateway().Options = &gloov1.HttpListenerOptions{
-				DynamicForwardProxy: &dynamic_forward_proxy.FilterConfig{
-					DnsCacheConfig: &dynamic_forward_proxy.DnsCacheConfig{
-						DnsLookupFamily: dynamic_forward_proxy.DnsLookupFamily_V4_ONLY,
-						HostTtl:         &durationpb.Duration{Seconds: 86400},
-					},
-					SslConfig: &ssl.UpstreamSslConfig{
-						SslSecrets: &ssl.UpstreamSslConfig_SslFiles{
-							SslFiles: &ssl.SSLFiles{
-								RootCa: "/etc/ssl/certs/ca-certificates.crt",
-							},
-						},
-					},
-				},
-				HttpConnectionManagerSettings: &hcm.HttpConnectionManagerSettings{
-					Upgrades: []*protocol_upgrade.ProtocolUpgradeConfig{
-						{
-							UpgradeType: &protocol_upgrade.ProtocolUpgradeConfig_Connect{
-								Connect: &protocol_upgrade.ProtocolUpgradeConfig_ProtocolUpgradeSpec{
-									Enabled: &wrapperspb.BoolValue{Value: true},
-								},
-							},
-						},
-					},
-				},
-			}
-
-			// Replicates customer reproducer VS configuration:
-			// /Users/jasoncigan/Git/customer-success-reproducer-agent/reproductions/7973-https-connect-tunnel/fix/vs-ssl-fixed-with-connect-terminate.yaml
-			vs := helpers.NewVirtualServiceBuilder().
-				WithName(e2e.DefaultVirtualServiceName).
-				WithNamespace(writeNamespace).
-				WithDomain(e2e.DefaultHost).
-				WithRouteMatcher(e2e.DefaultRouteName, &gloomatchers.Matcher{
-					PathSpecifier: &gloomatchers.Matcher_ConnectMatcher_{
-						ConnectMatcher: &gloomatchers.Matcher_ConnectMatcher{},
-					},
-				}).
-				WithRouteAction(e2e.DefaultRouteName, &gloov1.RouteAction{
-					Destination: &gloov1.RouteAction_DynamicForwardProxy{
-						DynamicForwardProxy: &dynamic_forward_proxy.PerRouteConfig{
-							HostRewriteSpecifier: &dynamic_forward_proxy.PerRouteConfig_AutoHostRewriteHeader{
-								AutoHostRewriteHeader: "x-dfp-host",
-							},
-						},
-					},
-				}).
-				WithRouteOptions(e2e.DefaultRouteName, &gloov1.RouteOptions{
-					Upgrades: []*protocol_upgrade.ProtocolUpgradeConfig{
-						{
-							UpgradeType: &protocol_upgrade.ProtocolUpgradeConfig_ConnectTerminate{
-								ConnectTerminate: &protocol_upgrade.ProtocolUpgradeConfig_ProtocolUpgradeSpec{
-									Enabled: &wrapperspb.BoolValue{Value: true},
-								},
-							},
-						},
-					},
-				}).
-				Build()
-
-			resourceToCreate := testContext.ResourcesToCreate()
-			resourceToCreate.Gateways = gatewayv1.GatewayList{
-				gw,
-			}
-			resourceToCreate.VirtualServices = gatewayv1.VirtualServiceList{
-				vs,
-			}
-		})
-
-		// Replicates manual validation test from customer reproducer:
-		// curl --proxy https://localhost:8443 --proxy-insecure https://httpbin.org/get
-		// (adapted for e2e test framework using HTTP proxy instead of HTTPS)
-		It("should establish CONNECT tunnel for HTTPS proxying", func() {
-			Eventually(func(g Gomega) {
-				proxyAddr := fmt.Sprintf("%s:%d", testContext.EnvoyInstance().LocalAddr(), testContext.EnvoyInstance().HttpPort)
-
-				conn, err := net.Dial("tcp", proxyAddr)
-				g.Expect(err).NotTo(HaveOccurred(), "Should connect to Envoy proxy")
-				defer conn.Close()
-
-				// Send CONNECT request exactly as curl does with --proxy flag
-				// Host header must match VirtualService domain for Gloo routing
-				// x-dfp-host header tells DFP which upstream to connect to (via autoHostRewriteHeader)
-				connectRequest := "CONNECT httpbin.org:443 HTTP/1.1\r\n" +
-					"Host: " + e2e.DefaultHost + "\r\n" +
-					"x-dfp-host: httpbin.org\r\n" +
-					"\r\n"
-
-				_, err = conn.Write([]byte(connectRequest))
-				g.Expect(err).NotTo(HaveOccurred(), "Should send CONNECT request")
-
-				reader := bufio.NewReader(conn)
-				response, err := reader.ReadString('\n')
-				g.Expect(err).NotTo(HaveOccurred(), "Should read CONNECT response")
-
-				// Verify 200 Connection Established - proves TCP tunnel was established
-				// Without connect_terminate (before fix): returns 400 Bad Request
-				// With connect_terminate (after fix): returns 200 OK
-				g.Expect(response).To(ContainSubstring("HTTP/1.1 200"),
-					fmt.Sprintf("Expected 200 for CONNECT tunnel, got: %s", strings.TrimSpace(response)))
 			}, "10s", ".1s").Should(Succeed())
 		})
 	})
