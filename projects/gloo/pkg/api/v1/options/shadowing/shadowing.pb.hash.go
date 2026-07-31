97d8af849efb2b34938d6ff590c4f06a8e3e8fba
Jason Cigan <jason.cigan@solo.io>
2026-07-31 10:48:53 -0700

    feat: host rewrite literal for shadow hosts (#11347)

--- projects/gloo/pkg/api/v1/options/shadowing/shadowing.pb.hash.go
+++ projects/gloo/pkg/api/v1/options/shadowing/shadowing.pb.hash.go
@@ -72,5 +72,9 @@ func (m *RouteShadowing) Hash(hasher hash.Hash64) (uint64, error) {
 		return 0, err
 	}
 
+	if _, err = hasher.Write([]byte(m.GetHostRewriteLiteral())); err != nil {
+		return 0, err
+	}
+
 	return hasher.Sum64(), nil
 }
